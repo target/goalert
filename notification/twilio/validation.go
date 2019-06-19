7c4d33c0619711cefb50ed516a8c912a48e3bcc4
Scott Murch <scott.murch@creditsafe.com>
2019-06-19 22:53:31 +0100

    Reference the UK for the Supported Country Code

--- notification/twilio/validation.go
+++ notification/twilio/validation.go
@@ -80,7 +80,7 @@ func validSID(n string) string {
 }
 
 // Supported Country Codes
-// +1 = USA, +91 = India
+// +1 = USA, +91 = India, +44 = United Kingdom
 func supportedCountryCode(n string) bool {
 	if strings.HasPrefix(n, "+1") || strings.HasPrefix(n, "+91") || strings.HasPrefix(n, "+44") {
 		return true
