1484a4f3a13ba2b9631426b5373688eeeae8f761
Anderson Day <dayander@msu.edu>
2019-06-19 16:25:44 -0500

    fixed smoketest for migration

--- migrate/migrations/20190613111645-add-rotation-favorite.sql
+++ migrate/migrations/20190613111645-add-rotation-favorite.sql
@@ -1,8 +1,17 @@
 -- +migrate Up
 ALTER TABLE user_favorites ADD COLUMN tgt_rotation_id UUID REFERENCES rotations (id);
 
+ALTER TABLE user_favorites ALTER COLUMN tgt_service_id DROP NOT NULL;
+
+ALTER TABLE user_favorites ADD CONSTRAINT user_fav_rotations UNIQUE (user_id, tgt_rotation_id);
+
+
 
 -- +migrate Down
+ALTER TABLE user_favorites DROP CONSTRAINT user_fav_rotations;
+
 ALTER TABLE user_favorites DROP COLUMN tgt_rotation_id;
+
+ALTER TABLE user_favorites ALTER COLUMN tgt_service_id SET NOT NULL;
 
 
