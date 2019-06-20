7c109bca1431a3c6ab3c0431e0a4fa1bf1928520
Kaitlyn.Mollenkopf <z0041g5@a45e60ec68f9.hq.target.com>
2019-06-20 11:09:22 -0500

    Smoketest and migration tweak

--- migrate/migrations/20190613111312-add-schedule-favorites.sql
+++ migrate/migrations/20190613111312-add-schedule-favorites.sql
@@ -3,6 +3,20 @@ ALTER TABLE user_favorites
 ADD COLUMN tgt_schedule_id UUID 
 REFERENCES schedules(id);
 
+ALTER TABLE user_favorites 
+ALTER COLUMN tgt_service_id 
+DROP NOT NULL;
+
+ALTER TABLE user_favorites 
+ADD CONSTRAINT user_fav_schedules UNIQUE (user_id, tgt_schedule_id);
+
 -- +migrate Down
+ALTER TABLE user_favorites
+DROP CONSTRAINT user_fav_schedules;
+
 ALTER TABLE user_favorites 
 DROP COLUMN tgt_schedule_id;
+
+ALTER TABLE user_favorites 
+ALTER COLUMN tgt_service_id 
+SET NOT NULL;
