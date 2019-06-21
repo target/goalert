a605b1ece0f7044a7c2619bebad03d1fe3166609
Katy <kmm5278@rit.edu>
2019-06-21 13:11:03 -0500

    Migration tweaks

--- migrate/migrations/20190613111312-add-schedule-favorites.sql
+++ migrate/migrations/20190613111312-add-schedule-favorites.sql
@@ -1,22 +1,11 @@
 -- +migrate Up
-ALTER TABLE user_favorites 
-ADD COLUMN tgt_schedule_id UUID 
-REFERENCES schedules(id);
-
-ALTER TABLE user_favorites 
-ALTER COLUMN tgt_service_id 
-DROP NOT NULL;
-
-ALTER TABLE user_favorites 
-ADD CONSTRAINT user_fav_schedules UNIQUE (user_id, tgt_schedule_id);
+ALTER TABLE user_favorites
+    ADD COLUMN tgt_schedule_id UUID REFERENCES schedules(id),
+    ALTER COLUMN tgt_service_id DROP NOT NULL,
+    ADD CONSTRAINT user_favorites_user_id_tgt_schedule_id_key UNIQUE(user_id, tgt_schedule_id);
 
 -- +migrate Down
 ALTER TABLE user_favorites
-DROP CONSTRAINT user_fav_schedules;
-
-ALTER TABLE user_favorites 
-DROP COLUMN tgt_schedule_id;
-
-ALTER TABLE user_favorites 
-ALTER COLUMN tgt_service_id 
-SET NOT NULL;
+    DROP CONSTRAINT user_favorites_user_id_tgt_schedule_id_key,
+    DROP COLUMN tgt_schedule_id,
+    ALTER COLUMN tgt_service_id SET NOT NULL;
