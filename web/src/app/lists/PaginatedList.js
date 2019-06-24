d6fbd678d941b63ec199cb0d00c09bdf5e5c96fd
Anderson Day <dayander@msu.edu>
2019-06-24 09:58:42 -0500

    added data for tests

--- web/src/app/lists/PaginatedList.js
+++ web/src/app/lists/PaginatedList.js
@@ -256,7 +256,7 @@ export class PaginatedList extends React.PureComponent {
       favIcon = (
         <ListItemSecondaryAction>
           <Avatar className={classes.favoriteIcon}>
-            <FavoriteIcon />
+            <FavoriteIcon data-cy={'fav-icon'} />
           </Avatar>
         </ListItemSecondaryAction>
       )
