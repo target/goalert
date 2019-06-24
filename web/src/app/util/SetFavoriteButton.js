ac0caceda314445a3a3f3fea2e88aa2125f0c185
Anderson Day <dayander@msu.edu>
2019-06-24 10:00:30 -0500

    dynamic set favorite button

---
+++ web/src/app/util/SetFavoriteButton.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import p from 'prop-types'
+import IconButton from '@material-ui/core/IconButton'
+import FavoriteFilledIcon from '@material-ui/icons/Star'
+import FavoriteBorderIcon from '@material-ui/icons/StarBorder'
+
+export default class SetFavoriteButton extends React.PureComponent {
+  static propTypes = {
+    type: p.oneOf(['rotation', 'service']),
+    onSubmit: p.func,
+    isFavorite: p.bool,
+  }
+
+  render() {
+    const { type, isFavorite, onSubmit } = this.props
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          onSubmit()
+        }}
+      >
+        <IconButton
+          aria-label={
+            isFavorite
+              ? `Unset as a Favorite ${type}`
+              : `Set as a Favorite ${type}`
+          }
+          type='submit'
+          color='inherit'
+          data-cy={'set-fav'}
+        >
+          {isFavorite ? <FavoriteFilledIcon /> : <FavoriteBorderIcon />}
+        </IconButton>
+      </form>
+    )
+  }
+}
