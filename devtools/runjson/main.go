dcf5e41f39705ef8ea1b4cc74feaeae412ab83ac
Nathaniel Caza <mastercactapus@gmail.com>
2019-06-25 14:09:15 -0500

    add env var replace support to runjson

--- devtools/runjson/main.go
+++ devtools/runjson/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"flag"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -11,13 +13,92 @@ import (
 
 var logDir string
 
+func isVar(b byte) bool {
+	if b >= 'a' && b <= 'z' {
+		return true
+	}
+	if b >= 'A' && b <= 'Z' {
+		return true
+	}
+	if b == '_' {
+		return true
+	}
+
+	return false
+}
+
+func envRep(r io.Reader) io.Reader {
+	br := bufio.NewReader(r)
+
+	rt, wt := io.Pipe()
+	go func() {
+		defer wt.Close()
+		w := bufio.NewWriter(wt)
+		defer w.Flush()
+
+		var escape bool
+		for {
+			b, err := br.ReadByte()
+			if err != nil {
+				w.Flush()
+				wt.CloseWithError(err)
+				return
+			}
+
+			if escape {
+				w.WriteByte(b)
+				if b == '}' {
+					w.Flush()
+				}
+				escape = false
+				continue
+			}
+			if b == '\\' {
+				escape = true
+				continue
+			}
+
+			if b == '$' {
+				var name string
+				for {
+					b, err = br.ReadByte()
+					if err != nil {
+						w.Flush()
+						wt.CloseWithError(err)
+						return
+					}
+					if !isVar(b) {
+						br.UnreadByte()
+						break
+					}
+					name += string(b)
+				}
+				io.WriteString(w, os.Getenv(name))
+				continue
+			}
+
+			w.WriteByte(b)
+			if b == '}' {
+				w.Flush()
+			}
+		}
+	}()
+
+	return rt
+}
+
 func main() {
 	flag.StringVar(&logDir, "logs", "", "Directory to store copies of all logs. Overwritten on each start.")
+	rep := flag.Bool("replace", false, "Replace env vars specified with $VAR with the current value.")
 	flag.Parse()
 	log.SetFlags(log.Lshortfile)
 
 	var tasks []Task
-	dec := json.NewDecoder(os.Stdin)
+	in := io.Reader(os.Stdin)
+	if *rep {
+		in = envRep(in)
+	}
+	dec := json.NewDecoder(in)
 	dec.DisallowUnknownFields()
 	err := dec.Decode(&tasks)
 	if err != nil {
