1de709f76f058cb1c894fe710e5fc026351596e3
Anderson Day <dayander@msu.edu>
2019-07-01 09:09:27 -0500

    allowing favorited rotation to be deleted

--- migrate/migrations/20190613111645-add-rotation-favorite.sql
+++ migrate/migrations/20190613111645-add-rotation-favorite.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE user_favorites
-    ADD COLUMN tgt_rotation_id UUID REFERENCES rotations (id),
+    ADD COLUMN tgt_rotation_id UUID REFERENCES rotations (id) ON DELETE CASCADE,
     ALTER COLUMN tgt_service_id DROP NOT NULL,
     ADD CONSTRAINT user_favorites_user_id_tgt_rotation_id_key UNIQUE (user_id, tgt_rotation_id);
 
