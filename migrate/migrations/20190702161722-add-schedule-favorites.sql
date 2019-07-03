dfde3da7264001f775a9cd44d7583b25450baf6e
Katy <kmm5278@rit.edu>
2019-07-03 08:26:04 -0500

    Migration tweaks

--- migrate/migrations/20190702161722-add-schedule-favorites.sql
+++ migrate/migrations/20190702161722-add-schedule-favorites.sql
@@ -1,11 +1,9 @@
 -- +migrate Up
 ALTER TABLE user_favorites
     ADD COLUMN tgt_schedule_id UUID REFERENCES schedules(id) ON DELETE CASCADE,
-    ALTER COLUMN tgt_service_id DROP NOT NULL,
     ADD CONSTRAINT user_favorites_user_id_tgt_schedule_id_key UNIQUE(user_id, tgt_schedule_id);
 
 -- +migrate Down
 ALTER TABLE user_favorites
     DROP CONSTRAINT user_favorites_user_id_tgt_schedule_id_key,
-    DROP COLUMN tgt_schedule_id,
-    ALTER COLUMN tgt_service_id SET NOT NULL;
+    DROP COLUMN tgt_schedule_id;
