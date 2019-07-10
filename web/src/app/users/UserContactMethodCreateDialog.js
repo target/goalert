78595e327d5849c85d97e829663a134024fffec3
Katy <kmm5278@rit.edu>
2019-07-10 09:58:15 -0500

    Verify form appears on creating a new cm

--- web/src/app/users/UserContactMethodCreateDialog.js
+++ web/src/app/users/UserContactMethodCreateDialog.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { useMutation } from 'react-apollo-hooks'
 import p from 'prop-types'
 
 import { graphql2Client } from '../apollo'
@@ -10,7 +9,7 @@ import { fieldErrors, nonFieldErrors } from '../util/errutil'
 import FormDialog from '../dialogs/FormDialog'
 import UserContactMethodForm from './UserContactMethodForm'
 import UserContactMethodVerificationForm from './UserContactMethodVerificationForm'
-import { sendVerificationCodeMutation } from './UserContactMethodVerificationDialog'
+import { formatPhoneNumber } from './util'
 
 const createMutation = gql`
   mutation($input: CreateUserContactMethodInput!) {
@@ -36,43 +35,55 @@ export default function UserContactMethodCreateDialog(props) {
   const [sendError, setSendError] = useState('') // error if verification code send fails
   const [contactMethodID, setContactMethodID] = useState(null) // used for verification mutation
 
-  function getInputVariables() {
-    return {
-      variables: {
-        input: {
-          ...cmValue,
-          userID: props.userID,
-          newUserNotificationRule: {
-            delayMinutes: 0,
-          },
-        },
-      },
+  const onComplete = (data, errors) => {
+    if (!contactMethodID) {
+      setContactMethodID(data.createUserContactMethod.id)
+    } else {
+      props.onClose()
     }
   }
-  const sendCode = useMutation(sendVerificationCodeMutation, {
-    // mutation options
-    variables: {
-      input: {
-        contactMethodID: contactMethodID,
-      },
-    },
-  })
+
+  function getInputVariables() {
+    if (contactMethodID) {
+      return {
+        variables: {
+          input: {
+            contactMethodID: contactMethodID,
+            code: verValue.code,
+          },
+        },
+      }
+    } else {
+      return {
+        variables: {
+          input: {
+            ...cmValue,
+            userID: props.userID,
+            newUserNotificationRule: {
+              delayMinutes: 0,
+            },
+          },
+        },
+      }
+    }
+  }
 
   function renderDialog(commit, status) {
     const { loading, error } = status
     return (
       <FormDialog
         title='Create New Contact Method'
-        subTitle={contactMethodID ? 'Verify contact method to continue' : null}
+        subTitle={
+          contactMethodID
+            ? `A verification code has been sent to ${formatPhoneNumber(
+                cmValue.value,
+              )} (${cmValue.type})`
+            : null
+        }
         loading={loading}
         errors={sendError ? [{ message: sendError }] : nonFieldErrors(error)}
         onClose={props.onClose}
-        onSubmit={() => {
-          const input = getInputVariables()
-          return commit(input).then(() =>
-            sendCode().catch(err => setSendError(err.message)),
-          )
-        }}
+        onSubmit={() => commit(getInputVariables())}
         form={renderForm(status)}
       />
     )
@@ -90,7 +101,7 @@ export default function UserContactMethodCreateDialog(props) {
           contactMethodID={contactMethodID}
           disabled={loading}
           errors={fieldErrs}
-          onChange={verValue => setVerValue({ verValue })}
+          onChange={verValue => setVerValue(verValue)}
           setSendError={setSendError}
           value={verValue}
         />
@@ -113,7 +124,7 @@ export default function UserContactMethodCreateDialog(props) {
       mutation={contactMethodID ? verifyContactMethodMutation : createMutation}
       awaitRefetchQueries
       refetchQueries={['nrList', 'cmList']}
-      onCompleted={data => setContactMethodID(data.createUserContactMethod.id)}
+      onCompleted={onComplete}
     >
       {(commit, status) => renderDialog(commit, status)}
     </Mutation>
@@ -124,130 +135,3 @@ UserContactMethodCreateDialog.propTypes = {
   userID: p.string.isRequired,
   onClose: p.func,
 }
-
-/*
-export default class UserContactMethodCreateDialog extends React.PureComponent {
-  static propTypes = {
-    userID: p.string.isRequired,
-    onClose: p.func,
-  }
-
-  state = {
-    // values for contact method form
-    cmValue: {
-      name: '',
-      type: 'SMS',
-      value: '+1',
-    },
-    // value for verification form
-    verValue: {
-      code: '',
-    },
-    sendError: '', // error if verification code send fails
-    errors: [],
-    contactMethodID: null, // used for verification mutation
-  }
-
-  onComplete = data => {
-    if (this.state.contactMethodID) {
-      this.props.onClose()
-    } else {
-      this.setState({
-        contactMethodID: data.createUserContactMethod.id, // output from create mutation
-      })
-    }
-  }
-
-  getInputVariables() {
-    if (this.state.contactMethodID) {
-      return {
-        variables: {
-          input: {
-            contactMethodID: this.state.contactMethodID,
-            code: this.state.verValue.code,
-          },
-        },
-      }
-    } else {
-      return {
-        variables: {
-          input: {
-            ...this.state.cmValue,
-            userID: this.props.userID,
-            newUserNotificationRule: {
-              delayMinutes: 0,
-            },
-          },
-        },
-      }
-    }
-  }
-
-  render() {
-    return (
-      <Mutation
-        client={graphql2Client}
-        mutation={
-          this.state.contactMethodID
-            ? verifyContactMethodMutation
-            : createMutation
-        }
-        awaitRefetchQueries
-        refetchQueries={['nrList', 'cmList']}
-        onCompleted={this.onComplete}
-      >
-        {(commit, status) => this.renderDialog(commit, status)}
-      </Mutation>
-    )
-  }
-
-  renderDialog(commit, status) {
-    const { loading, error } = status
-    const { contactMethodID } = this.state
-
-    return (
-      <FormDialog
-        title='Create New Contact Method'
-        subTitle={contactMethodID ? 'Verify contact method to continue' : null}
-        loading={loading}
-        errors={nonFieldErrors(error)}
-        onClose={this.props.onClose}
-        onSubmit={() => {
-          const input = this.getInputVariables()
-          return commit(input)
-        }}
-        form={this.renderForm(status)}
-      />
-    )
-  }
-
-  renderForm(status) {
-    // these values are different depending on which
-    // mutation wraps the dialog
-    const { loading, error } = status
-    const fieldErrs = fieldErrors(error)
-
-    if (this.state.contactMethodID) {
-      return (
-        <UserContactMethodVerificationForm
-          contactMethodID={this.state.contactMethodID}
-          disabled={loading}
-          errors={fieldErrs}
-          onChange={verValue => this.setState({ verValue })}
-          setSendError={sendError => this.setState({ sendError })}
-          value={this.state.verValue}
-        />
-      )
-    } else {
-      return (
-        <UserContactMethodForm
-          disabled={loading}
-          errors={fieldErrs}
-          onChange={cmValue => this.setState({ cmValue })}
-          value={this.state.cmValue}
-        />
-      )
-    }
-  }
-}
-*/
