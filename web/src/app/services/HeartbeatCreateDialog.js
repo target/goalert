b6dcc8634e6365261f9f09d1cdea0e7cfa9b3d91
Katy <kmm5278@rit.edu>
2019-07-15 09:03:14 -0500

    Placeholder components for heartbeat UI

---
+++ web/src/app/services/HeartbeatCreateDialog.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import p from 'prop-types'
+
+import { graphql2Client } from '../apollo'
+// import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
+import { fieldErrors, nonFieldErrors } from '../util/errutil'
+
+import FormDialog from '../dialogs/FormDialog'
+import HearbeatForm from './HearbeatForm'
+
+/* const mutation = gql`
+  mutation($input: CreateIntegrationKeyInput!) {
+    createIntegrationKey(input: $input) {
+      id
+      name
+      type
+      href
+    }
+  }
+`
+const query = gql`
+  query($serviceID: ID!) {
+    service(id: $serviceID) {
+      id
+      integrationKeys {
+        id
+        name
+        type
+        href
+      }
+    }
+  }
+` */
+
+export default class HeartbeatCreateDialog extends React.PureComponent {
+  static propTypes = {
+    serviceID: p.string.isRequired,
+    onClose: p.func,
+  }
+
+  state = {
+    value: { name: '', type: 'generic' },
+    errors: [],
+  }
+
+  render() {
+    // TODO, THEN REMOVE THESE
+    let query = null
+    let mutation = null
+    // ----
+    return (
+      <Mutation
+        client={graphql2Client}
+        mutation={mutation}
+        onCompleted={this.props.onClose}
+        update={(cache, { data: { createIntegrationKey } }) => {
+          const { service } = cache.readQuery({
+            query,
+            variables: { serviceID: this.props.serviceID },
+          })
+          cache.writeData({
+            query,
+            variables: { serviceID: this.props.serviceID },
+            data: {
+              service: {
+                ...service,
+                integrationKeys: (service.integrationKeys || []).concat(
+                  createIntegrationKey,
+                ),
+              },
+            },
+          })
+        }}
+      >
+        {(commit, status) => this.renderDialog(commit, status)}
+      </Mutation>
+    )
+  }
+
+  renderDialog(commit, status) {
+    const { loading, error } = status
+    return (
+      <FormDialog
+        maxWidth='sm'
+        title='Create New Integration Key'
+        loading={loading}
+        errors={nonFieldErrors(error)}
+        onClose={this.props.onClose}
+        onSubmit={() => {
+          return commit({
+            variables: {
+              input: { ...this.state.value, serviceID: this.props.serviceID },
+            },
+          })
+        }}
+        form={
+          <HearbeatForm
+            errors={fieldErrors(error)}
+            disabled={loading}
+            value={this.state.value}
+            onChange={value => this.setState({ value })}
+          />
+        }
+      />
+    )
+  }
+}
