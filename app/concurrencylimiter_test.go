fafb486b2a04687707a128ff954821cb523009f9
Nathaniel Caza <mastercactapus@gmail.com>
2019-07-15 13:35:00 -0500

    http: request queue and timeouts (#48)
    * implement request queue and timeouts

--- app/concurrencylimiter_test.go
+++ app/concurrencylimiter_test.go
@@ -3,40 +3,109 @@ package app
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPendingList(t *testing.T) {
+	list := &pendingList{}
+
+	r1 := list.newReq()
+	assert.Equal(t, r1, list.pop())
+
+	r2 := list.newReq()
+	assert.Equal(t, r2, list.pop())
+	assert.Nil(t, list.head)
+	assert.Nil(t, list.tail)
+
+	assert.Nil(t, list.pop())
+
+	r3 := list.newReq()
+	r4 := list.newReq()
+	r5 := list.newReq()
+
+	assert.True(t, list.remove(r4))
+	assert.Nil(t, r4.prev)
+	assert.Nil(t, r4.next)
+	assert.False(t, list.remove(r4))
+
+	assert.Equal(t, r3, list.pop())
+	assert.Equal(t, r5, list.pop())
+	assert.Nil(t, list.head)
+	assert.Nil(t, list.tail)
+}
+
 func TestConcurrencyLimiter(t *testing.T) {
-	lim := newConcurrencyLimiter(2)
+	t.Run("lock count", func(t *testing.T) {
+		lim := newConcurrencyLimiter(2, 10)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	err := lim.Lock(ctx, "foo")
-	assert.Nil(t, err)
+		ctx, cancel := context.WithCancel(context.Background())
+		err := lim.Lock(ctx, "foo")
+		assert.Nil(t, err)
 
-	err = lim.Lock(ctx, "foo")
-	assert.Nil(t, err)
+		err = lim.Lock(ctx, "foo")
+		assert.Nil(t, err)
 
-	err = lim.Lock(ctx, "bar")
-	assert.Nil(t, err)
+		err = lim.Lock(ctx, "bar")
+		assert.Nil(t, err)
 
-	cancel()
-	err = lim.Lock(ctx, "foo")
-	assert.Error(t, err, "context canceled")
+		cancel()
+		err = lim.Lock(ctx, "foo")
+		assert.Error(t, err, "context canceled")
 
-	lim.Unlock("bar")
-	err = lim.Lock(ctx, "foo")
-	assert.Error(t, err, "context canceled")
-
-	assert.Panics(t, func() {
-		// unlock twice (only locked once)
 		lim.Unlock("bar")
+		err = lim.Lock(ctx, "foo")
+		assert.Error(t, err, "context canceled")
+
+		assert.Panics(t, func() {
+			// unlock twice (only locked once)
+			lim.Unlock("bar")
+		})
+
+		lim.Unlock("foo")
+		ctx, cancel = context.WithCancel(context.Background())
+		err = lim.Lock(ctx, "foo")
+		assert.Nil(t, err)
+
+		cancel()
+		assert.Equal(t, 2, lim.lockCount["foo"])
+		assert.Equal(t, 0, lim.lockCount["bar"])
 	})
 
-	lim.Unlock("foo")
-	ctx, cancel = context.WithCancel(context.Background())
-	err = lim.Lock(ctx, "foo")
-	assert.Nil(t, err)
+	t.Run("queue", func(t *testing.T) {
+		lim := newConcurrencyLimiter(1, 3)
+		ctx := context.Background()
 
-	cancel()
+		gotLockCh := make(chan struct{})
+		lock := func() { lim.Lock(ctx, "foo"); gotLockCh <- struct{}{} }
+		for i := 0; i < 4; i++ {
+			go lock()
+		}
+		<-gotLockCh
+		assert.Equal(t, 1, lim.lockCount["foo"])
+		time.Sleep(time.Millisecond) // ensure other goroutines have a chance to fill the queue
+		assert.Error(t, lim.Lock(ctx, "foo"))
+		assert.Equal(t, 3, lim.queueCount["foo"])
+
+		lim.Unlock("foo")
+		go lock()
+
+		<-gotLockCh
+		lim.Unlock("foo")
+
+		<-gotLockCh
+		lim.Unlock("foo")
+
+		<-gotLockCh
+		lim.Unlock("foo")
+
+		<-gotLockCh
+		lim.Unlock("foo")
+
+		assert.Equal(t, 0, lim.lockCount["foo"])
+		assert.Equal(t, 0, lim.queueCount["foo"])
+
+		assert.Nil(t, lim.Lock(ctx, "foo"))
+	})
 }
