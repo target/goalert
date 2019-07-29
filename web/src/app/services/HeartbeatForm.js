1a20001086b3aa894359abd6cf323feaaaf8efb2
Nathaniel Cook <Nathaniel.Cook@target.com>
2019-07-29 10:29:29 -0500

    remove clamp on edit

--- web/src/app/services/HeartbeatForm.js
+++ web/src/app/services/HeartbeatForm.js
@@ -11,13 +11,6 @@ const styles = theme => ({
   },
 })
 
-const clampTimeout = val => {
-  const num = parseInt(val, 10)
-  if (Number.isNaN(num)) return val
-
-  return Math.min(Math.max(5, num), 9000)
-}
-
 @withStyles(styles)
 export default class HeartbeatForm extends React.PureComponent {
   static propTypes = {
@@ -58,9 +51,8 @@ export default class HeartbeatForm extends React.PureComponent {
               type='number'
               label='Timeout (minutes)'
               name='timeoutMinutes'
-              min={5}
+              min={1}
               max={9000}
-              mapOnChangeValue={clampTimeout}
             />
           </Grid>
         </Grid>
