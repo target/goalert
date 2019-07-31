ee68a6b431884ec6f897b9257bb35304eab4d479
Nathaniel Cook <Nathaniel.Cook@target.com>
2019-07-31 12:54:35 -0500

    update to latest mui versions from over the weekend

--- web/src/app/mui.js
+++ web/src/app/mui.js
@@ -1,4 +1,4 @@
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import { createMuiTheme } from '@material-ui/core/styles'
 import grey from '@material-ui/core/colors/grey'
 import red from '@material-ui/core/colors/red'
 
