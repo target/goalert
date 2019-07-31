809ad85b069341063deaf58ebd75faec6c7f7274
Nathaniel Cook <Nathaniel.Cook@target.com>
2019-07-31 11:01:10 -0500

    fix colors

--- web/src/app/util/statusStyles.js
+++ web/src/app/util/statusStyles.js
@@ -1,16 +1,23 @@
 import { green, red, yellow } from '@material-ui/core/colors'
 
+export const colors = {
+  noStatus: 'transparent',
+  statusOK: green[600],
+  statusWarning: yellow[600],
+  statusError: red[600],
+}
+
 export default {
   noStatus: {
     borderLeft: '3px solid transparent',
   },
   statusOK: {
-    borderLeft: '3px solid ' + green[600],
+    borderLeft: '3px solid ' + colors.statusOK,
   },
   statusWarning: {
-    borderLeft: '3px solid ' + yellow[600],
+    borderLeft: '3px solid ' + colors.statusWarning,
   },
   statusError: {
-    borderLeft: '3px solid ' + red[600],
+    borderLeft: '3px solid ' + colors.statusError,
   },
 }
