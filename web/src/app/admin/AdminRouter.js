190cc0c03232fe784348a6fe0c20475a518da951
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-01 15:19:47 -0500

    update hook components to use new hooks

--- web/src/app/admin/AdminRouter.js
+++ web/src/app/admin/AdminRouter.js
@@ -2,21 +2,24 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { GenericError, PageNotFound } from '../error-pages/Errors'
 import AdminConfig from './AdminConfig'
-import RequireConfig from '../util/RequireConfig'
+import { useSessionInfo } from '../util/RequireConfig'
+import Spinner from '../loading/components/Spinner'
 
-export const AdminRouter = () => (
-  <Switch>
-    <Route
-      exact
-      path='/admin/config'
-      render={() => (
-        <RequireConfig isAdmin else={<GenericError error='Access Denied' />}>
-          <AdminConfig />
-        </RequireConfig>
-      )}
-    />
-    <Route component={PageNotFound} />
-  </Switch>
-)
+export function AdminRouter() {
+  const { isAdmin, ready } = useSessionInfo()
+  if (!ready) {
+    return <Spinner />
+  }
+  if (!isAdmin) {
+    return <GenericError error='Access Denied' />
+  }
+
+  return (
+    <Switch>
+      <Route exact path='/admin/config' component={AdminConfig} />
+      <Route component={PageNotFound} />
+    </Switch>
+  )
+}
 
 export default AdminRouter
