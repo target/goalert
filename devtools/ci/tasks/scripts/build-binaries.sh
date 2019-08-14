d825b9b2f709e1c072d23d075dff12257e7dd47a
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-14 11:46:03 -0500

    path typo

--- devtools/ci/tasks/scripts/build-binaries.sh
+++ devtools/ci/tasks/scripts/build-binaries.sh
@@ -6,7 +6,7 @@ VERSION=$(./bin/goalert version | head -n 1 |awk '{print $2}')
 
 tar czf ../bin/goalert-${VERSION}-linux-amd64.tgz -C .. goalert/bin/goalert
 mkdir -p ../bin/goalert/bin
-cp goalert/bin/goalert ../bin/goalert/bin/
+cp bin/goalert ../bin/goalert/bin/
 
 rm -rf bin && make bin/goalert BUNDLE=1 GOOS=darwin
 tar czf ../bin/goalert-${VERSION}-darwin-amd64.tgz -C .. goalert/bin/goalert
