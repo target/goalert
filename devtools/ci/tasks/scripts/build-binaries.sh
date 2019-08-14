8813355756ef9191b106e000ccbf66b135812b0b
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-14 13:38:47 -0500

    update archive name

--- devtools/ci/tasks/scripts/build-binaries.sh
+++ devtools/ci/tasks/scripts/build-binaries.sh
@@ -3,7 +3,7 @@ set -ex
 
 BINARIES="bin/goalert"
 
-if [ "$BUILD_TEST" = "1" ]
+if [ "$BUILD_INTEGRATION" = "1" ]
 then
     BINARIES="bin/goalert bin/waitfor bin/runjson bin/mockslack bin/simpleproxy"
 fi
@@ -13,13 +13,13 @@ VERSION=$(./bin/goalert version | head -n 1 |awk '{print $2}')
 
 tar czf ../bin/goalert-${VERSION}-linux-amd64.tgz -C .. goalert/bin/goalert
 
-if [ "$BUILD_TEST" = "1" ]
+if [ "$BUILD_INTEGRATION" = "1" ]
 then
-    echo Building test files...
+    echo Building integration test files...
     mkdir cypress
     (cd web/src && yarn webpack --config webpack.cypress.js && cp -r cypress/fixtures ../../cypress/ && cp cypress.json ../../)
     sed -i 's/\.ts/\.js/' cypress.json
-    tar czf ../bin/goalert-test-${VERSION}-linux-amd64.tgz -C .. goalert/bin goalert/cypress goalert/cypress.json
+    tar czf ../bin/integration-${VERSION}-linux-amd64.tgz -C .. goalert/bin goalert/cypress goalert/cypress.json
 fi
 
 rm -rf bin && make bin/goalert BUNDLE=1 GOOS=darwin
