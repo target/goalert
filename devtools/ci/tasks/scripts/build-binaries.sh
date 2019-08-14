3b5eb79feba2ebc77070cd8fbb152b5a6795edcc
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-14 10:55:49 -0500

    wait on test binaries

--- devtools/ci/tasks/scripts/build-binaries.sh
+++ devtools/ci/tasks/scripts/build-binaries.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-BINARIES="bin/runjson bin/waitfor bin/simpleproxy bin/mockslack bin/goalert"
 set -ex
 
-make $BINARIES BUNDLE=1
+make bin/goalert BUNDLE=1
 VERSION=$(./bin/goalert version | head -n 1 |awk '{print $2}')
 
-tar czf ../bin/goalert-${VERSION}-linux-amd64.tgz -C .. goalert/bin
+tar czf ../bin/goalert-${VERSION}-linux-amd64.tgz -C .. goalert/bin/goalert
 
-rm -rf bin && make $BINARIES BUNDLE=1 GOOS=darwin
-tar czf ../bin/goalert-${VERSION}-darwin-amd64.tgz -C .. goalert/bin
+rm -rf bin && make bin/goalert BUNDLE=1 GOOS=darwin
+tar czf ../bin/goalert-${VERSION}-darwin-amd64.tgz -C .. goalert/bin/goalert
