299db238879ed2a9a3ee49f76becb5a97f2a177d
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-14 12:28:41 -0500

    add build all option

--- devtools/ci/tasks/scripts/build-binaries.sh
+++ devtools/ci/tasks/scripts/build-binaries.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 set -ex
 
-make bin/goalert bin/waitfor bin/runjson bin/mockslack bin/simpleproxy BUNDLE=1
+BINARIES="bin/goalert"
+
+if [ -n "$BUILD_ALL" ]
+then
+    BINARIES="bin/goalert bin/waitfor bin/runjson bin/mockslack bin/simpleproxy"
+fi
+
+make $BINARIES BUNDLE=1
 VERSION=$(./bin/goalert version | head -n 1 |awk '{print $2}')
 
 tar czf ../bin/goalert-${VERSION}-linux-amd64.tgz -C .. goalert/bin/goalert
 mkdir -p ../bin/goalert/bin
 cp bin/goalert ../bin/goalert/bin/
 
-tar czf ../bin/goalert-test-binaries-${VERSION}-linux-amd64.tgz -C .. goalert/bin
+if [ -n "$BUILD_ALL" ]
+then
+    tar czf ../bin/goalert-all-${VERSION}-linux-amd64.tgz -C .. goalert/bin
+fi
 
 rm -rf bin && make bin/goalert BUNDLE=1 GOOS=darwin
 tar czf ../bin/goalert-${VERSION}-darwin-amd64.tgz -C .. goalert/bin/goalert
