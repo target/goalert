5b6674faa530802b446186b3b763486ae85c5168
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-15 16:25:17 -0500

    use harness split code for psql-lite

--- devtools/psql-lite/main.go
+++ devtools/psql-lite/main.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"flag"
 	"log"
-	"strings"
 	"time"
 
-	"github.com/jackc/pgx"
+	"github.com/target/goalert/smoketest/harness"
 )
 
 func main() {
@@ -17,64 +16,16 @@ func main() {
 	inTx := flag.Bool("tx", false, "Run in transaction (faster).")
 	flag.Parse()
 
-	queries := strings.Split(*cmd, ";")
-	q := queries[:0]
-	for _, str := range queries {
-		str = strings.TrimSpace(str)
-		if str == "" {
-			continue
-		}
-		q = append(q, str)
-	}
-	queries = q
-	if len(queries) == 0 {
-		return
-	}
-
-	cfg, err := pgx.ParseConnectionString(*db)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conn, err := pgx.Connect(cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	if !*inTx {
-		// just run them one by one
-		for _, q := range queries {
-			tag, err := conn.ExecEx(ctx, q, nil)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %d", strings.SplitN(q, " ", 2)[0], tag.RowsAffected())
-		}
-		return
+	var err error
+	if *inTx {
+		err = harness.ExecSQLBatch(ctx, *db, *cmd)
+	} else {
+		err = harness.ExecSQL(ctx, *db, *cmd)
 	}
-
-	b := conn.BeginBatch()
-	defer b.Close()
-
-	for _, q := range queries {
-		b.Queue(q, nil, nil, nil)
-	}
-
-	err = b.Send(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for _, q := range queries {
-		tag, err := b.ExecResults()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("%s %d", strings.SplitN(q, " ", 2)[0], tag.RowsAffected())
-	}
-
 }
