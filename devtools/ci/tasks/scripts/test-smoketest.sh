bed4e8dd958f682843d36e5c4d6de5ad6023401b
Nathaniel Caza <mastercactapus@gmail.com>
2019-08-15 16:36:35 -0500

    pass env vars into makefile

--- devtools/ci/tasks/scripts/test-smoketest.sh
+++ devtools/ci/tasks/scripts/test-smoketest.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-export GOALERT_DB_URL="$DB_URL"
 set -x
 
 trap "tar czf ../debug/debug-$(git rev-parse HEAD)-smoketest.tgz -C .. goalert/smoketest/smoketest_db_dump" EXIT
 
-make smoketest
+make -e smoketest
