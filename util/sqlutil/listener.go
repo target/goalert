2fc0a6acb42939a8c34b21a415178c83aa31642b
Arundhati Rao <Arundhati.Rao@target.com>
2019-08-27 14:30:46 -0500

    formatting

--- util/sqlutil/listener.go
+++ util/sqlutil/listener.go
@@ -7,64 +7,61 @@ import (
 	"time"
 
 	"github.com/jackc/pgx"
-	"github.com/jackc/pgx/stdlib"
 	"github.com/target/goalert/switchover"
 )
 
-
 type ListenerEventType int
+
 const (
-    // ListenerEventConnected is emitted only when the database connection
-    // has been initially initialized. The err argument of the callback
-    // will always be nil.
-    ListenerEventConnected ListenerEventType = iota
+	// ListenerEventConnected is emitted only when the database connection
+	// has been initially initialized. The err argument of the callback
+	// will always be nil.
+	ListenerEventConnected ListenerEventType = iota
 
-    // ListenerEventDisconnected is emitted after a database connection has
-    // been lost, either because of an error or because Close has been
-    // called. The err argument will be set to the reason the database
-    // connection was lost.
-    ListenerEventDisconnected
+	// ListenerEventDisconnected is emitted after a database connection has
+	// been lost, either because of an error or because Close has been
+	// called. The err argument will be set to the reason the database
+	// connection was lost.
+	ListenerEventDisconnected
 
-    // ListenerEventReconnected is emitted after a database connection has
-    // been re-established after connection loss. The err argument of the
-    // callback will always be nil. After this event has been emitted, a
-    // nil pq.Notification is sent on the Listener.Notify channel.
-    ListenerEventReconnected
+	// ListenerEventReconnected is emitted after a database connection has
+	// been re-established after connection loss. The err argument of the
+	// callback will always be nil. After this event has been emitted, a
+	// nil pq.Notification is sent on the Listener.Notify channel.
+	ListenerEventReconnected
 
-    // ListenerEventConnectionAttemptFailed is emitted after a connection
-    // to the database was attempted, but failed. The err argument will be
-    // set to an error describing why the connection attempt did not
-    // succeed.
-    ListenerEventConnectionAttemptFailed
+	// ListenerEventConnectionAttemptFailed is emitted after a connection
+	// to the database was attempted, but failed. The err argument will be
+	// set to an error describing why the connection attempt did not
+	// succeed.
+	ListenerEventConnectionAttemptFailed
 )
-
 
 // Listener represents ....
 type Listener struct {
-    // Channel for receiving notifications from the database.  In some cases a
-    // nil value will be sent.
+	// Channel for receiving notifications from the database.  In some cases a
+	// nil value will be sent.
 	Notify chan *pgx.Notification
 
-	EventCallback EventCallbackType
-	Name string
+	EventCallback        EventCallbackType
+	Name                 string
 	MinReconnectInterval time.Duration
 	maxReconnectInterval time.Duration
 }
 
-
 func NewListener(name string, minReconnectInterval time.Duration, maxReconnectInterval time.Duration, eventCallback EventCallbackType) *Listener {
 	l := &Listener{
-		eventCallback: eventCallback,
-		name: name
+		eventCallback:        eventCallback,
+		name:                 name,
 		minReconnectInterval: minReconnectInterval,
 		maxReconnectInterval: maxReconnectInterval,
-		notify: make(chan *pgx.Notification, 32),
+		notify:               make(chan *pgx.Notification, 32),
 	}
 
 	return l
 }
 
-func (l *Listener) ListenerEventType(e EventCallbackType) EventCallbackType{
+func (l *Listener) ListenerEventType(e EventCallbackType) EventCallbackType {
 	return l.EventCallback
 }
 
@@ -78,12 +75,12 @@ func (l *Listener) Listen(db *sql.DB) {
 			}
 
 			c, err := pgx.ParseConnectionString(c.DBURL)
-			if err != nil{
+			if err != nil {
 				return err
 			}
-			
+
 			conn, err := pgx.Connect(c)
-			if err!= nil{
+			if err != nil {
 				return err
 			}
 
