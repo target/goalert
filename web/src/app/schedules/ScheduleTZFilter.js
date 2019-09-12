f891e7dce54487b9fb36204d7d8a432180d0bf69
Nathaniel Caza <mastercactapus@gmail.com>
2019-09-12 16:07:22 -0500

    convert ScheduleTZFilter to hooks (fixes jump)

--- web/src/app/schedules/ScheduleTZFilter.js
+++ web/src/app/schedules/ScheduleTZFilter.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import p from 'prop-types'
-import { connect } from 'react-redux'
 import { urlParamSelector } from '../selectors'
 import { setURLParam } from '../actions'
-import Query from '../util/Query'
 import gql from 'graphql-tag'
 import { FormControlLabel, Switch } from '@material-ui/core'
 import { oneOfShape } from '../util/propTypes'
+import { useQuery } from 'react-apollo'
+import { useSelector, useDispatch } from 'react-redux'
 
 const tzQuery = gql`
   query($id: ID!) {
@@ -17,53 +17,50 @@ const tzQuery = gql`
   }
 `
 
-@connect(
-  state => ({ zone: urlParamSelector(state)('tz', 'local') }),
-  dispatch => ({
-    setZone: value => dispatch(setURLParam('tz', value, 'local')),
+export function ScheduleTZFilter(props) {
+  const params = useSelector(urlParamSelector)
+  const zone = params('tz', 'local')
+  const dispatch = useDispatch()
+  const setZone = value => dispatch(setURLParam('tz', value, 'local'))
+  const { data, loading, error } = useQuery(tzQuery, {
+    pollInterval: 0,
+    variables: { id: props.scheduleID },
+  })
+
+  let label, tz
+  if (error) {
+    label = 'Error: ' + (error.message || error)
+  } else if (loading) {
+    label = 'Fetching timezone information...'
+  } else {
+    tz = data.schedule.timeZone
+    label = props.label ? props.label(tz) : `Show times in ${tz}`
+  }
+
+  return (
+    <FormControlLabel
+      control={
+        <Switch
+          checked={zone !== 'local'}
+          onChange={e => setZone(e.target.checked ? tz : 'local')}
+          value={tz}
+          disabled={Boolean(loading || error)}
+        />
+      }
+      label={label}
+    />
+  )
+}
+ScheduleTZFilter.propTypes = {
+  label: p.func,
+
+  // one of scheduleID or scheduleTimeZone must be specified
+  _tz: oneOfShape({
+    scheduleID: p.string,
+    scheduleTimeZone: p.string,
   }),
-)
-export class ScheduleTZFilter extends React.PureComponent {
-  static propTypes = {
-    label: p.func,
 
-    // one of scheduleID or scheduleTimeZone must be specified
-    _tz: oneOfShape({
-      scheduleID: p.string,
-      scheduleTimeZone: p.string,
-    }),
-
-    // provided by connect
-    zone: p.string,
-    setZone: p.func,
-  }
-  render() {
-    const { scheduleID, scheduleTimeZone } = this.props
-    if (scheduleTimeZone) return this.renderControl(scheduleTimeZone)
-
-    return (
-      <Query
-        variables={{ id: scheduleID }}
-        query={tzQuery}
-        noPoll
-        render={({ data }) => this.renderControl(data.schedule.timeZone)}
-      />
-    )
-  }
-
-  renderControl(tz) {
-    const { zone, label, setZone } = this.props
-    return (
-      <FormControlLabel
-        control={
-          <Switch
-            checked={zone !== 'local'}
-            onChange={e => setZone(e.target.checked ? tz : 'local')}
-            value={tz}
-          />
-        }
-        label={label ? label(tz) : `Show times in ${tz}`}
-      />
-    )
-  }
+  // provided by connect
+  zone: p.string,
+  setZone: p.func,
 }
