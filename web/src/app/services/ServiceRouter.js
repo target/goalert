8a76ad9f6f7076d299c43495abb0418f2da169d5
Nathaniel Cook <Nathaniel.Cook@target.com>
2019-10-02 14:52:31 -0500

    adjust filter button's aria label

--- web/src/app/services/ServiceRouter.js
+++ web/src/app/services/ServiceRouter.js
@@ -72,7 +72,7 @@ export default function ServiceRouter() {
       <FilterContainer
         iconButtonProps={{
           color: 'default',
-          'aria-label': 'Show Label Filters',
+          'aria-label': 'Show Services Filters',
         }}
         onReset={() => setSearchParam()}
       >
