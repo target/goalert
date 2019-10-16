81a6ba960549e17da0c7585434ee1a364e78b546
dctalbot <dctalbot@umich.edu>
2019-10-16 15:00:18 -0500

    merge clean smoketests

--- smoketest/prioritization_test.go
+++ smoketest/prioritization_test.go
@@ -2,9 +2,10 @@ package smoketest
 
 import (
 	"bytes"
-	"github.com/target/goalert/smoketest/harness"
 	"strconv"
 	"testing"
+
+	"github.com/target/goalert/smoketest/harness"
 )
 
 // TestPrioritization tests that notifications for new users/alerts get
@@ -61,9 +62,12 @@ func TestPrioritization(t *testing.T) {
 	tw := h.Twilio()
 	d1 := tw.Device(h.Phone("1"))
 
-	d1.ExpectSMS("service-1-alert")
-	d1.ExpectSMS("service-1-alert")
 	d1.IgnoreUnexpectedSMS("service-1-alert")
+
+	d1.ExpectSMS("service-1-alert")
+	tw.WaitAndAssert()
+
+	d1.ExpectSMS("service-1-alert")
 	tw.WaitAndAssert()
 
 	h.CreateAlert(h.UUID("s2"), "service-2-alert")
