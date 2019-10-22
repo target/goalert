4f6f49e80eeeff73ceab4be52b656c9818d09fa4
dctalbot <dctalbot@umich.edu>
2019-10-22 14:19:12 -0500

    fab -> speed dial

--- web/src/app/alerts/components/CreateAlertSpeedDial.js
+++ web/src/app/alerts/components/CreateAlertSpeedDial.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import AddIcon from '@material-ui/icons/Add'
+import LabelIcon from '@material-ui/icons/Label'
 import classnames from 'classnames'
 import AlertForm from './AlertForm'
+import CreateAlertByLabelDialog from './CreateAlertByLabelDialog'
 import withStyles from '@material-ui/core/styles/withStyles'
 import SpeedDial from '../../util/SpeedDial'
 
 const styles = theme => ({
-  fab: {
+  speedDial: {
     position: 'fixed',
     bottom: '2em',
     right: '2em',
@@ -26,7 +28,7 @@ const styles = theme => ({
       easing: theme.transitions.easing.easeOut,
     }),
   },
-  fabClose: {
+  speedDialClose: {
     transform: 'translate3d(0, 0, 0)',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.leavingScreen,
@@ -38,44 +40,69 @@ const styles = theme => ({
 @withStyles(styles)
 export default class CreateAlertSpeedDial extends Component {
   state = {
-    showForm: false,
+    showCreateAlertForm: false,
+    showCreateAlertByLabelDialog: false,
   }
 
-  handleShowForm = bool => {
-    this.setState({
-      showForm: bool,
-    })
+  showForm = form => {
+    switch (form) {
+      case 'createAlert':
+        this.setState({
+          showCreateAlertForm: true,
+        })
+        break
+      case 'createAlertByLabel':
+        this.setState({
+          showCreateAlertByLabelDialog: true,
+        })
+        break
+      default:
+        this.setState({
+          showCreateAlertForm: false,
+          showCreateAlertByLabelDialog: false,
+        })
+        break
+    }
   }
 
   render() {
     const { classes, showFavoritesWarning, transition } = this.props
 
-    let fabOpen = classes.transitionUp
+    let speedDialOpen = classes.transitionUp
     // use set padding for the larger verticle height on warning snackbar
     if (showFavoritesWarning) {
-      fabOpen = classes.warningTransitionUp
+      speedDialOpen = classes.warningTransitionUp
     }
 
-    const transitionClass = transition ? fabOpen : classes.fabClose
+    const transitionClass = transition ? speedDialOpen : classes.speedDialClose
 
     return (
       <React.Fragment>
         <SpeedDial
           data-cy='page-speed-dial'
           color='primary'
-          className={classnames(classes.fab, transitionClass)}
-          label='chill'
+          className={classnames(classes.speedDial, transitionClass)}
+          label='Create Alert'
           actions={[
             {
-              label: 'Create Alert',
-              onClick: () => this.handleShowForm(true),
+              label: 'Single Service',
+              onClick: () => this.showForm('createAlert'),
               icon: <AddIcon />,
+            },
+            {
+              label: 'Multi-Service',
+              onClick: () => this.showForm('createAlertByLabel'),
+              icon: <LabelIcon />,
             },
           ]}
         />
         <AlertForm
-          open={this.state.showForm}
-          handleRequestClose={() => this.handleShowForm(false)}
+          open={this.state.showCreateAlertForm}
+          handleRequestClose={() => this.showForm(null)}
+        />
+        <CreateAlertByLabelDialog
+          open={this.state.showCreateAlertByLabelDialog}
+          handleRequestClose={() => this.showForm(null)}
         />
       </React.Fragment>
     )
