70d28bf6723dd46c3ec916a898e07b26096ff839
dctalbot <dctalbot@umich.edu>
2019-10-23 10:32:06 -0500

    extract StepContent component

--- web/src/app/dialogs/CreateAlertDialog/index.js
+++ web/src/app/dialogs/CreateAlertDialog/index.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react'
 import {
-  Grid,
-  TextField,
   Dialog,
   Stepper,
   Step,
   StepLabel,
   DialogContent,
-  InputAdornment,
 } from '@material-ui/core'
 // import gql from 'graphql-tag'
 
 import { makeStyles } from '@material-ui/core/styles'
 // import Typography from '@material-ui/core/Typography'
 import classnames from 'classnames'
-import { FormContainer, FormField } from '../../forms'
-import ServiceLabelFilterContainer from '../../services/ServiceLabelFilterContainer'
-import { Search as SearchIcon } from '@material-ui/icons'
 import DialogNavigation from './DialogNavigation'
+import StepContent from './StepContent'
 
 const useStyles = makeStyles(theme => ({
   instructions: {
@@ -25,77 +20,6 @@ const useStyles = makeStyles(theme => ({
     marginBottom: theme.spacing(1),
   },
 }))
-
-const StepContent = props => {
-  switch (props.activeStep) {
-    case 0:
-      return (
-        <Grid container spacing={2}>
-          <FormContainer onChange={props.onChange}>
-            <Grid item xs={12}>
-              <FormField
-                fullWidth
-                label='Alert Summary'
-                name='summary'
-                required
-                component={TextField}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormField
-                fullWidth
-                label='Alert Details'
-                name='details'
-                required
-                component={TextField}
-              />
-            </Grid>
-          </FormContainer>
-        </Grid>
-      )
-    case 1:
-      return (
-        <Grid item xs={12}>
-          <FormContainer onChange={props.onChange}>
-            {/* <Grid item xs={12}>
-              <SelectedServices />
-            </Grid> */}
-            <FormField
-              fullWidth
-              label='Search Query'
-              name='searchQuery'
-              fieldName='searchQuery'
-              required
-              component={TextField}
-              InputProps={{
-                // ref: fieldRef,
-                startAdornment: (
-                  <InputAdornment position='start'>
-                    <SearchIcon color='action' />
-                  </InputAdornment>
-                ),
-                endAdornment: (
-                  <ServiceLabelFilterContainer
-                    // anchorRef={fieldRef}
-                    labelKey={'key'}
-                    labelValue={'() => console.log(value)'}
-                    onKeyChange={() => console.log('onKeyChange')}
-                    onValueChange={() => console.log('onValueChange')}
-                    // onReset={() => setSearchParam()}
-                  />
-                ),
-              }}
-            />
-          </FormContainer>
-        </Grid>
-      )
-
-    case 2:
-      return 'plz confirm ur info'
-    default:
-      return 'Unknown step'
-  }
-}
 
 export default function CreateAlertByLabelDialog(props) {
   const classes = useStyles()
