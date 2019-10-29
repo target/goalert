41f36449b78509cdc3be2044ac994f394a759d55
dctalbot <dctalbot@umich.edu>
2019-10-29 10:36:03 -0500

    WIP: multi insert

--- web/src/app/dialogs/CreateAlertDialog/DialogNavigation.js
+++ web/src/app/dialogs/CreateAlertDialog/DialogNavigation.js
@@ -3,14 +3,41 @@ import { DialogActions, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import gql from 'graphql-tag'
 import { useMutation } from 'react-apollo'
+import { fieldAlias, mergeFields, mapInputVars } from '../../util/graphql'
 
-const mutation = gql`
+const baseMutation = gql`
   mutation CreateAlertMutation($input: CreateAlertInput!) {
     createAlert(input: $input) {
       id
     }
   }
 `
+/*
+TRANSFORM TO
+mutation {
+  alias0: createAlert(
+    input: {
+      summary: "mysummary"
+      details: "mydetails"
+      serviceID: "292a2d74-213c-440d-b28b-7606b0dd02f1"
+    }
+  ) {
+    id
+  },
+  alias1: createAlert(
+    input: {
+      summary: "mysummary"
+      details: "mydetails"
+      serviceID: "djf82d74-213c-we0d-b28b-fefv0dd02f1"
+    }
+  ) {
+    id
+  },
+
+  ...
+
+}
+*/
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -43,15 +70,53 @@ export default props => {
 
   const onLastStep = () => activeStep === steps.length - 1
 
-  const [createAlert] = useMutation(mutation, {
-    variables: {
-      input: {
-        serviceID: formFields.selectedServices[0],
-        summary: formFields.summary.trim(),
-        details: formFields.details.trim(),
+  const getAliasedMutation = (mutation, index) =>
+    mapInputVars(fieldAlias(mutation, 'data' + index), {
+      input: 'input' + index,
+    })
+
+  const makeCreateAlerts = () => {
+    // 1. build mutation
+    let m = getAliasedMutation(baseMutation, 0)
+
+    for (let i = 1; i < formFields.selectedServices.length; i++) {
+      m = mergeFields(m, getAliasedMutation(baseMutation, i))
+    }
+
+    console.log(m)
+
+    // 2. build variables
+    /*
+    {
+      input1: {
+        summary: 'my summary',
+        details: 'my details',
+        serviceID: 'wefwe-ewf-wef-wef'
       },
-    },
-  })
+      input2: {
+        summary: 'my summary',
+        details: 'my details',
+        serviceID: 'rtyt-rty-rty-rty'
+      },
+      ...
+    }
+    */
+    let variables = {}
+    formFields.selectedServices.forEach((ss, i) => {
+      variables[`input${i}`] = {
+        summary: formFields.summary,
+        details: formFields.details,
+        serviceID: ss,
+      }
+    })
+
+    // 3. execute mutation with variables
+    return useMutation(m, {
+      variables,
+    })
+  }
+
+  const [createAlerts] = makeCreateAlerts()
 
   return (
     <DialogActions>
@@ -66,7 +131,7 @@ export default props => {
       <Button
         variant='contained'
         color='primary'
-        onClick={onLastStep() ? () => createAlert() : handleNext}
+        onClick={onLastStep() ? () => createAlerts() : handleNext}
         className={classes.button}
         disabled={nextIsDisabled(activeStep, formFields)}
       >
