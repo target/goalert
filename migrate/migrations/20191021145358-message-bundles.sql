4b8e1729e58dda8d96316d0b27c15fd408b51b02
Nathaniel Caza <mastercactapus@gmail.com>
2019-10-29 10:45:03 -0500

    update status column

--- migrate/migrations/20191021145358-message-bundles.sql
+++ migrate/migrations/20191021145358-message-bundles.sql
@@ -7,8 +7,8 @@ ALTER TABLE twilio_sms_callbacks
 CREATE INDEX idx_twilio_sms_service_id ON twilio_sms_callbacks (service_id);
 
 ALTER TABLE outgoing_messages
-    ADD status_count INT,
-    ADD CONSTRAINT om_status_bundle_count CHECK (message_type <> 'alert_status_update_bundle' OR status_count NOTNULL),
+    ADD status_alert_ids BIGINT[],
+    ADD CONSTRAINT om_status_alert_ids CHECK (message_type <> 'alert_status_update_bundle' OR status_alert_ids NOTNULL),
     DROP CONSTRAINT om_processed_no_fired_sent,
     ADD CONSTRAINT om_processed_no_fired_sent CHECK(
         last_status in ('pending','sending','failed','bundled') or
@@ -20,3 +20,12 @@ ALTER TABLE outgoing_messages
 ALTER TABLE twilio_sms_callbacks DROP service_id;
 DELETE FROM twilio_sms_callbacks WHERE alert_id ISNULL;
 ALTER TABLE twilio_sms_callbacks ALTER alert_id SET NOT NULL;
+
+DELETE FROM outgoing_messages WHERE message_type IN ('alert_notification_bundle', 'alert_status_update_bundle');
+ALTER TABLE outgoing_messages
+    DROP status_alert_ids,
+    DROP CONSTRAINT om_processed_no_fired_sent,
+    ADD CONSTRAINT om_processed_no_fired_sent CHECK(
+        last_status in ('pending','sending','failed') or
+        (fired_at isnull and sent_at notnull)
+    );
