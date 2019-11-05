17e6d4b917b6380ac57dc502b4fc28f5f88b1786
Nathaniel Caza <mastercactapus@gmail.com>
2019-11-05 16:17:21 -0600

    fix tasks

--- devtools/ci/tasks/scripts/test-integration.sh
+++ devtools/ci/tasks/scripts/test-integration.sh
@@ -16,7 +16,7 @@ then
   DEBUG_SUFFIX=mobile
 fi
 
-trap "tar czf ../../debug/debug-$(date +%Y%m%d%H%M%S)-$COMMIT-$DEBUG_SUFFIX.tgz cypress" EXIT
+trap "stop_postgres; tar czf ../../debug/debug-$(date +%Y%m%d%H%M%S)-$COMMIT-$DEBUG_SUFFIX.tgz cypress" EXIT
 
 mockslack \
   -client-id=000000000000.000000000000 \
