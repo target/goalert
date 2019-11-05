1c82af3bf330f422c10853a947850dbda1211077
dctalbot <dctalbot@umich.edu>
2019-11-05 11:38:06 -0600

    style step2

--- web/src/app/alerts/CreateAlertDialog/StepContent/step2.js
+++ web/src/app/alerts/CreateAlertDialog/StepContent/step2.js
@@ -4,8 +4,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import { ServiceChip } from '../../../util/Chips'
 
 const useStyles = makeStyles(theme => ({
+  hrUnderline: {
+    marginTop: 0,
+    marginBottom: 3,
+  },
+  item: { marginBottom: theme.spacing(2) },
+
+  itemTitle: {
+    paddingBottom: 0,
+  },
   nudgeRight: {
-    marginLeft: theme.spacing(1.3),
+    marginLeft: theme.spacing(1),
   },
 }))
 
@@ -14,39 +23,49 @@ export default props => {
 
   const classes = useStyles()
 
+  const Item = props => (
+    <div className={classes.item}>
+      <Typography
+        variant='subtitle1'
+        component='h3'
+        className={classes.itemTitle}
+      >
+        {props.title}
+      </Typography>
+      <hr className={classes.hrUnderline} />
+
+      {props.description && (
+        <Typography
+          variant='body1'
+          component='p'
+          className={classes.nudgeRight}
+        >
+          {props.description}
+        </Typography>
+      )}
+
+      {props.children}
+    </div>
+  )
+
   return (
     <Paper elevation={0}>
-      <Typography variant='subtitle1' component='h3'>
-        Summary
-      </Typography>
-      <Typography variant='body1' component='p' className={classes.nudgeRight}>
-        {formFields.summary}
-      </Typography>
-      <Typography variant='subtitle1' component='h3'>
-        Details
-      </Typography>
-      <Typography variant='body1' component='p' className={classes.nudgeRight}>
-        {formFields.details}
-      </Typography>
-      <Typography variant='subtitle1' component='h3'>
-        {`Selected Services (${formFields.selectedServices.length})`}
-      </Typography>
+      <Item title={'Summary'} description={formFields.summary} />
+      <Item title={'Details'} description={formFields.details} />
 
-      {formFields.selectedServices.length > 0 && (
-        <span>
-          <Paper elevation={0}>
-            {formFields.selectedServices.map((id, key) => (
-              <ServiceChip
-                key={key}
-                clickable={false}
-                id={id}
-                style={{ margin: 3 }}
-                onClick={e => e.preventDefault()}
-              />
-            ))}
-          </Paper>
-        </span>
-      )}
+      <Item title={`Selected Services (${formFields.selectedServices.length})`}>
+        <Paper elevation={0}>
+          {formFields.selectedServices.map((id, key) => (
+            <ServiceChip
+              key={key}
+              clickable={false}
+              id={id}
+              style={{ margin: 3 }}
+              onClick={e => e.preventDefault()}
+            />
+          ))}
+        </Paper>
+      </Item>
     </Paper>
   )
 }
