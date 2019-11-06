e8280b34da43cf131cbf28dc761a11b60dae5f8d
dctalbot <dctalbot@umich.edu>
2019-11-06 14:02:47 -0600

    hooks on speed-dial

--- web/src/app/alerts/components/CreateAlertSpeedDial.js
+++ web/src/app/alerts/components/CreateAlertSpeedDial.js
@@ -1,110 +1,70 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { KeyChange as ServicesIcon } from 'mdi-material-ui'
 import { Notifications as AlertsIcon } from '@material-ui/icons/'
+import { makeStyles } from '@material-ui/styles'
 import classnames from 'classnames'
 import AlertForm from './AlertForm'
 import CreateAlertDialog from '../CreateAlertDialog'
-import withStyles from '@material-ui/core/styles/withStyles'
 import SpeedDial from '../../util/SpeedDial'
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   speedDial: {
     position: 'fixed',
     bottom: '2em',
     right: '2em',
     zIndex: 9001,
   },
-  transitionUp: {
-    transform: 'translate3d(0, -62px, 0)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.enteringScreen,
-      easing: theme.transitions.easing.easeOut,
-    }),
-  },
-  warningTransitionUp: {
-    transform: 'translate3d(0, -7.75em, 0)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.enteringScreen,
-      easing: theme.transitions.easing.easeOut,
-    }),
-  },
-  speedDialClose: {
-    transform: 'translate3d(0, 0, 0)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.leavingScreen,
-      easing: theme.transitions.easing.sharp,
-    }),
-  },
-})
+}))
 
-@withStyles(styles)
-export default class CreateAlertSpeedDial extends Component {
-  state = {
-    showCreateAlertForm: false,
-    showCreateAlertDialog: false,
-  }
+export default props => {
+  const classes = useStyles()
 
-  showForm = form => {
+  const [showCreateAlertForm, setShowCreateAlertForm] = useState(false)
+  const [showCreateAlertDialog, setShowCreateAlertDialog] = useState(false)
+
+  const showForm = form => {
     switch (form) {
       case 'createAlert':
-        this.setState({
-          showCreateAlertForm: true,
-        })
+        setShowCreateAlertForm(true)
         break
       case 'createAlertByLabel':
-        this.setState({
-          showCreateAlertDialog: true,
-        })
+        setShowCreateAlertDialog(true)
         break
       default:
-        this.setState({
-          showCreateAlertForm: false,
-          showCreateAlertDialog: false,
-        })
+        setShowCreateAlertForm(false)
+        setShowCreateAlertDialog(false)
         break
     }
   }
 
-  render() {
-    const { classes, showFavoritesWarning, transition } = this.props
-
-    let speedDialOpen = classes.transitionUp
-    // use set padding for the larger verticle height on warning snackbar
-    if (showFavoritesWarning) {
-      speedDialOpen = classes.warningTransitionUp
-    }
-
-    const transitionClass = transition ? speedDialOpen : classes.speedDialClose
-
-    return (
-      <React.Fragment>
-        <SpeedDial
-          data-cy='page-speed-dial'
-          color='primary'
-          className={classnames(classes.speedDial, transitionClass)}
-          label='Create Alert'
-          actions={[
-            {
-              label: 'Alert Multiple Services',
-              onClick: () => this.showForm('createAlertByLabel'),
-              icon: <ServicesIcon />,
-            },
-            {
-              label: 'Create Single Alert',
-              onClick: () => this.showForm('createAlert'),
-              icon: <AlertsIcon />,
-            },
-          ]}
-        />
-        <AlertForm
-          open={this.state.showCreateAlertForm}
-          handleRequestClose={() => this.showForm(null)}
-        />
-        <CreateAlertDialog
-          open={this.state.showCreateAlertDialog}
-          handleRequestClose={() => this.showForm(null)}
-        />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <SpeedDial
+        data-cy='page-speed-dial'
+        color='primary'
+        className={classnames(classes.speedDial)}
+        label='Create Alert'
+        actions={[
+          {
+            label: 'Alert Multiple Services',
+            onClick: () => showForm('createAlertByLabel'),
+            icon: <ServicesIcon />,
+          },
+          {
+            label: 'Create Single Alert',
+            onClick: () => showForm('createAlert'),
+            icon: <AlertsIcon />,
+          },
+        ]}
+      />
+      <AlertForm
+        open={showCreateAlertForm}
+        handleRequestClose={() => showForm(null)}
+      />
+      <CreateAlertDialog
+        open={showCreateAlertDialog}
+        handleRequestClose={() => showForm(null)}
+      />
+    </React.Fragment>
+  )
 }
