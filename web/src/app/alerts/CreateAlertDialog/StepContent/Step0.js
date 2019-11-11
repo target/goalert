af1fdccaebc6749ebb4e264948b865348aa1b845
dctalbot <dctalbot@umich.edu>
2019-11-11 10:27:16 -0600

    name export

--- web/src/app/alerts/CreateAlertDialog/StepContent/Step0.js
+++ web/src/app/alerts/CreateAlertDialog/StepContent/Step0.js
@@ -2,28 +2,30 @@ import React from 'react'
 import { Grid, TextField } from '@material-ui/core'
 import { FormField } from '../../../forms'
 
-export default () => (
-  <Grid container spacing={2}>
-    <Grid item xs={12}>
-      <FormField
-        fullWidth
-        label='Alert Summary'
-        name='summary'
-        required
-        component={TextField}
-      />
+export default function Step0() {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <FormField
+          fullWidth
+          label='Alert Summary'
+          name='summary'
+          required
+          component={TextField}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <FormField
+          fullWidth
+          multiline
+          rows={7}
+          variant='outlined'
+          placeholder='Alert Details'
+          name='details'
+          required
+          component={TextField}
+        />
+      </Grid>
     </Grid>
-    <Grid item xs={12}>
-      <FormField
-        fullWidth
-        multiline
-        rows={7}
-        variant='outlined'
-        placeholder='Alert Details'
-        name='details'
-        required
-        component={TextField}
-      />
-    </Grid>
-  </Grid>
-)
+  )
+}
