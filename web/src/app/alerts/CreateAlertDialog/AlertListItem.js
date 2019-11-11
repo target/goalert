f7800f3d707c9cf5e265cf7669289f747ba82440
dctalbot <dctalbot@umich.edu>
2019-11-11 09:45:35 -0600

    absURLSelector generates absolute urls

--- web/src/app/alerts/CreateAlertDialog/AlertListItem.js
+++ web/src/app/alerts/CreateAlertDialog/AlertListItem.js
@@ -11,11 +11,16 @@ import OpenInNewIcon from '@material-ui/icons/OpenInNew'
 import ContentCopyIcon from 'mdi-material-ui/ContentCopy'
 
 import copyToClipboard from '../../util/copyToClipboard-v2'
+import { absURLSelector } from '../../selectors'
 
 export default function AlertListItem(props) {
   const { id } = props
 
-  const alertUrl = `${window.location.origin}/alerts/${id}`
+  const selectAlertUrl = absURLSelector({
+    router: { location: { pathname: '/alerts' } },
+  })
+
+  const alertUrl = `${window.location.origin}/${selectAlertUrl(id)}`
 
   return (
     <ListItem key={id} divider>
