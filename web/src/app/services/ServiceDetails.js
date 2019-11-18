c686af502c8e33498c18e6701077ee5b882eb89e
Nathaniel Cook <NathanielJCook@outlook.com>
2019-11-18 09:51:48 -0600

    tweak loading/error checks

--- web/src/app/services/ServiceDetails.js
+++ web/src/app/services/ServiceDetails.js
@@ -74,9 +74,10 @@ export default function ServiceDetails({ serviceID }) {
     returnPartialData: true,
   })
 
+  if (loading) return <Spinner />
   if (error) return <GenericError error={error.message} />
+
   if (!_.get(data, 'service.id')) {
-    if (loading) return <Spinner />
     return showDelete ? <Redirect to='/services' push /> : <ObjectNotFound />
   }
 
