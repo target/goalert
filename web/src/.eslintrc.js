1857909ee831089614a407cd90e36bcd2c8ef236
Nathaniel Caza <mastercactapus@gmail.com>
2019-11-20 15:20:45 -0600

    fix eslint rules

--- web/src/.eslintrc.js
+++ web/src/.eslintrc.js
@@ -6,23 +6,23 @@ module.exports = {
   parserOptions: {
     ecmaFeatures: { legacyDecorators: true },
   },
-  plugins: ['cypress', 'prettier', 'jsx-a11y', 'react-hooks'],
+  plugins: ['cypress', 'jsx-a11y', 'react-hooks', 'prettier'],
   extends: [
     'standard',
     'standard-jsx',
     'plugin:cypress/recommended',
-    'plugin:prettier/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'no-else-return': ['error', { allowElseIf: false }],
     'prettier/prettier': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react/jsx-fragments': ['error', 'element'],
-    'react/jsx-curly-newline': [
-      'error',
-      { multiline: 'consistent', singleline: 'consistent' },
-    ],
+
+    // handled by prettier
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-indent': 'off',
   },
   env: {
     'cypress/globals': true,
