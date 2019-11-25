a1dcd2fedf541344f4bc82b623b975472bf68827
Nathaniel Caza <mastercactapus@gmail.com>
2019-11-25 13:35:46 -0600

    fix LogEntryFetcher interface signature

--- alert/logentryfetcher.go
+++ alert/logentryfetcher.go
@@ -10,7 +10,7 @@ import (
 
 type LogEntryFetcher interface {
 	// LogEntry fetchs the latest log entry for a given alertID and type.
-	LogEntry(ctx context.Context) (alertlog.Entry, error)
+	LogEntry(ctx context.Context) (*alertlog.Entry, error)
 }
 
 type logError struct {
