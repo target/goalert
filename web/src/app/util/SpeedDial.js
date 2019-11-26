543ebd00ce0c1c748d4be6770b39dde7d175c2c7
David Talbot <dctalbot@umich.edu>
2019-11-26 14:14:00 -0600

    ui: update @material-ui/lab and convert SpeedDial to hooks (#204)
    * upgrade @hot-loader/react-dom @material-ui/core @material-ui/icons @types/luxon connected-react-router
    
    * upgrade history luxon mdi-material-ui husky
    
    * upgrade react react-dom react-ga react-router-dom terser-webpack-plugin ts-loader typescript webpack webpack-dev-server
    
    * oops, forgot the lockfile
    
    * WIP: convert speed dial to hooks, upgrade pkg
    
    * style tooltip labels
    
    * stop reversing every actions array
    
    * update speed dial tests
    
    * combine selectors
    
    * upgrade @material-ui/lab
    
    * bundle
    
    * Revert "stop reversing every actions array"
    
    This reverts commit 41cd2fe698de243b98a11986db3ff7081a4ce039.
    
    * remove comments of perplexion
    
    * upgrade @material-ui/lab
    
    * use the handle naming convention set by jsx-handler-names
    
    * makeStyles with obj
    
    * inline w anonymous funs

--- web/src/app/util/SpeedDial.js
+++ web/src/app/util/SpeedDial.js
@@ -1,74 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import p from 'prop-types'
-import SpeedDialAction from '@material-ui/lab/SpeedDialAction'
-import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon'
-import SpeedDial from '@material-ui/lab/SpeedDial'
+import { SpeedDial, SpeedDialIcon, SpeedDialAction } from '@material-ui/lab'
+import { makeStyles } from '@material-ui/styles'
 
-export default class ScheduleNewOverrideFAB extends React.PureComponent {
-  static propsTypes = {
-    label: p.string.isRequired,
-    actions: p.arrayOf(
-      p.shape({
-        icon: p.element.isRequired,
-        onClick: p.func.isRequired,
-        label: p.string.isRequired,
-      }),
-    ).isRequired,
-  }
+const useStyles = makeStyles({
+  speedDial: {
+    position: 'fixed',
+    bottom: '2em',
+    right: '2em',
+    zIndex: 9001,
+  },
+  staticTooltipLabel: {
+    whiteSpace: 'nowrap',
+  },
+})
 
-  state = {
-    open: false,
-  }
+export default function CustomSpeedDial(props) {
+  const [open, setOpen] = useState(false)
+  const classes = useStyles()
 
-  shownState = false
-  _shownTimeout = -1
+  return (
+    <SpeedDial
+      ariaLabel={props.label}
+      FabProps={{
+        'data-cy': 'page-fab',
+      }}
+      icon={<SpeedDialIcon />}
+      onClick={() => setOpen(!open)}
+      onClose={() => setOpen(false)}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      open={open}
+      className={classes.speedDial}
+    >
+      {props.actions
+        .slice()
+        .reverse()
+        .map((action, idx) => (
+          <SpeedDialAction
+            key={idx}
+            icon={action.icon}
+            tooltipTitle={action.label}
+            tooltipOpen
+            classes={{ staticTooltipLabel: classes.staticTooltipLabel }}
+            aria-label={action.label}
+            onClick={() => {
+              setOpen(false)
+              action.onClick()
+            }}
+          />
+        ))}
+    </SpeedDial>
+  )
+}
 
-  render() {
-    if (this.state.open !== this.shownState) {
-      clearTimeout(this._shownTimeout)
-      this._shownTimeout = setTimeout(() => {
-        this.shownState = this.state.open
-      }, 350)
-    }
-    const doToggle = () => this.setState({ open: !this.shownState })
-    const doOpen = () => this.setState({ open: true })
-    const doClose = () => this.setState({ open: false })
-    return (
-      <SpeedDial
-        ariaLabel={this.props.label}
-        ButtonProps={{
-          'data-cy': 'page-fab',
-        }}
-        icon={<SpeedDialIcon />}
-        onClick={doToggle}
-        onClose={doClose}
-        onMouseEnter={doOpen}
-        onMouseLeave={doClose}
-        open={this.state.open}
-        style={{
-          position: 'fixed',
-          bottom: '2em',
-          right: '2em',
-          zIndex: 9001,
-        }}
-      >
-        {this.props.actions
-          .slice()
-          .reverse()
-          .map((action, idx) => (
-            <SpeedDialAction
-              key={idx}
-              icon={action.icon}
-              tooltipOpen
-              tooltipTitle={action.label}
-              aria-label={action.label}
-              onClick={() => {
-                this.setState({ open: false })
-                action.onClick()
-              }}
-            />
-          ))}
-      </SpeedDial>
-    )
-  }
+CustomSpeedDial.propsTypes = {
+  label: p.string.isRequired,
+  actions: p.arrayOf(
+    p.shape({
+      icon: p.element.isRequired,
+      onClick: p.func.isRequired,
+      label: p.string.isRequired,
+    }),
+  ).isRequired,
 }
