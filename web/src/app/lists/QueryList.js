4e1e5a63ea3bdb7ece951f5d089f19a8809cea83
dctalbot <dctalbot@umich.edu>
2019-12-11 11:20:12 -0600

    convert to hooks

--- web/src/app/lists/QueryList.js
+++ web/src/app/lists/QueryList.js
@@ -4,68 +4,41 @@ import Grid from '@material-ui/core/Grid'
 import { PaginatedList } from './PaginatedList'
 import { ITEMS_PER_PAGE } from '../config'
 import { once } from 'lodash-es'
-import { connect } from 'react-redux'
-import { searchSelector } from '../selectors'
+import { useSelector } from 'react-redux'
+import { searchSelector, urlKeySelector } from '../selectors'
 import Query from '../util/Query'
 import { fieldAlias } from '../util/graphql'
 import Search from '../util/Search'
-import withStyles from '@material-ui/core/styles/withStyles'
+import { makeStyles } from '@material-ui/core'
 
-const styles = {
+const useStyles = makeStyles({
   flexGrow: {
     flexGrow: 1,
   },
-}
-
-const mapStateToProps = state => ({
-  search: searchSelector(state),
-  routeKey: state.router.location.key,
 })
 
-@withStyles(styles)
-@connect(mapStateToProps)
-export default class QueryList extends React.PureComponent {
-  static propTypes = {
-    // query must provide a single field that returns nodes
-    //
-    // For example:
-    // ```graphql
-    // query Services {
-    //   services {
-    //     nodes {
-    //       id
-    //       name
-    //       description
-    //     }
-    //   }
-    // }
-    // ```
-    query: p.object.isRequired,
+export default function QueryList(props) {
+  const classes = useStyles()
+  const searchParam = useSelector(searchSelector)
+  const urlKey = useSelector(urlKeySelector)
 
-    // mapDataNode should map the struct from each node in `nodes` to the struct required by a PaginatedList item.
-    mapDataNode: p.func,
+  const { noSearch, query, searchAdornment } = props
+  const { input, ...vars } = props.variables
 
-    // variables will be added to the initial query. Useful for things like `favoritesFirst` or alert filters.
-    // Note: The `input.search` and `input.first` parameters are included by default, but can be overridden.
-    variables: p.object,
-
-    // If set, the search string param is ignored.
-    noSearch: p.bool,
-
-    // provided by redux
-    search: p.string,
-    routeKey: p.string,
-
-    // filters additional to search, set in the search text field.
-    searchAdornment: p.node,
+  const variables = {
+    ...vars,
+    input: {
+      first: ITEMS_PER_PAGE,
+      search: searchParam,
+      ...input,
+    },
   }
 
-  static defaultProps = {
-    mapDataNode: n => ({ title: n.name, url: n.id, subText: n.description }),
-    variables: {},
+  if (noSearch) {
+    delete variables.input.search
   }
 
-  buildFetchMore = (fetchMore, after) => {
+  const buildFetchMore = (fetchMore, after) => {
     return once(num =>
       fetchMore({
         variables: {
@@ -89,7 +62,7 @@ export default class QueryList extends React.PureComponent {
     )
   }
 
-  renderContent = ({ data, loading, fetchMore }) => {
+  const renderContent = ({ data, loading, fetchMore }) => {
     let items = []
     let loadMore
     const {
@@ -100,19 +73,19 @@ export default class QueryList extends React.PureComponent {
       noSearch,
       search,
       ...listProps
-    } = this.props
+    } = props
 
     if (data && data.data && data.data.nodes) {
-      items = data.data.nodes.map(this.props.mapDataNode)
+      items = data.data.nodes.map(props.mapDataNode)
       if (data.data.pageInfo.hasNextPage) {
-        loadMore = this.buildFetchMore(fetchMore, data.data.pageInfo.endCursor)
+        loadMore = buildFetchMore(fetchMore, data.data.pageInfo.endCursor)
       }
     }
 
     return (
       <PaginatedList
         {...listProps}
-        key={this.props.routeKey}
+        key={urlKey}
         items={items}
         loadMore={loadMore}
         isLoading={loading}
@@ -120,43 +93,61 @@ export default class QueryList extends React.PureComponent {
     )
   }
 
-  render() {
-    const { classes, noSearch, query, search, searchAdornment } = this.props
-    const { input, ...vars } = this.props.variables
+  return (
+    <Grid container spacing={2}>
+      <Grid item className={classes.flexGrow} />
 
-    const variables = {
-      ...vars,
-      input: {
-        first: ITEMS_PER_PAGE,
-        search,
-        ...input,
-      },
-    }
-
-    if (noSearch) {
-      delete variables.input.search
-    }
-
-    return (
-      <Grid container spacing={2}>
-        <Grid item className={classes.flexGrow} />
-
-        {/* Such that filtering/searching isn't re-rendered with the page content */}
-        <Grid item>
-          <Search endAdornment={searchAdornment} />
-        </Grid>
-
-        <Grid item xs={12}>
-          <Query
-            query={fieldAlias(query, 'data')}
-            variables={variables}
-            noPoll
-            noSpin
-            notifyOnNetworkStatusChange
-            render={this.renderContent}
-          />
-        </Grid>
+      {/* Such that filtering/searching isn't re-rendered with the page content */}
+      <Grid item>
+        <Search endAdornment={searchAdornment} />
       </Grid>
-    )
-  }
+
+      <Grid item xs={12}>
+        <Query
+          query={fieldAlias(query, 'data')}
+          variables={variables}
+          noPoll
+          noSpin
+          notifyOnNetworkStatusChange
+          render={renderContent}
+        />
+      </Grid>
+    </Grid>
+  )
+}
+
+QueryList.propTypes = {
+  // query must provide a single field that returns nodes
+  //
+  // For example:
+  // ```graphql
+  // query Services {
+  //   services {
+  //     nodes {
+  //       id
+  //       name
+  //       description
+  //     }
+  //   }
+  // }
+  // ```
+  query: p.object.isRequired,
+
+  // mapDataNode should map the struct from each node in `nodes` to the struct required by a PaginatedList item.
+  mapDataNode: p.func,
+
+  // variables will be added to the initial query. Useful for things like `favoritesFirst` or alert filters.
+  // Note: The `input.search` and `input.first` parameters are included by default, but can be overridden.
+  variables: p.object,
+
+  // If set, the search string param is ignored.
+  noSearch: p.bool,
+
+  // filters additional to search, set in the search text field.
+  searchAdornment: p.node,
+}
+
+QueryList.defaultProps = {
+  mapDataNode: n => ({ title: n.name, url: n.id, subText: n.description }),
+  variables: {},
 }
