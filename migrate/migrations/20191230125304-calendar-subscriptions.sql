f6009f3c4dc9825ca2ed8a844dce2eca61ea66b7
Nathaniel Cook <NathanielJCook@outlook.com>
2019-12-30 14:59:34 -0600

    fix migration

--- migrate/migrations/20191230125304-calendar-subscriptions.sql
+++ migrate/migrations/20191230125304-calendar-subscriptions.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 -- id, name, user_id, last_access, disabled, url(?)
 CREATE TABLE calendar_subscriptions (
-    id SERIAL PRIMARY KEY,
+    id uuid NOT NULL UNIQUE,
     name TEXT NOT NULL,
     user_id uuid NOT NULL UNIQUE REFERENCES users(id),
     last_access TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
