f72944b519694407ba11de412d12f95009817d75
Nathaniel Caza <mastercactapus@gmail.com>
2019-12-30 14:23:18 -0600

    refactor promiseBatch for readability - no longer mutates Promise.prototype (fixes electron crash)

--- web/src/app/util/promiseBatch.js
+++ web/src/app/util/promiseBatch.js
@@ -1,54 +1,62 @@
-const BATCH_DELAY = 10
+import { BATCH_DELAY } from '../config'
 
-let current, timeout
-function Batch(initActive = 0) {
-  this._active = initActive
-  this.promise = new Promise(resolve => {
-    this._resolve = resolve
-  })
-  this.start = () => {
-    this._active++
-  }
-  this.done = () => {
-    this._active--
-    if (this._active === 0) this._resolve()
-  }
-}
+function _finally(p, fn) {
+  if (p.finally) return p.finally(fn)
 
-function start() {
-  if (!current) current = new Batch(1)
-  current.start()
-  clearTimeout(timeout)
-  const done = current.done
-  timeout = setTimeout(() => {
-    current = null
-    done()
-  }, BATCH_DELAY)
-  return [current.promise, current.done]
-}
-
-function _finally(fn) {
-  return this.then(
+  // fallback to manual implementation
+  return p.then(
     val => {
       const v = () => val
-      return Promise.resolve(fn()).then(v, v)
+      return Promise.resolve()
+        .then(fn)
+        .then(v)
     },
     err => {
       const e = () => Promise.reject(err)
-      return Promise.resolve(fn()).then(e, e)
+      return Promise.resolve()
+        .then(fn)
+        .then(e)
     },
   )
 }
 
-// polyfill finally
-if (!Promise.prototype.finally) {
-  // eslint-disable-next-line
-  Object.defineProperty(Promise.prototype, 'finally', { value: _finally })
+// BatchPromise allows batching promises together so they resolve at the same time.
+// It differes from Promise.all and Promise.allSettled in that you can add
+// additional promises after creation.
+class BatchPromise {
+  _p = new Promise(resolve => {
+    this._resolveP = resolve
+  })
+
+  _unresolvedCount = 1 // start with +1 for the timer
+
+  resolveOne = () => {
+    this._unresolvedCount--
+    if (this._unresolvedCount === 0) this._resolveP()
+    return this._p
+  }
+
+  addPromise = p => {
+    this._unresolvedCount++
+    return _finally(p, this.resolveOne)
+  }
 }
 
-// promiseBatch will wrap a promise so that it resolves at the same time as any
-// others within the delay.
-export default function promiseBatch(p) {
-  const [wait, done] = start()
-  return p.finally(done).finally(() => wait)
+let currentBatch, t
+function batchReset() {
+  currentBatch.resolveOne()
+  currentBatch = null
+}
+
+// promiseBatch will return a new Promise that will resolve after all Promises
+// that were provided during the debounce (ms) delay.
+export default function promiseBatch(p, debounce = BATCH_DELAY) {
+  if (!currentBatch) {
+    currentBatch = new BatchPromise()
+  }
+
+  clearTimeout(t)
+  t = setTimeout(batchReset, debounce)
+
+  return currentBatch.addPromise(p)
 }
