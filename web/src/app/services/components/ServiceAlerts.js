a746d4fb3aeca70a0c1114c03f572c7c7933c2e0
dctalbot <dctalbot@umich.edu>
2019-12-31 15:37:34 -0600

    convert to hooks

--- web/src/app/services/components/ServiceAlerts.js
+++ web/src/app/services/components/ServiceAlerts.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { PropTypes as p } from 'prop-types'
 import AlertsList from '../../alerts/components/AlertsList'
 import gql from 'graphql-tag'
@@ -9,7 +9,7 @@ import AlertsListFilter from '../../alerts/components/AlertsListFilter'
 import Search from '../../util/Search'
 import { sendGAEvent } from '../../util/GoogleAnalytics'
 
-const updateAllAlertsMutation = gql`
+const mutation = gql`
   mutation UpdateAlertStatusByServiceMutation(
     $input: UpdateAlertStatusByServiceInput!
   ) {
@@ -27,84 +27,70 @@ const updateAllAlertsMutation = gql`
   }
 `
 
-export default class ServiceAlerts extends Component {
-  static propTypes = {
-    serviceID: p.string.isRequired,
+export default function ServiceAlerts(props) {
+  const { serviceID } = props
+
+  const [alertStatus, setAlertStatus] = useState('')
+  const [showDialog, setShowDialog] = useState(false)
+
+  const handleClickAckAll = () => {
+    setAlertStatus('acknowledge')
+    setShowDialog(true)
   }
 
-  state = {
-    alertStatus: '',
-    showUpdateConfirmation: false,
+  const handleClickCloseAll = () => {
+    setAlertStatus('close')
+    setShowDialog(true)
   }
 
-  handleClickAckAll = () => {
-    this.setState({ alertStatus: 'acknowledge', showUpdateConfirmation: true })
-  }
-
-  handleClickCloseAll = () => {
-    this.setState({ alertStatus: 'close', showUpdateConfirmation: true })
-  }
-
-  handleShowForm = (key, bool) => {
-    this.setState({
-      [key]: bool,
-    })
-  }
-
-  handleAllAlertsSuccess = () => {
+  const handleAllAlertsSuccess = () => {
     sendGAEvent({
       category: 'Service',
-      action: this.state.alertStatus + ' All Action Completed',
+      action: alertStatus + ' All Action Completed',
     })
   }
 
-  getMenuOptions = () => {
+  const getMenuOptions = () => {
     return [
       {
         text: 'Acknowledge All Alerts',
-        onClick: this.handleClickAckAll,
+        onClick: handleClickAckAll,
       },
       {
         text: 'Close All Alerts',
-        onClick: this.handleClickCloseAll,
+        onClick: handleClickCloseAll,
       },
     ]
   }
 
-  render() {
-    const { serviceID } = this.props
+  return (
+    <React.Fragment>
+      <PageActions key='actions'>
+        <Search key='search' />
+        <AlertsListFilter key='filter' serviceID={serviceID} />
+        <Options key='options' options={getMenuOptions()} />
+      </PageActions>
+      <ConfirmationDialog
+        key='update-alerts-form'
+        mutation={mutation}
+        refetchQueries={['alerts']}
+        mutationVariables={{
+          input: {
+            service_id: serviceID,
+            status: alertStatus + 'd', // closed or acknowledged
+          },
+        }}
+        onMutationSuccess={handleAllAlertsSuccess}
+        onRequestClose={() => setShowDialog(false)}
+        open={showDialog}
+        message={`This will ${alertStatus} all the alerts for this service.`}
+        warning='This will stop all notifications from being sent out for all alerts with this service.'
+      />
+      <AlertsList serviceID={serviceID} />
+    </React.Fragment>
+  )
+}
 
-    return (
-      <React.Fragment>
-        <PageActions key='actions'>
-          <Search key='search' />
-          <AlertsListFilter key='filter' serviceID={serviceID} />
-          <Options key='options' options={this.getMenuOptions()} />
-        </PageActions>
-        <ConfirmationDialog
-          key='update-alerts-form'
-          mutation={updateAllAlertsMutation}
-          refetchQueries={['alerts']}
-          mutationVariables={{
-            input: {
-              service_id: serviceID,
-              status: this.state.alertStatus + 'd', // closed or acknowledged
-            },
-          }}
-          onMutationSuccess={this.handleAllAlertsSuccess}
-          onRequestClose={() =>
-            this.handleShowForm('showUpdateConfirmation', false)
-          }
-          open={this.state.showUpdateConfirmation}
-          message={
-            'This will ' +
-            this.state.alertStatus +
-            ' all the alerts for this service.'
-          }
-          warning='This will stop all notifications from being sent out for all alerts with this service.'
-        />
-        <AlertsList serviceID={serviceID} />
-      </React.Fragment>
-    )
-  }
+ServiceAlerts.propTypes = {
+  serviceID: p.string.isRequired,
 }
