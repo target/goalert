65752b2b132c015335bfbd185438430908073c13
Nathaniel Caza <mastercactapus@gmail.com>
2019-12-31 14:06:18 -0600

    update profile with dialog helpers

--- web/src/cypress/support/dialog.ts
+++ web/src/cypress/support/dialog.ts
@@ -3,6 +3,9 @@ declare namespace Cypress {
     /** Click a dialog button with the given text and wait for it to dissapear. */
     dialogFinish: typeof dialogFinish
 
+    /** Click a dialog button with the given text. */
+    dialogClick: typeof dialogClick
+
     /** Assert a dialog is present with the given title string. */
     dialogTitle: typeof dialogTitle
 
@@ -31,6 +34,11 @@ function fillFormField(name: string, value: string | string[]) {
       throw new TypeError('arrays only supported for search-select inputs')
     }
 
+    // material Select
+    if (el.attr('type') === 'hidden') {
+      return cy.get(selector).selectByLabel(value)
+    }
+
     return cy
       .get(selector)
       .clear()
@@ -39,35 +47,48 @@ function fillFormField(name: string, value: string | string[]) {
 }
 
 function dialogForm(values: { [key: string]: string | string[] }): void {
+  dialog()
   for (let key in values) {
     fillFormField(key, values[key])
   }
 }
-
-function dialogTitle(title: string): Cypress.Chainable {
+function dialog(): Cypress.Chainable {
   return cy
-    .get('[role=dialog] [data-cy=dialog-title]')
-    .contains(title)
+    .get('[data-cy=unmounting]')
+    .should('not.exist')
+    .get('[role=dialog]')
+    .should('have.length', 1)
     .should('be.visible')
 }
+function dialogTitle(title: string): Cypress.Chainable {
+  return dialog()
+    .find('[data-cy=dialog-title]')
+    .should('contain', title)
+}
 function dialogContains(content: string): Cypress.Chainable {
-  return cy
-    .get('[role=dialog]')
-    .contains(content)
-    .should('be.visible')
+  return dialog().should('contain', content)
+}
+
+function dialogClick(s: string): Cypress.Chainable {
+  return dialog()
+    .contains('button', s)
+    .click()
 }
 
 function dialogFinish(s: string): Cypress.Chainable {
-  return cy
-    .get('[role=dialog]')
-    .should('be.visible')
-    .contains('button', s)
-    .click()
-    .get('[role=dialog]')
-    .should('not.exist')
+  return dialog()
+    .get('[data-cy-gu]')
+    .then(el => {
+      const id = el.data('cyGu')
+      return cy
+        .dialogClick(s)
+        .get(`[data-cy-gu=${id}]`)
+        .should('not.exist')
+    })
 }
 
 Cypress.Commands.add('dialogFinish', dialogFinish)
 Cypress.Commands.add('dialogTitle', dialogTitle)
 Cypress.Commands.add('dialogForm', dialogForm)
 Cypress.Commands.add('dialogContains', dialogContains)
+Cypress.Commands.add('dialogClick', dialogClick)
