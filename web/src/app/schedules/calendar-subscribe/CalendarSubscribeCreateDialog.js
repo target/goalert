14d74eebff5ec6267d45659beb9a226831ca16fb
Nathaniel Cook <NathanielJCook@outlook.com>
2020-01-14 16:02:53 -0600

    link create subscription dialog with gql

--- web/src/app/schedules/calendar-subscribe/CalendarSubscribeCreateDialog.js
+++ web/src/app/schedules/calendar-subscribe/CalendarSubscribeCreateDialog.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react'
 import { PropTypes as p } from 'prop-types'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 import FormDialog from '../../dialogs/FormDialog'
 import CalendarSubscribeForm from './CalendarSubscribeForm'
 import { getForm, FormTitle, getSubtitle } from './formHelper'
+import { fieldErrors, nonFieldErrors } from '../../util/errutil'
+
+const mutation = gql`
+  mutation($input: CreateUserCalendarSubscriptionInput!) {
+    createUserCalendarSubscription(input: $input) {
+      id
+    }
+  }
+`
 
 const MOCK_URL =
   'www.calendarlabs.com/ical-calendar/ics/22/Chicago_Cubs_-_MLB.ics'
@@ -18,13 +29,23 @@ export default function CalendarSubscribeCreateDialog(props) {
     reminderMinutes: [],
   })
 
-  function submit() {
-    setIsComplete(true)
-  }
+  const [createSubscription, status] = useMutation(mutation, {
+    variables: {
+      input: {
+        scheduleID: props.scheduleID,
+        name: value.name,
+        reminderMinutes: value.reminderMinutes.map(r => parseInt(r.value)),
+        disabled: false,
+      },
+    },
+    onCompleted: () => setIsComplete(true),
+  })
 
   const form = (
     <CalendarSubscribeForm
       disableSchedField={Boolean(props.scheduleID)}
+      errors={fieldErrors(status.error)}
+      loading={status.loading}
       onChange={setValue}
       value={value}
     />
@@ -36,8 +57,9 @@ export default function CalendarSubscribeCreateDialog(props) {
       subTitle={getSubtitle(isComplete, SUBTITLE)}
       onClose={props.onClose}
       alert={isComplete}
+      errors={nonFieldErrors(status.error)}
       primaryActionLabel={isComplete ? 'Done' : null}
-      onSubmit={() => (isComplete ? props.onClose() : submit())}
+      onSubmit={() => (isComplete ? props.onClose() : createSubscription())}
       form={getForm(isComplete, form, MOCK_URL)}
     />
   )
