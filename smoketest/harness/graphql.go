d557a4abd06d1af4939f07d6416476eb1cdc17b5
dctalbot <dctalbot@umich.edu>
2020-01-15 15:57:17 -0600

    add SetSystemLimit helper

--- smoketest/harness/graphql.go
+++ smoketest/harness/graphql.go
@@ -14,6 +14,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/target/goalert/auth"
+	"github.com/target/goalert/limit"
 	"github.com/target/goalert/permission"
 	"github.com/target/goalert/user"
 )
@@ -57,6 +58,20 @@ func (h *Harness) SetConfigValue(id, value string) {
 	h.t.Helper()
 	res := h.GraphQLQuery2(fmt.Sprintf(`mutation{setConfig(input:[{id: %s, value: %s}])}`, strconv.Quote(id), strconv.Quote(value)))
 	assert.Empty(h.t, res.Errors)
+
+	// wait for engine cycle to complete to ensure next action
+	// uses new config only
+	h.Trigger()
+}
+
+// SetSystemLimit will update the value of a system limit given an id (e.g. `RulesPerSchedule`).
+func (h *Harness) SetSystemLimit(id limit.ID, value int) {
+	h.t.Helper()
+	h.execQuery(fmt.Sprintf(`
+		UPDATE config_limits
+		SET max = %d
+		WHERE id='%s'; 
+	`, value, id))
 
 	// wait for engine cycle to complete to ensure next action
 	// uses new config only
