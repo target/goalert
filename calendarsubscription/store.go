7286a1fc03a1acfba001cd258809526ea87abdca
arurao <Arundhati.Rao@target.com>
2020-01-20 15:23:55 -0600

    clean up

--- calendarsubscription/store.go
+++ calendarsubscription/store.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"encoding/json"
 
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/target/goalert/keyring"
 	"github.com/target/goalert/oncall"
 	"github.com/target/goalert/permission"
