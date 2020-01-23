03a3410c974c3ff71905ad699ac3ef9e05ac11d2
dctalbot <dctalbot@umich.edu>
2020-01-23 14:22:04 -0600

    nonidentifiable var names

--- web/src/app/util/luxon-helpers.test.js
+++ web/src/app/util/luxon-helpers.test.js
@@ -20,22 +20,22 @@ test('it should yield 12am on Sunday of the week of the date given', () => {
   const result2 = getNativeStartOfWeek()
   expect(result1.toMillis()).toBe(result2.getTime())
 
-  const davidsBirthday = new Date('November 26, 1997 03:24:00') // wednesday
-  const startOfDavidsWeek = new Date('November 23, 1997 00:00:00')
-  expect(getLuxonStartOfWeek(davidsBirthday).toMillis()).toBe(
-    startOfDavidsWeek.getTime(),
+  const randomWednesday = new Date('November 26, 1997 03:24:00')
+  const startOfRandomWednesdaysWeek = new Date('November 23, 1997 00:00:00')
+  expect(getLuxonStartOfWeek(randomWednesday).toMillis()).toBe(
+    startOfRandomWednesdaysWeek.getTime(),
   )
 
-  const katiesBirthday = new Date('December 15, 1996 03:24:30') // sunday
-  const startOfKatiesWeek = new Date('December 15, 1996 00:00:00')
-  expect(getLuxonStartOfWeek(katiesBirthday).toMillis()).toBe(
-    startOfKatiesWeek.getTime(),
+  const randomSunday = new Date('December 15, 1996 03:24:30')
+  const startOfRandomSundaysWeek = new Date('December 15, 1996 00:00:00')
+  expect(getLuxonStartOfWeek(randomSunday).toMillis()).toBe(
+    startOfRandomSundaysWeek.getTime(),
   )
 
-  const cooksBirthday = new Date('September 1, 2018 12:24:30') // saturday
-  const startOfCooksWeek = new Date('August 26, 2018 00:00:00')
-  expect(getLuxonStartOfWeek(cooksBirthday).toMillis()).toBe(
-    startOfCooksWeek.getTime(),
+  const randomSaturday = new Date('September 1, 2018 12:24:30')
+  const startOfRandomSaturdaysWeek = new Date('August 26, 2018 00:00:00')
+  expect(getLuxonStartOfWeek(randomSaturday).toMillis()).toBe(
+    startOfRandomSaturdaysWeek.getTime(),
   )
 
   const midnight = new Date('January 19, 2020 00:00:00') // 12am sunday
@@ -48,22 +48,22 @@ test('it should yield almost midnight on Saturday of the week of the date given'
   const result2 = getNativeEndOfWeek()
   expect(result1.toMillis()).toBe(result2.getTime())
 
-  const davidsBirthday = new Date('November 26, 1997 03:24:00') // wednesday
-  const endOfDavidsWeek = new Date('November 29, 1997 23:59:59:999')
-  expect(getLuxonEndOfWeek(davidsBirthday).toMillis()).toBe(
-    endOfDavidsWeek.getTime(),
+  const randomWednesday = new Date('November 26, 1997 03:24:00')
+  const endOfRandomWednesdaysWeek = new Date('November 29, 1997 23:59:59:999')
+  expect(getLuxonEndOfWeek(randomWednesday).toMillis()).toBe(
+    endOfRandomWednesdaysWeek.getTime(),
   )
 
-  const katiesBirthday = new Date('December 15, 1996 03:24:30') // sunday
-  const endOfKatiesWeek = new Date('December 21, 1996 23:59:59:999')
-  expect(getLuxonEndOfWeek(katiesBirthday).toMillis()).toBe(
-    endOfKatiesWeek.getTime(),
+  const randomSunday = new Date('December 15, 1996 03:24:30')
+  const endOfRandomSundaysWeek = new Date('December 21, 1996 23:59:59:999')
+  expect(getLuxonEndOfWeek(randomSunday).toMillis()).toBe(
+    endOfRandomSundaysWeek.getTime(),
   )
 
-  const cooksBirthday = new Date('September 1, 2018 12:24:30') // saturday
-  const endOfCooksWeek = new Date('September 1, 2018 23:59:59:999')
-  expect(getLuxonEndOfWeek(cooksBirthday).toMillis()).toBe(
-    endOfCooksWeek.getTime(),
+  const randomSaturday = new Date('September 1, 2018 12:24:30')
+  const endOfRandomSaturdaysWeek = new Date('September 1, 2018 23:59:59:999')
+  expect(getLuxonEndOfWeek(randomSaturday).toMillis()).toBe(
+    endOfRandomSaturdaysWeek.getTime(),
   )
 
   const almostMidnight = new Date('January 18, 2020 23:59:59:999') // saturday
