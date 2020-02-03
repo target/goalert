4edd0a426eb13a5a640abd7d2b2c260ac5faff03
Nathaniel Cook <NathanielJCook@outlook.com>
2020-02-03 15:35:51 -0600

    retain "confirm" as button text in admindialog

--- web/src/app/admin/AdminDialog.js
+++ web/src/app/admin/AdminDialog.js
@@ -56,6 +56,7 @@ export default class AdminDialog extends React.PureComponent {
             },
           })
         }
+        primaryActionLabel='Confirm'
         errors={nonFieldErrors(error).concat(
           fieldErrors(error).map(e => ({
             message: `${e.field}: ${e.message}`,
