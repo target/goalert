83484e58cf75ab6a6fcaa9afe33f8875f13f97a4
Nathaniel Cook <NathanielJCook@outlook.com>
2020-02-05 13:09:58 -0600

    update AlertRouter to use hooks

--- web/src/app/alerts/AlertRouter.js
+++ web/src/app/alerts/AlertRouter.js
@@ -5,14 +5,12 @@ import AlertList from './pages/AlertsIndexPage'
 import AlertDetails from './pages/AlertDetailPage'
 import { PageNotFound } from '../error-pages/Errors'
 
-export default class AlertRouter extends React.PureComponent {
-  render() {
-    return (
-      <Switch>
-        <Route exact path='/alerts' component={AlertList} />
-        <Route exact path='/alerts/:alertID' component={AlertDetails} />
-        <Route component={PageNotFound} />
-      </Switch>
-    )
-  }
+export default function AlertRouter() {
+  return (
+    <Switch>
+      <Route exact path='/alerts' component={AlertList} />
+      <Route exact path='/alerts/:alertID' component={AlertDetails} />
+      <Route component={PageNotFound} />
+    </Switch>
+  )
 }
