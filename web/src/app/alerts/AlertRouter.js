8b6cc41f9c133d4666879696d47b6e32f232cd63
Nathaniel Cook <NathanielJCook@outlook.com>
2020-02-05 13:20:52 -0600

    remove redundant index page

--- web/src/app/alerts/AlertRouter.js
+++ web/src/app/alerts/AlertRouter.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import AlertList from './pages/AlertsIndexPage'
 import AlertDetails from './pages/AlertDetailPage'
 import { PageNotFound } from '../error-pages/Errors'
+import PageActions from '../util/PageActions'
+import AlertsListFilter from './components/AlertsListFilter'
+import Search from '../util/Search'
+import AlertsList from './components/AlertsList'
 
 export default function AlertRouter() {
   return (
     <Switch>
-      <Route exact path='/alerts' component={AlertList} />
+      <Route
+        exact
+        path='/alerts'
+        render={() => (
+          <React.Fragment>
+            <PageActions>
+              <AlertsListFilter key='filter' />
+              <Search key='search' />
+            </PageActions>
+            <AlertsList />
+          </React.Fragment>
+        )}
+      />
       <Route exact path='/alerts/:alertID' component={AlertDetails} />
       <Route component={PageNotFound} />
     </Switch>
