626d22091ee4262dcce12a528b2e41ab70770f03
Nathaniel Cook <NathanielJCook@outlook.com>
2020-02-12 16:09:53 -0600

    render search controls with querylist instead

--- web/src/app/alerts/AlertRouter.js
+++ web/src/app/alerts/AlertRouter.js
@@ -3,27 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 
 import AlertDetails from './pages/AlertDetailPage'
 import { PageNotFound } from '../error-pages/Errors'
-import PageActions from '../util/PageActions'
-import AlertsListFilter from './components/AlertsListFilter'
-import Search from '../util/Search'
-import AlertsList from './components/AlertsList'
+import AlertsList from './AlertsList'
 
 export default function AlertRouter() {
   return (
     <Switch>
-      <Route
-        exact
-        path='/alerts'
-        render={() => (
-          <React.Fragment>
-            <PageActions>
-              <AlertsListFilter key='filter' />
-              <Search key='search' />
-            </PageActions>
-            <AlertsList />
-          </React.Fragment>
-        )}
-      />
+      <Route exact path='/alerts' component={AlertsList} />
       <Route exact path='/alerts/:alertID' component={AlertDetails} />
       <Route component={PageNotFound} />
     </Switch>
