563d0d9b507e3c806badbc5d1d4f9e4417dcc4a8
dctalbot <dctalbot@umich.edu>
2020-02-17 14:18:15 -0600

    convert to typescript

--- web/src/app/util/safeURL.ts
+++ web/src/app/util/safeURL.ts
@@ -1,7 +1,7 @@
 // safeURL will determine if a url is safe for linking.
 //
 // It tries to determine if the label is misleading.
-export function safeURL(url, label) {
+export function safeURL(url: string, label: string) {
   if (!/https?:\/\//.test(url)) return false // require absolute URLs
   if (!/[./]/.test(label)) return true // don't consider it a path/url without slashes or periods
   if (url.startsWith(label)) return true // if it matches the begining, then it's fine
