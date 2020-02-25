09264c65326d445d855753cfdddf2ac546790b97
dctalbot <dctalbot@umich.edu>
2020-02-25 13:03:29 -0600

    convert to hooks

--- web/src/app/util/Diff.js
+++ web/src/app/util/Diff.js
@@ -2,9 +2,9 @@ import React from 'react'
 import p from 'prop-types'
 import { diffChars, diffWords } from 'diff'
 import Typography from '@material-ui/core/Typography'
-import withStyles from '@material-ui/core/styles/withStyles'
+import { makeStyles } from '@material-ui/core'
 
-const styles = {
+const useStyles = makeStyles({
   // bg color for the whole first line
   oldLine: {
     backgroundColor: '#FFEEF0',
@@ -26,72 +26,71 @@ const styles = {
     minWidth: '2em',
     display: 'inline-block',
   },
-}
+})
 
 /*
- * Displays a difference in characters or words from
- * an old and new value
+ * Diff displays a difference in characters
+ * or words from an old and new value
  */
-@withStyles(styles)
-export default class Diff extends React.PureComponent {
-  static propTypes = {
-    oldValue: p.string,
-    newValue: p.string,
-    type: p.oneOf(['chars', 'words']),
-  }
+export default function Diff(props) {
+  const { oldValue, newValue, type } = props
 
-  static defaultProps = {
-    oldValue: '',
-    newValue: '',
-    type: 'chars',
-  }
+  const classes = useStyles()
+  const { oldLine, removed, newLine, added } = classes
 
-  render() {
-    const { classes, oldValue, newValue, type } = this.props
-    const { oldLine, removed, newLine, added } = classes
+  let diff = []
+  if (type === 'chars') diff = diffChars(oldValue, newValue)
+  if (type === 'words') diff = diffWords(oldValue, newValue)
 
-    let diff = []
-    if (type === 'chars') diff = diffChars(oldValue, newValue)
-    if (type === 'words') diff = diffWords(oldValue, newValue)
-
-    const oldWithRemoved = diff.map((part, idx) => {
-      if (part.added) return
-
-      return (
-        <span key={idx} className={part.removed ? removed : null}>
-          {part.value}
-        </span>
-      )
-    })
-
-    const newWithAdded = diff.map((part, idx) => {
-      if (part.removed) return
-
-      return (
-        <span key={idx} className={part.added ? added : null}>
-          {part.value}
-        </span>
-      )
-    })
-
-    const hideRemoved = diff.length === 1 && diff[0].added // net new string
-    const hideAdded = diff.length === 1 && diff[0].removed // deleted whole string
+  const oldWithRemoved = diff.map((part, idx) => {
+    if (part.added) return
 
     return (
-      <React.Fragment>
-        {!hideRemoved && (
-          <Typography className={oldLine} data-cy='old'>
-            <span className={classes.symbol}>&nbsp;-</span>
-            {oldWithRemoved}
-          </Typography>
-        )}
-        {!hideAdded && (
-          <Typography className={newLine} data-cy='new'>
-            <span className={classes.symbol}>&nbsp;+</span>
-            {newWithAdded}
-          </Typography>
-        )}
-      </React.Fragment>
+      <span key={idx} className={part.removed ? removed : null}>
+        {part.value}
+      </span>
     )
-  }
+  })
+
+  const newWithAdded = diff.map((part, idx) => {
+    if (part.removed) return
+
+    return (
+      <span key={idx} className={part.added ? added : null}>
+        {part.value}
+      </span>
+    )
+  })
+
+  const hideRemoved = diff.length === 1 && diff[0].added // net new string
+  const hideAdded = diff.length === 1 && diff[0].removed // deleted whole string
+
+  return (
+    <React.Fragment>
+      {!hideRemoved && (
+        <Typography className={oldLine} data-cy='old'>
+          <span className={classes.symbol}>&nbsp;-</span>
+          {oldWithRemoved}
+        </Typography>
+      )}
+      {!hideAdded && (
+        <Typography className={newLine} data-cy='new'>
+          <span className={classes.symbol}>&nbsp;+</span>
+          {newWithAdded}
+        </Typography>
+      )}
+    </React.Fragment>
+  )
+}
+
+Diff.propTypes = {
+  oldValue: p.string,
+  newValue: p.string,
+  type: p.oneOf(['chars', 'words']),
+}
+
+Diff.defaultProps = {
+  oldValue: '',
+  newValue: '',
+  type: 'chars',
 }
