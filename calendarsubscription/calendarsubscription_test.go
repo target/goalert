39de98e961ab6193c966718dae82a46e6d66c57f
Mitch Cimenski <mitch@m17ch.com>
2020-02-27 15:20:45 -0600

    add UID to calendarsubscription_test.go

--- calendarsubscription/calendarsubscription_test.go
+++ calendarsubscription/calendarsubscription_test.go
@@ -14,7 +14,8 @@ func TestCalendarSubscription_RenderICalFromShifts(t *testing.T) {
 	cs.Config.ReminderMinutes = []int{5, 10}
 	shifts := []oncall.Shift{{Start: time.Date(2020, 1, 1, 8, 0, 0, 0, time.UTC), End: time.Date(2020, 1, 15, 8, 0, 0, 0, time.UTC)}}
 	generatedAt := time.Date(2020, 1, 1, 5, 0, 0, 0, time.UTC)
-
+	cs.UserID = "01020304-0506-0708-090a-0b0c0d0e0f10"
+	cs.ScheduleID = "100f0e0d-0c0b-0a09-0807-060504030201"
 	iCal, err := cs.renderICalFromShifts(shifts, generatedAt)
 	assert.NoError(t, err)
 	expected := strings.Join([]string{
@@ -24,6 +25,7 @@ func TestCalendarSubscription_RenderICalFromShifts(t *testing.T) {
 		"CALSCALE:GREGORIAN",
 		"METHOD:PUBLISH",
 		"BEGIN:VEVENT",
+		"UID:2fb557c318579e8a60869c58ece5e019b767a1a898e1b31e75528dff632ccc44",
 		"SUMMARY:On-Call Shift",
 		"DTSTAMP:20200101T050000Z",
 		"DTSTART:20200101T080000Z",
