189116af36c5ebe55859abbe422fa69e3f9c49e8
KatriannaSydlik-Badgerow <katrianna.sydlik-badgerow@target.com>
2020-03-10 11:58:33 -0500

    adding typescript-eslint

--- web/src/.eslintrc.js
+++ web/src/.eslintrc.js
@@ -2,17 +2,26 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: { legacyDecorators: true },
   },
-  plugins: ['cypress', 'jsx-a11y', 'react-hooks', 'prettier'],
+  plugins: [
+    'cypress',
+    'jsx-a11y',
+    'react-hooks',
+    'prettier',
+    '@typescript-eslint',
+  ],
   extends: [
     'standard',
     'standard-jsx',
     'plugin:cypress/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
   ],
   rules: {
     'no-else-return': ['error', { allowElseIf: false }],
@@ -23,6 +32,11 @@ module.exports = {
     // handled by prettier
     'react/jsx-curly-newline': 'off',
     'react/jsx-indent': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
   settings: {
     react: { version: 'detect' },
