08c3c692fe548fc37d49ed24172957ad1d78e7a4
KatriannaSydlik-Badgerow <katrianna.sydlik-badgerow@target.com>
2020-03-17 10:45:03 -0500

    adding no-unused-vars tag with ignoreRestSiblings

--- web/src/.eslintrc.js
+++ web/src/.eslintrc.js
@@ -41,6 +41,12 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
   },
   settings: {
     react: { version: 'detect' },
