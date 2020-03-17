2e5657a55b4a5baf26707279648dff26c4385144
KatriannaSydlik-Badgerow <katrianna.sydlik-badgerow@target.com>
2020-03-17 14:31:04 -0500

    adding TODOs

--- web/src/.eslintrc.js
+++ web/src/.eslintrc.js
@@ -34,12 +34,18 @@ module.exports = {
     'react/jsx-indent': 'off',
 
     // typescript-eslint rules
+    // TODO: add options { allowExpressions: true, allowTypedFunctionExpressions: false }
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // TODO: use defaults
     '@typescript-eslint/no-namespace': 'off',
+    // TODO: use defaults
     '@typescript-eslint/no-use-before-define': 'off',
+    // TODO: no-explicit-any: on
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    // TODO: on, but off in config files
     '@typescript-eslint/no-var-requires': 'off',
+    // TODO: on, but bypass alerts files
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
