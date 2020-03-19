0da03e89b431e32be7a9f4c19d271bff3e3d2333
dctalbot <dctalbot@umich.edu>
2020-03-19 13:24:28 -0500

    extract types

--- web/src/cypress/support/calendar-subscription.ts
+++ web/src/cypress/support/calendar-subscription.ts
@@ -2,41 +2,6 @@ import { Chance } from 'chance'
 
 const c = new Chance()
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /**
-       * Creates a new calendar subscription.
-       */
-      createCalendarSubscription: typeof createCalendarSubscription
-
-      /**
-       * Deletes all calendar subscriptions given the specified user ID.
-       * Will default to deleting all subscriptions for 'profile' if no ID provided.
-       */
-      resetCalendarSubscriptions: typeof resetCalendarSubscriptions
-    }
-  }
-
-  interface CalendarSubscription {
-    id: string
-    name: string
-    reminderMinutes: Array<number>
-    scheduleID: string
-    schedule: Schedule
-    lastAccess: string
-    disabled: boolean
-  }
-
-  interface CalendarSubscriptionOptions {
-    name?: string
-    reminderMinutes?: Array<number>
-    scheduleID?: string
-    schedule?: ScheduleOptions
-    disabled?: boolean
-  }
-}
-
 /*
  * Generate a random array for the reminderMinutes variable
  */
