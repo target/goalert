0f9af955d35d36d9c847d7aa2121ef66200840a3
dctalbot <dctalbot@umich.edu>
2020-03-19 13:29:00 -0500

    extract types

--- web/src/cypress/support/rotation.ts
+++ web/src/cypress/support/rotation.ts
@@ -2,49 +2,6 @@ import { Chance } from 'chance'
 
 const c = new Chance()
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /**
-       * Creates a new rotation.
-       */
-      createRotation: typeof createRotation
-
-      /** Delete the rotation with the specified ID */
-      deleteRotation: typeof deleteRotation
-    }
-  }
-
-  type RotationType = 'hourly' | 'daily' | 'weekly'
-  interface Rotation {
-    id: string
-    name: string
-    description: string
-    timeZone: string
-    shiftLength: number
-    type: RotationType
-    start: string
-    users: Array<{
-      id: string
-      name: string
-      email: string
-    }>
-  }
-
-  interface RotationOptions {
-    name?: string
-    description?: string
-    timeZone?: string
-    shiftLength?: number
-    type?: RotationType
-    start?: string
-    favorite?: boolean
-
-    /** Number of participants to add to the rotation. */
-    count?: number
-  }
-}
-
 function createRotation(rot?: RotationOptions): Cypress.Chainable<Rotation> {
   const query = `mutation createRotation($input: CreateRotationInput!){
           createRotation(input: $input) {
---
+++ web/src/cypress/types/rotation.d.ts
@@ -0,0 +1,41 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace Cypress {
+  interface Chainable {
+    /**
+     * Creates a new rotation.
+     */
+    createRotation: typeof createRotation
+
+    /** Delete the rotation with the specified ID */
+    deleteRotation: typeof deleteRotation
+  }
+}
+
+type RotationType = 'hourly' | 'daily' | 'weekly'
+interface Rotation {
+  id: string
+  name: string
+  description: string
+  timeZone: string
+  shiftLength: number
+  type: RotationType
+  start: string
+  users: Array<{
+    id: string
+    name: string
+    email: string
+  }>
+}
+
+interface RotationOptions {
+  name?: string
+  description?: string
+  timeZone?: string
+  shiftLength?: number
+  type?: RotationType
+  start?: string
+  favorite?: boolean
+
+  /** Number of participants to add to the rotation. */
+  count?: number
+}
