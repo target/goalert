7fac5d30142b6eda468669b3213c269c4a8009e8
dctalbot <dctalbot@umich.edu>
2020-03-19 13:29:19 -0500

    extract types

---
+++ web/src/cypress/types/select-by-label.d.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace Cypress {
+  interface Chainable<Subject> {
+    /**
+     * Selects an item from a dropdown by it's label. Automatically accounts for search-selects.
+     */
+    selectByLabel: selectByLabelFn
+
+    /**
+     * Finds an item from a dropdown by it's label. Automatically accounts for search-selects.
+     */
+    findByLabel: findByLabelFn
+
+    /**
+     * Finds an item from a dropdown by it's label and removes it if it is a multiselect.
+     */
+    multiRemoveByLabel: multiRemoveByLabelFn
+  }
+}
+
+type selectByLabelFn = (label: string) => Cypress.Chainable
+type findByLabelFn = (label: string) => Cypress.Chainable
+type multiRemoveByLabelFn = (label: string) => Cypress.Chainable
