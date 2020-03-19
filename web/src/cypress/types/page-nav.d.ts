19b3ce7b5f44da6c21974e6293191b346bdbd93e
dctalbot <dctalbot@umich.edu>
2020-03-19 13:28:23 -0500

    extract types

---
+++ web/src/cypress/types/page-nav.d.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace Cypress {
+  interface Chainable {
+    /** Navigate to a link on the side/nav bar. */
+    pageNav: typeof pageNav
+  }
+}
