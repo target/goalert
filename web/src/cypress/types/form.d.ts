c6557c4a71153d5e21be61d8e9667f088cedfbaa
dctalbot <dctalbot@umich.edu>
2020-03-19 13:25:48 -0500

    extract types

---
+++ web/src/cypress/types/form.d.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace Cypress {
+  interface Chainable {
+    /** Update form fields with the given values. */
+    form: typeof form
+  }
+}
