427da48d621ea5207e42b2f6a56bf6afad2dc02d
dctalbot <dctalbot@umich.edu>
2020-03-19 13:24:53 -0500

    extract types

--- web/src/cypress/support/dialog.ts
+++ web/src/cypress/support/dialog.ts
@@ -1,24 +1,4 @@
 import { DateTime } from 'luxon'
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /** Click a dialog button with the given text and wait for it to disappear. */
-      dialogFinish: typeof dialogFinish
-
-      /** Click a dialog button with the given text. */
-      dialogClick: typeof dialogClick
-
-      /** Assert a dialog is present with the given title string. */
-      dialogTitle: typeof dialogTitle
-
-      /** Assert a dialog with the given content is present. */
-      dialogContains: typeof dialogContains
-
-      /** Update a dialog's form fields with the given values. */
-      dialogForm: typeof dialogForm
-    }
-  }
-}
 
 function dialogForm(values: {
   [key: string]: string | string[] | null | boolean | DateTime
---
+++ web/src/cypress/types/dialog.d.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace Cypress {
+  interface Chainable {
+    /** Click a dialog button with the given text and wait for it to disappear. */
+    dialogFinish: typeof dialogFinish
+
+    /** Click a dialog button with the given text. */
+    dialogClick: typeof dialogClick
+
+    /** Assert a dialog is present with the given title string. */
+    dialogTitle: typeof dialogTitle
+
+    /** Assert a dialog with the given content is present. */
+    dialogContains: typeof dialogContains
+
+    /** Update a dialog's form fields with the given values. */
+    dialogForm: typeof dialogForm
+  }
+}
