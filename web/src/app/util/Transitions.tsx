f14294eeebc5e3a90583139a3815f59431ad668e
dctalbot <dctalbot@umich.edu>
2020-03-30 11:57:37 -0500

    cast children as ReactElements

--- web/src/app/util/Transitions.tsx
+++ web/src/app/util/Transitions.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import Fade from '@material-ui/core/Fade'
 import Slide from '@material-ui/core/Slide'
 
 export const DefaultTransition = React.forwardRef((props, ref) => (
-  <Fade {...props} ref={ref} />
+  <Fade ref={ref}>{props.children as ReactElement}</Fade>
 ))
 
 export const FullscreenTransition = React.forwardRef((props, ref) => (
-  <Slide direction='left' {...props} ref={ref} />
+  <Slide direction='left' ref={ref}>
+    {props.children as ReactElement}
+  </Slide>
 ))
 
 export const FullscreenExpansion = React.forwardRef((props, ref) => (
-  <Slide direction='right' {...props} ref={ref} />
+  <Slide direction='right' ref={ref}>
+    {props.children as ReactElement}
+  </Slide>
 ))
