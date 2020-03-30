4a51ec000ea1398b5f4d8df9d139267656d8b8c4
dctalbot <dctalbot@umich.edu>
2020-03-30 12:18:18 -0500

    pass all props to transitions

--- web/src/app/util/Transitions.tsx
+++ web/src/app/util/Transitions.tsx
@@ -2,18 +2,26 @@ import React, { ReactElement } from 'react'
 import Fade from '@material-ui/core/Fade'
 import Slide from '@material-ui/core/Slide'
 
-export const DefaultTransition = React.forwardRef((props, ref) => (
-  <Fade ref={ref}>{props.children as ReactElement}</Fade>
-))
+export const DefaultTransition = React.forwardRef(
+  ({ children, ...props }, ref) => (
+    <Fade {...props} ref={ref}>
+      {children as ReactElement}
+    </Fade>
+  ),
+)
 
-export const FullscreenTransition = React.forwardRef((props, ref) => (
-  <Slide direction='left' ref={ref}>
-    {props.children as ReactElement}
-  </Slide>
-))
+export const FullscreenTransition = React.forwardRef(
+  ({ children, ...props }, ref) => (
+    <Slide {...props} direction='left' ref={ref}>
+      {children as ReactElement}
+    </Slide>
+  ),
+)
 
-export const FullscreenExpansion = React.forwardRef((props, ref) => (
-  <Slide direction='right' ref={ref}>
-    {props.children as ReactElement}
-  </Slide>
-))
+export const FullscreenExpansion = React.forwardRef(
+  ({ children, ...props }, ref) => (
+    <Slide {...props} direction='right' ref={ref}>
+      {children as ReactElement}
+    </Slide>
+  ),
+)
