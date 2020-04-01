ae1b9bde33d882d3e7cf9119dbc3040411d163c8
dctalbot <dctalbot@umich.edu>
2020-04-01 14:26:28 -0500

    convert to ts

---
+++ web/src/app/lists/SimpleListPage.tsx
@@ -0,0 +1,33 @@
+import React, { useState, ReactElement } from 'react'
+import QueryList, { QueryListProps } from './QueryList'
+import CreateFAB from './CreateFAB'
+
+interface SimpleListPageProps extends QueryListProps {
+  createForm: ReactElement
+  createLabel: string
+}
+
+export default function SimpleListPage(
+  props: SimpleListPageProps,
+): JSX.Element {
+  const { createForm, createLabel, ...rest } = props
+  const [create, setCreate] = useState(false)
+
+  return (
+    <React.Fragment>
+      <QueryList {...rest} />
+
+      {createForm && (
+        <CreateFAB
+          onClick={() => setCreate(true)}
+          title={`Create ${createLabel}`}
+        />
+      )}
+
+      {create &&
+        React.cloneElement(createForm, {
+          onClose: () => () => setCreate(false),
+        })}
+    </React.Fragment>
+  )
+}
