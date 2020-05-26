66b61e82a4f59ce529ffd367db0bd297bd5e1d14
Nathaniel Cook <NathanielJCook@outlook.com>
2020-05-26 12:04:52 -0500

    converting ServiceForm to hooks
    Co-authored-by: Quynhd07 <Quynhd07@yahoo.com>

--- web/src/app/services/ServiceForm.js
+++ web/src/app/services/ServiceForm.js
@@ -5,80 +5,67 @@ import TextField from '@material-ui/core/TextField'
 import { EscalationPolicySelect } from '../selection/EscalationPolicySelect'
 import { FormContainer, FormField } from '../forms'
 
-export default class ServiceForm extends React.PureComponent {
-  static propTypes = {
-    value: p.shape({
-      name: p.string,
-      description: p.string,
-      escalationPolicyID: p.string,
-    }).isRequired,
+export default function ServiceForm(props) {
+  const { epRequired, ...containerProps } = props
 
-    // indicates that the escalation policy is a required field
-    epRequired: p.bool,
-
-    errors: p.arrayOf(
-      p.shape({
-        field: p.oneOf(['name', 'description', 'escalationPolicyID'])
-          .isRequired,
-        message: p.string.isRequired,
-      }),
-    ),
-
-    onChange: p.func,
-
-    disabled: p.bool,
-  }
-
-  fieldError(name) {
-    if (!this.props.errors) return null
-    const err = this.props.errors.find((err) => err.field === name)
-    return err ? err.message : null
-  }
-
-  onChange = (values) => {
-    if (!this.props.onChange) return
-    this.props.onChange({
-      ...this.props.value,
-      ...values,
-    })
-  }
-
-  render() {
-    const { epRequired, ...containerProps } = this.props
-
-    return (
-      <FormContainer {...containerProps} optionalLabels={this.props.epRequired}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <FormField
-              fullWidth
-              label='Name'
-              name='name'
-              required
-              component={TextField}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormField
-              fullWidth
-              label='Description'
-              name='description'
-              multiline
-              component={TextField}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormField
-              fullWidth
-              label='Escalation Policy'
-              name='escalation-policy'
-              fieldName='escalationPolicyID'
-              required={epRequired}
-              component={EscalationPolicySelect}
-            />
-          </Grid>
+  return (
+    <FormContainer {...containerProps} optionalLabels={epRequired}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <FormField
+            fullWidth
+            label='Name'
+            name='name'
+            required
+            component={TextField}
+          />
         </Grid>
-      </FormContainer>
-    )
-  }
+        <Grid item xs={12}>
+          <FormField
+            fullWidth
+            label='Description'
+            name='description'
+            multiline
+            component={TextField}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormField
+            fullWidth
+            label='Escalation Policy'
+            name='escalation-policy'
+            fieldName='escalationPolicyID'
+            required={epRequired}
+            component={EscalationPolicySelect}
+          />
+        </Grid>
+      </Grid>
+    </FormContainer>
+  )
+}
+
+ServiceForm.propTypes = {
+  value: p.shape({
+    name: p.string,
+    description: p.string,
+    escalationPolicyID: p.string,
+  }).isRequired,
+
+  // indicates that the escalation policy is a required field
+  epRequired: p.bool,
+
+  errors: p.arrayOf(
+    p.shape({
+      field: p.oneOf(['name', 'description', 'escalationPolicyID']).isRequired,
+      message: p.string.isRequired,
+    }),
+  ),
+
+  onChange: p.func,
+
+  disabled: p.bool,
+}
+
+ServiceForm.defaultProps = {
+  epRequired: false,
 }
