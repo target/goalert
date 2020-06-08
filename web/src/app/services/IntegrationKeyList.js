870879d9b63dee2a5bb0baf7282b318a41442099
Jacob Gleason <Jacob.gleason@target.com>
2020-06-08 15:45:18 -0500

    Revert IntegrationKeyList

--- web/src/app/services/IntegrationKeyList.js
+++ web/src/app/services/IntegrationKeyList.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import p from 'prop-types'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import withStyles from '@material-ui/core/styles/withStyles'
 import gql from 'graphql-tag'
 import CreateFAB from '../lists/CreateFAB'
 import FlatList from '../lists/FlatList'
+import Query from '../util/Query'
 import IconButton from '@material-ui/core/IconButton'
 import { Trash } from '../icons'
 import IntegrationKeyCreateDialog from './IntegrationKeyCreateDialog'
@@ -13,10 +15,6 @@ import IntegrationKeyDeleteDialog from './IntegrationKeyDeleteDialog'
 import RequireConfig from '../util/RequireConfig'
 import CopyText from '../util/CopyText'
 import { AppLink } from '../util/AppLink'
-import { useQuery } from 'react-apollo'
-import { makeStyles } from '@material-ui/core'
-import Spinner from '../loading/components/Spinner'
-import { GenericError } from '../error-pages'
 
 const query = gql`
   query($serviceID: ID!) {
@@ -32,7 +30,7 @@ const query = gql`
   }
 `
 
-const useStyles = makeStyles(() => ({
+const styles = {
   copyIcon: {
     paddingRight: '0.25em',
     color: 'black',
@@ -45,7 +43,7 @@ const useStyles = makeStyles(() => ({
   spacing: {
     marginBottom: 96,
   },
-}))
+}
 
 const sortItems = (a, b) => {
   if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
@@ -55,115 +53,134 @@ const sortItems = (a, b) => {
   return 0
 }
 
-export function IntegrationKeyDetails(props) {
-  let tooltip = <CopyText title={'Copy ' + props.label} value={props.href} />
+@withStyles(styles)
+class IntegrationKeyDetails extends React.PureComponent {
+  static propTypes = {
+    href: p.string.isRequired,
+    label: p.string.isRequired,
+    type: p.string.isRequired,
 
-  // if link is not properly present, do not display to copy
-  if (props.type === 'email' && !props.href.startsWith('mailto:')) {
-    tooltip = null
+    // provided by withStyles
+    classes: p.object,
   }
 
-  return (
-    <React.Fragment>
-      {tooltip}
-      {props.type === 'email' && (
-        <RequireConfig
-          configID='Mailgun.Enable'
-          else='Email integration keys are currently disabled.'
-        />
-      )}
-    </React.Fragment>
-  )
+  state = {
+    showTooltip: false,
+  }
+
+  render() {
+    let tooltip = (
+      <CopyText title={'Copy ' + this.props.label} value={this.props.href} />
+    )
+
+    // if link is not properly present, do not display to copy
+    if (this.props.type === 'email' && !this.props.href.startsWith('mailto:')) {
+      tooltip = null
+    }
+
+    return (
+      <React.Fragment>
+        {tooltip}
+        {this.props.type === 'email' && (
+          <RequireConfig
+            configID='Mailgun.Enable'
+            else='Email integration keys are currently disabled.'
+          />
+        )}
+      </React.Fragment>
+    )
+  }
 }
 
-export default function IntegrationKeyList(props) {
-  const classes = useStyles()
-
-  const [create, setCreate] = useState(false)
-  const [deleteDialog, setDeleteDialog] = useState(null)
-
-  const { loading, error, data } = useQuery(query, {
-    variables: { serviceID: props.serviceID },
-  })
-
-  const typeLabels = {
-    generic: 'Generic API Key',
-    grafana: 'Grafana Webhook URL',
-    site24x7: 'Site24x7 Webhook URL',
-    email: 'Email Address',
+@withStyles(styles)
+export default class IntegrationKeyList extends React.PureComponent {
+  static propTypes = {
+    serviceID: p.string.isRequired,
   }
-  if (loading && !data) return <Spinner />
-  if (error) return <GenericError error={error.message} />
 
-  const items = (data.service.integrationKeys || [])
-    .slice()
-    .sort(sortItems)
-    .map((key) => ({
-      title: key.name,
-      subText: (
-        <IntegrationKeyDetails
-          key={key.id}
-          href={key.href}
-          label={typeLabels[key.type]}
-          type={key.type}
-          classes={props.classes}
+  state = {
+    create: false,
+    delete: null,
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Grid item xs={12} className={this.props.classes.spacing}>
+          <Card>
+            <CardContent>{this.renderQuery()}</CardContent>
+          </Card>
+        </Grid>
+        <CreateFAB
+          onClick={() => this.setState({ create: true })}
+          title='Create Integration Key'
         />
-      ),
-      secondaryAction: (
-        <IconButton onClick={() => setDeleteDialog(key.id)}>
-          <Trash />
-        </IconButton>
-      ),
-    }))
+        {this.state.create && (
+          <IntegrationKeyCreateDialog
+            serviceID={this.props.serviceID}
+            onClose={() => this.setState({ create: false })}
+          />
+        )}
+        {this.state.delete && (
+          <IntegrationKeyDeleteDialog
+            integrationKeyID={this.state.delete}
+            onClose={() => this.setState({ delete: null })}
+          />
+        )}
+      </React.Fragment>
+    )
+  }
 
-  return (
-    <React.Fragment>
-      <Grid item xs={12} className={classes.spacing}>
-        <Card>
-          <CardContent>
-            <FlatList
-              data-cy='int-keys'
-              headerNote={
-                <React.Fragment>
-                  API Documentation is available{' '}
-                  <AppLink to='/docs'>here</AppLink>.
-                </React.Fragment>
-              }
-              emptyMessage='No integration keys exist for this service.'
-              items={items}
-            />
-          </CardContent>
-        </Card>
-      </Grid>
-      <CreateFAB
-        onClick={() => setCreate(true)}
-        title='Create Integration Key'
+  renderQuery() {
+    return (
+      <Query
+        query={query}
+        variables={{ serviceID: this.props.serviceID }}
+        render={({ data }) => this.renderList(data.service.integrationKeys)}
       />
-      {create && (
-        <IntegrationKeyCreateDialog
-          serviceID={props.serviceID}
-          onClose={() => setCreate(false)}
-        />
-      )}
-      {deleteDialog && (
-        <IntegrationKeyDeleteDialog
-          integrationKeyID={deleteDialog}
-          onClose={() => setDeleteDialog(null)}
-        />
-      )}
-    </React.Fragment>
-  )
-}
+    )
+  }
 
-IntegrationKeyDetails.propTypes = {
-  href: p.string.isRequired,
-  label: p.string.isRequired,
-  type: p.string.isRequired,
+  renderList(keys) {
+    const typeLabels = {
+      generic: 'Generic API Key',
+      grafana: 'Grafana Webhook URL',
+      site24x7: 'Site24x7 Webhook URL',
+      email: 'Email Address',
+    }
 
-  // provided by withStyles
-  classes: p.object,
-}
+    const items = (keys || [])
+      .slice()
+      .sort(sortItems)
+      .map((key) => ({
+        title: key.name,
+        subText: (
+          <IntegrationKeyDetails
+            key={key.id}
+            href={key.href}
+            label={typeLabels[key.type]}
+            type={key.type}
+            classes={this.props.classes}
+          />
+        ),
+        secondaryAction: (
+          <IconButton onClick={() => this.setState({ delete: key.id })}>
+            <Trash />
+          </IconButton>
+        ),
+      }))
 
-IntegrationKeyList.propTypes = {
-  serviceID: p.string.isRequired,
+    return (
+      <FlatList
+        data-cy='int-keys'
+        headerNote={
+          <React.Fragment>
+            API Documentation is available <AppLink to='/docs'>here</AppLink>.
+          </React.Fragment>
+        }
+        emptyMessage='No integration keys exist for this service.'
+        items={items}
+      />
+    )
+  }
 }
