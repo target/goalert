14bd1f7c3a85a55f22f6d8d53e954337ba47be08
QuynhDo <quynh.do@target.com>
2020-06-09 11:29:20 -0700

    convert RotationUserListItem to hooks

--- web/src/app/rotations/RotationUserListItem.js
+++ web/src/app/rotations/RotationUserListItem.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PropTypes as p } from 'prop-types'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import { UserAvatar } from '../util/avatars'
-import withStyles from '@material-ui/core/styles/withStyles'
 import RotationUpdateDialog from './RotationUpdateDialog'
 import OtherActions from '../util/OtherActions'
 
-const styles = {
+const useStyles = {
   activeUser: {
     borderLeft: '6px solid #93ed94',
     background: '#defadf',
@@ -20,69 +19,62 @@ const styles = {
   },
 }
 
-@withStyles(styles)
-export default class RotationUserListItem extends React.PureComponent {
-  static propTypes = {
-    rotationID: p.string.isRequired,
-    userIDs: p.array.isRequired,
-    user: p.object,
-    index: p.number,
-    activeUserIndex: p.number.isRequired,
-  }
+export default function RotationUserListItem(props) {
+  const classes = useStyles()
+  const [active, setActive] = useState(-1)
+  const [deleteUser, setDeleteUser] = useState(false)
 
-  state = {
-    active: -1,
-    delete: false,
-  }
-
-  getActiveStyle = (index) => {
-    const { activeUserIndex, classes } = this.props
-
+  function getActiveStyle(index) {
     // light green left border, lighter green background
-    return activeUserIndex === index ? classes.activeUser : null
+    return props.activeUserIndex === index ? classes.activeUser : null
   }
 
-  render() {
-    const { index, user } = this.props
-    return (
-      <React.Fragment key={user.id}>
-        <ListItem key={index} className={this.getActiveStyle(index)}>
-          <UserAvatar userID={user.id} />
-          <ListItemText
-            primary={user.name}
-            secondary='Placeholder HandoffTimes Text'
-          />
-          <OtherActions
-            actions={[
-              {
-                label: 'Set Active',
-                onClick: () => this.setState({ active: index }),
-              },
-              {
-                label: 'Remove',
-                onClick: () => this.setState({ delete: user.id }),
-              },
-            ]}
-          />
-        </ListItem>
+  return (
+    <React.Fragment key={props.user.id}>
+      <ListItem key={props.index} className={getActiveStyle(props.index)}>
+        <UserAvatar userID={props.user.id} />
+        <ListItemText
+          primary={props.user.name}
+          secondary='Placeholder HandoffTimes Text'
+        />
+        <OtherActions
+          actions={[
+            {
+              label: 'Set Active',
+              onClick: () => setActive(props.index),
+            },
+            {
+              label: 'Remove',
+              onClick: () => setDeleteUser(props.user.id),
+            },
+          ]}
+        />
+      </ListItem>
 
-        {this.state.active !== -1 && (
-          <RotationUpdateDialog
-            rotationID={this.props.rotationID}
-            onClose={() => this.setState({ active: -1 })}
-            activeUserIndex={this.state.active}
-            userIDs={this.props.userIDs}
-          />
-        )}
-        {this.state.delete && (
-          <RotationUpdateDialog
-            rotationID={this.props.rotationID}
-            onClose={() => this.setState({ delete: false })}
-            userID={this.state.delete}
-            userIDs={this.props.userIDs}
-          />
-        )}
-      </React.Fragment>
-    )
-  }
+      {active !== -1 && (
+        <RotationUpdateDialog
+          rotationID={props.rotationID}
+          onClose={() => setActive(-1)}
+          activeUserIndex={active}
+          userIDs={props.userIDs}
+        />
+      )}
+      {deleteUser && (
+        <RotationUpdateDialog
+          rotationID={props.rotationID}
+          onClose={() => setDeleteUser(false)}
+          userID={deleteUser}
+          userIDs={props.userIDs}
+        />
+      )}
+    </React.Fragment>
+  )
+}
+
+RotationUserListItem.PropTypes = {
+  rotationID: p.string.isRequired,
+  userIDs: p.array.isRequired,
+  user: p.object,
+  index: p.number,
+  activeUserIndex: p.number.isRequired,
 }
