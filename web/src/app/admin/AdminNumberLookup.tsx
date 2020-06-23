979aae6fc65e28289e8a50813417345d9a87df86
Nathaniel Cook <NathanielJCook@outlook.com>
2020-06-23 15:49:54 -0500

    add error dialog if lookup mutation fails
    Co-authored-by: Quynhd07 <Quynhd07@yahoo.com>

--- web/src/app/admin/AdminNumberLookup.tsx
+++ web/src/app/admin/AdminNumberLookup.tsx
@@ -6,6 +6,9 @@ import {
   CardContent,
   CardActions,
   Checkbox,
+  Dialog,
+  DialogActions,
+  DialogTitle,
   Divider,
   List,
   ListItem,
@@ -18,6 +21,7 @@ import gql from 'graphql-tag'
 import { useMutation } from 'react-apollo'
 import CopyText from '../util/CopyText'
 import LoadingButton from '../loading/components/LoadingButton'
+import DialogContentError from '../dialogs/components/DialogContentError'
 
 const mutNoCarrier = gql`
   mutation($number: String!) {
@@ -57,11 +61,13 @@ const mutation = gql`
 export default function AdminNumberLookup(): JSX.Element {
   const [number, setNumber] = useState('')
   const [inclCarrier, setInclCarrier] = useState(false)
+  const [showErrorDialog, setShowErrorDialog] = useState(false)
 
-  const [lookup, { data, loading }] = useMutation(
+  const [lookup, { data, loading, error }] = useMutation(
     inclCarrier ? mutation : mutNoCarrier,
     {
       variables: { number },
+      onError: () => setShowErrorDialog(true),
     },
   )
 
@@ -86,72 +92,88 @@ export default function AdminNumberLookup(): JSX.Element {
   }
 
   return (
-    <Form>
-      <Card>
-        <CardContent>
-          <TextField
-            onChange={(e) => setNumber(e.target.value)}
-            value={number}
-            label='Phone Number'
-            helperText='Including + and country code'
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={inclCarrier}
-                onChange={(e) => setInclCarrier(e.target.checked)}
-              />
-            }
-            label='Include carrier information'
-          />
-        </CardContent>
-
-        <CardActions>
-          <Tooltip title='May incur Twilio charges' placement='right'>
-            <LoadingButton
-              buttonText='Lookup'
-              onClick={() => {
-                lookup()
-              }}
-              loading={loading}
+    <React.Fragment>
+      <Form>
+        <Card>
+          <CardContent>
+            <TextField
+              onChange={(e) => setNumber(e.target.value)}
+              value={number}
+              label='Phone Number'
+              helperText='Including + and country code'
             />
-          </Tooltip>
-        </CardActions>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={inclCarrier}
+                  onChange={(e) => setInclCarrier(e.target.checked)}
+                />
+              }
+              label='Include carrier information'
+            />
+          </CardContent>
 
-        {data?.debugPhoneNumberInfo && (
-          <List dense={true}>
-            {renderListItem(
-              'Region Code',
-              data.debugPhoneNumberInfo.regionCode,
-            )}
-            {renderListItem(
-              'Formatted Phone Number',
-              data.debugPhoneNumberInfo.formatted,
-            )}
-            {data?.debugPhoneNumberInfo?.carrier && (
-              <React.Fragment>
-                {renderListItem(
-                  'Carrier Name',
-                  data.debugPhoneNumberInfo.carrier.name,
-                  true,
-                )}
-                {renderListItem(
-                  'Carrier Type',
-                  data.debugPhoneNumberInfo.carrier.type,
-                )}
-                {renderListItem(
-                  'Mobile Network Code',
-                  data.debugPhoneNumberInfo.carrier.mobileNetworkCode,
-                )}
-                {renderListItem(
-                  'Mobile Country Code',
-                  data.debugPhoneNumberInfo.carrier.mobileCountryCode,
-                )}
-              </React.Fragment>
-            )}
-          </List>
-        )}
-      </Card>
-    </Form>
+          <CardActions>
+            <Tooltip title='May incur Twilio charges' placement='right'>
+              <LoadingButton
+                buttonText='Lookup'
+                onClick={() => {
+                  lookup()
+                }}
+                loading={loading}
+              />
+            </Tooltip>
+          </CardActions>
+
+          {data?.debugPhoneNumberInfo && (
+            <List dense={true}>
+              {renderListItem(
+                'Region Code',
+                data.debugPhoneNumberInfo.regionCode,
+              )}
+              {renderListItem(
+                'Formatted Phone Number',
+                data.debugPhoneNumberInfo.formatted,
+              )}
+              {data?.debugPhoneNumberInfo?.carrier && (
+                <React.Fragment>
+                  {renderListItem(
+                    'Carrier Name',
+                    data.debugPhoneNumberInfo.carrier.name,
+                    true,
+                  )}
+                  {renderListItem(
+                    'Carrier Type',
+                    data.debugPhoneNumberInfo.carrier.type,
+                  )}
+                  {renderListItem(
+                    'Mobile Network Code',
+                    data.debugPhoneNumberInfo.carrier.mobileNetworkCode,
+                  )}
+                  {renderListItem(
+                    'Mobile Country Code',
+                    data.debugPhoneNumberInfo.carrier.mobileCountryCode,
+                  )}
+                </React.Fragment>
+              )}
+            </List>
+          )}
+        </Card>
+      </Form>
+
+      <Dialog open={showErrorDialog} onClose={() => setShowErrorDialog(false)}>
+        <DialogTitle>An error occurred</DialogTitle>
+        <DialogContentError error={error?.message ?? ''} />
+        <DialogActions>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={() => setShowErrorDialog(false)}
+          >
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   )
 }
