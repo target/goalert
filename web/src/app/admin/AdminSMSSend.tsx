68bbc179b4c86b32479c3f9fc3891541ad924404
QuynhDo <quynh.do@target.com>
2020-06-23 18:51:16 -0700

    style SendSMS cardcontent, add mutation error dialog

--- web/src/app/admin/AdminSMSSend.tsx
+++ web/src/app/admin/AdminSMSSend.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react'
 import { Form } from '../forms'
-import { Card, TextField, Button } from '@material-ui/core'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  InputAdornment,
+  TextField,
+} from '@material-ui/core'
 import gql from 'graphql-tag'
 import { useMutation } from 'react-apollo'
 import { useConfigValue } from '../util/RequireConfig'
-import Spinner from '../loading/components/Spinner'
 import { AppLink } from '../util/AppLink'
+import LoadingButton from '../loading/components/LoadingButton'
+import DialogContentError from '../dialogs/components/DialogContentError'
 
 const sendSMSMutation = gql`
   mutation DebugSendSMS($input: DebugSendSMSInput!) {
@@ -21,10 +32,12 @@ const sendSMSMutation = gql`
   - Generic error display
 */
 export default function AdminSMSSend(): JSX.Element {
+  // const classes = useStyles()
   const [cfgFromNumber] = useConfigValue('Twilio.FromNumber')
   const [fromNumber, setFromNumber] = useState(cfgFromNumber)
   const [toNumber, setToNumber] = useState('')
   const [body, setBody] = useState('')
+  const [showErrorDialog, setShowErrorDialog] = useState(false)
 
   const [send, sendStatus] = useMutation(sendSMSMutation, {
     variables: {
@@ -34,50 +47,103 @@ export default function AdminSMSSend(): JSX.Element {
         body,
       },
     },
+    onError: () => setShowErrorDialog(true),
   })
 
   return (
-    <Form>
-      <Card>
-        <TextField
-          onChange={(e) => setFromNumber(e.target.value)}
-          fullWidth
-          value={fromNumber}
-          label='From Number'
-          helperText='Including + and country code'
-        />
-        <TextField
-          onChange={(e) => setToNumber(e.target.value)}
-          fullWidth
-          value={toNumber}
-          label='To Number'
-          helperText='Including + and country code'
-        />
-        <TextField
-          onChange={(e) => setBody(e.target.value)}
-          fullWidth
-          value={body}
-          label='Body'
-          multiline
-        />
+    <React.Fragment>
+      <Form>
+        <Card>
+          <CardContent>
+            <div>
+              <TextField
+                onChange={(e) => setFromNumber(e.target.value)}
+                value={fromNumber}
+                label='From Number'
+                helperText='Please provide your country code e.g. +1 (USA), +91 (India), +44
+              (UK)'
+                type='tel'
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment
+                      position='start'
+                      style={{ marginBottom: '0.1em' }}
+                    >
+                      +
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <br />
+            <div>
+              <TextField
+                onChange={(e) => setToNumber(e.target.value)}
+                value={toNumber}
+                label='To Number'
+                helperText='Please provide your country code e.g. +1 (USA), +91 (India), +44
+              (UK)'
+                type='tel'
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment
+                      position='start'
+                      style={{ marginBottom: '0.1em' }}
+                    >
+                      +
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <br />
+            <div>
+              <TextField
+                onChange={(e) => setBody(e.target.value)}
+                value={body}
+                fullWidth
+                label='Body'
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                multiline
+              />
+            </div>
+          </CardContent>
 
-        <Button
-          onClick={() => {
-            send()
-          }}
-        >
-          Send
-        </Button>
+          <CardActions>
+            <LoadingButton
+              buttonText='Send'
+              onClick={() => {
+                send()
+              }}
+              loading={sendStatus.loading}
+            />
+          </CardActions>
 
-        <hr />
-        {sendStatus.loading && <Spinner />}
-        {sendStatus.error && sendStatus.error.message}
-        {sendStatus.data?.debugSendSMS && (
-          <AppLink to={sendStatus.data.debugSendSMS.providerURL} newTab>
-            {sendStatus.data.debugSendSMS.id}
-          </AppLink>
-        )}
-      </Card>
-    </Form>
+          <CardContent>
+            {sendStatus.data?.debugSendSMS && (
+              <AppLink to={sendStatus.data.debugSendSMS.providerURL} newTab>
+                {sendStatus.data.debugSendSMS.id}
+              </AppLink>
+            )}
+          </CardContent>
+        </Card>
+      </Form>
+
+      <Dialog open={showErrorDialog} onClose={() => setShowErrorDialog(false)}>
+        <DialogTitle>An error occurred</DialogTitle>
+        <DialogContentError error={sendStatus.error?.message ?? ''} />
+        <DialogActions>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={() => setShowErrorDialog(false)}
+          >
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   )
 }
