29cea3c9f5fc18ccd20bd715d791e3fbae4948c4
Nathaniel Caza <mastercactapus@gmail.com>
2020-06-26 14:03:20 -0500

    use resetdb instead of init.sql

--- devtools/ci/dockerfiles/all-in-one/start.sh
+++ devtools/ci/dockerfiles/all-in-one/start.sh
@@ -3,7 +3,7 @@ set -e
 
 export PGDATA=/var/lib/postgresql/data PGUSER=postgres DB_URL=postgresql://postgres@
 
-if test -f /bin/init.sql
+if ! test -f /init-db
 then
   mkdir -p ${PGDATA} /run/postgresql /var/log/postgresql &&\
   chown postgres ${PGDATA} /run/postgresql /var/log/postgresql &&\
@@ -12,15 +12,17 @@ then
   echo "listen_addresses='*'" >> $PGDATA/postgresql.conf &&\
   echo "fsync = off" >> $PGDATA/postgresql.conf &&\
   echo "full_page_writes = off" >> $PGDATA/postgresql.conf
+  touch /init-db
 fi
 
 su postgres -s /bin/sh -c "pg_ctl start -w -l /var/log/postgresql/server.log"
 
-if test -f /bin/init.sql
+if ! test -f /init-data
 then
   echo Seeding DB with demo data...
-  psql -d "$DB_URL" < /bin/init.sql > /dev/null
-  mv /bin/init.sql /bin/init.sql.applied
+  /bin/resetdb -with-rand-data -admin-id=00000000-0000-0000-0000-000000000000 -db-url "$DB_URL" -skip-drop
+  /bin/goalert add-user --user-id=00000000-0000-0000-0000-000000000000 --user admin --pass admin123 --db-url "$DB_URL"
+  touch /init-data
 fi
 
 exec /bin/goalert --db-url "$DB_URL" --log-requests
