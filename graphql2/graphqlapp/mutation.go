4a1899d7f4a9c91257a57ba512b62f0f4f495599
Nathaniel Caza <mastercactapus@gmail.com>
2020-06-30 11:37:01 -0500

    move carrier info mut. to toolbox.go

--- graphql2/graphqlapp/mutation.go
+++ graphql2/graphqlapp/mutation.go
@@ -2,15 +2,12 @@ package graphqlapp
 
 import (
 	context "context"
-	"net/url"
 
 	"github.com/target/goalert/assignment"
 	"github.com/target/goalert/graphql2"
-	"github.com/target/goalert/notification/twilio"
 	"github.com/target/goalert/permission"
 	"github.com/target/goalert/user"
 	"github.com/target/goalert/validation"
-	"github.com/target/goalert/validation/validate"
 
 	"github.com/pkg/errors"
 )
@@ -31,38 +28,6 @@ func (a *Mutation) SetFavorite(ctx context.Context, input graphql2.SetFavoriteIn
 		return false, err
 	}
 	return true, nil
-}
-
-type safeErr struct{ error }
-
-func (safeErr) ClientError() bool { return true }
-
-func (a *Mutation) DebugSendSms(ctx context.Context, input graphql2.DebugSendSMSInput) (*graphql2.DebugSendSMSInfo, error) {
-	err := permission.LimitCheckAny(ctx, permission.Admin)
-	if err != nil {
-		return nil, err
-	}
-
-	err = validate.Many(
-		validate.Phone("To", input.To),
-		validate.Phone("From", input.From),
-		validate.Text("Body", input.Body, 1, 1000),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	msg, err := a.Twilio.SendSMS(ctx, input.To, input.Body, &twilio.SMSOptions{
-		FromNumber: input.From,
-	})
-	if err != nil {
-		return nil, safeErr{error: err}
-	}
-
-	return &graphql2.DebugSendSMSInfo{
-		ID:          msg.SID,
-		ProviderURL: "https://www.twilio.com/console/sms/logs/" + url.PathEscape(msg.SID),
-	}, nil
 }
 
 func (a *Mutation) TestContactMethod(ctx context.Context, id string) (bool, error) {
--- graphql2/graphqlapp/toolbox.go
+++ graphql2/graphqlapp/toolbox.go
@@ -6,14 +6,49 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	"github.com/target/goalert/config"
 	"github.com/target/goalert/graphql2"
+	"github.com/target/goalert/notification/twilio"
 	"github.com/target/goalert/permission"
+	"github.com/target/goalert/validation/validate"
 	"github.com/ttacon/libphonenumber"
 )
 
 const twLookupURL = "https://lookups.twilio.com/v1/PhoneNumbers/"
+
+type safeErr struct{ error }
+
+func (safeErr) ClientError() bool { return true }
+
+func (a *Mutation) DebugSendSms(ctx context.Context, input graphql2.DebugSendSMSInput) (*graphql2.DebugSendSMSInfo, error) {
+	err := permission.LimitCheckAny(ctx, permission.Admin)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validate.Many(
+		validate.Phone("To", input.To),
+		validate.Phone("From", input.From),
+		validate.Text("Body", input.Body, 1, 1000),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	msg, err := a.Twilio.SendSMS(ctx, input.To, input.Body, &twilio.SMSOptions{
+		FromNumber: input.From,
+	})
+	if err != nil {
+		return nil, safeErr{error: err}
+	}
+
+	return &graphql2.DebugSendSMSInfo{
+		ID:          msg.SID,
+		ProviderURL: "https://www.twilio.com/console/sms/logs/" + url.PathEscape(msg.SID),
+	}, nil
+}
 
 func (a *Mutation) DebugCarrierInfo(ctx context.Context, input graphql2.DebugCarrierInfoInput) (*graphql2.DebugCarrierInfo, error) {
 	// must be admin to fetch carrier info
