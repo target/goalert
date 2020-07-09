975c1c7a1d92b51af7187fa83d94c2a07c1f6e9f
Nathaniel Cook <NathanielJCook@outlook.com>
2020-07-09 10:27:14 -0500

    updated query

--- smoketest/graphqlnotifiedalerts_test.go
+++ smoketest/graphqlnotifiedalerts_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/target/goalert/smoketest/harness"
 )
 
@@ -12,35 +13,34 @@ func TestNotifiedAlerts(t *testing.T) {
 
 	sql := `
 	insert into users (id, name, email, role) 
-	values 
-		({{uuid "user"}}, 'bob', 'joe', 'user');
+	values ({{uuid "user"}}, 'bob', 'joe', 'user');
 
 	insert into user_contact_methods (id, user_id, name, type, value) 
-	values
-		({{uuid "cm1"}}, {{uuid "user"}}, 'personal', 'SMS', {{phone "1"}});
+	values ({{uuid "cm1"}}, {{uuid "user"}}, 'personal', 'SMS', {{phone "1"}});
 
 	insert into user_notification_rules (user_id, contact_method_id, delay_minutes) 
-	values
-		({{uuid "user"}}, {{uuid "cm1"}}, 0),
+	values ({{uuid "user"}}, {{uuid "cm1"}}, 0);
 
 	insert into escalation_policies (id, name) 
 	values
 		({{uuid "eid"}}, 'esc policy'),
-		({{uuid "eid2"}}, 'esc policy');
+		({{uuid "eid2"}}, 'esc policy 2');
 
 	insert into escalation_policy_steps (id, escalation_policy_id) 
-	values
-		({{uuid "esid"}}, {{uuid "eid"}});
+	values ({{uuid "esid"}}, {{uuid "eid"}});
 
 	insert into services (id, escalation_policy_id, name) 
 	values
 		({{uuid "sid"}}, {{uuid "eid"}}, 'service'),
-		({{uuid "sid2"}}, {{uuid "eid2"}}, 'service');
+		({{uuid "sid2"}}, {{uuid "eid2"}}, 'service 2');
 
-	insert into alerts (id, service_id, summary, status, created_at) 
+	insert into user_favorites (id, user_id, tgt_service_id)
+	values (9999, {{uuid "user"}}, {{uuid "sid2"}});
+
+	insert into alerts (id, service_id, summary, status, dedup_key)
 	values
-		(1, {{uuid "sid"}}, 'testing', 'unacknowledged', now()),
-		(2, {{uuid "sid2"}}, 'testing', 'unacknowledged', now() - '2 days'::interval);
+		(1, {{uuid "sid"}}, 'testing notified', 'triggered', 'dedupenotified'),
+		(2, {{uuid "sid2"}}, 'testing favorite', 'triggered', 'dedupefavorited');
 	`
 
 	h := harness.NewHarness(t, sql, "add-no-notification-alert-log")
@@ -65,32 +65,68 @@ func TestNotifiedAlerts(t *testing.T) {
 		}
 	}
 
-	var alerts1, alerts2 struct {
-		AlertConnection struct {
+	type Alerts struct {
+		Alerts struct {
 			Nodes []struct {
 				ID string
 			}
 		}
 	}
 
-	doQL(t, h, "", &alerts1)
+	var alerts1, alerts2 Alerts
 
 	// notes
-	// - "user" is assigned to "sid1"
-	// - "sid1" is not favorited by "user"
+	// - "user" is assigned to "sid"
+	// - "sid" is not favorited by "user"
 	// - it has 1 alert that "user" should be notified for
 
 	// - "sid2" created and has no assignments
-	// - TODO: it is favorited by "user"
+	// - it is favorited by "user"
 	// - it has 1 alert
 
 	// test:
 	// includeNotified: false
 	// favoritesOnly: true
 	// output: 1 alert (the favorited one)
+	doQL(t, h, `query {
+		alerts(input: {
+			first: 2
+			includeNotified: false
+			favoritesOnly: true
+		}) {
+			nodes {
+				id
+			}
+		}
+	}`, &alerts1)
+
+	var e [2]int
+	e[0] = 2
+
+	assert.Equal(t, e, alerts1.Alerts.Nodes)
 
 	// test:
 	// includeNotified: true
 	// favoritesOnly: true
 	// output: 2 alerts (1 from favorited, 1 from notified)
+	doQL(t, h, `query {
+		alerts(input: {
+			first: 2
+			includeNotified: true
+			favoritesOnly: true
+		}) {
+			nodes {
+				id
+			}
+		}
+	}`, &alerts2)
 }
+
+// `query {
+// 	users(first: 100) {
+// 		nodes {
+// 			id
+// 		}
+// 	}
+// }
+// `
