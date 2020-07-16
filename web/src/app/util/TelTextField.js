94a42757585ac73bc2a9342e031415962f81c1f6
KaihuaLin <Kaihua.Lin@target.com>
2020-07-16 09:24:45 -0500

    refactor useQuery in teltextfield and add debounce to the query

--- web/src/app/util/TelTextField.js
+++ web/src/app/util/TelTextField.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import { Check, Close } from '@material-ui/icons'
 import _ from 'lodash-es'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import { makeStyles } from '@material-ui/core'
+import { DEBOUNCE_DELAY } from '../config'
 
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
@@ -28,36 +29,42 @@ const useStyles = makeStyles({
 
 export default function TelTextField({ InputProps, ...props }) {
   const classes = useStyles()
-  const { value, error } = props
-  const phoneNumber = '+' + value
+  const [phoneNumber, setPhoneNumber] = useState(null)
+
+  // debounce to set the phone number
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setPhoneNumber(props.value)
+    }, DEBOUNCE_DELAY)
+    return () => clearTimeout(t)
+  }, [props.value])
 
   // check validation of the input phoneNumber through graphql
   const { data } = useQuery(isValidNumber, {
-    pollInterval: 1000,
-    variables: { number: phoneNumber },
+    pollInterval: 0,
+    variables: { number: '+' + phoneNumber },
+    fetchPolicy: 'cache-first',
+    skip: !phoneNumber,
   })
 
   // fetch validation
   const valid = _.get(data, 'phoneNumberInfo.valid', null)
 
+  let adorn
+  if (valid) {
+    adorn = <Check className={classes.valid} />
+  } else if (props.error) {
+    adorn = <Close className={classes.invalid} />
+  }
+
   // add live validation icon to the right of the textfield as an endAdornment
-  const iprops = Object.assign(
-    {
-      endAdornment: (
-        <InputAdornment position='end'>
-          {valid ? (
-            // change to makeStyle
-            <Check className={classes.valid} />
-          ) : error ? (
-            <Close className={classes.invalid} />
-          ) : (
-            <div />
-          )}
-        </InputAdornment>
-      ),
-    },
-    InputProps,
-  )
+  let iprops = InputProps
+  if (adorn) {
+    iprops = {
+      endAdornment: <InputAdornment position='end'>{adorn}</InputAdornment>,
+      ...InputProps,
+    }
+  }
 
   return (
     <div>
