c0ff4a13c430780526d9a3e448acd631840900c0
Nathaniel Cook <nathanieljcook@outlook.com>
2020-08-04 11:35:34 -0500

    return nil when no notices present

--- escalation/store.go
+++ escalation/store.go
@@ -1017,7 +1017,9 @@ func (db *DB) FindAllNotices(ctx context.Context, policyID string) ([]notice.Not
 		notices[0].Type = notice.Warning
 		notices[0].Message = "Not assigned to a service"
 		notices[0].Details = "To receive alerts for this configuration, assign this escalation policy to its relavent service."
+		return notices, nil
 	}
 
-	return notices, nil
+	// no results
+	return nil, nil
 }
