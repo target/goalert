7abf1cab86653d7aa48a407519edd9ad462e5542
Nathaniel Cook <nathanieljcook@outlook.com>
2020-08-06 16:45:08 -0500

    show first step

--- web/src/app/users/LinkToMobile.tsx
+++ web/src/app/users/LinkToMobile.tsx
@@ -1,26 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, ReactNode } from 'react'
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
+  DialogContentText,
   Grid,
-  Stepper,
-  Step,
+  Typography,
   makeStyles,
   isWidthDown,
 } from '@material-ui/core'
 import PhonelinkIcon from '@material-ui/icons/Phonelink'
 import QRCode from 'qrcode.react'
+import SwipeableViews from 'react-swipeable-views'
+import { virtualize, bindKeyboard } from 'react-swipeable-views-utils'
 import DialogTitleWrapper from '../dialogs/components/DialogTitleWrapper'
 import useWidth from '../util/useWidth'
 import { styles as globalStyles } from '../styles/materialStyles'
+
+interface SlideParams {
+  index: number
+  key: number
+}
+
+const VirtualizeAnimatedViews = bindKeyboard(virtualize(SwipeableViews))
 
 const useStyles = makeStyles((theme) => {
   const { cancelButton } = globalStyles(theme)
 
   return {
     cancelButton,
+    centerItemContent: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    manualCodeTypography: {
+      '&:hover': {
+        cursor: 'pointer',
+        textDecoration: 'underline',
+      },
+    },
   }
 })
 
@@ -29,6 +48,24 @@ export default function LinkToMobile(): JSX.Element {
   const width = useWidth()
   const fullscreen = isWidthDown('md', width)
   const [showDialog, setShowDialog] = useState(false)
+  const [index, setIndex] = useState(0)
+
+  // todo: add useEffects changing index as things are updated
+
+  function slideRenderer({ index, key }: SlideParams): ReactNode {
+    switch (index) {
+      case 0:
+        return <ClaimCodeDisplay key={key} />
+      case 1:
+        return <VerifyCodeField key={key} />
+      case 2:
+        return <Success key={key} />
+      case 3:
+        return <Retry key={key} />
+      default:
+        return null
+    }
+  }
 
   return (
     <React.Fragment>
@@ -41,12 +78,18 @@ export default function LinkToMobile(): JSX.Element {
         Link to Mobile
       </Button>
       <Dialog
-        open={showDialog}
+        open={true}
         fullScreen={fullscreen}
+        fullWidth={true}
+        maxWidth='xs'
         onClose={() => setShowDialog(false)}
       >
         <DialogTitleWrapper title='Link to Mobile' />
-        <DialogContent>Test</DialogContent>
+        <VirtualizeAnimatedViews
+          index={index}
+          onChangeIndex={(i: number) => setIndex(i)}
+          slideRenderer={slideRenderer}
+        />
         <DialogActions>
           <Button
             className={classes.cancelButton}
@@ -58,4 +101,45 @@ export default function LinkToMobile(): JSX.Element {
       </Dialog>
     </React.Fragment>
   )
+}
+
+function ClaimCodeDisplay() {
+  const classes = useStyles()
+
+  return (
+    <DialogContent>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <DialogContentText>
+            Scan this QR code from the GoAlert app on your mobile device to
+            authenticate.
+          </DialogContentText>
+        </Grid>
+        <Grid className={classes.centerItemContent} item xs={12}>
+          <QRCode value='test' />
+        </Grid>
+        <Grid className={classes.centerItemContent} item xs={12}>
+          <Typography
+            className={classes.manualCodeTypography}
+            variant='caption'
+            color='textSecondary'
+          >
+            Code not scanning? Click here to enter manually.
+          </Typography>
+        </Grid>
+      </Grid>
+    </DialogContent>
+  )
+}
+
+function VerifyCodeField() {
+  return null
+}
+
+function Success() {
+  return null
+}
+
+function Retry() {
+  return null
 }
