7bd6767925c895c9fca13f33ec9f6e7627ef72fe
Nathaniel Cook <nathanieljcook@outlook.com>
2020-08-10 12:18:04 -0500

    finish setting up notifed and favorited services
    Co-authored-by: jakegleason <jacob.gleason07@gmail.com>

--- smoketest/graphqlnotifiedalerts_test.go
+++ smoketest/graphqlnotifiedalerts_test.go
@@ -3,11 +3,21 @@ package smoketest
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/target/goalert/smoketest/harness"
 )
 
+/*
+ * This file tests that the alerts GraphQL query shows the proper amount
+ * of results when flipping between "includeNotified" and "favoritesOnly"
+ * query options.
+ *
+ * Service 1: Notified alert
+ * Service 2: Favorited alert
+ */
 func TestNotifiedAlerts(t *testing.T) {
 	t.Parallel()
 
@@ -15,36 +25,20 @@ func TestNotifiedAlerts(t *testing.T) {
 	insert into users (id, name, email, role) 
 	values ({{uuid "user"}}, 'bob', 'joe', 'user');
 
-	insert into user_contact_methods (id, user_id, name, type, value) 
-	values ({{uuid "cm1"}}, {{uuid "user"}}, 'personal', 'SMS', {{phone "1"}});
-
-	insert into user_notification_rules (user_id, contact_method_id, delay_minutes) 
-	values ({{uuid "user"}}, {{uuid "cm1"}}, 0);
-
 	insert into escalation_policies (id, name) 
-	values
-		({{uuid "eid"}}, 'esc policy'),
-		({{uuid "eid2"}}, 'esc policy 2');
-
-	insert into escalation_policy_steps (id, escalation_policy_id) 
-	values ({{uuid "esid"}}, {{uuid "eid"}});
- 
-	insert into escalation_policy_actions (id, escalation_policy_step_id, user_id)
-	values ({{uuid "epa"}}, {{uuid "esid"}}, {{uuid "user"}});
+	values ({{uuid "eid2"}}, 'esc policy 2');
 
 	insert into services (id, escalation_policy_id, name) 
-	values
-		({{uuid "sid"}}, {{uuid "eid"}}, 'service'),
-		({{uuid "sid2"}}, {{uuid "eid2"}}, 'service 2');
+	values ({{uuid "sid2"}}, {{uuid "eid2"}}, 'service 2');
 	`
 
-	h := harness.NewHarness(t, sql, "add-no-notification-alert-log")
+	h := harness.NewHarness(t, sql, "prometheus-alertmanager-integration")
 	defer h.Close()
 
 	doQL := func(t *testing.T, h *harness.Harness, query string, res interface{}) {
 		t.Helper()
-		// g := h.GraphQLQueryUserT(t, h.UUID("user"), query)
-		g := h.GraphQLQuery2(query)
+		g := h.GraphQLQueryUserT(t, h.UUID("user"), query)
+		// g := h.GraphQLQuery2(query)
 		for _, err := range g.Errors {
 			t.Error("GraphQL Error:", err.Message)
 		}
@@ -61,6 +55,107 @@ func TestNotifiedAlerts(t *testing.T) {
 		}
 	}
 
+	userID := h.UUID("user")
+	phone := h.Phone("1")
+
+	// create bob's contact method
+	var cm struct {
+		CreateUserContactMethod struct {
+			ID string
+		}
+	}
+
+	doQL(t, h, fmt.Sprintf(`
+		mutation {
+			createUserContactMethod(input:{
+				userID: "%s",
+				name: "default",
+				type: SMS,
+				value: "%s"
+			}) {
+				id
+			}
+		}
+	`, userID, phone), &cm)
+
+	// verify bob's contact method
+	doQL(t, h, fmt.Sprintf(`
+		mutation {
+			sendContactMethodVerification(input:{
+				contactMethodID: "%s"
+			})
+		}
+	`, cm.CreateUserContactMethod.ID), nil)
+	msg := h.Twilio(t).Device(phone).ExpectSMS("verification")
+	digits := func(r rune) rune {
+		if r >= '0' && r <= '9' {
+			return r
+		}
+		return -1
+	}
+	codeStr := strings.Map(digits, msg.Body())
+	code, _ := strconv.Atoi(codeStr)
+	doQL(t, h, fmt.Sprintf(`
+		mutation {
+			verifyContactMethod(input:{
+				contactMethodID:  "%s",
+				code: %d
+			})
+		}
+	`, cm.CreateUserContactMethod.ID, code), nil)
+
+	// create bob's notification rule
+	doQL(t, h, fmt.Sprintf(`
+		mutation {
+			createUserNotificationRule(input:{
+				userID: "%s",
+				contactMethodID: "%s",
+				delayMinutes: 0
+			}){
+				id
+			}
+		}
+
+	`, userID, cm.CreateUserContactMethod.ID), nil)
+
+	// create escalation policy with bob on a step
+	var esc struct{ CreateEscalationPolicy struct{ ID string } }
+	doQL(t, h, `
+		mutation {
+			createEscalationPolicy(input:{
+				repeat: 0,
+				name: "default"
+			}){id}
+		}
+	`, &esc)
+
+	var step struct {
+		CreateEscalationPolicyStep struct{ Step struct{ ID string } }
+	}
+	doQL(t, h, fmt.Sprintf(`
+		mutation {
+			createEscalationPolicyStep(input:{
+				delayMinutes: 60,
+				escalationPolicyID: "%s",
+				targets: [{id: "%s", type: user}]
+			}){
+				id
+			}
+		}
+	`, esc.CreateEscalationPolicy.ID, userID), &step)
+
+	// create service for notified alert
+	var svc struct{ CreateService struct{ ID string } }
+	doQL(t, h, fmt.Sprintf(`
+		mutation {
+			createService(input:{
+				name: "service 1",
+				escalationPolicyID: "%s"
+			}){id}
+		}
+	`, esc.CreateEscalationPolicy.ID), &svc)
+
+	// favorite the second service from initSQL
 	doQL(t, h, fmt.Sprintf(`
 		mutation{
 			setFavorite(input:{
@@ -79,6 +174,7 @@ func TestNotifiedAlerts(t *testing.T) {
 		}
 	}
 
+	// assert the second service was favorited
 	doQL(t, h, fmt.Sprintf(`
 		query {
 			service(id: "%s") { 
@@ -86,11 +182,11 @@ func TestNotifiedAlerts(t *testing.T) {
 			}	
 		}
 	`, h.UUID("sid2")), &s)
-
 	if s.Service.IsFavorite != true {
 		t.Fatalf("ERROR: ServiceID %s IsUserFavorite=%t; want true", h.UUID("sid2"), s.Service.IsFavorite)
 	}
 
+	// create alerts against both services (notifed version & favorited version)
 	h.CreateAlert(h.UUID("sid"), "alert1")
 	h.CreateAlert(h.UUID("sid2"), "alert2")
 
@@ -104,7 +200,7 @@ func TestNotifiedAlerts(t *testing.T) {
 
 	var alerts1, alerts2, alerts3 Alerts
 
-	// output: 1 alert (the favorited one)
+	// query for only favorites: 1 alert (the favorited one)
 	doQL(t, h, `query {
 		alerts(input: {
 			includeNotified: false
@@ -120,7 +216,7 @@ func TestNotifiedAlerts(t *testing.T) {
 		t.Errorf("got %d alerts; want 1", len(alerts1.Alerts.Nodes))
 	}
 
-	// output: 2 alerts
+	// query for favorites & notified: 2 alerts
 	doQL(t, h, `query {
 			alerts(input: {
 				includeNotified: true
@@ -132,8 +228,8 @@ func TestNotifiedAlerts(t *testing.T) {
 			}
 		}`, &alerts2)
 
-	if len(alerts2.Alerts.Nodes) != 1 {
-		t.Errorf("got %d alerts; want 1", len(alerts2.Alerts.Nodes))
+	if len(alerts2.Alerts.Nodes) != 2 {
+		t.Errorf("got %d alerts; want 2", len(alerts2.Alerts.Nodes))
 	}
 
 	// All Services test (favoritesOnly: false)
@@ -153,6 +249,6 @@ func TestNotifiedAlerts(t *testing.T) {
 		t.Errorf("got %d alerts; want 2", len(alerts3.Alerts.Nodes))
 	}
 
-	// Expect 1 SMS for the created alert
-	h.Twilio(t).Device(h.Phone("1")).ExpectSMS("alert1")
+	// Expect 1 SMS for the created alert against bob's CM
+	h.Twilio(t).Device(phone).ExpectSMS("alert1")
 }
