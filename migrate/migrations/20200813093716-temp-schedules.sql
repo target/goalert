6f2ba798f0cfa7bf87624194ec498788a4c23272
Nathaniel Caza <mastercactapus@gmail.com>
2020-08-19 14:15:42 -0500

    remove unused columns

--- migrate/migrations/20200813093716-temp-schedules.sql
+++ migrate/migrations/20200813093716-temp-schedules.sql
@@ -2,8 +2,6 @@
 
 CREATE TABLE schedule_data (
     schedule_id UUID REFERENCES schedules (id) PRIMARY KEY ON DELETE CASCADE,
-    last_updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    last_updated_by UUID REFERENCES users (id) ON DELETE SET NULL,
     last_cleanup_at TIMESTAMPTZ,
     data JSONB NOT NULL
 );
