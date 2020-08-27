7924e3eec31ba75f8c9691381a4e2670775cd5f5
Nathaniel Caza <mastercactapus@gmail.com>
2020-08-27 15:58:38 -0500

    sort schedule data for cleanup

--- engine/cleanupmanager/db.go
+++ engine/cleanupmanager/db.go
@@ -52,7 +52,13 @@ func NewDB(ctx context.Context, db *sql.DB) (*DB, error) {
 		cleanupAlerts:  p.P(`delete from alerts where id = any(select id from alerts where status = 'closed' AND created_at < (now() - $1::interval) order by id limit 100 for update skip locked)`),
 		cleanupAPIKeys: p.P(`update user_calendar_subscriptions set disabled = true where id = any(select id from user_calendar_subscriptions where greatest(last_access, last_update) < (now() - $1::interval) order by id limit 100 for update skip locked)`),
 
-		schedData:    p.P(`select schedule_id, data from schedule_data where data notnull and (last_cleanup_at isnull or last_cleanup_at < now() - '1 day'::interval) for update limit 100`),
+		schedData: p.P(`
+			select schedule_id, data from schedule_data
+			where data notnull and (last_cleanup_at isnull or last_cleanup_at <= now() - '1 day'::interval)
+			order by last_cleanup_at asc nulls first
+			for update
+			limit 100
+		`),
 		setSchedData: p.P(`update schedule_data set last_cleanup_at = now(), data = $2 where schedule_id = $1`),
 	}, p.Err
 }
