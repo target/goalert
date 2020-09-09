1fe12497079da8f5d7ca6ce7af71bd00a213320b
QuynhDo <quynh.do@target.com>
2020-09-09 14:13:29 -0400

    create error link to conflicting user

--- web/src/app/users/UserContactMethodCreateDialog.js
+++ web/src/app/users/UserContactMethodCreateDialog.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import p from 'prop-types'
 
 import gql from 'graphql-tag'
 import { fieldErrors, nonFieldErrors } from '../util/errutil'
+import Typography from '@material-ui/core/Typography'
 
 import FormDialog from '../dialogs/FormDialog'
 import UserContactMethodForm from './UserContactMethodForm'
 import { useMutation, useQuery } from '@apollo/react-hooks'
+
+import { AppLink } from '../util/AppLink'
+
+import { useURLParam, useResetURLParams } from '../actions'
 
 const createMutation = gql`
   mutation($input: CreateUserContactMethodInput!) {
@@ -20,6 +25,7 @@ const query = gql`
   query($input: UserSearchOptions) {
     users(input: $input) {
       nodes {
+        id
         name
       }
     }
@@ -34,12 +40,44 @@ export default function UserContactMethodCreateDialog(props) {
     value: '',
   })
 
-  const { data } = useQuery(query, {
+  const [submitted, setSubmitted] = useState(false)
+
+  const { data, loading: queryLoading } = useQuery(query, {
     variables: {
-      cmValue: cmValue.value,
-      cmType: cmValue.type,
+      input: {
+        cmValue: cmValue.value,
+        cmType: cmValue.type,
+      },
     },
   })
+
+  const [userConflict, setUserConflict] = useURLParam('userConflict', '')
+
+  const resetUserConflict = useResetURLParams('userConflict')
+
+  useEffect(() => {
+    if (data && !queryLoading) {
+      setUserConflict(
+        data.users?.nodes[0]?.id + ',' + data.users?.nodes[0]?.name,
+      )
+    }
+  }, [queryLoading])
+
+  const userConflictSplit = userConflict.split(',')
+  const existingCmName = userConflictSplit[1]
+  const existingCmId = userConflictSplit[0]
+  let errorLink = null
+
+  if (existingCmName && existingCmId && submitted) {
+    errorLink = (
+      <Typography>
+        <AppLink to={`/users/${existingCmId}`}>
+          Contact method already exists for that type and value:{' '}
+          {existingCmName}
+        </AppLink>
+      </Typography>
+    )
+  }
 
   const [createCM, createCMStatus] = useMutation(createMutation, {
     onCompleted: (result) => {
@@ -60,6 +98,8 @@ export default function UserContactMethodCreateDialog(props) {
   const fieldErrs = fieldErrors(error)
   const { title = 'Create New Contact Method', subtitle } = props
 
+  const errors = errorLink ? [errorLink] : nonFieldErrors(error)
+
   const form = (
     <UserContactMethodForm
       disabled={loading}
@@ -69,21 +109,26 @@ export default function UserContactMethodCreateDialog(props) {
       disclaimer={props.disclaimer}
     />
   )
+
   return (
     <FormDialog
       data-cy='create-form'
       title={title}
       subTitle={subtitle}
       loading={loading}
-      errors={nonFieldErrors(error)}
-      onClose={props.onClose}
+      errors={errors}
+      onClose={() => {
+        resetUserConflict()
+        props.onClose()
+      }}
       // wrapped to prevent event from passing into createCM
       onSubmit={() => {
-        if (data) {
-          console.log(data)
-        } else {
-          createCM()
+        // prevent submitting if a user conflict exists
+        if (existingCmName && existingCmId) {
+          setSubmitted(true)
+          return
         }
+        createCM()
       }}
       form={form}
     />
