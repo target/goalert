857cd5a56b0b3944337e662ae3e664f452d24679
Nathaniel Cook <nathanieljcook@outlook.com>
2020-09-16 13:14:21 -0700

    add subtitle to dialog

--- web/src/app/schedules/fixed-sched/ScheduleTimesStep.tsx
+++ web/src/app/schedules/fixed-sched/ScheduleTimesStep.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, DialogContentText } from '@material-ui/core'
+import { Grid, DialogContentText, Typography } from '@material-ui/core'
 import { FormField } from '../../forms'
 import { ISODateTimePicker } from '../../util/ISOPickers'
 
@@ -7,10 +7,15 @@ export default function ScheduleTimesStep() {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
+        <Typography variant='h6' component='h2'>
+          Determine start and end times.
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
         <DialogContentText>
           This will define a span of time with a fixed set of shifts. It ignores
           all rules, rotations, and overrides. From the specified start to end
-          times this on-call schedule will behave exactly as defined here.
+          times this on-call schedule will behave exactly as configured here.
         </DialogContentText>
       </Grid>
       <Grid item xs={6}>
