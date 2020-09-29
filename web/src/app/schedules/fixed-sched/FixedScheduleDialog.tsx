0d53d895d0c3b22fe95b2a873497c3f6dc6731f0
Nathaniel Caza <mastercactapus@gmail.com>
2020-09-29 15:40:55 -0500

    remove unused code

--- web/src/app/schedules/fixed-sched/FixedScheduleDialog.tsx
+++ web/src/app/schedules/fixed-sched/FixedScheduleDialog.tsx
@@ -9,7 +9,6 @@ import { FormContainer } from '../../forms'
 import { bindKeyboard, virtualize } from 'react-swipeable-views-utils'
 import SwipeableViews from 'react-swipeable-views'
 import AddShiftsStep from './AddShiftsStep'
-import ReviewStep from './ReviewStep'
 import ScheduleTimesStep from './ScheduleTimesStep'
 // allows changing the index programatically
 const VirtualizeAnimatedViews = bindKeyboard(virtualize(SwipeableViews))
@@ -33,23 +32,6 @@ export default function FixedScheduleDialog({
 }: FixedScheduleDialogProps) {
   const edit = Boolean(_value)
 
-  // MOCK DATA
-  // const mockStart = '2020-09-15T00:00:00.000Z'
-  // const mockEnd = '2020-09-30T00:00:00.000Z'
-  // const mockShift: Shift = {
-  //   end: '2020-09-24T21:02:00.000Z',
-  //   start: '2020-09-23T21:02:00.000Z',
-  //   userID: '307e25a3-2377-4b19-9fce-68c5569d2d12',
-  // }
-  // const mockShifts: Shift[] = _.fill(Array(1), mockShift)
-  // const [step, setStep] = useState(1) // edit starting on step 2
-  // const [value, setValue] = useState({
-  //   start: mockStart,
-  //   end: mockEnd,
-  //   shifts: mockShifts,
-  // })
-
-  // NOT MOCK DATA
   const [step, setStep] = useState(edit ? 1 : 0) // edit starting on step 2
   const [value, setValue] = useState({
     start: _value?.start ?? '',
@@ -108,16 +90,6 @@ export default function FixedScheduleDialog({
             scheduleID={scheduleID}
             start={value.start}
             end={value.end}
-          />
-        )
-      case 2:
-        if (step !== 2) return null
-        return (
-          <ReviewStep
-            key={key}
-            value={value}
-            scheduleID={scheduleID}
-            stepText={edit ? 'STEP 2 of 2' : 'STEP 3 of 3'}
           />
         )
       default:
