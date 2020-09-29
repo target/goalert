14ab562e83a555c706e188c86072a58abfa8e57e
Nathaniel Caza <mastercactapus@gmail.com>
2020-09-29 12:25:04 -0500

    add shift validation function

--- web/src/app/schedules/fixed-sched/sharedUtils.tsx
+++ web/src/app/schedules/fixed-sched/sharedUtils.tsx
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon'
+import { DateTime, Interval } from 'luxon'
 import React, { ReactNode } from 'react'
 
 export type Value = {
@@ -11,6 +11,27 @@ export type Shift = {
   start: string
   end: string
   userID: string
+}
+
+const parseInterval = (start: string, end: string) =>
+  Interval.fromDateTimes(DateTime.fromISO(start), DateTime.fromISO(end))
+
+export function validateShift(
+  schedStart: string,
+  schedEnd: string,
+  shift: Shift,
+) {
+  const schedSpan = parseInterval(schedStart, schedEnd)
+  const shiftSpan = parseInterval(shift.start, shift.end)
+
+  // these two just for completeness but should never happen
+  if (!shiftSpan.isValid) return new Error('invalid shift times')
+  if (!schedSpan.isValid) return new Error('invalid schedule times')
+
+  if (!schedSpan.engulfs(shiftSpan))
+    return new Error('shift extends beyond fixed schedule')
+
+  return null
 }
 
 // removes bottom margin from content text so form fields
