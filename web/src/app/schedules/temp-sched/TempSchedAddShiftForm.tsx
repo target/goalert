84c85a9f83a82feb089c5c4c11d8d01ac67bee35
dctalbot <dctalbot@umich.edu>
2020-10-15 16:08:31 -0500

    Revert "hint text as an achor"
    This reverts commit 84290e949a01cfe8b8ebe40b9f1b8fd4135326f6.

--- web/src/app/schedules/temp-sched/TempSchedAddShiftForm.tsx
+++ web/src/app/schedules/temp-sched/TempSchedAddShiftForm.tsx
@@ -1,13 +1,22 @@
-import { Grid, TextField } from '@material-ui/core'
+import { Grid, TextField, Typography, makeStyles } from '@material-ui/core'
 import { DateTime } from 'luxon'
 import React, { useState } from 'react'
 import { FormField } from '../../forms'
 import { UserSelect } from '../../selection'
-import { AppLink } from '../../util/AppLink'
 import { ISODateTimePicker } from '../../util/ISOPickers'
 import { Value } from './sharedUtils'
 
+const useStyles = makeStyles({
+  typography: {
+    '&:hover': {
+      cursor: 'pointer',
+      textDecoration: 'underline',
+    },
+  },
+})
+
 export default function TempSchedAddShiftForm(): JSX.Element {
+  const classes = useStyles()
   const [manualEntry, setManualEntry] = useState(false)
 
   return (
@@ -45,15 +54,14 @@ export default function TempSchedAddShiftForm(): JSX.Element {
             label='Shift End'
             name='end'
             hint={
-              <AppLink
-                to='#'
-                onClick={(e) => {
-                  e.preventDefault()
-                  setManualEntry(false)
-                }}
+              <Typography
+                className={classes.typography}
+                variant='caption'
+                color='textSecondary'
+                onClick={() => setManualEntry(false)}
               >
-                Configure as duration
-              </AppLink>
+                Configure as duration?
+              </Typography>
             }
           />
         ) : (
@@ -81,15 +89,14 @@ export default function TempSchedAddShiftForm(): JSX.Element {
             }}
             min={0.25}
             hint={
-              <AppLink
-                to='#'
-                onClick={(e) => {
-                  e.preventDefault()
-                  setManualEntry(true)
-                }}
+              <Typography
+                className={classes.typography}
+                variant='caption'
+                color='textSecondary'
+                onClick={() => setManualEntry(true)}
               >
-                Configure as date/time
-              </AppLink>
+                Configure as date/time?
+              </Typography>
             }
           />
         )}
