62de21cbea990fd9d4c80c43d926ac54c5efdf09
Nathaniel Caza <mastercactapus@gmail.com>
2020-10-27 14:14:13 -0500

    fix retry with fail-fast

--- web/src/cypress/support/fail-fast.ts
+++ web/src/cypress/support/fail-fast.ts
@@ -1,19 +1,15 @@
 // NOTE the runner method is not inherent to Cypress
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CY: any = Cypress
-// Fail-fast-all-files
-before(function () {
-  cy.getCookie('has-failed-test').then((cookie) => {
-    if (cookie && typeof cookie === 'object' && cookie.value === 'true') {
-      CY.runner.stop()
-    }
-  })
-})
 
 // Fail-fast-single-file
 afterEach(function () {
-  if (this.currentTest && this.currentTest.state === 'failed') {
-    cy.setCookie('has-failed-test', 'true')
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore _currentRetry is private but required to work properly
+  if (
+    this.currentTest?.state === 'failed' &&
+    this.currentTest._currentRetry === this.currentTest.retries()
+  ) {
     CY.runner.stop()
   }
 })
