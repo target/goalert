16476eff4e6404709ef0bb7f8786bea5ae455297
Nathaniel Cook <nathanieljcook@outlook.com>
2020-10-27 09:55:24 -0700

    set shift spans role to button

--- web/src/app/schedules/CalendarEventWrapper.js
+++ web/src/app/schedules/CalendarEventWrapper.js
@@ -232,6 +232,7 @@ export default class CalendarEventWrapper extends Component {
           },
           onFocus: this.handleFocusTooltip,
           onBlur: this.handleBlurTooltip,
+          role: 'button',
         })}
       </Tooltip>
     )
