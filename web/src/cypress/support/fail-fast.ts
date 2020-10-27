ab8e376c75f206c6edbf63abe6ac4c77b7ba1110
Nathaniel Caza <mastercactapus@gmail.com>
2020-10-27 16:22:34 -0500

    move comments (formatting)

--- web/src/cypress/support/fail-fast.ts
+++ web/src/cypress/support/fail-fast.ts
@@ -4,10 +4,10 @@ const CY: any = Cypress
 
 // Fail-fast-single-file
 afterEach(function () {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore _currentRetry is private but required to work properly
   if (
     this.currentTest?.state === 'failed' &&
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore _currentRetry is private but required to work properly
     this.currentTest._currentRetry === this.currentTest.retries()
   ) {
     CY.runner.stop()
