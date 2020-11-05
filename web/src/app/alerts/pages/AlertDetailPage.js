63215246d77b960e186182e9aab54c1b005b6e89
dctalbot <dctalbot@umich.edu>
2020-11-05 17:24:34 -0600

    remove polling config

--- web/src/app/alerts/pages/AlertDetailPage.js
+++ web/src/app/alerts/pages/AlertDetailPage.js
@@ -4,7 +4,6 @@ import Spinner from '../../loading/components/Spinner'
 import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo'
 import AlertDetails from '../components/AlertDetails'
-import { POLL_ERROR_INTERVAL, POLL_INTERVAL } from '../../config'
 
 const query = gql`
   query AlertDetailsPageQuery($id: Int!) {
@@ -40,21 +39,16 @@ const query = gql`
 `
 
 function AlertDetailPage(props) {
-  const { loading, error, data, startPolling } = useQuery(query, {
+  const { loading, error, data } = useQuery(query, {
     variables: { id: props.match.params.alertID },
-    pollInterval: POLL_INTERVAL,
   })
 
   if (!data && loading) return <Spinner />
 
-  if (error) {
-    startPolling(POLL_ERROR_INTERVAL)
-    return <GenericError error={error.message} />
-  }
+  if (error) return <GenericError error={error.message} />
 
   if (!data.alert) return <ObjectNotFound type='alert' />
 
-  startPolling(POLL_INTERVAL)
   return <AlertDetails data={data.alert} />
 }
 
