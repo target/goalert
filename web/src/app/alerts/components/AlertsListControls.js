2238bf0a25e8cf223a4e162b802c4ad175620fd3
dctalbot <dctalbot@umich.edu>
2020-11-05 14:57:01 -0600

    useURLParam

--- web/src/app/alerts/components/AlertsListControls.js
+++ web/src/app/alerts/components/AlertsListControls.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
-import { useDispatch, useSelector } from 'react-redux'
-import { setAlertsStatusFilter } from '../../actions'
-import { alertFilterSelector } from '../../selectors/url'
+import { useURLParam } from '../../actions'
 
 const tabs = ['active', 'unacknowledged', 'acknowledged', 'closed', 'all']
 
 function AlertsListControls() {
-  const dispatch = useDispatch()
-  const filter = useSelector(alertFilterSelector)
+  const [filter, setFilter] = useURLParam('filter', 'active')
 
   let currTab = tabs.indexOf(filter)
   if (currTab === -1) currTab = 0 // handle jargon input from url params
@@ -17,7 +14,7 @@ function AlertsListControls() {
   return (
     <Tabs
       value={currTab}
-      onChange={(e, idx) => dispatch(setAlertsStatusFilter(tabs[idx]))}
+      onChange={(e, idx) => setFilter(tabs[idx])}
       centered
       indicatorColor='primary'
       textColor='primary'
