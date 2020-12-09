e4d1c1a299e1281f7e06275980c5014f8bc554d0
Nathaniel Caza <mastercactapus@gmail.com>
2020-12-09 13:41:24 -0600

    cleanup orphaned alert logs

--- engine/cleanupmanager/db.go
+++ engine/cleanupmanager/db.go
@@ -17,6 +17,10 @@ type DB struct {
 	cleanupAPIKeys  *sql.Stmt
 	setTimeout      *sql.Stmt
 	cleanupSessions *sql.Stmt
+
+	cleanupAlertLogs *sql.Stmt
+
+	logIndex int
 }
 
 // Name returns the name of the module.
@@ -44,5 +48,21 @@ func NewDB(ctx context.Context, db *sql.DB) (*DB, error) {
 		cleanupAlerts:   p.P(`delete from alerts where id = any(select id from alerts where status = 'closed' AND created_at < (now() - $1::interval) order by id limit 100 for update skip locked)`),
 		cleanupAPIKeys:  p.P(`update user_calendar_subscriptions set disabled = true where id = any(select id from user_calendar_subscriptions where greatest(last_access, last_update) < (now() - $1::interval) order by id limit 100 for update skip locked)`),
 		cleanupSessions: p.P(`DELETE FROM auth_user_sessions WHERE id = any(select id from auth_user_sessions where last_access_at < (now() - '30 days'::interval) LIMIT 100 for update skip locked)`),
+
+		cleanupAlertLogs: p.P(`
+			with
+				scope as (select id from alert_logs where id > $1 order by id limit 1000),
+				id_range as (select min(id), max(id) from scope),
+				_delete as (
+					delete from alert_logs where id = any(
+						select id from alert_logs
+						where
+							id between (select min from id_range) and (select max from id_range) and
+							not exists (select 1 from alerts where alert_id = id)
+						for update skip locked
+					)
+				)
+			select id from scope offset 999
+		`),
 	}, p.Err
 }
