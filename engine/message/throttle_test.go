ac90bd16483a58eb1b237838d44e94ede99371a4
KatieMSB <katriannasb@gmail.com>
2020-12-23 09:57:27 -0600

    removing extra test

--- engine/message/throttle_test.go
+++ engine/message/throttle_test.go
@@ -1,87 +1,27 @@
-package message
+package message_test
 
 import (
 	"github.com/stretchr/testify/require"
+	"github.com/target/goalert/engine/message"
 	"github.com/target/goalert/notification"
 	"testing"
 	"time"
 )
 
-func TestThrottle_Record(t *testing.T) {
-	n := time.Now()
-	trTime := time.Minute
-
-	var cfg = ThrottleConfig{
-		notification.DestTypeSMS: {
-			{Count: 1, Per: trTime},
-		},
-	}
-
-	throttle := NewThrottle(cfg, n, false)
-
-	firstMsg := Message{
-		ID:        "0",
-		Type:      TypeAlertNotification,
-		UserID:    "User A",
-		ServiceID: "Service A",
-		Dest:      notification.Dest{Type: notification.DestTypeSMS, ID: "SMS A"},
-		SentAt:    n,
-	}
-
-	throttle.Record(firstMsg)
-	require.NotNil(t, throttle)
-	require.Equal(t, throttle, &Throttle{
-		cfg:      cfg,
-		ignoreID: false,
-		now:      n,
-		count: map[ThrottleItem]int{
-			{Dest: firstMsg.Dest, BucketDur: trTime}: 1,
-		},
-		cooldown: map[notification.Dest]bool{
-			firstMsg.Dest: true,
-		},
-	})
-
-	secondMsg := Message{
-		ID:        "1",
-		Type:      TypeAlertNotification,
-		UserID:    "User B",
-		ServiceID: "Service B",
-		Dest:      notification.Dest{Type: notification.DestTypeSMS, ID: "SMS B"},
-		SentAt:    n.Add(15 * time.Minute),
-	}
-
-	throttle.Record(secondMsg)
-	require.NotNil(t, throttle)
-	require.Equal(t, throttle, &Throttle{
-		cfg:      cfg,
-		ignoreID: false,
-		now:      n,
-		count: map[ThrottleItem]int{
-			{Dest: firstMsg.Dest, BucketDur: trTime}:  1,
-			{Dest: secondMsg.Dest, BucketDur: trTime}: 1,
-		},
-		cooldown: map[notification.Dest]bool{
-			firstMsg.Dest:  true,
-			secondMsg.Dest: true,
-		},
-	})
-}
-
 func TestThrottle_InCooldown(t *testing.T) {
 	n := time.Now()
 
-	var cfg = ThrottleConfig{
+	var cfg = message.ThrottleConfig{
 		notification.DestTypeSMS: {
 			{Count: 1, Per: time.Minute},
 		},
 	}
 
-	throttle := NewThrottle(cfg, n, false)
+	throttle := message.NewThrottle(cfg, n, false)
 
-	msg := Message{
+	msg := message.Message{
 		ID:        "0",
-		Type:      TypeAlertNotification,
+		Type:      message.TypeAlertNotification,
 		UserID:    "User A",
 		ServiceID: "Service A",
 		Dest:      notification.Dest{Type: notification.DestTypeSMS, ID: "SMS A"},
@@ -94,5 +34,5 @@ func TestThrottle_InCooldown(t *testing.T) {
 	throttle.Record(msg)
 
 	pending = throttle.InCooldown(msg)
-	require.Equal(t, pending, true)
+	require.Equal(t, true, pending)
 }
