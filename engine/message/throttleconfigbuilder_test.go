a6297b9f4b92b004fe49758c29b1c57ec198c5ad
Nathaniel Caza <mastercactapus@gmail.com>
2021-01-06 09:17:13 -0600

    add config builder test

---
+++ engine/message/throttleconfigbuilder_test.go
@@ -0,0 +1,77 @@
+package message_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/target/goalert/engine/message"
+	"github.com/target/goalert/notification"
+)
+
+func TestThrottleConfigBuilder(t *testing.T) {
+	var b message.ThrottleConfigBuilder
+
+	b.AddRules([]message.ThrottleRule{{Count: 1, Per: 2 * time.Minute}})
+
+	b.WithDestTypes(notification.DestTypeSMS).AddRules([]message.ThrottleRule{{Count: 2, Per: 3 * time.Minute}})
+
+	b.WithMsgTypes(notification.MessageTypeAlert).AddRules([]message.ThrottleRule{{Count: 3, Per: 5 * time.Minute}})
+
+	b.WithDestTypes(notification.DestTypeVoice).WithMsgTypes(notification.MessageTypeTest).AddRules([]message.ThrottleRule{{Count: 5, Per: 7 * time.Minute}})
+
+	cfg := b.Config()
+
+	assert.Equal(t, 7*time.Minute, cfg.MaxDuration())
+
+	check := func(dest notification.DestType, msg notification.MessageType, expRules []message.ThrottleRule) {
+		t.Helper()
+		assert.EqualValues(t, expRules, cfg.Rules(message.Message{Type: msg, Dest: notification.Dest{Type: dest}}))
+	}
+
+	check(
+		notification.DestTypeUnknown,
+		notification.MessageTypeUnknown,
+		[]message.ThrottleRule{
+			{Count: 1, Per: 2 * time.Minute},
+		},
+	)
+
+	check(
+		notification.DestTypeSMS,
+		notification.MessageTypeUnknown,
+		[]message.ThrottleRule{
+			{Count: 1, Per: 2 * time.Minute},
+			{Count: 2, Per: 3 * time.Minute},
+		},
+	)
+
+	check(
+		notification.DestTypeVoice,
+		notification.MessageTypeAlert,
+		[]message.ThrottleRule{
+			{Count: 1, Per: 2 * time.Minute},
+			{Count: 3, Per: 5 * time.Minute},
+		},
+	)
+
+	check(
+		notification.DestTypeVoice,
+		notification.MessageTypeTest,
+		[]message.ThrottleRule{
+			{Count: 1, Per: 2 * time.Minute},
+			{Count: 5, Per: 7 * time.Minute},
+		},
+	)
+
+	check(
+		notification.DestTypeSMS,
+		notification.MessageTypeAlert,
+		[]message.ThrottleRule{
+			{Count: 1, Per: 2 * time.Minute},
+			{Count: 2, Per: 3 * time.Minute},
+			{Count: 3, Per: 5 * time.Minute},
+		},
+	)
+
+}
