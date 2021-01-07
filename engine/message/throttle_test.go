ad3a12976938c2170c1dc768a6c6dc0f8ee18050
Nathaniel Caza <mastercactapus@gmail.com>
2021-01-07 10:34:56 -0600

    add next-message tests for throttle

--- engine/message/throttle_test.go
+++ engine/message/throttle_test.go
@@ -4,32 +4,58 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"github.com/target/goalert/engine/message"
-	"github.com/target/goalert/notification"
 )
 
 func TestThrottle(t *testing.T) {
 	n := time.Now()
-
-	cfg := message.ThrottleRules{{Count: 1, Per: time.Minute}}
-
-	throttle := message.NewThrottle(cfg, n, false)
-
-	msg := message.Message{
-		ID:        "0",
-		Type:      notification.MessageTypeAlert,
-		UserID:    "User A",
-		ServiceID: "Service A",
-		Dest:      notification.Dest{Type: notification.DestTypeSMS, ID: "SMS A"},
-		SentAt:    n,
+	check := func(desc string, cfg message.ThrottleConfig, times []time.Time, nextMessage time.Time) {
+		t.Helper()
+		t.Run(desc, func(t *testing.T) {
+			t.Helper()
+			i := 0
+			cur := n
+			for {
+				th := message.NewThrottle(cfg, cur, true)
+				for _, ts := range times {
+					th.Record(message.Message{SentAt: ts})
+				}
+				if !th.InCooldown(message.Message{}) {
+					break
+				}
+				i++
+				if i > 100 {
+					t.Fatal("never found next message after 100 minutes")
+				}
+				cur = cur.Add(time.Minute)
+			}
+			assert.Equal(t, nextMessage.String(), cur.String())
+		})
 	}
 
-	pending := throttle.InCooldown(msg)
-	require.Equal(t, pending, false)
+	check("simple",
+		message.ThrottleRules{{Count: 1, Per: time.Minute}},
+		[]time.Time{n},
+		n.Add(time.Minute),
+	)
 
-	throttle.Record(msg)
+	check("burst",
+		message.ThrottleRules{{Count: 1, Per: time.Minute}, {Count: 3, Per: 15 * time.Minute}},
+		[]time.Time{n.Add(-10 * time.Minute), n.Add(-12 * time.Minute), n},
+		n.Add(3*time.Minute),
+	)
 
-	pending = throttle.InCooldown(msg)
-	require.Equal(t, true, pending)
+	check("smooth_rate",
+		message.ThrottleRules{{Count: 3, Per: 15 * time.Minute, Smooth: true}},
+		[]time.Time{n},
+		n.Add(5*time.Minute),
+	)
+
+	check("smooth_burst",
+		message.ThrottleRules{{Count: 3, Per: 10 * time.Minute}, {Count: 5, Per: 30 * time.Minute, Smooth: true}},
+		[]time.Time{n, n, n},
+		n.Add(10*time.Minute),
+	)
+
 }
