6aea5f33fa24c554ef110a1aa3c47ca344f6679f
Nathaniel Caza <mastercactapus@gmail.com>
2021-01-11 14:36:01 -0600

    test rate times

--- engine/message/throttle_test.go
+++ engine/message/throttle_test.go
@@ -10,27 +10,33 @@ import (
 
 func TestThrottle(t *testing.T) {
 	n := time.Now()
-	check := func(desc string, cfg message.ThrottleConfig, times []time.Time, nextMessage time.Time) {
+	check := func(desc string, cfg message.ThrottleConfig, times []time.Time, nextMessages ...time.Time) {
 		t.Helper()
 		t.Run(desc, func(t *testing.T) {
+			sentTimes := make([]time.Time, len(times))
+			copy(sentTimes, times)
 			t.Helper()
 			i := 0
 			cur := n
-			for {
-				th := message.NewThrottle(cfg, cur, true)
-				for _, ts := range times {
-					th.Record(message.Message{SentAt: ts})
+			for _, nextMessage := range nextMessages {
+				for {
+					th := message.NewThrottle(cfg, cur, true)
+					for _, ts := range sentTimes {
+						th.Record(message.Message{SentAt: ts})
+					}
+					if !th.InCooldown(message.Message{}) {
+						break
+					}
+					i++
+					if i > 100 {
+						t.Fatal("never found next message after 100 minutes")
+					}
+					cur = cur.Add(time.Minute)
 				}
-				if !th.InCooldown(message.Message{}) {
-					break
-				}
-				i++
-				if i > 100 {
-					t.Fatal("never found next message after 100 minutes")
-				}
-				cur = cur.Add(time.Minute)
+
+				assert.Equal(t, nextMessage.String(), cur.String())
+				sentTimes = append(sentTimes, nextMessage)
 			}
-			assert.Equal(t, nextMessage.String(), cur.String())
 		})
 	}
 
@@ -38,6 +44,7 @@ func TestThrottle(t *testing.T) {
 		message.ThrottleRules{{Count: 1, Per: time.Minute}},
 		[]time.Time{n},
 		n.Add(time.Minute),
+		n.Add(2*time.Minute),
 	)
 
 	check("burst",
@@ -56,12 +63,14 @@ func TestThrottle(t *testing.T) {
 		message.ThrottleRules{{Count: 3, Per: 15 * time.Minute, Smooth: true}},
 		[]time.Time{n},
 		n.Add(5*time.Minute),
+		n.Add(10*time.Minute),
 	)
 
 	check("smooth_burst",
-		message.ThrottleRules{{Count: 3, Per: 10 * time.Minute}, {Count: 5, Per: 30 * time.Minute, Smooth: true}},
+		message.ThrottleRules{{Count: 3, Per: 15 * time.Minute}, {Count: 18, Per: 60 * time.Minute, Smooth: true}},
 		[]time.Time{n, n, n},
-		n.Add(10*time.Minute),
+		n.Add(15*time.Minute),
+		n.Add(18*time.Minute),
 	)
 
 }
