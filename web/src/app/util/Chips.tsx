cf8b82d4210e8ad3230b54bc0379e6ee21e5a7d3
dctalbot <david.talbot@target.com>
2021-02-01 11:21:06 -0600

    use label instead of custom name prop

--- web/src/app/util/Chips.tsx
+++ web/src/app/util/Chips.tsx
@@ -4,7 +4,6 @@ import { push } from 'connected-react-router'
 import { useDispatch } from 'react-redux'
 import { useQuery, gql } from '@apollo/client'
 import {
-  Layers as PolicyIcon,
   RotateRight as RotationIcon,
   Today as ScheduleIcon,
 } from '@material-ui/icons'
@@ -22,26 +21,23 @@ const serviceQuery = gql`
   }
 `
 
-interface ServiceChipProps extends ChipProps {
-  id: string
-  name?: string
-}
+type WithID<T> = { id: string } & T
 
-export function ServiceChip(props: ServiceChipProps): JSX.Element {
-  const { id, name, onClick, ...rest } = props
+export function ServiceChip(props: WithID<ChipProps>): JSX.Element {
+  const { id, label, onClick, ...rest } = props
   const dispatch = useDispatch()
 
   const { data, loading, error } = useQuery(serviceQuery, {
     variables: {
       id,
     },
-    skip: Boolean(name),
+    skip: Boolean(label),
     fetchPolicy: 'cache-first',
     pollInterval: 0,
   })
 
-  const getLabel = (): string => {
-    if (name) return name
+  const getLabel = (): typeof label => {
+    if (label) return label
     if (!data && loading) return 'Loading...'
     if (error) return `Error: ${error.message}`
     return data.service.name
@@ -58,13 +54,8 @@ export function ServiceChip(props: ServiceChipProps): JSX.Element {
   )
 }
 
-interface UserChipProps extends ChipProps {
-  id: string
-  name: string
-}
-
-export function UserChip(props: UserChipProps): JSX.Element {
-  const { id, name, onClick, ...rest } = props
+export function UserChip(props: WithID<ChipProps>): JSX.Element {
+  const { id, onClick, ...rest } = props
   const dispatch = useDispatch()
 
   return (
@@ -72,19 +63,13 @@ export function UserChip(props: UserChipProps): JSX.Element {
       data-cy='user-chip'
       avatar={<UserAvatar userID={id} />}
       onClick={onClick || (() => dispatch(push(`/users/${id}`)))}
-      label={name}
       {...rest}
     />
   )
 }
 
-interface RotationChipProps extends ChipProps {
-  id: string
-  name: string
-}
-
-export function RotationChip(props: RotationChipProps): JSX.Element {
-  const { id, name, onClick, ...rest } = props
+export function RotationChip(props: WithID<ChipProps>): JSX.Element {
+  const { id, onClick, ...rest } = props
   const dispatch = useDispatch()
 
   return (
@@ -96,61 +81,13 @@ export function RotationChip(props: RotationChipProps): JSX.Element {
         </Avatar>
       }
       onClick={onClick || (() => dispatch(push(`/rotations/${id}`)))}
-      label={name}
       {...rest}
     />
   )
 }
 
-function formatPolicyName(name: string, stepNum: number): JSX.Element {
-  if (stepNum !== null) {
-    return (
-      <div>
-        <strong> Step {stepNum + 1}:</strong> {name}
-      </div>
-    )
-  }
-
-  const parts = name.split(' - ')
-  return (
-    <div>
-      <strong> Step {parseInt(parts[0]) + 1}:</strong> {parts[1]}
-    </div>
-  )
-}
-
-interface PolicyChipProps extends ChipProps {
-  id: string
-  name: string
-  stepNum: number
-}
-
-export function PolicyChip(props: PolicyChipProps): JSX.Element {
-  const { id, name, stepNum, onClick, ...rest } = props
-  const dispatch = useDispatch()
-
-  return (
-    <Chip
-      data-cy='ep-chip'
-      avatar={
-        <Avatar>
-          <PolicyIcon />
-        </Avatar>
-      }
-      onClick={onClick || (() => dispatch(push(`/escalation-policies/${id}`)))}
-      label={formatPolicyName(name, stepNum)}
-      {...rest}
-    />
-  )
-}
-
-interface ScheduleChipProps extends ChipProps {
-  id: string
-  name: string
-}
-
-export function ScheduleChip(props: ScheduleChipProps): JSX.Element {
-  const { id, name, onClick, ...rest } = props
+export function ScheduleChip(props: WithID<ChipProps>): JSX.Element {
+  const { id, onClick, ...rest } = props
   const dispatch = useDispatch()
 
   return (
@@ -162,19 +99,12 @@ export function ScheduleChip(props: ScheduleChipProps): JSX.Element {
         </Avatar>
       }
       onClick={onClick || (() => dispatch(push(`/schedules/${id}`)))}
-      label={name}
       {...rest}
     />
   )
 }
 
-interface SlackChipProps extends ChipProps {
-  name: string
-}
-
-export function SlackChip(props: SlackChipProps): JSX.Element {
-  const { name, ...rest } = props
-
+export function SlackChip(props: ChipProps): JSX.Element {
   return (
     <Chip
       data-cy='slack-chip'
@@ -183,8 +113,7 @@ export function SlackChip(props: SlackChipProps): JSX.Element {
           <SlackBW />
         </Avatar>
       }
-      label={name}
-      {...rest}
+      {...props}
     />
   )
 }
