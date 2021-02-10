e40ad0693d281fae1b017cdc06b4eaad3229c230
dctalbot <david.talbot@target.com>
2021-02-10 17:06:41 -0600

    use publicURL, fallback to origin + prefix

--- web/src/app/documentation/components/IntegrationKeyAPI.js
+++ web/src/app/documentation/components/IntegrationKeyAPI.js
@@ -4,11 +4,15 @@ import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import markdownText from '../IntegrationKeys.md'
 import Markdown from '../../util/Markdown'
+import { useConfigValue } from '../../util/RequireConfig'
+import { pathPrefix } from '../../env'
 
 export default function IntegrationKeyAPI() {
+  const [publicURL] = useConfigValue('General.PublicURL')
+
   const finalText = markdownText.replaceAll(
     'https://<example.goalert.me>',
-    window.location.origin,
+    publicURL || `${window.location.origin}${pathPrefix}`,
   )
 
   return (
