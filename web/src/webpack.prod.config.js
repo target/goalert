ad2121aae4119ca7a623dc8b7de57b4228a9f672
dctalbot <david.talbot@target.com>
2021-03-02 14:25:56 -0600

    revert postcss refactor

--- web/src/webpack.prod.config.js
+++ web/src/webpack.prod.config.js
@@ -34,6 +34,7 @@ module.exports = (env) => ({
         use: [
           'style-loader',
           { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
         ],
       },
       {
