1bee497bd939f57f182b40bf488a0ae021347fee
Nathaniel Cook <nathanieljcook@outlook.com>
2021-03-08 14:48:56 -0800

    get oauth 2.0 response from slack

--- notification/slack/auth.go
+++ notification/slack/auth.go
@@ -3,6 +3,9 @@ package slack
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/slack-go/slack"
+	"github.com/target/goalert/config"
 )
 
 // 4. given a "code" field that expires after 10m
@@ -17,7 +20,17 @@ import (
 // - provide a user_scope parameter with requested user scopes instead of, or in addition to, the scope parameter
 
 func (h *Handler) ServeUserAuthCallback(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	cfg := config.FromContext(ctx)
 	code := req.FormValue("code")
-	fmt.Println("req: ", req)
-	fmt.Println("code: ", code)
+	uri := cfg.General.PublicURL + "/api/v2/slack/auth"
+	resp, err := slack.GetOAuthV2ResponseContext(ctx, http.DefaultClient, cfg.Slack.ClientID, cfg.Slack.ClientSecret, code, uri)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// todo: store in slack_users
+	fmt.Println("Team ID: ", resp.Team.ID)
+	fmt.Println("User ID: ", resp.AuthedUser.ID)
+	fmt.Println("Access token: ", resp.AuthedUser.AccessToken)
 }
