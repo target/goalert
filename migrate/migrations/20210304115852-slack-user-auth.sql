df2e2c700f4472c86c182d6b3ccca082903235fc
Nathaniel Cook <nathanieljcook@outlook.com>
2021-03-08 14:48:31 -0800

    add team_id column per slack docs

--- migrate/migrations/20210304115852-slack-user-auth.sql
+++ migrate/migrations/20210304115852-slack-user-auth.sql
@@ -3,9 +3,11 @@
 ALTER TABLE user_slack_data RENAME TO slack_users;
 ALTER TABLE slack_users RENAME COLUMN id TO user_id;
 ALTER TABLE slack_users ADD COLUMN slack_id TEXT;
+ALTER TABLE slack_users ADD COLUMN team_id TEXT;
 
 -- +migrate Down
 
+ALTER TABLE slack_users DROP COLUMN team_id;
 ALTER TABLE slack_users DROP COLUMN slack_id;
 ALTER TABLE slack_users RENAME COLUMN user_id TO id;
 ALTER TABLE slack_users RENAME TO user_slack_data;
