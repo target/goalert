d93dae65688e74420e5c2422d66597f9113f0e3a
Nathaniel Cook <nathanieljcook@outlook.com>
2021-03-17 09:14:29 -0700

    update formatting of migration

--- migrate/migrations/20210304115852-slack-user-auth.sql
+++ migrate/migrations/20210304115852-slack-user-auth.sql
@@ -1,5 +1,4 @@
 -- +migrate Up
-
 ALTER TABLE user_slack_data RENAME COLUMN id TO user_id;
 ALTER TABLE user_slack_data ADD COLUMN slack_id TEXT NOT NULL;
 ALTER TABLE user_slack_data ADD COLUMN team_id TEXT NOT NULL;
@@ -14,32 +13,20 @@ CREATE TABLE notification_channel_last_alert_log
     PRIMARY KEY (notification_channel_id, alert_id)
 );
 
-ALTER TABLE alert_logs
-    DROP CONSTRAINT alert_logs_one_subject;
-​
 CREATE TRIGGER trg_insert_alert_logs_notification_channel_last_alert
-AFTER
-INSERT
-ON
-alert_logs
-FOR
-EACH
-ROW
-WHEN
-(NEW.event = 'notification_sent' AND NEW.sub_type = 'channel')
-EXECUTE PROCEDURE fn_insert_notification_channel_last_alert_log
-();
+AFTER INSERT ON alert_logs
+FOR EACH ROW WHEN
+    (NEW.event = 'notification_sent' AND NEW.sub_type = 'channel')
+EXECUTE PROCEDURE fn_insert_notification_channel_last_alert_log();
 
 -- +migrate StatementBegin
 CREATE FUNCTION fn_insert_notification_channel_last_alert_log() RETURNS trigger AS $$
 BEGIN
-    ​
     INSERT INTO notification_channel_last_alert_log
         (notification_channel_id, alert_id, log_id, next_log_id)
     VALUES
         (NEW.sub_channel_id, NEW.alert_id, NEW.id, NEW.id)
     ON CONFLICT DO NOTHING;
-​
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -48,24 +35,17 @@ $$ LANGUAGE plpgsql;
 -- +migrate StatementBegin
 CREATE FUNCTION fn_update_notification_channel_last_alert_log() RETURNS trigger AS $$
 BEGIN
-    ​
     UPDATE notification_channel_last_alert_log last
-    SET next_log_id
-    = NEW.id
+    SET next_log_id = NEW.id
     WHERE
         last.alert_id = NEW.alert_id AND
         NEW.id > last.next_log_id;
-​
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 -- +migrate StatementEnd
 
 -- +migrate Down
-
-ALTER TABLE alert_logs
-    ADD CONSTRAINT alert_logs_one_subject CHECK (NOT (sub_channel_id IS NOT NULL AND sub_integration_key_id IS NOT NULL AND sub_hb_monitor_id IS NOT NULL));
-​
 DROP TRIGGER trg_insert_alert_logs_notification_channel_last_alert
 ON alert_logs;
 
