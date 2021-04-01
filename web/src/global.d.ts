1eeee9f3207856efcfe554e49e96aa5d2e0c2f6b
dctalbot <david.talbot@target.com>
2021-04-01 07:58:30 -0500

    document ElementType

--- web/src/global.d.ts
+++ web/src/global.d.ts
@@ -9,6 +9,9 @@ declare namespace NodeJS {
   declare module '*.md'
 }
 
+// ElementType yields a union of values present in a const array
+// e.g. const xyz = [1, "2", 3.5] as const
+// type XYZ = ElementType<typeof counts> === "2" | 1 | 3.5
 export type ElementType<
   T extends ReadonlyArray<unknown>
 > = T extends ReadonlyArray<infer ElementType> ? ElementType : never
