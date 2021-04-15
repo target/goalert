f2ae22d38e2cfddc30a9885e0ce67de8e16bf1e0
Nathaniel Cook <nathanieljcook@outlook.com>
2021-04-15 13:18:02 -0700

    remove redundant validation on add shift step

--- web/src/app/schedules/temp-sched/TempSchedAddShiftsStep.tsx
+++ web/src/app/schedules/temp-sched/TempSchedAddShiftsStep.tsx
@@ -148,12 +148,7 @@ export default function TempSchedAddShiftsStep({
       add('end', 'must be after shift start time')
       add('start', 'must be before shift end time')
     }
-    if (isISOBefore(shift.start, start)) {
-      add('start', 'must not be before temporary schedule start time')
-    }
-    if (isISOAfter(shift.end, end)) {
-      add('end', 'must not extend beyond temporary schedule end time')
-    }
+
     return result
   }
 
