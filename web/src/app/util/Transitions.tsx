851d2c62ef77974944059f9aacaa65f9db762844
dctalbot <david.talbot@target.com>
2021-04-21 14:07:40 -0500

    rename transitions

--- web/src/app/util/Transitions.tsx
+++ web/src/app/util/Transitions.tsx
@@ -2,29 +2,20 @@ import React, { ReactElement } from 'react'
 import Fade from '@material-ui/core/Fade'
 import Slide from '@material-ui/core/Slide'
 
-export const DefaultTransition = React.forwardRef(
+export const FadeTransition = React.forwardRef(
   ({ children, ...props }, ref) => (
     <Fade {...props} ref={ref}>
       {children as ReactElement}
     </Fade>
   ),
 )
-DefaultTransition.displayName = 'DefaultTransition'
+FadeTransition.displayName = 'FadeTransition'
 
-export const FullscreenTransition = React.forwardRef(
+export const SlideTransition = React.forwardRef(
   ({ children, ...props }, ref) => (
     <Slide {...props} direction='left' ref={ref}>
       {children as ReactElement}
     </Slide>
   ),
 )
-FullscreenTransition.displayName = 'FullscreenTransition'
-
-export const FullscreenExpansion = React.forwardRef(
-  ({ children, ...props }, ref) => (
-    <Slide {...props} direction='right' ref={ref}>
-      {children as ReactElement}
-    </Slide>
-  ),
-)
-FullscreenExpansion.displayName = 'FullscreenExpansion'
+SlideTransition.displayName = 'SlideTransition'
