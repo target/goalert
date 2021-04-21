8ce9916f6875a5974066dbe31da68163da9d24ca
Nathaniel Caza <mastercactapus@gmail.com>
2021-04-21 12:05:54 -0500

    scroll new shifts into view

--- web/src/app/lists/FlatList.tsx
+++ web/src/app/lists/FlatList.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import List, { ListProps } from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
@@ -78,7 +78,7 @@ export interface FlatListItem {
   secondaryAction?: JSX.Element | null
   url?: string
   id?: string
-  render?: (item: FlatListItem) => JSX.Element
+  scrollIntoView?: boolean
 }
 
 export type FlatListListItem = FlatListSub | FlatListItem | FlatListNotice
@@ -109,6 +109,21 @@ const severityMap: { [K in NoticeType]: Color } = {
   WARNING: 'warning',
   ERROR: 'error',
   OK: 'success',
+}
+
+interface ScrollIntoViewListItemProps extends ListItemProps {
+  scrollIntoView?: boolean
+}
+
+function ScrollIntoViewListItem(props: ScrollIntoViewListItemProps) {
+  const { scrollIntoView, ...other } = props
+  const ref = React.useRef<HTMLElement>(null)
+  useEffect(() => {
+    if (scrollIntoView) {
+      ref.current?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+    }
+  }, [scrollIntoView])
+  return <ListItem ref={ref} {...other} />
 }
 
 export default function FlatList({
@@ -166,10 +181,6 @@ export default function FlatList({
   }
 
   function renderItem(item: FlatListItem, idx: number): JSX.Element {
-    if (item.render) {
-      return item.render(item)
-    }
-
     let itemProps = {}
     if (item.url) {
       itemProps = {
@@ -178,8 +189,10 @@ export default function FlatList({
         button: true,
       }
     }
+
     return (
-      <ListItem
+      <ScrollIntoViewListItem
+        scrollIntoView={item.scrollIntoView}
         key={idx}
         {...itemProps}
         className={item.highlight ? classes.highlightedItem : classes.listItem}
@@ -196,7 +209,7 @@ export default function FlatList({
             {item.secondaryAction}
           </ListItemSecondaryAction>
         )}
-      </ListItem>
+      </ScrollIntoViewListItem>
     )
   }
 
