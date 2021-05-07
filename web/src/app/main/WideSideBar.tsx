ad08ad8e84a8551e7a4d28ce7bec9c6bc11913c2
Nathaniel Cook <nathanieljcook@outlook.com>
2021-05-07 11:37:32 -0700

    merge commit fixes

--- web/src/app/main/WideSideBar.tsx
+++ web/src/app/main/WideSideBar.tsx
@@ -1,20 +1,12 @@
 import React, { ReactNode } from 'react'
 import Drawer from '@material-ui/core/Drawer'
 import { makeStyles } from '@material-ui/core'
-import logo from '../public/goalert-alt-logo-scaled.png'
 
 export const drawerWidth = '220px'
 
 const useStyles = makeStyles((theme) => ({
-  logoDiv: {
-    ...theme.mixins.toolbar,
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   sidebarPaper: {
-    width: 220,
+    width: drawerWidth,
     position: 'fixed',
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
@@ -37,9 +29,6 @@ function WideSideBar(props: WideSideBarProps): JSX.Element {
         paper: classes.sidebarPaper,
       }}
     >
-      <div aria-hidden className={classes.logoDiv}>
-        <img height={32} src={logo} alt='GoAlert Logo' />
-      </div>
       {props.children}
     </Drawer>
   )
