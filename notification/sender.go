b5128ad5b69086abf32ea7f6bf93277e42d29893
Nathaniel Caza <mastercactapus@gmail.com>
2021-05-17 13:39:49 -0500

    Update notification/sender.go
    Co-authored-by: David Talbot <david.talbot@target.com>

--- notification/sender.go
+++ notification/sender.go
@@ -24,7 +24,7 @@ type StatusChecker interface {
 // ErrStatusUnsupported should be returned when a Status() check is not supported by the provider.
 var ErrStatusUnsupported = errors.New("status check unsupported by provider")
 
-// ReceiverSetter is an optinoal interface a Sender can implement for use with two-way interactions.
+// ReceiverSetter is an optional interface a Sender can implement for use with two-way interactions.
 type ReceiverSetter interface {
 	SetReceiver(Receiver)
 }
