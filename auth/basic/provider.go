9d89e5c01cfdde2d9f72233c89173889afb57d61
dctalbot <david.talbot@target.com>
2021-05-18 16:00:58 -0500

    Update basic.NewProvider to take the Store as an arg instead of *sql.DB and pass it this, so we don't initialize two

--- auth/basic/provider.go
+++ auth/basic/provider.go
@@ -2,7 +2,6 @@ package basic
 
 import (
 	"context"
-	"database/sql"
 )
 
 // Provider implements the auth.IdentityProvider interface.
@@ -11,11 +10,6 @@ type Provider struct {
 }
 
 // NewProvider creates a new Provider with the associated config.
-func NewProvider(ctx context.Context, db *sql.DB) (*Provider, error) {
-	b, err := NewStore(ctx, db)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Provider{b: b}, nil
+func NewProvider(ctx context.Context, store *Store) (*Provider, error) {
+	return &Provider{b: store}, nil
 }
