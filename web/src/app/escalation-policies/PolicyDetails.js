123fb6feac6f00c7e6d84e17400b78d8a698b7de
PollyNengchu <polly.nengchu@target.com>
2021-05-21 13:03:21 -0500

    build fix

--- web/src/app/escalation-policies/PolicyDetails.js
+++ web/src/app/escalation-policies/PolicyDetails.js
@@ -18,7 +18,7 @@ import Spinner from '../loading/components/Spinner'
 import { EPAvatar } from '../util/avatars'
 
 const query = gql`
-  query($id: ID!) {
+  query ($id: ID!) {
     escalationPolicy(id: $id) {
       id
       name
@@ -41,7 +41,11 @@ export default function PolicyDetails(props) {
   const [showDeleteDialog, setShowDeleteDialog] = useState(false)
   const [showEditDialog, setShowEditDialog] = useState(false)
 
-  const { loading, error, data: _data } = useQuery(query, {
+  const {
+    loading,
+    error,
+    data: _data,
+  } = useQuery(query, {
     variables: {
       id: props.escalationPolicyID,
     },
