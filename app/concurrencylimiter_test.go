3a65dea2b9696264a6641986e687df49ba72224b
Nathaniel Caza <mastercactapus@gmail.com>
2021-06-10 12:20:06 -0500

    add multilock tests

--- app/concurrencylimiter_test.go
+++ app/concurrencylimiter_test.go
@@ -5,7 +5,49 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestMultiLock_TryLock(t *testing.T) {
+	l := newMultiLock(1, 1)
+	ctx := context.Background()
+	assert.NoError(t, l.Lock(ctx))
+
+	wait, err := l.TryLock()
+	assert.NoError(t, err)
+
+	go wait(ctx)
+	assert.False(t, l.Unlock())
+	assert.True(t, l.Unlock())
+}
+
+func TestMultiLock(t *testing.T) {
+	l := newMultiLock(10, 10)
+
+	err := l.Lock(context.Background())
+	require.NoError(t, err)
+
+	for i := 0; i < 19; i++ {
+		wait, err := l.TryLock()
+		require.NoError(t, err)
+		if wait != nil {
+			go wait(context.Background())
+		}
+	}
+
+	for i := 0; i < 20; i++ {
+		assert.False(t, l.Unlock())
+		wait, err := l.TryLock()
+		require.NoError(t, err)
+		go wait(context.Background())
+	}
+
+	for i := 0; i < 19; i++ {
+		assert.False(t, l.Unlock())
+	}
+
+	assert.True(t, l.Unlock())
+}
 
 func TestConcurrencyLimiter(t *testing.T) {
 	t.Run("lock count", func(t *testing.T) {
