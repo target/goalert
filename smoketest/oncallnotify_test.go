5ddb0fa0e4c03313e79b3f0a6074448feff160fe
Nathaniel Caza <mastercactapus@gmail.com>
2021-06-15 15:41:17 -0500

    fix oncallnotify test

--- smoketest/oncallnotify_test.go
+++ smoketest/oncallnotify_test.go
@@ -1,9 +1,16 @@
 package smoketest
 
 import (
+	"context"
 	"testing"
 
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/target/goalert/assignment"
+	"github.com/target/goalert/permission"
+	"github.com/target/goalert/schedule/rule"
 	"github.com/target/goalert/smoketest/harness"
+	"github.com/target/goalert/util/timeutil"
 )
 
 func TestOnCallNotify(t *testing.T) {
@@ -35,4 +42,15 @@ func TestOnCallNotify(t *testing.T) {
 	defer h.Close()
 
 	h.Slack().Channel("test").ExpectMessage("on-call", "testschedule", "bob")
+
+	ctx := permission.SystemContext(context.Background(), "Test")
+	_, err := h.App().ScheduleRuleStore.CreateRuleTx(ctx, nil, &rule.Rule{
+		ID:            uuid.NewV4().String(),
+		ScheduleID:    h.UUID("sid"),
+		WeekdayFilter: timeutil.EveryDay(),
+		Target:        assignment.UserTarget(h.UUID("uid2")),
+	})
+	require.NoError(t, err)
+
+	h.Slack().Channel("test").ExpectMessage("on-call", "testschedule", "bob", "joe")
 }
