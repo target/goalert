722e3f715060b51ec807e26bb0c97619ec6b9fd1
Nathaniel Caza <mastercactapus@gmail.com>
2021-06-16 11:30:07 -0500

    update message module version

--- migrate/migrations/20210609141058-outgoing-messages-schedule-id.sql
+++ migrate/migrations/20210609141058-outgoing-messages-schedule-id.sql
@@ -1,9 +1,20 @@
 -- +migrate Up
+UPDATE engine_processing_versions
+SET version = 8
+WHERE type_id = 'message';
+
 ALTER TABLE
   outgoing_messages
 ADD
   COLUMN schedule_id UUID REFERENCES schedules(id) ON DELETE CASCADE;
 
 -- +migrate Down
+UPDATE engine_processing_versions
+SET version = 7
+WHERE type_id = 'message';
+
+DELETE FROM outgoing_messages
+WHERE message_type = 'schedule_on_call_status';
+
 ALTER TABLE
   outgoing_messages DROP COLUMN schedule_id;
