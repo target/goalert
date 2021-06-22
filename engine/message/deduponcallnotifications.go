1ed29333aba6ce3e957e006170de3e049aa752a1
Nathaniel Caza <mastercactapus@gmail.com>
2021-06-22 09:16:36 -0500

    fix comment

--- engine/message/deduponcallnotifications.go
+++ engine/message/deduponcallnotifications.go
@@ -6,7 +6,7 @@ import (
 	"github.com/target/goalert/notification"
 )
 
-// dedupOnCallNotifications will remove old on-call notifications if a newer one exists for the same alert/destination.
+// dedupOnCallNotifications will remove old on-call notifications if a newer one exists for the same schedule & destination.
 func dedupOnCallNotifications(messages []Message) ([]Message, []string) {
 	sort.Slice(messages, func(i, j int) bool { return messages[i].CreatedAt.After(messages[j].CreatedAt) })
 
