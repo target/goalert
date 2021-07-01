7fdec05aadb6a4213f3f718bccbcb3d0e9b5664d
Nathaniel Cook <nathanieljcook@outlook.com>
2021-07-01 12:20:29 -0700

    use correct colorsfor theme toggle button

--- web/src/app/main/components/ThemeButton.tsx
+++ web/src/app/main/components/ThemeButton.tsx
@@ -1,40 +1,27 @@
 import React, { useContext } from 'react'
 import IconButton from '@material-ui/core/IconButton'
+import makeStyles from '@material-ui/core/styles/makeStyles'
 import DarkModeIcon from '@material-ui/icons/Brightness4'
 import LightModeIcon from '@material-ui/icons/BrightnessHigh'
 import { AppContext } from '../context'
 
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    color: theme.palette.getContrastText(theme.palette.primary.main),
+  },
+}))
+
 export default function ThemeButton(): JSX.Element {
+  const classes = useStyles()
   const { theme, setTheme } = useContext(AppContext)
 
   return (
     <IconButton onClick={() => setTheme(theme)}>
-      {theme === 'dark' ? <DarkModeIcon /> : <LightModeIcon />}
+      {theme === 'dark' ? (
+        <DarkModeIcon className={classes.icon} />
+      ) : (
+        <LightModeIcon className={classes.icon} />
+      )}
     </IconButton>
   )
 }
-
-// function getThemePreference(): string {
-//   const theme = localStorage.getItem('theme')
-//   if (!theme) {
-//     const mq = window.matchMedia('(prefers-color-scheme: dark)')
-//     return mq.matches ? 'dark' : 'light'
-//   }
-//   return theme
-// }
-
-// export default function ThemeProvider(): JSX.Element {
-//   const [theme, setTheme] = useState(getThemePreference())
-
-//   useEffect(() => {
-//       localStorage.setItem('theme', theme)
-//   }, [theme])
-
-//   if (!theme) {
-//     const mq = window.matchMedia('(prefers-color-scheme: dark)')
-//     theme = mq.matches ? 'dark' : 'light'
-//     localStorage.setItem('theme', theme)
-//   }
-
-//   return null
-// }
