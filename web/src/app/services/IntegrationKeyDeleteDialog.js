524413cb04b2eb61aef299cdf96ec27876a0cdf5
dctalbot <david.talbot@target.com>
2021-07-07 14:01:56 -0500

    add no longer exists dialog

--- web/src/app/services/IntegrationKeyDeleteDialog.js
+++ web/src/app/services/IntegrationKeyDeleteDialog.js
@@ -5,7 +5,7 @@ import p from 'prop-types'
 
 import { nonFieldErrors } from '../util/errutil'
 import Spinner from '../loading/components/Spinner'
-import { GenericError, ObjectNotFound } from '../error-pages'
+import { GenericError } from '../error-pages'
 import FormDialog from '../dialogs/FormDialog'
 
 const query = gql`
@@ -38,7 +38,6 @@ const mutation = gql`
 
 export default function IntegrationKeyDeleteDialog(props) {
   const { loading, error, data } = useQuery(query, {
-    pollInterval: 0,
     variables: { id: props.integrationKeyID },
   })
 
@@ -66,15 +65,24 @@ export default function IntegrationKeyDeleteDialog(props) {
   })
 
   if (loading && !data) return <Spinner />
-  if (data && !data.integrationKey)
-    return <ObjectNotFound type='integration key' />
+  if (!loading && !deleteKeyStatus.loading && data?.integrationKey === null) {
+    return (
+      <FormDialog
+        alert
+        title='No longer exists'
+        onClose={() => props.onClose()}
+        subTitle='That integration key does not exist or is already deleted.'
+      />
+    )
+  }
+
   if (error) return <GenericError error={error.message} />
 
   return (
     <FormDialog
       title='Are you sure?'
       confirm
-      subTitle={`This will delete the integration key: ${data.integrationKey.name}`}
+      subTitle={`This will delete the integration key: ${data?.integrationKey?.name}`}
       caption='This will prevent the creation of new alerts using this integration key. If you wish to re-enable, a NEW integration key must be created and may require additional reconfiguration of the alert source.'
       loading={deleteKeyStatus.loading}
       errors={nonFieldErrors(deleteKeyStatus.error)}
