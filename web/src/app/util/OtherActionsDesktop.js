6d74fc9ed9f70c1246ef08aa44de7585ab02a366
sandy <sandy.rav@onja.org>
2021-07-09 08:44:05 +0300

    formatted code with prettier

--- web/src/app/util/OtherActionsDesktop.js
+++ web/src/app/util/OtherActionsDesktop.js
@@ -4,63 +4,68 @@ import p from 'prop-types'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 
-export default function OtherActionsMenuDesktop({placement,anchorEl,isOpen,actions,onClose}) {
+export default function OtherActionsMenuDesktop({
+  placement,
+  anchorEl,
+  isOpen,
+  actions,
+  onClose,
+}) {
+  // anchorDir is the opposite of the wanted menu placement
+  const anchorDir = placement === 'left' ? 'right' : 'left'
 
-    // anchorDir is the opposite of the wanted menu placement
-    const anchorDir = placement === 'left' ? 'right' : 'left'
-    
-    return (
-      <Menu
-        anchorEl={() => anchorEl}
-        getContentAnchorEl={null}
-        open={isOpen}
-        onClose={onClose}
-        PaperProps={{
-          style: {
-            minWidth: '15em',
-          },
-        }}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: anchorDir,
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: anchorDir,
-        }}
-      >
-        {actions.map((o, idx) => (
-          <MenuItem
-            key={idx}
-            onClick={() => {
-              onClose()
-              o.onClick()
-            }}
-          >
-            {o.label}
-          </MenuItem>
-        ))}
-      </Menu>
-    )
+  return (
+    <Menu
+      anchorEl={() => anchorEl}
+      getContentAnchorEl={null}
+      open={isOpen}
+      onClose={onClose}
+      PaperProps={{
+        style: {
+          minWidth: '15em',
+        },
+      }}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: anchorDir,
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: anchorDir,
+      }}
+    >
+      {actions.map((o, idx) => (
+        <MenuItem
+          key={idx}
+          onClick={() => {
+            onClose()
+            o.onClick()
+          }}
+        >
+          {o.label}
+        </MenuItem>
+      ))}
+    </Menu>
+  )
 }
 
 OtherActionsMenuDesktop.propTypes = {
-    anchorEl: p.object,
-    isOpen: p.bool,
-    onClose: p.func,
+  anchorEl: p.object,
+  isOpen: p.bool,
+  onClose: p.func,
 
-    actions: p.arrayOf(
-      p.shape({
-        label: p.string.isRequired,
-        onClick: p.func.isRequired,
-      }),
-    ),
+  actions: p.arrayOf(
+    p.shape({
+      label: p.string.isRequired,
+      onClick: p.func.isRequired,
+    }),
+  ),
 
-    placement: p.oneOf(['left', 'right']),
-  }
+  placement: p.oneOf(['left', 'right']),
+}
 
-  OtherActionsMenuDesktop.defaultProps = {
-    onClose: () => {},
-    actions: [],
-    placement: 'left',
-  }
+OtherActionsMenuDesktop.defaultProps = {
+  onClose: () => {},
+  actions: [],
+  placement: 'left',
+}
