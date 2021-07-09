562bdf6a156c5fd71e80823eb36b2de88a5caa91
sandy <sandy.rav@onja.org>
2021-07-09 10:52:41 +0300

    refactored OtherActionsMenuMobile class component to a functional component

--- web/src/app/util/OtherActionsMobile.js
+++ web/src/app/util/OtherActionsMobile.js
@@ -1,51 +1,46 @@
 import React from 'react'
 import p from 'prop-types'
-
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 
-export default class OtherActionsMenuDesktop extends React.PureComponent {
-  static propTypes = {
-    anchorEl: p.object,
+export default function OtherActionsMenuDesktop({ isOpen, onClose, actions }) {
+  return (
+    <SwipeableDrawer
+      anchor='bottom'
+      disableDiscovery
+      disableSwipeToOpen
+      open={isOpen}
+      onOpen={() => null}
+      onClose={onClose}
+    >
+      <List data-cy='mobile-actions'>
+        {actions.map((o, idx) => (
+          <ListItem
+            key={idx}
+            button
+            onClick={() => {
+              onClose()
+              o.onClick()
+            }}
+          >
+            <ListItemText primary={o.label} />
+          </ListItem>
+        ))}
+      </List>
+    </SwipeableDrawer>
+  )
+}
 
-    onClose: p.func,
-    isOpen: p.bool,
-
-    actions: p.arrayOf(
-      p.shape({
-        label: p.string.isRequired,
-        onClick: p.func.isRequired,
-      }),
-    ),
-  }
-
-  render() {
-    return (
-      <SwipeableDrawer
-        anchor='bottom'
-        disableDiscovery
-        disableSwipeToOpen
-        open={this.props.isOpen}
-        onOpen={() => null}
-        onClose={this.props.onClose}
-      >
-        <List data-cy='mobile-actions'>
-          {this.props.actions.map((o, idx) => (
-            <ListItem
-              key={idx}
-              button
-              onClick={() => {
-                this.props.onClose()
-                o.onClick()
-              }}
-            >
-              <ListItemText primary={o.label} />
-            </ListItem>
-          ))}
-        </List>
-      </SwipeableDrawer>
-    )
-  }
+OtherActionsMenuDesktop.propTypes = {
+  anchorEl: p.object,
+  onClose: p.func,
+  isOpen: p.bool,
+  actions: p.arrayOf(
+    p.shape({
+      label: p.string.isRequired,
+      onClick: p.func.isRequired,
+    }),
+  ),
 }
