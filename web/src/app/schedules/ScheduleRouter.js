a0e5d246e809494b599fead774ab8209f4cbf344
Rinon <rinon.ten@onja.org>
2021-07-12 09:01:25 +0300

    Converts a class component to a functional component

--- web/src/app/schedules/ScheduleRouter.js
+++ web/src/app/schedules/ScheduleRouter.js
@@ -6,7 +6,6 @@ import ScheduleDetails from './ScheduleDetails'
 import ScheduleOverrideList from './ScheduleOverrideList'
 import ScheduleAssignedToList from './ScheduleAssignedToList'
 import ScheduleShiftList from './ScheduleShiftList'
-
 import { PageNotFound } from '../error-pages/Errors'
 import ScheduleRuleList from './ScheduleRuleList'
 import SimpleListPage from '../lists/SimpleListPage'
@@ -46,46 +45,42 @@ class ScheduleList extends React.PureComponent {
   }
 }
 
-export default class ScheduleRouter extends React.PureComponent {
-  render() {
-    return (
-      <Switch>
-        <Route exact path='/schedules' component={ScheduleList} />
-        <Route
-          exact
-          path='/schedules/:scheduleID'
-          render={({ match }) => (
-            <ScheduleDetails scheduleID={match.params.scheduleID} />
-          )}
-        />
-
-        <Route
-          path='/schedules/:scheduleID/assignments'
-          render={({ match }) => (
-            <ScheduleRuleList scheduleID={match.params.scheduleID} />
-          )}
-        />
-        <Route
-          path='/schedules/:scheduleID/escalation-policies'
-          render={({ match }) => (
-            <ScheduleAssignedToList scheduleID={match.params.scheduleID} />
-          )}
-        />
-        <Route
-          path='/schedules/:scheduleID/overrides'
-          render={({ match }) => (
-            <ScheduleOverrideList scheduleID={match.params.scheduleID} />
-          )}
-        />
-        <Route
-          path='/schedules/:scheduleID/shifts'
-          render={({ match }) => (
-            <ScheduleShiftList scheduleID={match.params.scheduleID} />
-          )}
-        />
-
-        <Route component={PageNotFound} />
-      </Switch>
-    )
-  }
+export default function ScheduleRouter() {
+  return (
+    <Switch>
+      <Route exact path='/schedules' component={ScheduleList} />
+      <Route
+        exact
+        path='/schedules/:scheduleID'
+        render={({ match }) => (
+          <ScheduleDetails scheduleID={match.params.scheduleID} />
+        )}
+      />
+      <Route
+        path='/schedules/:scheduleID/assignments'
+        render={({ match }) => (
+          <ScheduleRuleList scheduleID={match.params.scheduleID} />
+        )}
+      />
+      <Route
+        path='/schedules/:scheduleID/escalation-policies'
+        render={({ match }) => (
+          <ScheduleAssignedToList scheduleID={match.params.scheduleID} />
+        )}
+      />
+      <Route
+        path='/schedules/:scheduleID/overrides'
+        render={({ match }) => (
+          <ScheduleOverrideList scheduleID={match.params.scheduleID} />
+        )}
+      />
+      <Route
+        path='/schedules/:scheduleID/shifts'
+        render={({ match }) => (
+          <ScheduleShiftList scheduleID={match.params.scheduleID} />
+        )}
+      />
+      <Route component={PageNotFound} />
+    </Switch>
+  )
 }
