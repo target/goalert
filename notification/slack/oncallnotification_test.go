63444b62701923171f1b72a76eba7993c94c8093
Nathaniel Caza <mastercactapus@gmail.com>
2021-07-12 14:23:26 -0500

    clearer naming

--- notification/slack/oncallnotification_test.go
+++ notification/slack/oncallnotification_test.go
@@ -8,21 +8,27 @@ import (
 )
 
 func TestRenderOnCallNotification(t *testing.T) {
-	slackUserIDs := map[string]string{"slack.1": "slack.1.ID", "slack.2": "slack.2.ID", "slack.3": "slack.3.ID"}
+	// static user id -> slack ID mappings for testing
+	slackUserIDs := map[string]string{
+		"slack.1": "slack.1.SLACKID",
+		"slack.2": "slack.2.SLACKID",
+		"slack.3": "slack.3.SLACKID",
+	}
 	msg := notification.ScheduleOnCallUsers{
 		ScheduleName: "schedule.name",
 		ScheduleURL:  "schedule.url",
 	}
 
-	check := func(desc, expected string, users []string) {
+	// test with `msg` for the provided user ids
+	check := func(desc, expected string, userIDs []string) {
 		t.Helper()
 		t.Run(desc, func(t *testing.T) {
 			msg.Users = nil
-			for _, u := range users {
+			for _, id := range userIDs {
 				msg.Users = append(msg.Users, notification.User{
-					ID:   u,
-					Name: u,
-					URL:  u + ".url",
+					ID:   id,
+					Name: id + ".name",
+					URL:  id + ".url",
 				})
 			}
 			assert.Equal(t, expected, renderOnCallNotificationMessage(msg, slackUserIDs))
@@ -31,24 +37,24 @@ func TestRenderOnCallNotification(t *testing.T) {
 
 	check("empty", "No users are on-call for <schedule.url|schedule.name>", nil)
 
-	check("fallback", "<foo.url|foo> is on-call for <schedule.url|schedule.name>", []string{"foo"})
+	check("fallback", "<foo.url|foo.name> is on-call for <schedule.url|schedule.name>", []string{"foo"})
 
-	check("1 user", "<@slack.1.ID> is on-call for <schedule.url|schedule.name>", []string{"slack.1"})
+	check("1 user", "<@slack.1.SLACKID> is on-call for <schedule.url|schedule.name>", []string{"slack.1"})
 	check("2 users",
-		"<@slack.1.ID> and <@slack.2.ID> are on-call for <schedule.url|schedule.name>",
+		"<@slack.1.SLACKID> and <@slack.2.SLACKID> are on-call for <schedule.url|schedule.name>",
 		[]string{"slack.1", "slack.2"})
 	check("3 users",
-		"<@slack.1.ID>, <@slack.2.ID>, and <@slack.3.ID> are on-call for <schedule.url|schedule.name>",
+		"<@slack.1.SLACKID>, <@slack.2.SLACKID>, and <@slack.3.SLACKID> are on-call for <schedule.url|schedule.name>",
 		[]string{"slack.1", "slack.2", "slack.3"})
 
 	check("3 users with fallback",
-		"<@slack.1.ID>, <foo.url|foo>, and <@slack.3.ID> are on-call for <schedule.url|schedule.name>",
+		"<@slack.1.SLACKID>, <foo.url|foo.name>, and <@slack.3.SLACKID> are on-call for <schedule.url|schedule.name>",
 		[]string{"slack.1", "foo", "slack.3"})
 
 	t.Run("no panic on nil map", func(t *testing.T) {
-		msg.Users = []notification.User{{ID: "foo.id", Name: "foo", URL: "foo.url"}}
+		msg.Users = []notification.User{{ID: "foo.SLACKID", Name: "foo.name", URL: "foo.url"}}
 
-		assert.Equal(t, "<foo.url|foo> is on-call for <schedule.url|schedule.name>", renderOnCallNotificationMessage(msg, nil))
+		assert.Equal(t, "<foo.url|foo.name> is on-call for <schedule.url|schedule.name>", renderOnCallNotificationMessage(msg, nil))
 	})
 
 }
