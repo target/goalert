ac022239c15c7b97efe57c77f1e42342f09649c0
sandy <sandy.rav@onja.org>
2021-07-19 09:51:46 +0300

    refactoring WizardScheduleForm class component to a functional component

--- web/src/app/wizard/WizardScheduleForm.js
+++ web/src/app/wizard/WizardScheduleForm.js
@@ -8,16 +8,17 @@ import Grid from '@material-ui/core/Grid'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import Tooltip from '@material-ui/core/Tooltip'
-import withStyles from '@material-ui/core/styles/withStyles'
-import withWidth, { isWidthDown } from '@material-ui/core/withWidth'
+import { isWidthDown } from '@material-ui/core/withWidth'
 import InfoIcon from '@material-ui/icons/Info'
 import { TimeZoneSelect, UserSelect } from '../selection'
 import { FormField } from '../forms'
 import { value as valuePropType } from './propTypes'
 import { set } from 'lodash'
 import { ISODateTimePicker } from '../util/ISOPickers'
+import { makeStyles } from '@material-ui/core'
+import useWidth from '../util/useWidth'
 
-const styles = {
+const useStyles = makeStyles(() => ({
   fieldItem: {
     marginLeft: '2.5em',
   },
@@ -28,98 +29,79 @@ const styles = {
   tooltip: {
     fontSize: 12,
   },
-}
+}))
 
 /**
  * Renders the form fields to be used in the wizard that
  * can be used for creating a primary and secondary schedule.
  */
-@withWidth()
-@withStyles(styles)
-export default class WizardScheduleForm extends React.Component {
-  static propTypes = {
-    onChange: p.func.isRequired,
-    value: valuePropType,
-    secondary: p.bool,
+export default function WizardScheduleForm({
+  value,
+  onChange,
+  secondary,
+  forceUpdate,
+}) {
+  const width = useWidth()
+  const classes = useStyles()
+
+  function renderFollowTheSun(key, schedType) {
+    if (value[key].followTheSunRotation.enable === 'yes') {
+      return (
+        <React.Fragment>
+          <Grid item xs={12} className={classes.fieldItem}>
+            <FormField
+              component={UserSelect}
+              multiple
+              name={`${key}.followTheSunRotation.users`}
+              label={
+                <React.Fragment>
+                  Who will be on call for your <b>{schedType}</b> follow the sun
+                  rotation?
+                </React.Fragment>
+              }
+              formLabel
+              fullWidth={isWidthDown('md', width)}
+              required
+              value={value[key].followTheSunRotation.users}
+            />
+          </Grid>
+          <Grid item xs={12} className={classes.fieldItem}>
+            <FormField
+              component={TimeZoneSelect}
+              name={`${key}.followTheSunRotation.timeZone`}
+              label={
+                <React.Fragment>
+                  What time zone is your <b>{schedType}</b> follow the sun
+                  rotation based in?
+                </React.Fragment>
+              }
+              formLabel
+              fullWidth={isWidthDown('md', width)}
+              required
+            />
+          </Grid>
+        </React.Fragment>
+      )
+    }
   }
 
-  getKey = () => {
-    const { secondary } = this.props
+  const getKey = () => {
     return secondary ? 'secondarySchedule' : 'primarySchedule'
   }
 
-  handleRotationTypeChange = (e) => {
-    const { onChange, value } = this.props
-    onChange(set(value, [this.getKey(), 'rotation', 'type'], e.target.value))
-    this.forceUpdate()
+  const handleRotationTypeChange = (e) => {
+    onChange(set(value, [getKey(), 'rotation', 'type'], e.target.value))
+    forceUpdate()
   }
 
-  handleFollowTheSunToggle = (e) => {
-    const { onChange, value } = this.props
+  const handleFollowTheSunToggle = (e) => {
     onChange(
-      set(
-        value,
-        [this.getKey(), 'followTheSunRotation', 'enable'],
-        e.target.value,
-      ),
+      set(value, [getKey(), 'followTheSunRotation', 'enable'], e.target.value),
     )
-    this.forceUpdate()
+    forceUpdate()
   }
 
-  /*
-   * Choose users
-   *  - if 1 user, add as user assignment
-   *  - if 2+ users, add as rotation assignment
-   * Create shifts for assignment, if needed
-   *
-   * Ask if second assignment is needed for evening shifts
-   *   - repeat choosing users if yes
-   */
-  render() {
-    const { classes, secondary, value, width } = this.props
-    const key = secondary ? 'secondarySchedule' : 'primarySchedule'
-    const schedType = secondary ? 'secondary' : 'primary'
-
-    return (
-      <React.Fragment>
-        <Grid item xs={12} className={classes.fieldItem}>
-          <FormField
-            component={TimeZoneSelect}
-            name={`${key}.timeZone`}
-            label={
-              <React.Fragment>
-                What time zone is your <b>{schedType}</b> schedule based in?
-              </React.Fragment>
-            }
-            formLabel
-            fullWidth={isWidthDown('md', width)}
-            required
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.fieldItem}>
-          <FormField
-            component={UserSelect}
-            data-cy={`${key}.users`}
-            multiple
-            name={`${key}.users`}
-            label={
-              <React.Fragment>
-                Who will be on call for your <b>{schedType}</b> schedule?
-              </React.Fragment>
-            }
-            formLabel
-            fullWidth={isWidthDown('md', width)}
-            required
-            value={value[key].users}
-          />
-        </Grid>
-        {this.renderRotationFields(key, schedType)}
-      </React.Fragment>
-    )
-  }
-
-  renderRotationFields(key, schedType) {
-    const { classes, value, width } = this.props
+  function renderRotationFields(key, schedType) {
     const hideRotationFields =
       value[key].users.length <= 1 ||
       value[key].rotation.type === 'never' ||
@@ -141,7 +123,7 @@ export default class WizardScheduleForm extends React.Component {
               aria-label='Rotation?'
               row
               value={value[key].rotation.type}
-              onChange={this.handleRotationTypeChange}
+              onChange={handleRotationTypeChange}
             >
               <FormControlLabel
                 data-cy={`${key}.rotationType.weekly`}
@@ -214,7 +196,7 @@ export default class WizardScheduleForm extends React.Component {
                   name={`${key}.fts`}
                   row
                   value={value[key].followTheSunRotation.enable}
-                  onChange={this.handleFollowTheSunToggle}
+                  onChange={handleFollowTheSunToggle}
                 >
                   <FormControlLabel
                     data-cy={`${key}.fts.yes`}
@@ -231,53 +213,64 @@ export default class WizardScheduleForm extends React.Component {
                 </RadioGroup>
               </FormControl>
             </Grid>
-            {this.renderFollowTheSun(key, schedType)}
+            {renderFollowTheSun(key, schedType)}
           </React.Fragment>
         )}
       </React.Fragment>
     )
   }
+  /*
+   * Choose users
+   *  - if 1 user, add as user assignment
+   *  - if 2+ users, add as rotation assignment
+   * Create shifts for assignment, if needed
+   *
+   * Ask if second assignment is needed for evening shifts
+   *   - repeat choosing users if yes
+   */
+  const key = secondary ? 'secondarySchedule' : 'primarySchedule'
+  const schedType = secondary ? 'secondary' : 'primary'
 
-  renderFollowTheSun(key, schedType) {
-    const { classes, value, width } = this.props
+  return (
+    <React.Fragment>
+      <Grid item xs={12} className={classes.fieldItem}>
+        <FormField
+          component={TimeZoneSelect}
+          name={`${key}.timeZone`}
+          label={
+            <React.Fragment>
+              What time zone is your <b>{schedType}</b> schedule based in?
+            </React.Fragment>
+          }
+          formLabel
+          fullWidth={isWidthDown('md', width)}
+          required
+        />
+      </Grid>
+      <Grid item xs={12} className={classes.fieldItem}>
+        <FormField
+          component={UserSelect}
+          data-cy={`${key}.users`}
+          multiple
+          name={`${key}.users`}
+          label={
+            <React.Fragment>
+              Who will be on call for your <b>{schedType}</b> schedule?
+            </React.Fragment>
+          }
+          formLabel
+          fullWidth={isWidthDown('md', width)}
+          required
+          value={value[key].users}
+        />
+      </Grid>
+      {renderRotationFields(key, schedType)}
+    </React.Fragment>
+  )
+}
 
-    if (value[key].followTheSunRotation.enable === 'yes') {
-      return (
-        <React.Fragment>
-          <Grid item xs={12} className={classes.fieldItem}>
-            <FormField
-              component={UserSelect}
-              multiple
-              name={`${key}.followTheSunRotation.users`}
-              label={
-                <React.Fragment>
-                  Who will be on call for your <b>{schedType}</b> follow the sun
-                  rotation?
-                </React.Fragment>
-              }
-              formLabel
-              fullWidth={isWidthDown('md', width)}
-              required
-              value={value[key].followTheSunRotation.users}
-            />
-          </Grid>
-          <Grid item xs={12} className={classes.fieldItem}>
-            <FormField
-              component={TimeZoneSelect}
-              name={`${key}.followTheSunRotation.timeZone`}
-              label={
-                <React.Fragment>
-                  What time zone is your <b>{schedType}</b> follow the sun
-                  rotation based in?
-                </React.Fragment>
-              }
-              formLabel
-              fullWidth={isWidthDown('md', width)}
-              required
-            />
-          </Grid>
-        </React.Fragment>
-      )
-    }
-  }
+WizardScheduleForm.propTypes = {
+  onChange: p.func.isRequired,
+  value: valuePropType,
+  secondary: p.bool,
 }
