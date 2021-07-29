f37cc3f81a5e89936771e4571cc378c1802fc6be
Nathaniel Cook <nathanieljcook@outlook.com>
2021-07-29 09:29:45 -0700

    use lazy query to get on button click

--- web/src/app/admin/SlackActions.tsx
+++ web/src/app/admin/SlackActions.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
-import Divider from '@material-ui/core/Divider'
-import CardActions from '../details/CardActions'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
-import { gql, useQuery } from '@apollo/client'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Divider from '@material-ui/core/Divider'
+import { gql, useLazyQuery } from '@apollo/client'
+import CardActions from '../details/CardActions'
 import Spinner from '../loading/components/Spinner'
 import { GenericError } from '../error-pages'
 import Markdown from '../util/Markdown'
-import {
-  Button,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-} from '@material-ui/core'
 import copyToClipboard from '../util/copyToClipboard'
 
 const query = gql`
@@ -24,11 +23,9 @@ export default function SlackActions(): JSX.Element {
   const [showManifest, setShowManifest] = useState(false)
   const [copied, setCopied] = useState(false)
 
-  // todo: generate this on click
-  // e.g. someone generates, makes a config change, then wants to re-generate
-  const { loading, error, data } = useQuery(query)
-  if (loading) return <Spinner />
-  if (error) return <GenericError error={error.message} />
+  const [getManifest, { called, loading, error, data }] = useLazyQuery(query, {
+    pollInterval: 0,
+  })
 
   return (
     <React.Fragment>
@@ -37,7 +34,10 @@ export default function SlackActions(): JSX.Element {
         primaryActions={[
           {
             label: 'Generate Slack App Manifest',
-            handleOnClick: () => setShowManifest(true),
+            handleOnClick: () => {
+              getManifest()
+              setShowManifest(true)
+            },
           },
         ]}
       />
