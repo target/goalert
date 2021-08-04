31a23a20c53958feba50bd1abeda2160aea91bcb
Arundhati Rao <arao4@buffalo.edu>
2021-08-04 17:39:55 -0400

    Update notification/slack/channel.go
    Co-authored-by: David Talbot <david.talbot@target.com>

--- notification/slack/channel.go
+++ notification/slack/channel.go
@@ -321,7 +321,7 @@ func (s *ChannelSender) Send(ctx context.Context, msg notification.Message) (str
 		if t.OriginalStatus != nil {
 			// Reply in thread if we already sent a message for this alert.
 			vals.Set("thread_ts", t.OriginalStatus.ProviderMessageID.ExternalID)
-			vals.Set("text", "Broadcasting to channel, due to repeat notification.")
+			vals.Set("text", "Broadcasting to channel due to repeat notification.")
 			vals.Set("reply_broadcast", "true")
 			break
 		}
