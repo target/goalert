5fe663626c29f57027b29732115d5823afbbda78
dctalbot <david.talbot@target.com>
2021-08-09 15:50:09 -0500

    add timezone hints to field labels

--- web/src/app/schedules/temp-sched/TempSchedTimesStep.tsx
+++ web/src/app/schedules/temp-sched/TempSchedTimesStep.tsx
@@ -34,22 +34,25 @@ type TempSchedTimesStepProps = {
 
 export default function TempSchedTimesStep({
   scheduleID,
-  stepText,
   value,
   edit,
 }: TempSchedTimesStepProps): JSX.Element {
   const classes = useStyles()
-  const { data, error, loading } = useQuery(schedTZQuery, {
+  const { data, loading, error } = useQuery(schedTZQuery, {
     variables: { id: scheduleID },
   })
   const zone = data?.schedule?.timeZone
-  console.log(zone === undefined)
+  const zoneAbbr = DateTime.fromObject({ zone }).toFormat('ZZZZ')
+  const isLocalZone = zone === DateTime.local().zoneName
 
   const [now] = useState(DateTime.utc().startOf('minute').toISO())
 
   function validate(): Error | null {
     if (isISOAfter(value.start, value.end)) {
       return new Error('Start date/time cannot be after end date/time.')
+    }
+    if (error) {
+      return new Error('Issue getting schedule time zone.')
     }
     return null
   }
@@ -58,7 +61,7 @@ export default function TempSchedTimesStep({
     <StepContainer width='35%' data-cy='sched-times-step'>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography variant='body2'>{stepText}</Typography>
+          <Typography variant='body2'>STEP 1 OF 2</Typography>
           <Typography variant='h6' component='h2'>
             Determine start and end times.
           </Typography>
@@ -74,10 +77,12 @@ export default function TempSchedTimesStep({
             fullWidth
             component={ISODateTimePicker}
             required
+            label={'Start' + (isLocalZone ? '' : ` (${zoneAbbr})`)}
             name='start'
             min={edit ? value.start : now}
             validate={() => validate()}
             timeZone={zone}
+            disabled={loading}
           />
         </Grid>
         <Grid item xs={6}>
@@ -85,10 +90,12 @@ export default function TempSchedTimesStep({
             fullWidth
             component={ISODateTimePicker}
             required
+            label={'End' + (isLocalZone ? '' : ` (${zoneAbbr})`)}
             name='end'
             min={edit ? value.start : now}
             validate={() => validate()}
             timeZone={zone}
+            disabled={loading}
           />
         </Grid>
       </Grid>
