dbca4b1db484f324c86b409f2ce84eea160ab11f
sandy <sandy.rav@onja.org>
2021-08-13 11:43:56 +0300

    converting URLErrorDialog class component to a functional component

--- web/src/app/main/URLErrorDialog.js
+++ web/src/app/main/URLErrorDialog.js
@@ -1,56 +1,37 @@
 import React from 'react'
-import { urlParamSelector } from '../selectors'
-import { connect } from 'react-redux'
-import { resetURLParams } from '../actions'
 import { Dialog, DialogContent, DialogActions } from '@material-ui/core'
 import DialogTitleWrapper from '../dialogs/components/DialogTitleWrapper'
 import DialogContentError from '../dialogs/components/DialogContentError'
 import LoadingButton from '../loading/components/LoadingButton'
 
-@connect(
-  (state) => ({
-    errorMessage: urlParamSelector(state)('errorMessage'),
-    errorTitle: urlParamSelector(state)('errorTitle'),
-  }),
-  (dispatch) => ({
-    resetError: () => dispatch(resetURLParams('errorMessage', 'errorTitle')),
-  }),
-)
-export default class URLErrorDialog extends React.Component {
-  handleDialogClose = () => {
-    this.props.resetError()
-  }
+export default function URLErrorDialog({ errorMessage, errorTitle, onClose }) {
+  const open = Boolean(errorMessage) || Boolean(errorTitle)
 
-  render() {
-    const { errorMessage, errorTitle } = this.props
-    const open = Boolean(errorMessage) || Boolean(errorTitle)
-
-    return (
-      open && (
-        <Dialog
-          open={open}
-          onClose={() => this.onClose()}
-          aria-labelledby='alert-dialog-title'
-          aria-describedby='alert-dialog-description'
-          disableBackdropClick
-        >
-          <DialogTitleWrapper id='alert-dialog-title' title={errorTitle} />
-          <DialogContent>
-            <DialogContentError
-              id='alert-dialog-description'
-              error={errorMessage}
-              noPadding
-            />
-          </DialogContent>
-          <DialogActions>
-            <LoadingButton
-              buttonText='Okay'
-              color='primary'
-              onClick={() => this.onClose()}
-            />
-          </DialogActions>
-        </Dialog>
-      )
+  return (
+    open && (
+      <Dialog
+        open={open}
+        onClose={() => onClose()}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+        disableBackdropClick
+      >
+        <DialogTitleWrapper id='alert-dialog-title' title={errorTitle} />
+        <DialogContent>
+          <DialogContentError
+            id='alert-dialog-description'
+            error={errorMessage}
+            noPadding
+          />
+        </DialogContent>
+        <DialogActions>
+          <LoadingButton
+            buttonText='Okay'
+            color='primary'
+            onClick={() => onClose()}
+          />
+        </DialogActions>
+      </Dialog>
     )
-  }
+  )
 }
