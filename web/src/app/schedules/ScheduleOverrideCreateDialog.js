d464a58cb40cc5843dda579b2cbb26bdab089799
Spencer Pauly <spencerpauly@gmail.com>
2021-08-17 09:37:38 -0500

    Removed duplicate copy text

--- web/src/app/schedules/ScheduleOverrideCreateDialog.js
+++ web/src/app/schedules/ScheduleOverrideCreateDialog.js
@@ -1,27 +1,12 @@
 import React, { useState } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import p from 'prop-types'
-import Button from '@material-ui/core/Button'
 import FormDialog from '../dialogs/FormDialog'
 import { DateTime } from 'luxon'
 import ScheduleOverrideForm from './ScheduleOverrideForm'
 import { fieldErrors, nonFieldErrors } from '../util/errutil'
 import useOverrideNotices from './useOverrideNotices'
-
-const copyText = {
-  add: {
-    title: 'Temporarily Add a User',
-    desc: 'This will add a new shift for the selected user, while the override is active. Existing shifts will remain unaffected.',
-  },
-  remove: {
-    title: 'Temporarily Remove a User',
-    desc: 'This will remove (or split/shorten) shifts belonging to the selected user, while the override is active.',
-  },
-  replace: {
-    title: 'Temporarily Replace a User',
-    desc: 'This will replace the selected user with another during any existing shifts, while the override is active. No new shifts will be created, only who is on-call will be changed.',
-  },
-}
+import { variantDetails } from './ScheduleCalendarOverrideDialog'
 
 const mutation = gql`
   mutation ($input: CreateUserOverrideInput!) {
@@ -30,7 +15,6 @@ const mutation = gql`
     }
   }
 `
-
 export default function ScheduleOverrideCreateDialog(props) {
   const [value, setValue] = useState({
     addUserID: '',
@@ -55,15 +39,15 @@ export default function ScheduleOverrideCreateDialog(props) {
   return (
     <FormDialog
       onClose={props.onClose}
-      title={copyText[props.variant].title}
-      subTitle={copyText[props.variant].desc}
+      title={variantDetails[props.variant].title}
+      subTitle={variantDetails[props.variant].desc}
       errors={nonFieldErrors(error)}
       notices={notices} // create and edit dialogue
       onSubmit={() => mutate()}
       form={
         <ScheduleOverrideForm
-          add={value.variant !== 'remove'}
-          remove={value.variant !== 'add'}
+          add={props.variant !== 'remove'}
+          remove={props.variant !== 'add'}
           scheduleID={props.scheduleID}
           disabled={loading}
           errors={fieldErrors(error)}
