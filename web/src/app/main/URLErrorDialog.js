07b8f4e33bc77f3faf887195455d176ed94f25ef
sandy <sandy.rav@onja.org>
2021-08-18 10:39:56 +0300

    converting decorators to use useURLParam

--- web/src/app/main/URLErrorDialog.js
+++ web/src/app/main/URLErrorDialog.js
@@ -3,8 +3,13 @@ import { Dialog, DialogContent, DialogActions } from '@material-ui/core'
 import DialogTitleWrapper from '../dialogs/components/DialogTitleWrapper'
 import DialogContentError from '../dialogs/components/DialogContentError'
 import LoadingButton from '../loading/components/LoadingButton'
+import { useURLParam } from '../actions'
 
-export default function URLErrorDialog({ errorMessage, errorTitle, onClose }) {
+export default function URLErrorDialog({ onClose }) {
+  const errorMessage = useURLParam('errorMessage')
+  const errorTitle = useURLParam('errorTitle')
+
+  const open = Boolean(errorMessage) || Boolean(errorTitle)
   const open = Boolean(errorMessage) || Boolean(errorTitle)
 
   return (
@@ -14,7 +19,6 @@ export default function URLErrorDialog({ errorMessage, errorTitle, onClose }) {
         onClose={() => onClose()}
         aria-labelledby='alert-dialog-title'
         aria-describedby='alert-dialog-description'
-        disableBackdropClick
       >
         <DialogTitleWrapper id='alert-dialog-title' title={errorTitle} />
         <DialogContent>
