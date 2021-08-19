134bf4285c97149d737b71a8b7bf44ba5906ac74
Nathaniel Cook <nathanieljcook@outlook.com>
2021-08-19 13:00:02 -0700

    create ctx inline, cleanup

--- notification/slack/actions.go
+++ notification/slack/actions.go
@@ -2,9 +2,8 @@ package slack
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -14,6 +13,7 @@ import (
 	"github.com/target/goalert/alert"
 	"github.com/target/goalert/config"
 	"github.com/target/goalert/permission"
+	"github.com/target/goalert/util/errutil"
 )
 
 // Handler responds to API requests from Slack
@@ -42,16 +42,12 @@ func NewHandler(c Config) *Handler {
 	return &Handler{c: c}
 }
 
-func httpErr(w http.ResponseWriter, err error) error {
-	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-}
-
 // validRequest is used to validate a request from Slack.
 // If the request is validated true is returned, false otherwise.
 // https://api.slack.com/authentication/verifying-requests-from-slack
 func validRequest(w http.ResponseWriter, req *http.Request) error {
 	if req.Method != "POST" {
-		return httpErr(w, errors.New("not a post"))
+		return errors.New("not a post")
 	}
 
 	secret := config.FromContext(req.Context()).Slack.SigningSecret
@@ -61,112 +57,98 @@ func validRequest(w http.ResponseWriter, req *http.Request) error {
 	}
 
 	defer req.Body.Close()
-	body, err := ioutil.ReadAll(io.TeeReader(req.Body, sv))
+	body, err := ioutil.ReadAll(io.TeeReader(req.Body, &sv))
 	if err != nil {
-		return httpErr(w, err)
+		return err
 	}
 	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 	return sv.Ensure()
 }
 
-func (h *Handler) processAction(ctx context.Context, w http.ResponseWriter, payload slack.InteractionCallback, action *slack.BlockAction, alertIDStr string) error {
-	cfg := config.FromContext(ctx)
-	var api = slack.New(cfg.Slack.AccessToken)
-
-	// add source info to ctx to enable writing the action to alert log
-	ncID, _, err := h.c.AlertLogStore.FindNCByValue(ctx, nil, payload.Channel.ID)
-	if err != nil {
-		return err
-	}
-	ctx = permission.UserSourceContext(ctx, payload.User.ID, permission.RoleUser, &permission.SourceInfo{
-		Type: permission.SourceTypeNotificationChannel,
-		ID:   ncID,
-	})
-
-	// handle button clicked within Slack
-	alertID, err := strconv.Atoi(alertIDStr)
-	if err != nil {
-		return err
-	}
-	var actionErr error
-	switch action.ActionID {
-	case "ack":
-		actionErr = h.c.AlertStore.UpdateStatus(ctx, alertID, alert.StatusActive)
-	case "esc":
-		actionErr = h.c.AlertStore.Escalate(ctx, alertID)
-	case "close":
-		actionErr = h.c.AlertStore.UpdateStatus(ctx, alertID, alert.StatusClosed)
-	}
-	if actionErr != nil {
-		return err
-	}
-
-	a, err := h.c.AlertStore.FindOne(ctx, alertID)
-	if err != nil {
-		return err
-	}
-	msgOpt := CraftAlertMessage(*a, cfg.CallbackURL("/alerts/"+alertIDStr), payload.ResponseURL)
-
-	// update original message in Slack
-	_, _, err = api.PostMessageContext(ctx, payload.Channel.ID, msgOpt...)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-	}
-
-	return nil
-}
-
 // ServeActionCallback processes POST requests from Slack. A callback ID is provided
 // to determine which action to take.
 func (h *Handler) ServeActionCallback(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Action received")
 	err := validRequest(w, req)
 	if err != nil {
-		httpErr(w, err)
+		errutil.HTTPError(req.Context(), w, err)
 	}
 
 	var payload slack.InteractionCallback
 	err = json.Unmarshal([]byte(req.FormValue("payload")), &payload)
 	if err != nil {
-		httpErr(w, err)
+		errutil.HTTPError(req.Context(), w, err)
 	}
 
-	process := func(ctx context.Context) {
-		cfg := config.FromContext(ctx)
-		var api = slack.New(cfg.Slack.AccessToken)
+	ctx := permission.UserSourceContext(req.Context(), payload.User.ID, permission.RoleUser, &permission.SourceInfo{
+		Type: permission.SourceTypeNotificationCallback,
+		ID:   "slack:" + payload.Team.ID + ":" + payload.User.ID,
+	})
+	cfg := config.FromContext(ctx)
+	var api = slack.New(cfg.Slack.AccessToken)
 
-		// actions may come in batches, range over
-		for _, action := range payload.ActionCallback.BlockActions {
-			if action.ActionID == "openLink" {
-				return
-			}
+	// actions may come in batches, range over
+	for _, action := range payload.ActionCallback.BlockActions {
+		if action.ActionID == "openLink" {
+			return
+		}
 
-			// check if user has linked Slack with their GoAlert account
-			userID, err := h.c.AuthHandler.FindUserIDForAuthSubject(ctx, "slack:"+payload.Team.ID, payload.User.ID)
+		// check if user has linked Slack with their GoAlert account
+		userID, err := h.c.AuthHandler.FindUserIDForAuthSubject(ctx, "slack:"+payload.Team.ID, payload.User.ID)
+		if err != nil {
+			errutil.HTTPError(ctx, w, err)
+		}
+
+		// send Unauthorized message if user is not linked
+		if userID == "" {
+			_, err := api.PostEphemeralContext(ctx, payload.Channel.ID, payload.User.ID, needsAuthMsgOpt())
 			if err != nil {
-				fmt.Println("error finding user")
-				httpErr(w, err)
+				errutil.HTTPError(ctx, w, err)
 			}
+			return
+		}
 
-			// send Unauthorized message if user is not linked
-			if userID == "" {
-				_, err := api.PostEphemeralContext(ctx, payload.Channel.ID, payload.User.ID, needsAuthMsgOpt())
-				if err != nil {
-					fmt.Println("error posting ephemeral auth msg")
-					httpErr(w, err)
-				}
-				return
-			}
+		alertIDStr := action.Value
 
-			alertIDStr := action.Value
-			err = h.processAction(ctx, w, payload, action, alertIDStr)
-			if err != nil {
-				fmt.Println("error processing action")
-				httpErr(w, err)
-			}
+		// add source info to ctx to enable writing the action to alert log
+		ncID, _, err := h.c.AlertLogStore.FindNCByValue(ctx, nil, payload.Channel.ID)
+		if err != nil {
+			errutil.HTTPError(ctx, w, err)
+		}
+		ctx = permission.UserSourceContext(ctx, payload.User.ID, permission.RoleUser, &permission.SourceInfo{
+			Type: permission.SourceTypeNotificationChannel,
+			ID:   ncID,
+		})
+
+		// handle button clicked within Slack
+		alertID, err := strconv.Atoi(alertIDStr)
+		if err != nil {
+			errutil.HTTPError(ctx, w, err)
+		}
+
+		var actionErr error
+		switch action.ActionID {
+		case "ack":
+			actionErr = h.c.AlertStore.UpdateStatus(ctx, alertID, alert.StatusActive)
+		case "esc":
+			actionErr = h.c.AlertStore.Escalate(ctx, alertID)
+		case "close":
+			actionErr = h.c.AlertStore.UpdateStatus(ctx, alertID, alert.StatusClosed)
+		}
+		if actionErr != nil {
+			errutil.HTTPError(ctx, w, err)
+		}
+
+		a, err := h.c.AlertStore.FindOne(ctx, alertID)
+		if err != nil {
+			errutil.HTTPError(ctx, w, err)
+		}
+		msgOpt := CraftAlertMessage(*a, cfg.CallbackURL("/alerts/"+alertIDStr), payload.ResponseURL)
+
+		// update original message in Slack
+		_, _, err = api.PostMessageContext(ctx, payload.Channel.ID, msgOpt...)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		}
 	}
-
-	permission.SudoContext(req.Context(), process)
 }
