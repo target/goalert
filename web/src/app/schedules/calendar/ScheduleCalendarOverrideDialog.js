04fd13c0af4364c80b3bd8c3d35523fb7e7240e6
Spencer Pauly <spencerpauly@gmail.com>
2021-09-07 16:21:45 -0500

    Refactor step form into scheduleCalendarOverrideDialog + add functionality to handle edge cases

--- web/src/app/schedules/calendar/ScheduleCalendarOverrideDialog.js
+++ web/src/app/schedules/calendar/ScheduleCalendarOverrideDialog.js
@@ -1,13 +1,23 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import p from 'prop-types'
 import FormDialog from '../../dialogs/FormDialog'
 import { DateTime } from 'luxon'
-import { fieldErrors, nonFieldErrors } from '../../util/errutil'
+import {
+  fieldErrors as getFieldErrors,
+  nonFieldErrors,
+} from '../../util/errutil'
 import useOverrideNotices from '../useOverrideNotices'
-import { ScheduleCalendarOverrideForm } from './ScheduleCalendarOverrideForm'
 import { ScheduleCalendarContext } from '../ScheduleDetails'
 import { variantDetails } from '../ScheduleOverrideCreateDialog'
+import ScheduleOverrideForm from '../ScheduleOverrideForm'
+import {
+  FormControlLabel,
+  FormHelperText,
+  Grid,
+  Radio,
+  RadioGroup,
+} from '@material-ui/core'
 
 const mutation = gql`
   mutation ($input: CreateUserOverrideInput!) {
@@ -16,18 +26,20 @@ const mutation = gql`
     }
   }
 `
-
 export default function ScheduleCalendarOverrideDialog(props) {
   const { variantOptions = ['replace', 'remove', 'add', 'temp'] } = props
-  const [step, setStep] = useState(0)
-  const [value, setValue] = useState({
+
+  const initialValue = {
     addUserID: '',
     removeUserID: '',
     start: DateTime.local().startOf('hour').toISO(),
     end: DateTime.local().startOf('hour').plus({ hours: 8 }).toISO(),
     ...props.defaultValue,
-  })
+  }
 
+  const [step, setStep] = useState(0)
+  const [value, setValue] = useState(initialValue)
+  const [fieldErrors, setFieldErrors] = useState([])
   const [activeVariant, setActiveVariant] = useState(variantOptions[0])
 
   const { onNewTempSched } = useContext(ScheduleCalendarContext)
@@ -44,21 +56,19 @@ export default function ScheduleCalendarOverrideDialog(props) {
     onCompleted: props.onClose,
   })
 
+  useEffect(() => {
+    setFieldErrors(getFieldErrors(error))
+  }, [error])
+
   const onNext = () => {
     if (activeVariant === 'temp') {
       onNewTempSched()
       props.onClose()
     } else {
+      setValue(initialValue)
+      setFieldErrors([])
       setStep(step + 1)
     }
-  }
-
-  const handleChange = (newValue) => {
-    setValue(newValue)
-  }
-
-  const handleVariantChange = (newVariant) => {
-    setActiveVariant(newVariant)
   }
 
   return (
@@ -75,21 +85,53 @@ export default function ScheduleCalendarOverrideDialog(props) {
           : variantDetails[activeVariant].desc
       }
       errors={nonFieldErrors(error)}
-      notices={notices} // create and edit dialogue
+      notices={step === 0 ? [] : notices} // create and edit dialogue
       loading={loading}
       form={
-        <ScheduleCalendarOverrideForm
-          scheduleID={props.scheduleID}
-          activeStep={step}
-          value={value}
-          onChange={handleChange}
-          onVariantChange={handleVariantChange}
-          activeVariant={activeVariant}
-          disabled={loading}
-          errors={fieldErrors(error)}
-          variantOptions={variantOptions}
-          removeUserReadOnly={props.removeUserReadOnly}
-        />
+        <React.Fragment>
+          {/* Step 0: Choose override variant page */}
+          {step === 0 && (
+            <Grid item xs={12}>
+              <RadioGroup
+                required
+                aria-label='Choose an override action'
+                name='variant'
+                onChange={(e) => setActiveVariant(e.target.value)}
+                value={activeVariant}
+              >
+                {variantOptions.map((variant) => (
+                  <FormControlLabel
+                    key={variant}
+                    data-cy={`variant.${variant}`}
+                    value={variant}
+                    control={<Radio />}
+                    label={
+                      <React.Fragment>
+                        {variantDetails[variant].name}
+                        <FormHelperText>
+                          {variantDetails[variant].helperText}
+                        </FormHelperText>
+                      </React.Fragment>
+                    }
+                  />
+                ))}
+              </RadioGroup>
+            </Grid>
+          )}
+          {/* Step 1: Schedule override based on variant selected */}
+          {step === 1 && (
+            <ScheduleOverrideForm
+              add={activeVariant !== 'remove'}
+              remove={activeVariant !== 'add'}
+              scheduleID={props.scheduleID}
+              disabled={loading}
+              errors={fieldErrors}
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+              removeUserReadOnly={props.removeUserReadOnly}
+            />
+          )}
+        </React.Fragment>
       }
       onSubmit={() => mutate()}
       onNext={step < 1 ? onNext : null}
