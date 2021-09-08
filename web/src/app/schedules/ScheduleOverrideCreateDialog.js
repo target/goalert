26070e51f0c748867abad222ae6db13906b774e8
Spencer Pauly <spencerpauly@gmail.com>
2021-09-08 13:28:48 -0500

    Change copy text for variant dialogs
    Co-authored-by: David Talbot <david.talbot@target.com>

--- web/src/app/schedules/ScheduleOverrideCreateDialog.js
+++ web/src/app/schedules/ScheduleOverrideCreateDialog.js
@@ -10,6 +10,39 @@ import useOverrideNotices from './useOverrideNotices'
 export const variantDetails = {
   add: {
     title: 'Temporarily Add User',
+    desc: 'Create a new shift for the selected user. Existing shifts will remain unaffected.',
+    name: 'Additional Coverage',
+    helperText: 'Add an additional on-call user for a given time frame.',
+  },
+  remove: {
+    title: 'Temporarily Remove User',
+    desc: 'Remove (or split/shorten) shifts belonging to the selected user during the specified time frame.',
+    name: 'Remove Coverage',
+    helperText: "Remove one user's shifts during a given time frame.",
+  },
+  replace: {
+    title: 'Temporarily Replace User',
+    desc: 'Select one user to take over the shifts of another user during the specified time frame.',
+    name: "Cover Someone's Shifts",
+    helperText:
+      'Have one user take over the shifts of another user during a given time frame.',
+  },
+  temp: {
+    title: 'Create Temporary Schedule',
+    desc: 'Replace the entire schedule for a given period of time.',
+    name: 'Temporary Schedule',
+    helperText:
+      'Define a fixed shift-by-shift schedule to use for a given time frame.',
+  },
+  choose: {
+    title: 'Choose Override Action',
+    desc: 'Select the type of override you would like to apply to this schedule.',
+    name: 'Choose',
+    helperText: 'Determine which override action you want to take.',
+  },
+}
+  add: {
+    title: 'Temporarily Add User',
     desc: 'This will add a new shift for the selected user while the override is active. Existing shifts will remain unaffected.',
     name: 'Additional Coverage',
     helperText: 'Add an additional on-call user for a specified time.',
