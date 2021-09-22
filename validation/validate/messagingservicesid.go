66654ec61d3a26e114178f2cc6abe5ee44d90262
KatieMSB <katriannasb@gmail.com>
2021-09-22 13:59:07 -0500

    update validation

--- validation/validate/messagingservicesid.go
+++ validation/validate/messagingservicesid.go
@@ -6,9 +6,8 @@ import (
 	"github.com/target/goalert/validation"
 )
 
-// TwilioMessageSID will validate an Message SID, returning a FieldError if invalid.
-func TwilioMessageSID(fname, value string) error {
-
+// MessagingServiceSID will validate an Messaging Service SID, returning a FieldError if invalid.
+func MessagingServiceSID(fname, value string) error {
 	if !strings.HasPrefix(value, "MG") {
 		return validation.NewFieldError(fname, "must begin with MG")
 	}
--- validation/validate/twiliofromvalue.go
+++ validation/validate/twiliofromvalue.go
@@ -7,9 +7,10 @@ import (
 // TwlioFromValue will validate a from value as either a phone number, or messenger SID
 func TwilioFromValue(fname, value string) error {
 	phoneErr := Phone(fname, value)
-	sidErr := TwilioMessageSID(fname, value)
+	sidErr := MessagingServiceSID(fname, value)
+	asciiErr := ASCII(fname, value, 2, 64)
 
-	if phoneErr != nil && sidErr != nil {
+	if phoneErr != nil && sidErr != nil && asciiErr != nil {
 		return validation.NewFieldError("From", "is not a valid phone number or alphanumeric sender ID.")
 	}
 
