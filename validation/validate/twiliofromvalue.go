d299309f30ef3d3ed0f1ac08ad73b178150c0564
Nathaniel Caza <mastercactapus@gmail.com>
2021-09-22 14:27:04 -0500

    use single validation func

--- validation/validate/twiliofromvalue.go
+++ validation/validate/twiliofromvalue.go
@@ -1,18 +1,19 @@
 package validate
 
 import (
+	"strings"
+
 	"github.com/target/goalert/validation"
 )
 
 // TwlioFromValue will validate a from value as either a phone number, or messenger SID
 func TwilioFromValue(fname, value string) error {
-	phoneErr := Phone(fname, value)
-	sidErr := MessagingServiceSID(fname, value)
-	asciiErr := ASCII(fname, value, 2, 64)
-
-	if phoneErr != nil && sidErr != nil && asciiErr != nil {
-		return validation.NewFieldError("From", "is not a valid phone number or alphanumeric sender ID.")
+	switch {
+	case strings.HasPrefix(value, "+"):
+		return Phone(fname, value)
+	case strings.HasPrefix(value, "MG"):
+		return ASCII(fname, value, 2, 64)
 	}
 
-	return nil
+	return validation.NewFieldError(fname, "must be a valid phone number or alphanumeric sender ID")
 }
