9a9689a7a92d8a60bbf8c7218d6d936c93de253c
dctalbot <david.talbot@target.com>
2021-09-30 10:24:26 -0500

    set height 100%

--- web/src/app/schedules/temp-sched/TempSchedDialog.tsx
+++ web/src/app/schedules/temp-sched/TempSchedDialog.tsx
@@ -156,6 +156,7 @@ export default function TempSchedDialog({
 
   return (
     <FormDialog
+      fullHeight
       maxWidth='lg'
       title='Define a Temporary Schedule'
       onClose={onClose}
