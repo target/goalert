93092463851093e293e58da034972c8f8c7974a7
dctalbot <david.talbot@target.com>
2021-11-17 23:40:26 -0600

    install and configure babel-plugin-import

--- web/src/babel.config.js
+++ web/src/babel.config.js
@@ -6,6 +6,24 @@ const plugins = [
   ['@babel/plugin-proposal-class-properties', { loose: true }],
   ['@babel/plugin-proposal-private-methods', { loose: true }],
   ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/core',
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false,
+    },
+    'core',
+  ],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/icons',
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false,
+    },
+    'icons',
+  ],
 ]
 if (process.env.NODE_ENV !== 'production') {
   plugins.push('babel-plugin-typescript-to-proptypes')
