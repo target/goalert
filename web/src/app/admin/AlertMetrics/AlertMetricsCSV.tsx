a0ca74edc42da34729cffd055e9610e6fa6fb2cb
dctalbot <david.talbot@target.com>
2022-01-06 12:19:47 -0600

    fix styles

--- web/src/app/admin/AlertMetrics/AlertMetricsCSV.tsx
+++ web/src/app/admin/AlertMetrics/AlertMetricsCSV.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Typography } from '@mui/material'
 import DownloadIcon from '@mui/icons-material/Download'
+import makeStyles from '@mui/styles/makeStyles'
 import { CSVLink } from 'react-csv'
 import { Alert } from '../../../schema'
 
@@ -8,9 +9,21 @@ interface AlertMetricsCSVProps {
   alerts: Alert[]
 }
 
+const useStyles = makeStyles({
+  paragraph: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    margin: '1rem',
+  },
+  anchor: {
+    display: 'flex',
+  },
+})
+
 export default function AlertMetricsCSV(
   props: AlertMetricsCSVProps,
 ): JSX.Element {
+  const classes = useStyles()
   // Note: the data object is ordered
   const data = props.alerts.map((a) => ({
     createdAt: a.createdAt,
@@ -25,15 +38,11 @@ export default function AlertMetricsCSV(
   return (
     <Grid container>
       <Grid item xs={12}>
-        <Typography>
+        <Typography className={classes.paragraph}>
           <CSVLink
             data={data}
             filename='GoAlert_alert_metrics.csv'
-            style={{
-              display: 'flex',
-              margin: '1rem',
-              justifyContent: 'flex-end',
-            }}
+            className={classes.anchor}
           >
             <React.Fragment>
               <DownloadIcon /> Export to CSV
