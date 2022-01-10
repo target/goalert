004c0fe573a70f49dfd34a8e01f177aa48cd891b
Nathaniel Caza <mastercactapus@gmail.com>
2022-01-10 14:18:12 -0600

    add support for v8 payloads

--- grafana/grafana.go
+++ grafana/grafana.go
@@ -1,9 +1,13 @@
 package grafana
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/pkg/errors"
@@ -16,14 +20,22 @@ import (
 	"github.com/target/goalert/validation/validate"
 )
 
-type grafanaPost struct {
-	RuleName string
-	RuleID   int
-	Message  string
-	State    string
-	Title    string
-	RuleURL  string
-}
+var detailsTmpl = template.Must(template.New("details").Parse(`
+Labels:
+{{- range $k, $v := .Labels }}
+- {{ $k }}: {{ $v }}
+{{- end }}
+
+Annotations:
+{{- range $k, $v := .Annotations }}
+- {{ $k }}: {{ $v }}
+{{- end }}
+
+Source: {{ .GeneratorURL }}
+
+Silence: {{ .SlienceURL }}
+
+Value: ` + "```\n{{ .ValueString }}\n```"))
 
 func clientError(w http.ResponseWriter, code int, err error) bool {
 	if err == nil {
@@ -32,6 +44,97 @@ func clientError(w http.ResponseWriter, code int, err error) bool {
 
 	http.Error(w, http.StatusText(code), code)
 	return true
+}
+
+func alertsFromLegacy(ctx context.Context, req *http.Request, serviceID string, data []byte) ([]alert.Alert, error) {
+	var g struct {
+		RuleName string
+		RuleID   int
+		Message  string
+		State    string
+		Title    string
+		RuleURL  string
+	}
+	err := json.Unmarshal(data, &g)
+	if err != nil {
+		return nil, err
+	}
+
+	var grafanaState alert.Status
+	switch g.State {
+	case "alerting":
+		grafanaState = alert.StatusTriggered
+	case "ok":
+		grafanaState = alert.StatusClosed
+	case "no_data":
+		// no data..
+		return nil, nil
+	default:
+		return nil, errors.Errorf("grafana: unknown state: %s", g.State)
+	}
+
+	var urlStr string
+	if validate.AbsoluteURL("RuleURL", g.RuleURL) == nil {
+		urlStr = g.RuleURL
+	}
+	body := strings.TrimSpace(urlStr + "\n\n" + g.Message)
+
+	//dedupe is description, source, and serviceID
+	return []alert.Alert{{
+		Summary:   validate.SanitizeText(g.RuleName, alert.MaxSummaryLength),
+		Details:   validate.SanitizeText(body, alert.MaxDetailsLength),
+		Status:    grafanaState,
+		ServiceID: serviceID,
+		Source:    alert.SourceGrafana,
+		Dedup:     alert.NewUserDedup(req.FormValue("dedup")),
+	}}, nil
+}
+
+func alertsFromV1(ctx context.Context, serviceID string, data []byte) ([]alert.Alert, error) {
+	var g struct {
+		Alerts []struct {
+			Status              string
+			Labels, Annotations map[string]string
+			ValueString         string
+			Fingerprint         string
+			GeneratorURL        string
+			SlienceURL          string
+		}
+	}
+	err := json.Unmarshal(data, &g)
+	if err != nil {
+		return nil, err
+	}
+
+	var alerts []alert.Alert
+	for _, a := range g.Alerts {
+		var alertStatus alert.Status
+		switch a.Status {
+		case "firing":
+			alertStatus = alert.StatusTriggered
+		case "resolved":
+			alertStatus = alert.StatusClosed
+		default:
+			return nil, errors.Errorf("grafana: unknown status: %s", a.Status)
+		}
+
+		var buf strings.Builder
+		err := detailsTmpl.Execute(&buf, a)
+		if err != nil {
+			return nil, err
+		}
+
+		alerts = append(alerts, alert.Alert{
+			Summary:   validate.SanitizeText(a.Labels["alertname"], alert.MaxSummaryLength),
+			Details:   validate.SanitizeText(buf.String(), alert.MaxDetailsLength),
+			Status:    alertStatus,
+			ServiceID: serviceID,
+			Source:    alert.SourceGrafana,
+			Dedup:     alert.NewUserDedup(a.Fingerprint),
+		})
+	}
+
+	return alerts, nil
 }
 
 func GrafanaToEventsAPI(aDB alert.Store, intDB integrationkey.Store) http.HandlerFunc {
@@ -46,58 +149,59 @@ func GrafanaToEventsAPI(aDB alert.Store, intDB integrationkey.Store) http.Handle
 		}
 		serviceID := permission.ServiceID(ctx)
 
-		var g grafanaPost
-		err = json.NewDecoder(r.Body).Decode(&g)
+		data, err := ioutil.ReadAll(r.Body)
+		if errutil.HTTPError(ctx, w, err) {
+			return
+		}
+
+		var versionInfo struct{ Version string }
+		err = json.Unmarshal(data, &versionInfo)
+		if clientError(w, http.StatusBadRequest, err) {
+			return
+		}
+
+		var alerts []alert.Alert
+		switch versionInfo.Version {
+		case "1":
+			alerts, err = alertsFromV1(ctx, serviceID, data)
+		case "":
+			alerts, err = alertsFromLegacy(ctx, r, serviceID, data)
+		default:
+			clientError(w, http.StatusBadRequest, errors.Errorf("grafana: unknown payload version: %s", versionInfo.Version))
+			return
+		}
+
 		if clientError(w, http.StatusBadRequest, err) {
 			log.Logf(ctx, "bad request from grafana: %v", err)
 			return
 		}
-
-		ctx = log.WithFields(ctx, log.Fields{
-			"RuleURL": g.RuleURL,
-			"State":   g.State,
-		})
-
-		var grafanaState alert.Status
-		switch g.State {
-		case "alerting":
-			grafanaState = alert.StatusTriggered
-		case "ok":
-			grafanaState = alert.StatusClosed
-		case "no_data":
-			// no data..
-			return
-		default:
-			log.Logf(ctx, "bad request from grafana: missing or invalid state")
-			http.Error(w, "invalid state", http.StatusBadRequest)
+		if len(alerts) == 0 {
+			// no data
 			return
 		}
-
-		var urlStr string
-		if validate.AbsoluteURL("RuleURL", g.RuleURL) == nil {
-			urlStr = g.RuleURL
-		}
-		body := strings.TrimSpace(urlStr + "\n\n" + g.Message)
-
-		//dedupe is description, source, and serviceID
-		msg := &alert.Alert{
-			Summary:   validate.SanitizeText(g.RuleName, alert.MaxSummaryLength),
-			Details:   validate.SanitizeText(body, alert.MaxDetailsLength),
-			Status:    grafanaState,
-			Source:    alert.SourceGrafana,
-			ServiceID: serviceID,
-			Dedup:     alert.NewUserDedup(r.FormValue("dedup")),
+		if len(alerts) > 10 {
+			log.Log(ctx, fmt.Errorf("grafana: too many alerts (truncating to 10): %d", len(alerts)))
+			alerts = alerts[:10]
 		}
 
-		err = retry.DoTemporaryError(func(int) error {
-			_, err = aDB.CreateOrUpdate(ctx, msg)
-			return err
-		},
-			retry.Log(ctx),
-			retry.Limit(10),
-			retry.FibBackoff(time.Second),
-		)
-		if errutil.HTTPError(ctx, w, errors.Wrap(err, "create or update alert for grafana")) {
+		var hasFailures bool
+		for _, a := range alerts {
+			err = retry.DoTemporaryError(func(int) error {
+				_, err = aDB.CreateOrUpdate(ctx, &a)
+				return err
+			},
+				retry.Log(ctx),
+				retry.Limit(10),
+				retry.FibBackoff(time.Second),
+			)
+			if err != nil {
+				log.Log(ctx, fmt.Errorf("grafana: create alert: %w", err))
+				hasFailures = true
+			}
+		}
+
+		if hasFailures {
+			http.Error(w, "failed to create alerts", http.StatusInternalServerError)
 			return
 		}
 	}
