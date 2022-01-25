a9e4cfae74c5e97a1b62530c2645381efde51d2c
dctalbot <david.talbot@target.com>
2022-01-25 13:45:22 -0600

    refactor

--- graphql2/graphqlapp/alert.go
+++ graphql2/graphqlapp/alert.go
@@ -194,12 +194,11 @@ func splitRangeByDuration(since, until time.Time, dur timeutil.ISODuration, aler
 	countAlertsUntil := func(ts time.Time) int {
 		var count int
 		for len(alerts) > 0 {
-			if alerts[0].CreatedAt.Before(ts) {
-				count++
-			} else {
+			if !alerts[0].CreatedAt.Before(ts) {
 				break
 			}
 
+			count++
 			alerts = alerts[1:]
 		}
 		return count
