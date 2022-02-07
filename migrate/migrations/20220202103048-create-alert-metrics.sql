07cae19c3c5b60536d80ba4221df227fa81c18a4
dctalbot <david.talbot@target.com>
2022-02-07 15:31:02 -0600

    add state column to engine_processing_versions

--- migrate/migrations/20220202103048-create-alert-metrics.sql
+++ migrate/migrations/20220202103048-create-alert-metrics.sql
@@ -13,8 +13,11 @@ CREATE INDEX idx_closed_events ON alert_logs (timestamp) WHERE event = 'closed';
 ALTER TYPE engine_processing_type ADD VALUE IF NOT EXISTS 'metrics';
 INSERT INTO engine_processing_versions (type_id) VALUES ('metrics');
 
+ALTER TABLE engine_processing_versions ADD COLUMN state JSONB NOT NULL DEFAULT '{}'::jsonb;
+
 
 -- +migrate Down
+ALTER TABLE engine_processing_versions DROP COLUMN state;
 DELETE FROM engine_processing_versions WHERE type_id = 'metrics';
 DROP INDEX idx_closed_events;
 DROP TABLE alert_metrics;
