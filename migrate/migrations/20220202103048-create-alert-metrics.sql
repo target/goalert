6b8c132bdd1a3b70aab73d9441ea8bad49d7772a
dctalbot <david.talbot@target.com>
2022-02-07 14:19:12 -0600

    remove closed_at, make tta nullable

--- migrate/migrations/20220202103048-create-alert-metrics.sql
+++ migrate/migrations/20220202103048-create-alert-metrics.sql
@@ -3,8 +3,7 @@
 CREATE TABLE alert_metrics (
     alert_id INT PRIMARY KEY REFERENCES alerts (id) ON DELETE CASCADE,
     service_id UUID NOT NULL REFERENCES services (id) ON DELETE CASCADE,
-    closed_at TIMESTAMPTZ NOT NULL,
-    time_to_ack INTERVAL NOT NULL,
+    time_to_ack INTERVAL,
     time_to_close INTERVAL NOT NULL,
     escalated BOOLEAN DEFAULT FALSE NOT NULL
 );
