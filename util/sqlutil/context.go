96668a1c19fec63450e14b97a653f13ae829947a
Nathaniel Caza <mastercactapus@gmail.com>
2022-02-14 15:21:30 -0600

    comments

--- util/sqlutil/context.go
+++ util/sqlutil/context.go
@@ -12,11 +12,15 @@ const (
 	ctxKeyDB = ctxKey("sqlutil")
 )
 
+// FromContext will return the DB object from the given context.
 func FromContext(ctx context.Context) *gorm.DB { return ctx.Value(ctxKeyDB).(*gorm.DB) }
+
+// Context will return a new context with the given DB object.
 func Context(ctx context.Context, db *gorm.DB) context.Context {
 	return context.WithValue(ctx, ctxKeyDB, db)
 }
 
+// Transaction will run the given function in a transaction handling commit/rollback and sub-transactions.
 func Transaction(ctx context.Context, fn func(context.Context, *gorm.DB) error) error {
 	db := FromContext(ctx)
 	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
