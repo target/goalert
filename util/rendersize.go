69410f1755b7d2fe37a6b2657a58175cd6979195
Nathaniel Caza <mastercactapus@gmail.com>
2022-02-17 16:09:19 -0600

    use strings directly

--- util/rendersize.go
+++ util/rendersize.go
@@ -1,21 +1,67 @@
 package util
 
-import "sort"
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+// ErrNoSolution is returned by RenderSize when it can't find a solution.
+var ErrNoSolution = fmt.Errorf("no solution")
 
 // RenderSize will return the rendered result as close as possible to the max size.
-func RenderSize(maxSize int, renderFunc func(n int) string) string {
-	result := renderFunc(maxSize)
+func RenderSize(maxSize int, input string, renderFunc func(string) (string, error)) (string, error) {
+	result, err := renderFunc(input)
+	if err != nil {
+		return "", err
+	}
 	if len(result) <= maxSize {
-		return result
+		return result, nil
 	}
 
-	newSize := sort.Search(maxSize, func(max int) bool {
-		return len(renderFunc(max)) > maxSize
+	newSize := sort.Search(len(input), func(n int) bool {
+		if err != nil {
+			return true
+		}
+
+		result, err = renderFunc(input[:n])
+		return len(result) > maxSize
 	})
-	if newSize == maxSize {
-		return ""
+	if err != nil {
+		return "", err
+	}
+	if newSize == 0 {
+		return "", fmt.Errorf("failed to render string to size %d: %w", maxSize, ErrNoSolution)
 	}
 
 	// newSize is the first size that is too large
-	return renderFunc(newSize - 1)
+	return renderFunc(input[:newSize-1])
+}
+
+// RenderSizeN works like RenderSize but accepts multiple inputs.
+//
+// Inputs are trimmed one by one starting with the fist element.
+func RenderSizeN(maxSize int, inputs []string, renderFunc func([]string) (string, error)) (result string, err error) {
+	if len(inputs) == 0 {
+		return RenderSize(maxSize, "", func(string) (string, error) {
+			return renderFunc(nil)
+		})
+	}
+
+	for i := range inputs {
+		result, err = RenderSize(maxSize, inputs[i], func(inputN string) (string, error) {
+			inputs[i] = inputN
+			return renderFunc(inputs)
+		})
+		if errors.Is(err, ErrNoSolution) {
+			inputs[i] = ""
+			continue
+		}
+		if err != nil {
+			return "", err
+		}
+		return result, err
+	}
+
+	return "", ErrNoSolution
 }
