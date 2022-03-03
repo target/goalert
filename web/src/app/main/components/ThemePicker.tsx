dcc617b426a66ba5a1ca2b5d4fe5fa51f5b84f84
Nathaniel Cook <nathanieljcook@outlook.com>
2022-03-03 14:06:35 -0800

    move settings buttons to popover

--- web/src/app/main/components/ThemePicker.tsx
+++ web/src/app/main/components/ThemePicker.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-import makeStyles from '@mui/styles/makeStyles'
 import ToggleButton from '@mui/material/ToggleButton'
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import Typography from '@mui/material/Typography'
@@ -8,26 +7,12 @@ import SystemIcon from '@mui/icons-material/SettingsBrightness'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import { ThemeContext } from '../themeConfig'
 
-const useStyles = makeStyles(() => ({
-  container: {
-    paddingTop: '1rem',
-  },
-  icon: {
-    color: 'white',
-  },
-}))
-
 export default function ThemePicker(): JSX.Element {
-  const classes = useStyles()
   const { themeMode, setThemeMode } = useContext(ThemeContext)
 
   return (
-    <div className={classes.container}>
-      <Typography
-        variant='body1'
-        color='textSecondary'
-        sx={{ paddingBottom: '0.5rem' }}
-      >
+    <div>
+      <Typography variant='body1' sx={{ paddingBottom: '0.5rem' }}>
         Theme Mode
       </Typography>
       <ToggleButtonGroup value={themeMode}>
--- web/src/app/main/components/UserSettingsPopover.tsx
+++ web/src/app/main/components/UserSettingsPopover.tsx
@@ -1,24 +1,95 @@
 import React from 'react'
+import Button from '@mui/material/Button'
+import ButtonBase from '@mui/material/ButtonBase'
+import Grid from '@mui/material/Grid'
+import Popover from '@mui/material/Popover'
+import Typography from '@mui/material/Typography'
+import { OpenInNew, Logout } from '@mui/icons-material'
+import { useDispatch } from 'react-redux'
+import { authLogout } from '../../actions'
+import ThemePicker from './ThemePicker'
 import { CurrentUserAvatar } from '../../util/avatars'
-// import { useSessionInfo } from '../../util/RequireConfig'
+import AppLink from '../../util/AppLink'
+import { useConfigValue, useSessionInfo } from '../../util/RequireConfig'
 
 export default function UserSettingsPopover(): JSX.Element {
-  //   const { userID } = useSessionInfo()
-  //   const dispatch = useDispatch()
-  //   const logout = () => dispatch(authLogout(true))
+  const [feedbackEnabled] = useConfigValue('Feedback.Enable')
+  const { userName } = useSessionInfo()
+  const firstName = userName?.split(' ')[0]
+  const dispatch = useDispatch()
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const logout = () => dispatch(authLogout(true))
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = (): void => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
 
   return (
     <React.Fragment>
-      <CurrentUserAvatar />
-      {/* <Config>
-        {(cfg) =>
-          cfg['Feedback.Enable'] &&
-          renderFeedback(
-            cfg['Feedback.OverrideURL'] ||
-              'https://www.surveygizmo.com/s3/4106900/GoAlert-Feedback',
-          )
-        }
-      </Config> */}
+      <ButtonBase onClick={handleClick}>
+        <CurrentUserAvatar />
+      </ButtonBase>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        PaperProps={{ sx: { p: 2 } }}
+      >
+        <Grid container direction='column' spacing={2}>
+          <Grid item>
+            <Typography variant='h5' component='span'>
+              Hello, {firstName}!
+            </Typography>
+          </Grid>
+          <Grid item>
+            <AppLink to='/profile' style={{ textDecoration: 'none' }}>
+              <Button variant='outlined'>Manage Profile</Button>
+            </AppLink>
+          </Grid>
+          <Grid item>
+            <ThemePicker />
+          </Grid>
+          <Grid item>
+            {feedbackEnabled && (
+              <AppLink
+                newTab
+                to='https://www.surveygizmo.com/s3/4106900/GoAlert-Feedback'
+                style={{ textDecoration: 'none' }}
+              >
+                <Button variant='outlined' endIcon={<OpenInNew />}>
+                  Feedback
+                </Button>
+              </AppLink>
+            )}
+          </Grid>
+          <Grid item>
+            <Button
+              variant='text'
+              startIcon={<Logout />}
+              onClick={(e) => {
+                e.preventDefault()
+                logout()
+              }}
+              sx={{ color: (theme) => theme.palette.error.main }}
+            >
+              Logout
+            </Button>
+          </Grid>
+        </Grid>
+      </Popover>
+
       {/* {renderSidebarLink(LogoutIcon, '/api/v2/identity/logout', 'Logout', {
         onClick: (e) => {
           e.preventDefault()
