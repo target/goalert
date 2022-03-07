7b4e755f489c589792359c1abdbdd2e2f86b2030
dctalbot <david.talbot@target.com>
2022-03-07 16:02:09 -0600

    add drop index stmt

--- migrate/migrations/20220307103253-add-alert-metrics.sql
+++ migrate/migrations/20220307103253-add-alert-metrics.sql
@@ -15,6 +15,7 @@ INSERT INTO engine_processing_versions (type_id, version) VALUES ('metrics', 1);
 
 -- +migrate Down
 
-ALTER TABLE engine_processing_versions DROP COLUMN state;
 DELETE FROM engine_processing_versions WHERE type_id = 'metrics';
+ALTER TABLE engine_processing_versions DROP COLUMN state;
+DROP INDEX idx_closed_events;
 DROP TABLE alert_metrics;
