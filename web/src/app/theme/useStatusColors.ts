31a74370c37831207c3ff780d2cb8b8f6eddc3a8
dctalbot <david.talbot@target.com>
2022-03-09 09:38:49 -0600

    fix warning color

--- web/src/app/theme/useStatusColors.ts
+++ web/src/app/theme/useStatusColors.ts
@@ -1,4 +1,5 @@
 import { useTheme } from '@mui/material'
+import { yellow } from '@mui/material/colors'
 
 type StatusColors = {
   ok: string
@@ -10,7 +11,7 @@ function useStatusColors(): StatusColors {
   const theme = useTheme()
   return {
     ok: theme.palette.success.main,
-    warn: theme.palette.warning.main,
+    warn: yellow[600], // TODO if practical, use theme.palette.warning.main,
     err: theme.palette.error.main,
   }
 }
