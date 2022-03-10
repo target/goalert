bf7ce190653f358740a98bc785d3584ea3cc859a
dctalbot <david.talbot@target.com>
2022-03-10 13:42:31 -0600

    rm Grid

--- web/src/app/rotations/UserForm.js
+++ web/src/app/rotations/UserForm.js
@@ -2,26 +2,21 @@ import React from 'react'
 import { PropTypes as p } from 'prop-types'
 import { FormContainer, FormField } from '../forms'
 import { UserSelect } from '../selection'
-import { Grid } from '@mui/material'
 
 export default function UserForm(props) {
   return (
     <FormContainer {...props}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <FormField
-            component={UserSelect}
-            disabled={false}
-            fieldName='users'
-            fullWidth
-            label='Select User(s)'
-            multiple
-            name='users'
-            required
-            value={props.value.users}
-          />
-        </Grid>
-      </Grid>
+      <FormField
+        component={UserSelect}
+        disabled={false}
+        fieldName='users'
+        fullWidth
+        label='Select User(s)'
+        multiple
+        name='users'
+        required
+        value={props.value.users}
+      />
     </FormContainer>
   )
 }
