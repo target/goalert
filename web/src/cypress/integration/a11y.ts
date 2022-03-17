69af071955c0f31d21fae8eddcd0a7a0ca305f8a
Nathaniel Cook <nathanieljcook@outlook.com>
2022-03-17 15:14:02 -0700

    add a test to check a11y for each main route

--- web/src/cypress/integration/a11y.ts
+++ web/src/cypress/integration/a11y.ts
@@ -2,12 +2,69 @@ import { testScreen } from '../support'
 
 function testA11y(): void {
   describe('A11y Compatibility', () => {
-    beforeEach(() => {
-      cy.visit('alerts/?poll=0')
+    it('has no detectable a11y violations on alerts list', () => {
+      cy.visit('/alerts')
       cy.injectAxe()
+      cy.checkA11y()
     })
 
-    it('test page accessibility', () => {
+    it('has no detectable a11y violations on rotations list', () => {
+      cy.visit('/rotations')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on schedules list', () => {
+      cy.visit('/schedules')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on escalation policies list', () => {
+      cy.visit('/escalation-policies')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on services list', () => {
+      cy.visit('/services')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on users list', () => {
+      cy.visit('/users')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it.skip('has no detectable a11y violations on wizard', () => {
+      cy.visit('/wizard')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on wizard', () => {
+      cy.visit('/admin/config')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on wizard', () => {
+      cy.visit('/admin/limits')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on wizard', () => {
+      cy.visit('/admin/toolbox')
+      cy.injectAxe()
+      cy.checkA11y()
+    })
+
+    it('has no detectable a11y violations on wizard', () => {
+      cy.visit('/admin/message-logs')
+      cy.injectAxe()
       cy.checkA11y()
     })
   })
