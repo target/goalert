a7c2d5a9951537544cb2afb642143630ff3484ab
Nathaniel Cook <nathanieljcook@outlook.com>
2022-03-17 15:46:44 -0700

    update names

--- web/src/cypress/integration/a11y.ts
+++ web/src/cypress/integration/a11y.ts
@@ -1,85 +1,85 @@
 import { testScreen } from '../support'
 
 function testA11y(): void {
-  describe('A11y Compatibility', () => {
-    it('has no detectable a11y violations on alerts list', () => {
+  describe('no detectable a11y violations', () => {
+    it('alerts list', () => {
       cy.visit('/alerts')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on rotations list', () => {
+    it('rotations list', () => {
       cy.visit('/rotations')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on schedules list', () => {
+    it('schedules list', () => {
       cy.visit('/schedules')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on escalation policies list', () => {
+    it('escalation policies list', () => {
       cy.visit('/escalation-policies')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on services list', () => {
+    it('services list', () => {
       cy.visit('/services')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on users list', () => {
+    it('users list', () => {
       cy.visit('/users')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on profile', () => {
+    it('profile', () => {
       cy.visit('/admin/profile')
       cy.injectAxe()
       cy.checkA11y()
     })
 
     // todo: failing, fix a11y issue
-    it.skip('has no detectable a11y violations on wizard', () => {
+    it.skip('wizard', () => {
       cy.visit('/wizard')
       cy.injectAxe()
       cy.checkA11y()
     })
 
     // todo: failing, fix a11y issue
-    it.skip('has no detectable a11y violations on admin config', () => {
+    it.skip('admin config', () => {
       cy.visit('/admin/config')
       cy.injectAxe()
       cy.checkA11y()
     })
 
     // todo: failing, fix a11y issue
-    it.skip('has no detectable a11y violations on admin system limits', () => {
+    it.skip('admin system limits', () => {
       cy.visit('/admin/limits')
       cy.injectAxe()
       cy.checkA11y()
     })
 
     // todo: failing, fix a11y issue
-    it.skip('has no detectable a11y violations on admin toolbox', () => {
+    it.skip('admin toolbox', () => {
       cy.visit('/admin/toolbox')
       cy.injectAxe()
       cy.checkA11y()
     })
 
-    it('has no detectable a11y violations on admin message logs', () => {
+    it('admin message logs', () => {
       cy.visit('/admin/message-logs')
       cy.injectAxe()
       cy.checkA11y()
     })
 
     // todo: failing, fix a11y issue
-    it.skip('has no detectable a11y violations on api docs', () => {
+    it.skip('api docs', () => {
       cy.visit('/docs')
       cy.injectAxe()
       cy.checkA11y()
@@ -87,4 +87,4 @@ function testA11y(): void {
   })
 }
 
-testScreen('A11y', testA11y, false, true)
+testScreen('a11y', testA11y, false, true)
