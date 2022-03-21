cc332530a702294ea7f62d0690a04d12174f10a4
Nathaniel Cook <nathanieljcook@outlook.com>
2022-03-21 14:40:48 -0700

    add switchover action buttons

--- web/src/app/admin/switchover/AdminSwitchover.tsx
+++ web/src/app/admin/switchover/AdminSwitchover.tsx
@@ -1,51 +1,65 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import makeStyles from '@mui/styles/makeStyles'
-import { Theme } from '@mui/material/styles'
+import PingIcon from 'mdi-material-ui/SourceCommitStartNextLocal'
+import RestartIcon from '@mui/icons-material/Refresh'
+import ExecuteIcon from '@mui/icons-material/Start'
+import { gql, useMutation, useQuery } from '@apollo/client'
 
-const useStyles = makeStyles((theme: Theme) => ({
-  gridContainer: {
-    [theme.breakpoints.up('md')]: {
-      justifyContent: 'center',
-    },
-  },
-  groupTitle: {
-    fontSize: '1.1rem',
-  },
-  saveDisabled: {
-    color: 'rgba(255, 255, 255, 0.5)',
-  },
-}))
+const query = gql`
+  query {
+    SwitchOverState {
+      actions
+      status
+      nodes {
+        id
+        status
+      }
+    }
+  }
+`
+
+const mutation = gql`
+  mutation ($action: SwitchoverAction!) {
+    switchoverAction(action: $action)
+  }
+`
 
 export default function AdminSwitchover(): JSX.Element {
-  const classes = useStyles()
+  const queryRes = useQuery(query)
+  const [commit, mutationRes] = useMutation(mutation)
 
   return (
-    <Grid container spacing={2} className={classes.gridContainer}>
-      <Grid container item xs={12}>
-        <Grid item xs={12}>
-          <Typography
-            component='h2'
-            variant='subtitle1'
-            color='textSecondary'
-            classes={{ subtitle1: classes.groupTitle }}
-          >
-            Twilio Number Lookup
-          </Typography>
-        </Grid>
+    <Grid container spacing={2} justifyContent='space-between'>
+      <Grid item>
+        <Button
+          onClick={() => commit({ variables: { action: 'refresh' } })}
+          size='large'
+          variant='outlined'
+          startIcon={<PingIcon />}
+        >
+          Ping
+        </Button>
       </Grid>
-      <Grid container item xs={12}>
-        <Grid item xs={12}>
-          <Typography
-            component='h2'
-            variant='subtitle1'
-            color='textSecondary'
-            classes={{ subtitle1: classes.groupTitle }}
-          >
-            Send SMS
-          </Typography>
-        </Grid>
+      <Grid item>
+        <Button
+          onClick={() => commit({ variables: { action: 'reset' } })}
+          size='large'
+          variant='outlined'
+          startIcon={<RestartIcon />}
+        >
+          Reset
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          onClick={() => commit({ variables: { action: 'execute' } })}
+          size='large'
+          variant='outlined'
+          startIcon={<ExecuteIcon />}
+        >
+          Execute
+        </Button>
       </Grid>
     </Grid>
   )
