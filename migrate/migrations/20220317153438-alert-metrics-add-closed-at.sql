1e55ef3be00fa6d852b82444c1cbc9f310f93359
dctalbot <david.talbot@target.com>
2022-03-22 15:01:15 -0500

    enforce non-null closed_at, truncate on down

--- migrate/migrations/20220317153438-alert-metrics-add-closed-at.sql
+++ migrate/migrations/20220317153438-alert-metrics-add-closed-at.sql
@@ -4,7 +4,7 @@ UPDATE engine_processing_versions
 SET "version" = 2
 WHERE type_id = 'metrics';
 
-ALTER TABLE alert_metrics ADD COLUMN closed_at TIMESTAMP WITH TIME ZONE;
+ALTER TABLE alert_metrics ADD COLUMN closed_at TIMESTAMP WITH TIME ZONE NOT NULL;
 
 TRUNCATE alert_metrics;
 
@@ -16,3 +16,5 @@ ALTER TABLE alert_metrics DROP COLUMN closed_at;
 UPDATE engine_processing_versions
 SET "version" = 1
 WHERE type_id = 'metrics';
+
+TRUNCATE alert_metrics;
