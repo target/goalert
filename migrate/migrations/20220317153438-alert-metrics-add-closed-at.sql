cb6a963b365fcaf21b2a20a91eb4b923939f02a4
dctalbot <david.talbot@target.com>
2022-03-23 09:20:23 -0500

    clear state on migration

--- migrate/migrations/20220317153438-alert-metrics-add-closed-at.sql
+++ migrate/migrations/20220317153438-alert-metrics-add-closed-at.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 
 UPDATE engine_processing_versions
-SET "version" = 2
+SET "version" = 2, state = DEFAULT
 WHERE type_id = 'metrics';
 
 ALTER TABLE alert_metrics ADD COLUMN closed_at TIMESTAMP WITH TIME ZONE NOT NULL;
@@ -14,7 +14,7 @@ TRUNCATE alert_metrics;
 ALTER TABLE alert_metrics DROP COLUMN closed_at;
 
 UPDATE engine_processing_versions
-SET "version" = 1
+SET "version" = 1, state = DEFAULT
 WHERE type_id = 'metrics';
 
 TRUNCATE alert_metrics;
