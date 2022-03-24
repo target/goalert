7a47b4a7fe32359351c28cf0a61b66de59714c3d
Nathaniel Cook <nathanieljcook@outlook.com>
2022-03-24 14:08:34 -0700

    refactor layout

--- web/src/app/admin/switchover/AdminSwitchover.tsx
+++ web/src/app/admin/switchover/AdminSwitchover.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
+import Divider from '@mui/material/Divider'
 import Grid from '@mui/material/Grid'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
 import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
-import { SvgIconProps, TypographyProps } from '@mui/material'
+import { useTheme, SvgIconProps } from '@mui/material'
 import PingIcon from 'mdi-material-ui/DatabaseMarker'
 import NoResetIcon from 'mdi-material-ui/DatabaseRefreshOutline'
 import ResetIcon from 'mdi-material-ui/DatabaseRefresh'
@@ -16,6 +22,8 @@ import ErrorIcon from 'mdi-material-ui/DatabaseAlert'
 import IdleIcon from 'mdi-material-ui/DatabaseSettings'
 import InProgressIcon from 'mdi-material-ui/DatabaseEdit'
 import { gql, useMutation, useQuery } from '@apollo/client'
+import TrueIcon from 'mdi-material-ui/CheckboxMarkedCircleOutline'
+import FalseIcon from 'mdi-material-ui/CloseCircleOutline'
 import Notices, { Notice } from '../../details/Notices'
 import { DateTime } from 'luxon'
 import { SWONode } from '../../../schema'
@@ -48,6 +56,7 @@ function cptlz(s: string): string {
 }
 
 export default function AdminSwitchover(): JSX.Element {
+  const theme = useTheme()
   const { loading, error, data: _data } = useQuery(query)
   const data = _data?.swoStatus
 
@@ -56,66 +65,43 @@ export default function AdminSwitchover(): JSX.Element {
 
   function getIcon(): React.ReactNode {
     const i: SvgIconProps = { color: 'primary', sx: { fontSize: '3.5rem' } }
-    const t: TypographyProps = {
-      variant: 'caption',
-      sx: { display: 'flex' },
-      flexDirection: 'column',
-    }
 
     if (error) {
-      return (
-        <Typography {...t}>
-          <ErrorIcon {...i} color='error' />
-          Error
-        </Typography>
-      )
+      return <ErrorIcon {...i} color='error' />
     }
-
     if (loading) {
       return (
-        <Typography {...t}>
-          <Skeleton variant='circular'>
-            <InProgressIcon {...i} />
-          </Skeleton>
-          Loading...
-        </Typography>
-      )
-    }
-
-    if (!data.isIdle && !data.isDone) {
-      return (
-        <Typography {...t}>
+        <Skeleton variant='circular'>
           <InProgressIcon {...i} />
-          In Progress
-        </Typography>
+        </Skeleton>
       )
     }
-
+    if (!data.isIdle && !data.isDone) {
+      return <InProgressIcon {...i} />
+    }
     if (data.isIdle) {
-      return (
-        <Typography {...t}>
-          <IdleIcon {...i} />
-          Idle
-        </Typography>
-      )
+      return <IdleIcon {...i} />
     }
+  }
+
+  function getSubheader(): React.ReactNode {
+    if (error) return 'Error'
+    if (loading) return 'Loading...'
+    if (!data.isIdle && !data.isDone) return 'In progress'
+    if (data.isIdle) return 'Idle'
+    return null
   }
 
   function getDetails(): React.ReactNode {
     if (error) {
       return <Typography color='error'>{cptlz(error.message)}</Typography>
     }
-
     if (data?.details) {
-      return <Typography>{cptlz(data.details)}</Typography>
+      return cptlz(data.details)
     }
-
-    return null
+    return 'Testing some details yeehaw'
   }
 
-  const minHeight = 90
-  const buttonSx = { display: 'grid', minHeight, minWidth: minHeight }
-  const iconSx = { justifySelf: 'center', height: '1.25em', width: '1.25em' }
   return (
     <Grid container spacing={4}>
       {statusNotices.length > 0 && (
@@ -124,126 +110,156 @@ export default function AdminSwitchover(): JSX.Element {
         </Grid>
       )}
 
-      <Grid item>
-        <Card sx={{ minWidth: 300, minHeight }}>
+      <Grid item xs={4}>
+        <Card sx={{ width: '100%' }}>
           <CardHeader
             title='Switchover Status'
             titleTypographyProps={{ sx: { fontSize: '1.25rem' } }}
             avatar={getIcon()}
-            subheader={getDetails()}
+            subheader={getSubheader()}
+            sx={{ '& .MuiCardHeader-content': { alignSelf: 'start' } }}
           />
+          <CardContent>
+            <Typography sx={{ pb: 4 }}>{getDetails()}</Typography>
+            <ButtonGroup orientation='vertical' sx={{ width: '100%' }}>
+              <Button
+                startIcon={<PingIcon />}
+                variant='outlined'
+                size='large'
+                onClick={() =>
+                  commit({
+                    variables: { action: 'ping' },
+                    onError: (error) => {
+                      setStatusNotices([
+                        ...statusNotices,
+                        {
+                          type: 'error',
+                          message: 'Failed to ping',
+                          details: cptlz(error.message),
+                          endNote: DateTime.local().toFormat('fff'),
+                        },
+                      ])
+                    },
+                  })
+                }
+              >
+                Ping
+              </Button>
+              <Button
+                startIcon={data?.isDone ? <NoResetIcon /> : <ResetIcon />}
+                disabled={data?.isDone}
+                variant='outlined'
+                size='large'
+                onClick={() =>
+                  commit({
+                    variables: { action: 'reset' },
+                    onError: (error) => {
+                      setStatusNotices([
+                        ...statusNotices,
+                        {
+                          type: 'error',
+                          message: 'Failed to reset',
+                          details: cptlz(error.message),
+                          endNote: DateTime.local().toFormat('fff'),
+                        },
+                      ])
+                    },
+                  })
+                }
+              >
+                Reset
+              </Button>
+              <Button
+                startIcon={!data?.isIdle ? <NoExecuteIcon /> : <ExecuteIcon />}
+                disabled={!data?.isIdle}
+                variant='outlined'
+                size='large'
+                onClick={() =>
+                  commit({
+                    variables: { action: 'execute' },
+                    onError: (error) => {
+                      setStatusNotices([
+                        ...statusNotices,
+                        {
+                          type: 'error',
+                          message: 'Failed to execute',
+                          details: cptlz(error.message),
+                          endNote: DateTime.local().toFormat('fff'),
+                        },
+                      ])
+                    },
+                  })
+                }
+              >
+                Execute
+              </Button>
+            </ButtonGroup>
+          </CardContent>
         </Card>
       </Grid>
 
-      <Grid item>
-        <Button
-          onClick={() =>
-            commit({
-              variables: { action: 'ping' },
-              onError: (error) => {
-                setStatusNotices([
-                  ...statusNotices,
-                  {
-                    type: 'error',
-                    message: 'Failed to ping',
-                    details: cptlz(error.message),
-                    endNote: DateTime.local().toFormat('fff'),
-                  },
-                ])
-              },
-            })
-          }
-          size='large'
-          variant='outlined'
-          sx={buttonSx}
-        >
-          <PingIcon sx={iconSx} />
-          Ping
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          onClick={() =>
-            commit({
-              variables: { action: 'reset' },
-              onError: (error) => {
-                setStatusNotices([
-                  ...statusNotices,
-                  {
-                    type: 'error',
-                    message: 'Failed to reset',
-                    details: cptlz(error.message),
-                    endNote: DateTime.local().toFormat('fff'),
-                  },
-                ])
-              },
-            })
-          }
-          disabled={data?.isDone}
-          size='large'
-          variant='outlined'
-          sx={buttonSx}
-        >
-          {data?.isDone ? (
-            <NoResetIcon sx={iconSx} />
-          ) : (
-            <ResetIcon sx={iconSx} />
-          )}
-          Reset
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          onClick={() =>
-            commit({
-              variables: { action: 'execute' },
-              onError: (error) => {
-                setStatusNotices([
-                  ...statusNotices,
-                  {
-                    type: 'error',
-                    message: 'Failed to execute',
-                    details: cptlz(error.message),
-                    endNote: DateTime.local().toFormat('fff'),
-                  },
-                ])
-              },
-            })
-          }
-          disabled={!data?.isIdle}
-          size='large'
-          variant='outlined'
-          sx={buttonSx}
-        >
-          {!data?.isIdle ? (
-            <NoExecuteIcon sx={iconSx} />
-          ) : (
-            <ExecuteIcon sx={iconSx} />
-          )}
-          Execute
-        </Button>
-      </Grid>
-
-      <Grid item xs={12} container>
+      <Grid item xs={8} container>
         {data?.nodes.length > 0 &&
-          data.nodes.map((node: SWONode, idx: number) => (
-            <Grid item key={idx}>
-              <Card>
-                <CardHeader details={node.status} />
-                <CardContent>
-                  <Typography>
-                    {node.canExec ? 'Executable' : 'Not Executable'}
+          data.nodes
+            .sort((a: SWONode, b: SWONode) => {
+              if (a.id < b.id) return 1
+              if (a.id > b.id) return -1
+              return 0
+            })
+            .map((node: SWONode, idx: number) => (
+              <Grid item key={idx} sx={{ minWidth: 300 }}>
+                <Card>
+                  <Typography color={theme.palette.primary.main} sx={{ p: 2 }}>
+                    Node {idx + 1}
                   </Typography>
-                  <Typography>
-                    {node.oldValid ? 'Old is valid' : 'Old is invalid'}
-                  </Typography>
-                  <Typography>
-                    {node.newValid ? 'New is valid' : 'New is invalid'}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
+                  <List
+                    subheader={
+                      <React.Fragment>
+                        <Divider />
+                        <Typography
+                          color={theme.palette.secondary.main}
+                          sx={{ p: 2 }}
+                        >
+                          Status: {node.status}
+                        </Typography>
+                        <Divider />
+                      </React.Fragment>
+                    }
+                  >
+                    <ListItem>
+                      <ListItemText primary='Executable?' />
+                      <ListItemSecondaryAction>
+                        {node.canExec ? (
+                          <TrueIcon color='success' />
+                        ) : (
+                          <FalseIcon color='error' />
+                        )}
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary='Is the old node valid?' />
+                      <ListItemSecondaryAction>
+                        {node.oldValid ? (
+                          <TrueIcon color='success' />
+                        ) : (
+                          <FalseIcon color='error' />
+                        )}
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary='Is the new node valid?' />
+                      <ListItemSecondaryAction>
+                        {node.newValid ? (
+                          <TrueIcon color='success' />
+                        ) : (
+                          <FalseIcon color='error' />
+                        )}
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  </List>
+                </Card>
+              </Grid>
+            ))}
       </Grid>
     </Grid>
   )
