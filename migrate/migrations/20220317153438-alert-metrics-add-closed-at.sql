e865ab53a5493c160c983661df2929ecc497897c
dctalbot <david.talbot@target.com>
2022-03-29 14:37:47 -0500

    truncate before alter table

--- migrate/migrations/20220317153438-alert-metrics-add-closed-at.sql
+++ migrate/migrations/20220317153438-alert-metrics-add-closed-at.sql
@@ -4,10 +4,9 @@ UPDATE engine_processing_versions
 SET "version" = 2, state = DEFAULT
 WHERE type_id = 'metrics';
 
-ALTER TABLE alert_metrics ADD COLUMN closed_at TIMESTAMP WITH TIME ZONE NOT NULL;
-
 TRUNCATE alert_metrics;
 
+ALTER TABLE alert_metrics ADD COLUMN closed_at TIMESTAMP WITH TIME ZONE NOT NULL;
 
 -- +migrate Down
 
