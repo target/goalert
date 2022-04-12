537912b7bed12deca1e9f89b9fcd3217bd7c91d6
KatieMSB <katriannasb@gmail.com>
2022-04-12 14:24:41 -0500

    merge latest

--- migrate/migrations/20220407133455-add-alert-metric-idx.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-
-CREATE INDEX alert_metrics_closed_date_idx ON alert_metrics (DATE(closed_at AT TIME ZONE 'UTC' ) ASC);
-
--- +migrate Down
-
-DROP INDEX alert_metrics_closed_date_idx;
