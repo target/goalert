b3a9b402af0eba72b17c7a4fcf9eac7e0cae8540
Tony Vu <tonyvu527@gmail.com>
2022-04-19 15:02:41 -0500

    fix: resolve psql test failing

--- web/src/cypress/support/sql.ts
+++ web/src/cypress/support/sql.ts
@@ -11,7 +11,7 @@ function sql(query: string): Cypress.Chainable {
   const dbURL =
     Cypress.env('DB_URL') || 'postgres://goalert@localhost:5432?sslmode=disable'
 
-  return cy.exec(`psql-lite -tx -d "$DB" -c "$QUERY"`, {
+  return cy.exec(`../../bin/psql-lite -tx -d "$DB" -c "$QUERY"`, {
     env: {
       DB: dbURL,
       QUERY: query,
