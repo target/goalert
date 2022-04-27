3911a5ade7de72a8f023711fed4c1f728d1882fb
Nathaniel Caza <mastercactapus@gmail.com>
2022-04-27 10:17:15 -0500

    get rid of unused bits in cypress config

--- web/src/esbuild.cypress.js
+++ web/src/esbuild.cypress.js
@@ -3,32 +3,6 @@
 const path = require('path')
 const glob = require('glob')
 
-const dynamicPublicPathPlugin = {
-  name: 'prefix-path',
-  setup(build) {
-    build.onResolve({ filter: /\.(png|webp)$/ }, (args) => {
-      const needsPrefix =
-        args.kind === 'import-statement' && args.pluginData !== 'dynamic'
-      return {
-        path: path.resolve(args.resolveDir, args.path),
-        namespace: needsPrefix ? 'prefix-path' : 'file',
-      }
-    })
-
-    build.onLoad({ filter: /\.*/, namespace: 'prefix-path' }, async (args) => {
-      return {
-        pluginData: 'dynamic',
-        contents: `
-          import p from ${JSON.stringify(args.path)}
-          const prefixPath = pathPrefix + "/static/" + p
-          export default prefixPath
-        `,
-        loader: 'js',
-      }
-    })
-  },
-}
-
 const intEntry = {}
 glob.sync(path.join(__dirname, 'cypress/integration/*')).forEach((file) => {
   const name = path.basename(file, '.ts')
@@ -44,23 +18,10 @@ require('esbuild')
     outdir: '../../bin/build/integration/cypress',
     logLevel: 'info',
     bundle: true,
-    define: {
-      global: 'window',
-    },
+    define: { global: 'window' },
     minify: true,
     sourcemap: 'linked',
-    plugins: [dynamicPublicPathPlugin],
     target: ['chrome80', 'firefox99', 'safari12', 'edge79'],
-    banner: {
-      js: `var GOALERT_VERSION=${JSON.stringify(process.env.GOALERT_VERSION)};`,
-    },
-    loader: {
-      '.png': 'file',
-      '.webp': 'file',
-      '.js': 'jsx',
-      '.svg': 'dataurl',
-      '.md': 'text',
-    },
     watch: process.argv.includes('--watch'),
   })
   .catch((err) => {
