5be4dcde7a3bb7c7a03a97a559f5c9572ca8475b
Nathaniel Cook <nathanieljcook@outlook.com>
2022-05-11 09:30:12 -0700

    attempt 2 at fixing axe-core in ci

--- web/src/cypress/support/a11y.ts
+++ web/src/cypress/support/a11y.ts
@@ -7,6 +7,17 @@ declare global {
   }
 }
 
+// https://github.com/component-driven/cypress-axe/issues/118
+Cypress.Commands.add('injectAxe', () => {
+  cy.task('getAxeSource').then((axeSource) =>
+    cy.window({ log: false }).then((window) => {
+      const script = window.document.createElement('script')
+      script.innerHTML = axeSource as string
+      window.document.head.appendChild(script)
+    }),
+  )
+})
+
 // no selector provided will result in the entire page being validated
 function validateA11y(selector = 'main[id="content"]'): void {
   cy.window().then((win: Cypress.AUTWindow & { _axeInjected?: boolean }) => {
