97aaa397f50d9e24ded2bfb70b7d75d0f0296b36
Nathaniel Cook <nathanieljcook@outlook.com>
2022-05-17 12:44:21 -0700

    cleanup

--- web/src/app/main/components/ToolbarPageTitle.tsx
+++ web/src/app/main/components/ToolbarPageTitle.tsx
@@ -1,31 +1,24 @@
 import * as React from 'react'
-import Link, { LinkProps } from '@mui/material/Link'
+import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 import Breadcrumbs from '@mui/material/Breadcrumbs'
 import { ChevronRight } from '@mui/icons-material'
 import { useQuery } from 'urql'
 import { Link as RouterLink, Route, Routes, useParams } from 'react-router-dom'
 import { Theme } from '@mui/material'
-import { startCase } from 'lodash'
+import { startCase, camelCase } from 'lodash'
 import { applicationName as appName } from '../../env'
 
-const detailsMap: { [key: string]: string } = {
+const typeMap: { [key: string]: string } = {
   alerts: 'Alert',
   schedules: 'Schedule',
-  'escalation-policies': 'Escalation-Policy',
+  'escalation-policies': 'Escalation Policy',
   rotations: 'Rotation',
   users: 'User',
   services: 'Service',
 }
 
-interface LinkRouterProps extends LinkProps {
-  to: string
-  replace?: boolean
-}
-const LinkRouter = (props: LinkRouterProps): JSX.Element => (
-  <Link {...props} component={RouterLink as any} />
-)
-
+// todo: not needed once appbar is using same color prop for dark/light modes
 const getContrastColor = (theme: Theme): string => {
   return theme.palette.getContrastText(
     theme.palette.mode === 'dark'
@@ -34,53 +27,65 @@ const getContrastColor = (theme: Theme): string => {
   )
 }
 
-const renderText = (title: string, asLink?: boolean): JSX.Element => {
-  let linkSx = {}
-  if (asLink) {
-    linkSx = {
-      '&:hover': {
-        cursor: 'pointer',
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        borderRadius: '6px',
-        padding: '4px',
-        textDecoration: 'none',
-      },
-    }
-  }
-
-  return (
+const renderText = (title: string, link?: string): JSX.Element => {
+  const typography = (
     <Typography
       noWrap
       component='h1'
       sx={{
-        ...linkSx,
         padding: '0 4px 0 4px',
         fontSize: '1.25rem',
         color: getContrastColor,
       }}
     >
-      {title.replace('-', ' ').replace('On Call', 'On-Call')}
+      {startCase(title.replace('-', ' ').replace('On Call', 'On-Call'))}
     </Typography>
   )
+
+  if (link) {
+    return (
+      <Link
+        component={RouterLink}
+        key={link}
+        to={link}
+        underline='hover'
+        color='inherit'
+        sx={{
+          '&:hover': {
+            textDecoration: 'none',
+          },
+          '&:hover > h1': {
+            cursor: 'pointer',
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            borderRadius: '6px',
+            padding: '4px',
+          },
+        }}
+      >
+        {typography}
+      </Link>
+    )
+  }
+
+  return typography
 }
 
-// todo: fix lowercase + hyphens in tab title
-function ToolbarBreadcrumbs(p: { isProfile?: boolean }): JSX.Element {
-  const { sub: _sub, type: _type, id } = useParams()
-  const sub = startCase(_sub)
-  const type = p.isProfile ? 'profile' : _type ?? '' // no type if on profile
+function ToolbarBreadcrumbs(p: { type?: string }): JSX.Element {
+  const typeFallback = p.type ?? ''
+  const { sub, type = typeFallback, id } = useParams()
 
-  const details = detailsMap[type ?? ''] ?? startCase(type)
-  const detailsTitle = details + ' Details'
+  const query = typeMap[type] ?? 'skipping'
+  const detailsTitle = typeMap[type] + ' Details'
 
   document.title = `${applicationName || appName} - ${
     sub || (type ? detailsTitle : type)
   }`
 
+  // query for details page name if on a subpage
   const [result] = useQuery({
-    pause: !id || !details,
+    pause: !query,
     query: `query ($id: ID!) {
-        data: ${details.replace('-', '')}(id: $id) {
+        data: ${camelCase(query)}(id: $id) {
           id
           name
         }
@@ -90,7 +95,7 @@ function ToolbarBreadcrumbs(p: { isProfile?: boolean }): JSX.Element {
 
   return (
     <Breadcrumbs
-      aria-label='breadcrumb'
+      aria-label='breadcrumbs'
       separator={
         <ChevronRight
           sx={{
@@ -99,33 +104,15 @@ function ToolbarBreadcrumbs(p: { isProfile?: boolean }): JSX.Element {
         />
       }
     >
-      <LinkRouter
-        underline='hover'
-        color='inherit'
-        to={'/' + type}
-        key={'/' + type}
-        sx={{
-          textTransform: 'capitalize',
-          '&:hover': { textDecoration: 'none' },
-        }}
-      >
-        {renderText(type, true)}
-      </LinkRouter>
+      {renderText(type, sub || id ? '/' + type : '')}
       {id && type && !sub && renderText(detailsTitle)}
-      {id && type && sub && (
-        <LinkRouter
-          key={'/' + type + '/' + id}
-          to={'/' + type + '/' + id}
-          underline='hover'
-          color='inherit'
-          sx={{
-            textTransform: 'capitalize',
-            '&:hover': { textDecoration: 'none' },
-          }}
-        >
-          {renderText(result?.data?.data?.name ?? detailsTitle, true)}
-        </LinkRouter>
-      )}
+      {id &&
+        type &&
+        sub &&
+        renderText(
+          result?.data?.data?.name ?? detailsTitle,
+          '/' + type + '/' + id,
+        )}
       {sub && renderText(sub)}
     </Breadcrumbs>
   )
@@ -134,13 +121,18 @@ function ToolbarBreadcrumbs(p: { isProfile?: boolean }): JSX.Element {
 export default function ToolbarPageTitle(): JSX.Element {
   return (
     <Routes>
+      {/* standard list/details/subpage route paths */}
       <Route path='/:type' element={<ToolbarBreadcrumbs />} />
       <Route path='/:type/:id' element={<ToolbarBreadcrumbs />} />
       <Route path='/:type/:id/:sub' element={<ToolbarBreadcrumbs />} />
 
-      <Route path='/profile/:sub' element={<ToolbarBreadcrumbs isProfile />} />
+      {/* everything else */}
+      <Route
+        path='/profile/:sub'
+        element={<ToolbarBreadcrumbs type='profile' />}
+      />
+      <Route path='/admin/:sub' element={<ToolbarBreadcrumbs type='admin' />} />
       <Route path='/wizard' element={renderText('Setup Wizard')} />
-      <Route path='/admin' element={renderText('Admin Page')} />
       <Route path='/docs' element={renderText('Documentation')} />
     </Routes>
   )
