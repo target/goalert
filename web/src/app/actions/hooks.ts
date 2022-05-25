b3c623bbbac0c3595d8643d658d31b5ec264b907
Nathaniel Caza <mastercactapus@gmail.com>
2022-05-25 14:23:34 -0500

    convert hooks to wouter

--- web/src/app/actions/hooks.ts
+++ web/src/app/actions/hooks.ts
@@ -1,5 +1,4 @@
-import { Location } from 'history'
-import { useNavigate, useLocation, NavigateFunction } from 'react-router-dom'
+import { useLocation } from 'wouter'
 
 export type Value = string | boolean | number | string[]
 
@@ -52,21 +51,17 @@ export function getParamValues<T extends Record<string, Value>>(
   return result as T
 }
 
-// setURLParams will replace the latest browser history entry with the provided params.
-function setURLParams(
-  navigate: NavigateFunction,
-  location: Location,
-  params: URLSearchParams,
-): void {
+// newSearch will return a normalized URL search string if different from the current one.
+function newSearch(params: URLSearchParams): string {
   if (params.sort) params.sort()
   let newSearch = params.toString()
   newSearch = newSearch ? '?' + newSearch : ''
-
   if (newSearch === location.search) {
     // no action for no param change
-    return
+    return ''
   }
-  navigate(location.pathname + newSearch + location.hash, { replace: true })
+
+  return newSearch
 }
 
 // useURLParams returns the values for the given URL params if present, else the given defaults.
@@ -76,8 +71,7 @@ function setURLParams(
 export function useURLParams<T extends Record<string, Value>>(
   params: T, // <name, default> pairs
 ): [T, (newValues: Partial<T>) => void] {
-  const location = useLocation()
-  const navigate = useNavigate()
+  const [path, navigate] = useLocation()
   const q = new URLSearchParams(location.search)
   let called = false
 
@@ -102,7 +96,13 @@ export function useURLParams<T extends Record<string, Value>>(
       }
     }
 
-    setURLParams(navigate, location, q)
+    const search = newSearch(q)
+    if (!search) {
+      // nothing to do
+      return
+    }
+
+    navigate(path + search + location.hash, { replace: true })
   }
 
   const values = getParamValues<T>(location, params)
@@ -129,8 +129,7 @@ export function useURLParam<T extends Value>(
 // The native history stack will not push a new entry; instead, its
 // latest entry will be replaced.
 export function useResetURLParams(...names: string[]): () => void {
-  const location = useLocation()
-  const navigate = useNavigate()
+  const [path, navigate] = useLocation()
   let called = false
 
   return function resetURLParams(): void {
@@ -150,6 +149,12 @@ export function useResetURLParams(...names: string[]): () => void {
     const params = new URLSearchParams(location.search)
     names.forEach((name) => params.delete(name))
 
-    setURLParams(navigate, location, params)
+    const search = newSearch(params)
+    if (!search) {
+      // nothing to do
+      return
+    }
+
+    navigate(path + search + location.hash, { replace: true })
   }
 }
