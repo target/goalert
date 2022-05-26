02aa892f1e98f073285225629e9bc1a5c4439dc9
Nathaniel Caza <mastercactapus@gmail.com>
2022-05-26 09:33:25 -0500

    refactor navbar

---
+++ web/src/app/main/NavBar.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Divider from '@mui/material/Divider'
+import List from '@mui/material/List'
+import makeStyles from '@mui/styles/makeStyles'
+import { styles as globalStyles } from '../styles/materialStyles'
+import {
+  Group,
+  Layers,
+  Notifications,
+  RotateRight,
+  Today,
+  VpnKey,
+  Build,
+} from '@mui/icons-material'
+import { WizardHat as WizardIcon } from 'mdi-material-ui'
+import { Theme, useTheme } from '@mui/material/styles'
+import RequireConfig from '../util/RequireConfig'
+import logo from '../public/logos/black/goalert-alt-logo.png'
+import darkModeLogo from '../public/logos/white/goalert-alt-logo-white.png'
+import NavBarLink, { NavBarSubLink } from './NavBarLink'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  ...globalStyles(theme),
+  logoDiv: {
+    ...theme.mixins.toolbar,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navIcon: {
+    width: '1em',
+    height: '1em',
+    fontSize: '24px',
+  },
+  list: {
+    padding: 0,
+  },
+}))
+
+export default function NavBar() {
+  const classes = useStyles()
+  const theme = useTheme()
+
+  return (
+    <React.Fragment>
+      <div aria-hidden className={classes.logoDiv}>
+        <img
+          height={38}
+          src={theme.palette.mode === 'dark' ? darkModeLogo : logo}
+          alt='GoAlert Logo'
+        />
+      </div>
+      <Divider />
+      <nav>
+        <List role='navigation' className={classes.list} data-cy='nav-list'>
+          <NavBarLink to='/alerts' title='Alerts' icon={<Notifications />} />
+          <NavBarLink
+            to='/rotations'
+            title='Rotations'
+            icon={<RotateRight />}
+          />
+          <NavBarLink to='/schedules' title='Schedules' icon={<Today />} />
+          <NavBarLink
+            to='/escalation-policies'
+            title='Escalation Policies'
+            icon={<Layers />}
+          />
+          <NavBarLink to='/services' title='Services' icon={<VpnKey />} />
+          <NavBarLink to='/users' title='Users' icon={<Group />} />
+
+          <RequireConfig isAdmin>
+            <NavBarLink to='/admin' title='Admin' icon={<Build />}>
+              <NavBarSubLink to='/admin/config' title='Config' />
+              <NavBarSubLink to='/admin/limits' title='System Limits' />
+              <NavBarSubLink to='/admin/toolbox' title='Toolbox' />
+              <NavBarSubLink to='/admin/message-logs' title='Message Logs' />
+            </NavBarLink>
+          </RequireConfig>
+
+          <NavBarLink to='/wizard' title='Wizard' icon={<WizardIcon />} />
+        </List>
+      </nav>
+    </React.Fragment>
+  )
+}
---
+++ web/src/app/main/NavBarLink.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import { useLocation } from 'wouter'
+import List from '@mui/material/List'
+import makeStyles from '@mui/styles/makeStyles'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Typography from '@mui/material/Typography'
+import { styles } from '../styles/materialStyles'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { Collapse, Theme } from '@mui/material'
+import AppLink from '../util/AppLink'
+
+const useStyles = makeStyles((theme: Theme) => {
+  const { nav, navSelected } = styles(theme)
+  return {
+    nav,
+    navSelected,
+    subMenu: {
+      padding: '0',
+    },
+    subMenuLinkText: {
+      paddingLeft: '3.5rem',
+      '& span': {
+        fontSize: '.9rem',
+      },
+    },
+    dropdown: {
+      transition: theme.transitions.create(['transform'], {
+        duration: theme.transitions.duration.short,
+      }),
+    },
+    dropdownOpen: {
+      transform: 'rotate(0)',
+    },
+    dropdownClosed: {
+      transform: 'rotate(-90deg)',
+    },
+  }
+})
+
+export type NavBarSubLinkProps = {
+  to: string
+  title: string
+}
+export function NavBarSubLink({ to, title }: NavBarSubLinkProps): JSX.Element {
+  const { navSelected, nav, subMenuLinkText } = useStyles()
+  const [path] = useLocation()
+  return (
+    <AppLink className={path.startsWith(to) ? navSelected : nav} to={to}>
+      <ListItem button tabIndex={-1}>
+        <ListItemText className={subMenuLinkText}>{title}</ListItemText>
+      </ListItem>
+    </AppLink>
+  )
+}
+
+export type NavBarLinkProps = {
+  icon: JSX.Element
+  title: string
+  to: string
+  children?: JSX.Element[] | JSX.Element
+}
+
+export default function NavBarLink({
+  icon,
+  title,
+  to,
+  children,
+}: NavBarLinkProps) {
+  const classes = useStyles()
+  const [path] = useLocation()
+  const isRoute = path.startsWith(to)
+
+  return (
+    <React.Fragment>
+      <AppLink
+        to={to}
+        className={!children && isRoute ? classes.navSelected : classes.nav}
+      >
+        <ListItem button tabIndex={-1}>
+          <ListItemIcon>{icon}</ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography variant='subtitle1' component='p'>
+                {title}
+              </Typography>
+            }
+          />
+          {children && (
+            <ExpandMoreIcon
+              color='action'
+              className={
+                classes.dropdown +
+                ' ' +
+                (isRoute ? classes.dropdownOpen : classes.dropdownClosed)
+              }
+            />
+          )}
+        </ListItem>
+      </AppLink>
+      {children && (
+        <Collapse in={isRoute} mountOnEnter>
+          <List className={classes.subMenu}>{children}</List>
+        </Collapse>
+      )}
+    </React.Fragment>
+  )
+}
