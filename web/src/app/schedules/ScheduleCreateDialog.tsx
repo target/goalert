4abefcbe3ebc2bf3cc447ab00ccadaacc05d3699
tony-tvu <Tony.T.Vu@outlook.com>
2022-06-15 13:41:04 -0500

    convert to typescript

---
+++ web/src/app/schedules/ScheduleCreateDialog.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { gql } from 'urql'
+import FormDialog from '../dialogs/FormDialog'
+import ScheduleForm from './ScheduleForm'
+import { nonFieldErrors, fieldErrors } from '../util/errutil'
+import { Redirect } from 'wouter'
+
+interface Value {
+  name: string
+  description: string
+  timeZone: string
+  favorite: boolean
+}
+
+const mutation = gql`
+  mutation ($input: CreateScheduleInput!) {
+    createSchedule(input: $input) {
+      id
+      name
+      description
+      timeZone
+    }
+  }
+`
+
+export default function ScheduleCreateDialog(props: {
+  onClose?: () => void
+}): JSX.Element {
+  const [value, setValue] = useState<Value>({
+    name: '',
+    description: '',
+    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    favorite: true,
+  })
+
+  const [createSchedule, { loading, data, error }] = useMutation(mutation)
+
+  if (data && data.createSchedule) {
+    return <Redirect to={`/schedules/${data.createSchedule.id}`} />
+  }
+
+  return (
+    <FormDialog
+      onClose={props.onClose}
+      title='Create New Schedule'
+      errors={nonFieldErrors(error)}
+      onSubmit={() =>
+        createSchedule({
+          variables: {
+            input: {
+              ...value,
+              targets: [
+                {
+                  target: { type: 'user', id: '__current_user' },
+                  rules: [{}],
+                },
+              ],
+            },
+          },
+        })
+      }
+      form={
+        <ScheduleForm
+          disabled={loading}
+          errors={fieldErrors(error)}
+          value={value}
+          onChange={(value) => setValue(value)}
+        />
+      }
+    />
+  )
+}
