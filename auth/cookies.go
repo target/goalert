0a6f108ffb64cfa6cbea420aa4222c45b90b7244
Nathaniel Caza <mastercactapus@gmail.com>
2022-06-17 08:32:21 -0500

    fix cookie path

--- auth/cookies.go
+++ auth/cookies.go
@@ -2,7 +2,11 @@ package auth
 
 import (
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
+
+	"github.com/target/goalert/config"
 )
 
 // SetCookie will set a cookie value for all API prefixes, respecting the current config parameters.
@@ -12,18 +16,37 @@ func SetCookie(w http.ResponseWriter, req *http.Request, name, value string) {
 
 // SetCookieAge behaves like SetCookie but also sets the MaxAge.
 func SetCookieAge(w http.ResponseWriter, req *http.Request, name, value string, age time.Duration) {
+	cfg := config.FromContext(req.Context())
+	u, err := url.Parse(cfg.PublicURL())
+	if err != nil {
+		panic(err)
+	}
+	if u.Path == "/" {
+		u.Path = "/"
+	}
+
+	cookiePath := "/"
+	secure := req.URL.Scheme == "https"
+	if cfg.ShouldUsePublicURL() {
+		// always clear old cookie, using old logic
+		http.SetCookie(w, &http.Cookie{
+			HttpOnly: true,
+			Secure:   req.URL.Scheme == "https",
+			Name:     name,
+			Path:     "/",
+			MaxAge:   -1,
+		})
+
+		cookiePath = strings.TrimSuffix(u.Path, "/") + "/api/"
+		secure = u.Scheme == "https"
+	}
+
 	http.SetCookie(w, &http.Cookie{
 		HttpOnly: true,
-		Secure:   req.URL.Scheme == "https",
+		Secure:   secure,
 		Name:     name,
 
-		// Until we can finish removing /v1 from all UI calls
-		// we need cookies available on both /api and /v1.
-		//
-		// Unfortunately we can't just set both paths without breaking integration tests...
-		// We'll keep this as `/` until Cypress fixes it's cookie handling, or we
-		// finish removing the `/v1` UI code. Whichever is sooner.
-		Path:   "/",
+		Path:   cookiePath,
 		Value:  value,
 		MaxAge: int(age.Seconds()),
 	})
