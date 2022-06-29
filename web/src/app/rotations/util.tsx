9a0fe1e603a0cbd0d564c5c5aea24f80388e7237
tony-tvu <Tony.T.Vu@outlook.com>
2022-06-29 13:42:34 -0500

    convert to ts

--- web/src/app/rotations/util.tsx
+++ web/src/app/rotations/util.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { DateTime } from 'luxon'
+import { CreateRotationInput } from '../../schema'
 
 // calcNewActiveIndex returns the newActiveIndex for a swap operation
 // -1 will be returned if there was no change
-export function calcNewActiveIndex(oldActiveIndex, oldIndex, newIndex) {
+export function calcNewActiveIndex(
+  oldActiveIndex: number,
+  oldIndex: number,
+  newIndex: number,
+): number {
   if (oldIndex === newIndex) {
     return -1
   }
@@ -21,7 +27,7 @@ export function calcNewActiveIndex(oldActiveIndex, oldIndex, newIndex) {
 }
 
 // formatTime returns the formatted time with the timezone (if different than local timezone)
-export function formatTime(time, tz) {
+export function formatTime(time: string, tz: string): string {
   const schedTime = DateTime.fromISO(time, { zone: tz }).toLocaleString(
     DateTime.TIME_SIMPLE,
   )
@@ -36,7 +42,7 @@ export function formatTime(time, tz) {
 }
 
 // formatDay returns the day given a time and timezone
-export function formatDay(time, tz) {
+export function formatDay(time: string, tz: string): string {
   const day = DateTime.fromISO(time, { zone: tz }).weekdayLong
   const localDay = DateTime.fromISO(time).weekdayLong
 
@@ -49,7 +55,11 @@ export function formatDay(time, tz) {
 
 // formatWeeklySummary returns the summary for a weekly rotation
 // taking into consideration extra formatting needed if timezone does not match with local timezone
-export function formatWeeklySummary(shiftLength, start, tz) {
+export function formatWeeklySummary(
+  shiftLength: number,
+  start: string,
+  tz: string,
+): string {
   let details = ''
   const day = DateTime.fromISO(start, { zone: tz }).weekdayLong
   const schedTime = DateTime.fromISO(start, { zone: tz }).toLocaleString(
@@ -72,7 +82,7 @@ export function formatWeeklySummary(shiftLength, start, tz) {
 }
 
 // handoffSummary returns the summary description for the rotation
-export function handoffSummary(rotation) {
+export function handoffSummary(rotation: Partial<CreateRotationInput>): string {
   const tz = rotation.timeZone
 
   if (!tz) return 'Loading handoff information...'
@@ -80,7 +90,7 @@ export function handoffSummary(rotation) {
   let details = ''
   switch (rotation.type) {
     case 'hourly':
-      details += 'First hand off time at ' + formatTime(rotation.start, tz)
+      details += 'First hand off time at ' + formatTime(rotation.start!, tz)
       details +=
         ', hands off every ' +
         (rotation.shiftLength === 1
@@ -94,10 +104,10 @@ export function handoffSummary(rotation) {
         rotation.shiftLength === 1
           ? 'daily at'
           : `every ${rotation.shiftLength} days at`
-      details += ' ' + formatTime(rotation.start, tz) + '.'
+      details += ' ' + formatTime(rotation.start!, tz) + '.'
       break
     case 'weekly':
-      details += formatWeeklySummary(rotation.shiftLength, rotation.start, tz)
+      details += formatWeeklySummary(rotation.shiftLength!, rotation.start!, tz)
       break
   }
 
@@ -106,7 +116,11 @@ export function handoffSummary(rotation) {
 
 // reorderList will move an item from the oldIndex to the newIndex, preserving order
 // returning the result as a new array.
-export function reorderList(_items, oldIndex, newIndex) {
+export function reorderList(
+  _items: unknown[],
+  oldIndex: number,
+  newIndex: number,
+): unknown[] {
   const items = _items.slice()
   items.splice(oldIndex, 1) // remove 1 element from oldIndex position
   items.splice(newIndex, 0, _items[oldIndex]) // add dest to newIndex position
