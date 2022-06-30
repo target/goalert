f592534e3805a4bec75059883ea72c7c7f39e637
Nathaniel Cook <nathanieljcook@outlook.com>
2022-06-30 12:55:18 -0700

    allow resizing of chart width

--- web/src/app/admin/admin-message-logs/DebugMessageGraph.tsx
+++ web/src/app/admin/admin-message-logs/DebugMessageGraph.tsx
@@ -14,11 +14,11 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
   LineChart,
   Line,
   Legend,
 } from 'recharts'
+import AutoSizer from 'react-virtualized-auto-sizer'
 import _ from 'lodash'
 
 interface Props {
@@ -46,68 +46,77 @@ export default function DebugMessageGraph(props: Props): JSX.Element {
           Total Count: {props.totalCount}
         </Typography>
       </AccordionSummary>
-      <AccordionDetails sx={{ overflowX: 'scroll' }}>
+      <AccordionDetails>
         <Grid
           container
           sx={{
             fontFamily: theme.typography.body2.fontFamily,
           }}
         >
-          <Grid item xs={12} data-cy='metrics-averages-graph'>
-            <ResponsiveContainer height={500}>
-              <LineChart
-                data={props.data}
-                margin={{
-                  top: 30,
-                  right: 50,
-                }}
-              >
-                <CartesianGrid
-                  strokeDasharray='4'
-                  vertical={false}
-                  stroke={theme.palette.text.secondary}
-                />
-                <XAxis
-                  dataKey='date'
-                  type='category'
-                  stroke={theme.palette.text.secondary}
-                />
-                <YAxis
-                  type='number'
-                  dataKey='count'
-                  allowDecimals={false}
-                  interval='preserveStart'
-                  stroke={theme.palette.text.secondary}
-                />
-                <Tooltip
-                  data-cy='message-count-tooltip'
-                  cursor={{ fill: theme.palette.background.default }}
-                  content={({ active, payload, label }) => {
-                    if (!active || !payload?.length) return null
-
-                    return (
-                      <Paper variant='outlined' sx={{ p: 1 }}>
-                        <Typography variant='body2'>{label}</Typography>
-                        <Typography variant='body2'>
-                          Count: {payload[0].payload.count}
-                        </Typography>
-                      </Paper>
-                    )
+          <Grid
+            item
+            xs={12}
+            data-cy='metrics-averages-graph'
+            sx={{ height: 500 }}
+          >
+            <AutoSizer>
+              {({ width, height }) => (
+                <LineChart
+                  data={props.data}
+                  width={width}
+                  height={height}
+                  margin={{
+                    top: 30,
+                    right: 50,
                   }}
-                />
-                <Legend />
-                <Line
-                  type='monotone'
-                  strokeWidth={2}
-                  stroke={theme.palette.primary.main}
-                  activeDot={{ r: 8 }}
-                  isAnimationActive={false}
-                  dot={(props) => <circle {..._.omit(props, 'dataKey')} />}
-                  name={getName()}
-                  dataKey='count'
-                />
-              </LineChart>
-            </ResponsiveContainer>
+                >
+                  <CartesianGrid
+                    strokeDasharray='4'
+                    vertical={false}
+                    stroke={theme.palette.text.secondary}
+                  />
+                  <XAxis
+                    dataKey='date'
+                    type='category'
+                    stroke={theme.palette.text.secondary}
+                  />
+                  <YAxis
+                    type='number'
+                    dataKey='count'
+                    allowDecimals={false}
+                    interval='preserveStart'
+                    stroke={theme.palette.text.secondary}
+                  />
+                  <Tooltip
+                    data-cy='message-count-tooltip'
+                    cursor={{ fill: theme.palette.background.default }}
+                    content={({ active, payload, label }) => {
+                      if (!active || !payload?.length) return null
+
+                      return (
+                        <Paper variant='outlined' sx={{ p: 1 }}>
+                          <Typography variant='body2'>{label}</Typography>
+                          <Typography variant='body2'>
+                            Count: {payload[0].payload.count}
+                          </Typography>
+                        </Paper>
+                      )
+                    }}
+                  />
+                  <Legend />
+                  <Line
+                    type='monotone'
+                    strokeWidth={2}
+                    stroke={theme.palette.primary.main}
+                    activeDot={{ r: 8 }}
+                    isAnimationActive={false}
+                    dot={(props) => <circle {..._.omit(props, 'dataKey')} />}
+                    name={getName()}
+                    dataKey='count'
+                  />
+                </LineChart>
+              )}
+            </AutoSizer>
           </Grid>
         </Grid>
       </AccordionDetails>
