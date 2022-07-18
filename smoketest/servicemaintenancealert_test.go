f62b24d7f2856d85c7e8ec1a87e2970f476a048e
Nathaniel Cook <nathanieljcook@outlook.com>
2022-07-18 10:11:39 -0700

    dont use global for initSQL

--- smoketest/servicemaintenancealert_test.go
+++ smoketest/servicemaintenancealert_test.go
@@ -8,29 +8,6 @@ import (
 	"github.com/target/goalert/smoketest/harness"
 )
 
-const initSQL = `
-insert into users (id, name, email)
-values ({{uuid "user"}}, 'bob', 'joe');
-
-insert into user_contact_methods (id, user_id, name, type, value)
-values ({{uuid "cm1"}}, {{uuid "user"}}, 'personal', 'SMS', {{phone "1"}});
-
-insert into user_notification_rules (user_id, contact_method_id, delay_minutes)
-values ({{uuid "user"}}, {{uuid "cm1"}}, 0), ({{uuid "user"}}, {{uuid "cm1"}}, 5);
-
-insert into escalation_policies (id, name)
-values ({{uuid "eid"}}, 'esc policy');
-
-insert into escalation_policy_steps (id, escalation_policy_id, delay)
-values ({{uuid "es1"}}, {{uuid "eid"}}, 5);
-
-insert into escalation_policy_actions (escalation_policy_step_id, user_id) 
-values ({{uuid "es1"}}, {{uuid "user"}});
-
-insert into services (id, escalation_policy_id, name)
-values ({{uuid "sid"}}, {{uuid "eid"}}, 'service');
-`
-
 // TestServiceMaintenanceAlert tests that an alert created for a user before
 // maintenance mode starts still follows the normal notification policy flow.
 //
@@ -38,6 +15,30 @@ values ({{uuid "sid"}}, {{uuid "eid"}}, 'service');
 // no message to be received.
 func TestServiceMaintenanceAlert(t *testing.T) {
 	t.Parallel()
+
+	const initSQL = `
+	insert into users (id, name, email)
+	values ({{uuid "user"}}, 'bob', 'joe');
+
+	insert into user_contact_methods (id, user_id, name, type, value)
+	values ({{uuid "cm1"}}, {{uuid "user"}}, 'personal', 'SMS', {{phone "1"}});
+
+	insert into user_notification_rules (user_id, contact_method_id, delay_minutes)
+	values ({{uuid "user"}}, {{uuid "cm1"}}, 0), ({{uuid "user"}}, {{uuid "cm1"}}, 5);
+
+	insert into escalation_policies (id, name)
+	values ({{uuid "eid"}}, 'esc policy');
+
+	insert into escalation_policy_steps (id, escalation_policy_id, delay)
+	values ({{uuid "es1"}}, {{uuid "eid"}}, 5);
+
+	insert into escalation_policy_actions (escalation_policy_step_id, user_id) 
+	values ({{uuid "es1"}}, {{uuid "user"}});
+
+	insert into services (id, escalation_policy_id, name)
+	values ({{uuid "sid"}}, {{uuid "eid"}}, 'service');
+	`
+
 	h := harness.NewHarness(t, initSQL, "add-service-maintenance-expires-at")
 	defer h.Close()
 
