b1032afbfc358a6a7246fbb3e931720e08bb6652
KatieMSB <katriannasb@gmail.com>
2022-08-03 14:30:45 -0500

    add loading status to worker result

--- web/src/app/worker/methods.ts
+++ web/src/app/worker/methods.ts
@@ -12,6 +12,10 @@ export type WorkerMethod<N extends WorkerMethodName> = typeof methods[N]
 export type WorkerResult<N extends WorkerMethodName> = ReturnType<
   WorkerMethod<N>
 >
+export type WorkerReturnType<N extends WorkerMethodName> = {
+  result: WorkerResult<N>
+  loading: boolean
+}
 export type WorkerParam<N extends WorkerMethodName> = Parameters<
   WorkerMethod<N>
 >[0]
