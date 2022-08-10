01885647fcc1ca2d7712849aa3e8b9d59cf08b6d
Nathaniel Caza <mastercactapus@gmail.com>
2022-08-10 18:25:31 -0500

    add additional tools

--- web/src/app/localdev/LocalDev.tsx
+++ web/src/app/localdev/LocalDev.tsx
@@ -12,43 +12,80 @@ import { OpenInNew } from 'mdi-material-ui'
 import { ConfigID } from '../../schema'
 import AdminDialog from '../admin/AdminDialog'
 
+type DevToolProps = {
+  name: string
+  desc: string
+
+  config?: Partial<Record<ConfigID, string>>
+  configDesc?: string
+
+  url?: string
+}
+
 export default function LocalDev(): JSX.Element {
   const [updateConfig, setUpdateConfig] = useState<Partial<
     Record<ConfigID, string>
   > | null>(null)
 
-  return (
-    <React.Fragment>
-      <Grid container>
+  function DevTool(props: DevToolProps): JSX.Element {
+    return (
+      <Grid item xs={6}>
         <Card>
-          <CardHeader title='SMTP' />
+          <CardHeader title={props.name} />
           <CardContent>
-            <Typography>
-              Allow email contact methods and a local mail server.
-            </Typography>
+            <Typography>{props.desc}</Typography>
           </CardContent>
           <CardActions>
-            <Button
-              title='Update GoAlert config to use the dev mail server.'
-              size='small'
-              onClick={() => {
-                setUpdateConfig({
-                  'SMTP.Enable': 'true',
-                  'SMTP.From': 'goalert@localhost',
-                  'SMTP.Address': 'localhost:1025',
-                  'SMTP.Username': '',
-                  'SMTP.Password': '',
-                  'SMTP.DisableTLS': 'true',
-                })
-              }}
-            >
-              Update Config
-            </Button>
-            <Button target='_blank' size='small' href='http://localhost:8025'>
-              View Messages <OpenInNew fontSize='small' />
-            </Button>
+            {props.config && (
+              <Button
+                title={props.configDesc}
+                size='small'
+                onClick={() => {
+                  setUpdateConfig(props.config || null)
+                }}
+              >
+                Update Config
+              </Button>
+            )}
+            {props.url && (
+              <Button target='_blank' size='small' href={props.url}>
+                Open <OpenInNew fontSize='small' />
+              </Button>
+            )}
           </CardActions>
         </Card>
+      </Grid>
+    )
+  }
+
+  return (
+    <React.Fragment>
+      <Grid container spacing={2}>
+        <DevTool
+          name='SMTP'
+          desc='Enable email contact methods to the local mail server.'
+          config={{
+            'SMTP.Enable': 'true',
+            'SMTP.From': 'goalert@localhost',
+            'SMTP.Address': 'localhost:1025',
+            'SMTP.Username': '',
+            'SMTP.Password': '',
+            'SMTP.DisableTLS': 'true',
+          }}
+          url='http://localhost:8025'
+        />
+
+        <DevTool
+          name='gRPC'
+          desc='UI for interacting with the gRPC server interface (sysapi).'
+          url='http://localhost:8234'
+        />
+
+        <DevTool
+          name='Prometheus'
+          desc='Prometheus UI for viewing app metrics.'
+          url='http://localhost:9090/graph?g0.expr=go_memstats_alloc_bytes&g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=1h'
+        />
       </Grid>
 
       {updateConfig && (
