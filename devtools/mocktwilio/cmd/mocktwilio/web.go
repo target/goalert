1739735401c1ab71230a1bce9dd6d1f4238c68cb
Nathaniel Caza <mastercactapus@gmail.com>
2022-08-13 14:53:48 -0500

    add timestamps

--- devtools/mocktwilio/cmd/mocktwilio/web.go
+++ devtools/mocktwilio/cmd/mocktwilio/web.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"time"
 )
 
 //go:embed assets
@@ -59,6 +60,19 @@ func (m Message) Status() string {
 	return "Unknown"
 }
 
+func timeHeader(eventTime time.Time) string {
+	if sameDate(eventTime, time.Now()) {
+		return eventTime.Format("3:04 PM")
+	}
+
+	// if yesterday
+	if sameDate(eventTime, time.Now().Add(-24*time.Hour)) {
+		return "Yesterday - " + eventTime.Format("3:04 PM")
+	}
+
+	return eventTime.Format("Monday - 3:04 PM")
+}
+
 func (s *State) renderUI(w http.ResponseWriter, req *http.Request) {
 	switch req.URL.Path {
 	case "/ui/action/sms":
@@ -89,7 +103,8 @@ func (s *State) renderUI(w http.ResponseWriter, req *http.Request) {
 	case "/ui/sms":
 		type msgWrap struct {
 			Message
-			TimeHeader string
+			TimeHeader   string
+			FriendlyTime string
 		}
 
 		var data struct {
@@ -100,12 +115,17 @@ func (s *State) renderUI(w http.ResponseWriter, req *http.Request) {
 		data.Device = req.FormValue("dev")
 		data.Name = formatNumber(data.Device)
 		msgs := s.SMS()
+		var prevTime time.Time
 		for _, msg := range msgs {
 			if msg.DeviceNumber != data.Device {
 				continue
 			}
-
-			data.Messages = append(data.Messages, msgWrap{Message: msg})
+			w := msgWrap{Message: msg, FriendlyTime: msg.Time.Format("Monday, Jan 2 3:04:05 PM MST 2006")}
+			if msg.Time.Sub(prevTime) > 5*time.Minute {
+				w.TimeHeader = timeHeader(msg.Time)
+			}
+			prevTime = msg.Time
+			data.Messages = append(data.Messages, w)
 		}
 
 		err := tmpl.ExecuteTemplate(w, "sms.html", data)
@@ -115,7 +135,6 @@ func (s *State) renderUI(w http.ResponseWriter, req *http.Request) {
 		}
 		return
 	case "/ui":
-
 		err := tmpl.ExecuteTemplate(w, "index.html", s)
 		if err != nil {
 			// print error to output
