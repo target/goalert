26f981653fc677306e03c3cb43ccff2653f76081
Nathaniel Caza <mastercactapus@gmail.com>
2022-09-01 12:30:54 -0500

    add 3rd test case

--- test/smoke/twiliosmsrestrictions_test.go
+++ test/smoke/twiliosmsrestrictions_test.go
@@ -18,12 +18,14 @@ func TestTwilioSMSRestrictions(t *testing.T) {
 	insert into user_contact_methods (id, user_id, name, type, value) 
 	values
 		({{uuid "cm1"}}, {{uuid "user"}}, 'personal', 'SMS', {{phoneCC "+86" "1"}}),
-		({{uuid "cm2"}}, {{uuid "user"}}, 'personal2', 'SMS', {{phoneCC "+1" "2"}});
+		({{uuid "cm2"}}, {{uuid "user"}}, 'personal2', 'SMS', {{phoneCC "+1" "1"}}),
+		({{uuid "cm3"}}, {{uuid "user"}}, 'personal3', 'SMS', {{phoneCC "+91" "1"}});
 
 	insert into user_notification_rules (user_id, contact_method_id, delay_minutes) 
 	values
 		({{uuid "user"}}, {{uuid "cm1"}}, 0),
-		({{uuid "user"}}, {{uuid "cm2"}}, 0);
+		({{uuid "user"}}, {{uuid "cm2"}}, 0),
+		({{uuid "user"}}, {{uuid "cm3"}}, 0);
 
 	insert into escalation_policies (id, name) 
 	values
@@ -45,14 +47,19 @@ func TestTwilioSMSRestrictions(t *testing.T) {
 	h.CreateAlert(h.UUID("sid"), "testing")
 
 	tw := h.Twilio(t)
-	cn := tw.Device(h.PhoneCC("+86", "1"))
-	us := tw.Device(h.PhoneCC("+1", "2"))
 
-	sms := us.ExpectSMS("testing")
+	// US number supports URLs and 2-way SMS
+	sms := tw.Device(h.PhoneCC("+1", "1")).ExpectSMS("testing")
 	assert.Contains(t, sms.Body(), "ack")
 	assert.Contains(t, sms.Body(), "http")
 
-	sms = cn.ExpectSMS("testing")
+	// CN number does not support URLs or 2-way SMS
+	sms = tw.Device(h.PhoneCC("+86", "1")).ExpectSMS("testing")
 	assert.NotContains(t, sms.Body(), "ack")
 	assert.NotContains(t, sms.Body(), "http")
+
+	// IN supports URLs but not 2-way SMS
+	sms = tw.Device(h.PhoneCC("+91", "1")).ExpectSMS("testing")
+	assert.NotContains(t, sms.Body(), "ack")
+	assert.Contains(t, sms.Body(), "http")
 }
