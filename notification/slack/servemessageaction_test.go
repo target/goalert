4f880f91a02b4a57b16185e96e548f12a0e95378
Nathaniel Caza <mastercactapus@gmail.com>
2022-10-14 13:05:06 -0500

    unit tests for signature generation

--- notification/slack/servemessageaction_test.go
+++ notification/slack/servemessageaction_test.go
@@ -3,8 +3,6 @@ package slack
 import (
 	"context"
 	"net/http"
-	"net/url"
-	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -12,28 +10,32 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/target/goalert/config"
+	"github.com/target/goalert/permission"
 )
 
 func TestValidateRequestSignature(t *testing.T) {
+	// Values pulled directly from: https://api.slack.com/authentication/verifying-requests-from-slack
 	var cfg config.Config
-	cfg.Slack.SigningSecret = "secret"
+	cfg.Slack.SigningSecret = "8f742231b10e8888abcd99yyyzzz85a5"
 
-	ts := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
-
-	v := make(url.Values)
-	v.Set("foo", "bar")
-
-	req, err := http.NewRequestWithContext(cfg.Context(context.Background()), "POST", "http://example.com", strings.NewReader(v.Encode()))
+	req, err := http.NewRequestWithContext(cfg.Context(context.Background()), "POST", "http://example.com", strings.NewReader("token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c"))
 	require.NoError(t, err)
 
-	req.Header.Set("X-Slack-Request-Timestamp", strconv.FormatInt(ts.Unix(), 10))
-	req.Header.Set("X-Slack-Signature", "v0=890648c7d227bf188d0261b6ab30587b0dbd39a3a1e24c83e38050aa6a3b6bb9")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("X-Slack-Request-Timestamp", "1531420618")
+	req.Header.Set("X-Slack-Signature", "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503")
 
-	err = validateRequestSignature(ts, req)
+	err = validateRequestSignature(time.Unix(1531420618, 0), req)
 	assert.NoError(t, err)
 
+	req, err = http.NewRequestWithContext(cfg.Context(context.Background()), "POST", "http://example.com", strings.NewReader("token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c"))
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("X-Slack-Request-Timestamp", "15314206189") // changed timestamp
+	req.Header.Set("X-Slack-Signature", "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503")
+
 	// different timestamp should invalidate the signature
-	req.Header.Set("X-Slack-Request-Timestamp", strconv.FormatInt(ts.Add(time.Second).Unix(), 10))
-	err = validateRequestSignature(ts, req)
-	assert.Error(t, err)
+	err = validateRequestSignature(time.Unix(1531420618, 0), req)
+	assert.True(t, permission.IsUnauthorized(err), "expected unauthorized error, got: %v", err)
 }
