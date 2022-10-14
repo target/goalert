e906019981c10f9cd86e5f8101d9eb3111ba3d8f
Nathaniel Caza <mastercactapus@gmail.com>
2022-10-14 10:50:44 -0500

    only impose printable requirement

--- validation/validate/jmespath.go
+++ validation/validate/jmespath.go
@@ -1,18 +1,21 @@
 package validate
 
 import (
+	"unicode"
+
 	"github.com/jmespath/go-jmespath"
 	"github.com/target/goalert/validation"
 )
 
 // JMESPath will validate a JMESPath expression.
 func JMESPath(fname, expression string) error {
-	err := Text(fname, expression, 0, 4096)
-	if err != nil {
-		return err
+	for _, c := range expression {
+		if !unicode.IsPrint(c) && c != '\t' && c != '\n' {
+			return validation.NewFieldError(fname, "only printable characters allowed")
+		}
 	}
 
-	_, err = jmespath.Compile(expression)
+	_, err := jmespath.Compile(expression)
 	if err != nil {
 		return validation.NewFieldError(fname, err.Error())
 	}
