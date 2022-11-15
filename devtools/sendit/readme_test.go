f6158d6f3397f14ce69d2f2a2ae2dfc88baba5cb
Nathaniel Caza <mastercactapus@gmail.com>
2022-11-15 10:51:00 -0600

    add functional test for sendit

---
+++ devtools/sendit/readme_test.go
@@ -0,0 +1,106 @@
+package sendit_test
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/target/goalert/devtools/sendit"
+)
+
+func logCmd(t *testing.T, name string, arg ...string) *exec.Cmd {
+	t.Helper()
+	t.Log("exec:", name, strings.Join(arg, " "))
+	return exec.Command(name, arg...)
+}
+
+// TestReadme is a test that runs the commands in the README and validates core functionality.
+func TestReadme(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+
+	const secret = "testing-secret"
+
+	cmd := logCmd(t, "go", "run", "./cmd/sendit-token",
+		"-secret", secret)
+	tokenData, err := cmd.Output()
+	require.NoError(t, err)
+	token := strings.TrimSpace(string(tokenData))
+
+	var c jwt.RegisteredClaims
+	tok, err := jwt.ParseWithClaims(token, &c, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	}, jwt.WithValidMethods([]string{"HS256"}))
+	require.NoError(t, err, "must be valid jwt")
+	assert.True(t, tok.Valid, "token must be valid")
+
+	assert.Equal(t, "sendit", c.Issuer)
+	assert.True(t, c.VerifyAudience(sendit.TokenAudienceAuth, true), "must have auth audience")
+	assert.True(t, c.VerifyIssuer(sendit.TokenIssuer, true), "must have issuer")
+
+	// start server
+	cmd = logCmd(t, "go", "run", "./cmd/sendit-server",
+		"-secret", secret,
+		"-addr", "localhost:0")
+	r, w := io.Pipe()
+	cmd.Stderr = io.MultiWriter(w, os.Stdout)
+	require.NoError(t, cmd.Start())
+	defer cmd.Process.Kill()
+
+	rd := bufio.NewReader(r)
+	s, err := rd.ReadString('\n')
+	go io.Copy(io.Discard, rd)
+	require.NoError(t, err)
+
+	_, srvAddr, ok := strings.Cut(strings.TrimSpace(s), "Listening: ")
+	require.True(t, ok, "must print Listening: <addr>")
+
+	testSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/server-prefix/test", r.URL.Path)
+		io.WriteString(w, "Hello, world!")
+	}))
+	defer testSrv.Close()
+
+	srcURL := fmt.Sprintf("http://%s/server-prefix", srvAddr)
+	// start client
+	cmd = logCmd(t, "go", "run", "./cmd/sendit",
+		"-token", token,
+		srcURL,
+		testSrv.URL,
+	)
+	r, w = io.Pipe()
+	cmd.Stderr = w
+	rd = bufio.NewReader(r)
+	require.NoError(t, cmd.Start())
+	defer cmd.Process.Kill()
+
+	for {
+		s, err = rd.ReadString('\n')
+		require.NoError(t, err)
+		t.Logf("client: %s", s)
+		if strings.Contains(s, "Ready") {
+			break
+		}
+	}
+	go io.Copy(io.Discard, rd)
+
+	resp, err := http.Get(srcURL + "/test")
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	data, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	assert.Equal(t, "Hello, world!", string(data))
+}
