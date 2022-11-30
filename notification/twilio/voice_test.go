e9f746a85482693b425dfaedd1b57d005773c815
Chris Marquis <christopher.marquis@target.com>
2022-11-30 10:20:51 -0600

    Finish the voice TestSent test and began work on testing the Twilio alert callback

--- notification/twilio/voice_test.go
+++ notification/twilio/voice_test.go
@@ -2,12 +2,15 @@ package twilio
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/target/goalert/config"
 	"github.com/target/goalert/notification"
 )
@@ -22,13 +25,49 @@ func TestSend(t *testing.T) {
 	mockConfig.Twilio.Enable = true
 	mockConfig.Twilio.VoiceName = "Polly.Joanna-Neural"
 	mockConfig.Twilio.VoiceLanguage = "en-US"
+	mockConfig.Twilio.FromNumber = "+16125551111"
 	ctx := mockConfig.Context(context.Background())
 
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		require.NoError(t, err)
+		to := ""
+		for k, v := range r.Form {
+			switch k {
+			case "To":
+				assert.Equal(t, "+16125551234", v[0])
+				to = v[0]
+			case "From":
+				assert.Equal(t, mockConfig.Twilio.FromNumber, v[0])
+			case "Url":
+				u, err := url.Parse(v[0])
+				require.NoError(t, err)
+				for queryKey, queryValue := range u.Query() {
+					switch queryKey {
+					case "msgBody":
+						assert.Len(t, queryValue, 3)
+						for i, msg := range queryValue {
+							b64enc = base64.URLEncoding.WithPadding(base64.NoPadding)
+							data, err := b64enc.DecodeString(msg)
+							require.NoError(t, err)
+							switch i {
+							case 0:
+								assert.Equal(t, "Hello! This is GoAlert with an alert notification.", string(data))
+							case 1:
+								assert.Equal(t, PAUSE, string(data))
+							case 2:
+								assert.Equal(t, "something happened.", string(data))
+							}
+						}
+					}
+				}
+			}
+		}
+
 		callResp := Call{
 			SID:       "CA0123456789abcdef",
-			To:        "+16125551234",
-			From:      "+16125550000",
+			To:        to,
+			From:      mockConfig.Twilio.FromNumber,
 			Status:    CallStatus("queued"),
 			Direction: "outbound",
 		}
@@ -44,14 +83,62 @@ func TestSend(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	result, err := voiceObj.Send(ctx, notification.Test{
+	result, err := voiceObj.Send(ctx, notification.Alert{
 		Dest: notification.Dest{
 			ID:    "1",
 			Type:  notification.DestTypeVoice,
 			Value: "+16125551234",
 		},
 		CallbackID: "test2",
+		AlertID:    2,
+		Summary:    "something happened",
+		Details:    "something bad happened",
 	})
 	assert.Equal(t, nil, err)
-	assert.Equal(t, nil, result)
+	assert.Equal(t, &notification.SentMessage{
+		ExternalID:   "CA0123456789abcdef",
+		State:        1,
+		StateDetails: "queued",
+		SrcValue:     mockConfig.Twilio.FromNumber,
+	}, result)
+}
+
+func TestTwilioCallback(t *testing.T) {
+	//todo see if we can get this working
+	t.Skip()
+	rec := httptest.NewRecorder()
+	mockConfig := config.Config{}
+	mockConfig.Twilio.Enable = true
+	mockConfig.Twilio.VoiceName = "Polly.Joanna-Neural"
+	mockConfig.Twilio.VoiceLanguage = "en-US"
+	mockConfig.Twilio.FromNumber = "+16125551111"
+	ctx := mockConfig.Context(context.Background())
+
+	req := httptest.NewRequest("GET", "/api/v2/twilio/call?type=alert", nil)
+	cfg := &Config{BaseURL: ""}
+	voiceObj, err := NewVoice(ctx, nil, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	handler := http.HandlerFunc(voiceObj.ServeAlert)
+	handler.ServeHTTP(rec, req)
+
+	resp := rec.Result()
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	//	assert.Contains(t, resp.Header.Get("Content-Type"), "application/xml")
+	//	data, err := io.ReadAll(resp.Body)
+	//	assert.NoError(t, err)
+	//	assert.Equal(t, `<?xml version="1.0" encoding="UTF-8"?>
+	//
+	// <Response>
+	//
+	//	<Say>
+	//		<prosody rate="slow">Hello</prosody>
+	//	</Say>
+	//	<Say>
+	//		<prosody rate="slow">Goodbye.</prosody>
+	//	</Say>
+	//	<Hangup></Hangup>
+	//
+	// </Response>`, string(data))
 }
