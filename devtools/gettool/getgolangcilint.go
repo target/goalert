3fa2e03614dc0abdc0a4bc074be2bec13d534f35
Nathaniel Caza <mastercactapus@gmail.com>
2022-12-07 11:02:36 -0600

    handle windows binary

--- devtools/gettool/getgolangcilint.go
+++ devtools/gettool/getgolangcilint.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"archive/tar"
+	"archive/zip"
 	"compress/gzip"
 	"fmt"
 	"runtime"
 )
 
 func getGolangCiLint(version, output string) error {
-    os := runtime.GOOS + "-" + runtime.GOARCH
-	url := fmt.Sprintf("https://github.com/golangci/golangci-lint/releases/download/v%s/golangci-lint-%s-%s.tar.gz",
-		version, version,
-		os,
+	ext := "tar.gz"
+	if runtime.GOOS == "windows" {
+		ext = "zip"
+	}
+
+	base := fmt.Sprintf("golangci-lint-%s-%s-%s", version, runtime.GOOS, runtime.GOARCH)
+	url := fmt.Sprintf("https://github.com/golangci/golangci-lint/releases/download/v%s/%s.%s",
+		version, base, ext,
 	)
 
-	fd, _, err := fetchFile(url)
+	fd, n, err := fetchFile(url)
 	if err != nil {
 		return fmt.Errorf("fetch: %w", err)
 	}
 	defer fd.Close()
+
+	if ext == "zip" {
+		z, err := zip.NewReader(fd, n)
+		if err != nil {
+			return fmt.Errorf("open zip: %w", err)
+		}
+		err = extractFromZip(z, fmt.Sprintf("%s/golangci-lint.exe", base), output, true)
+		if err != nil {
+			return fmt.Errorf("extract: %w", err)
+		}
+
+		return nil
+	}
 
 	gzr, err := gzip.NewReader(fd)
 	if err != nil {
