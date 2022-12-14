6857e7c9c8d0668798d3b6b9d4c4240f1b345aac
Nathaniel Caza <mastercactapus@gmail.com>
2022-12-14 10:51:40 -0600

    add assertion example

---
+++ devtools/mocktwilio/assertions_test.go
@@ -0,0 +1,43 @@
+package mocktwilio_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/target/goalert/devtools/mocktwilio"
+)
+
+func TestAssertSMS(t *testing.T) {
+	s := mocktwilio.NewServer(mocktwilio.Config{
+		AccountSID: "AC123",
+		AuthToken:  "abc123",
+	})
+	srv := httptest.NewServer(s)
+	defer srv.Close()
+
+	s.AddUpdateNumber(mocktwilio.Number{Number: "+12345678901"})
+
+	a := mocktwilio.NewAssertions(t, mocktwilio.AssertConfig{
+		ServerAPI:      s,
+		AppPhoneNumber: "+12345678901",
+	})
+
+	v := make(url.Values)
+	v.Set("Body", "Hello, world!")
+	v.Set("From", "+12345678901")
+	v.Set("To", "+23456789012")
+	resp, err := http.PostForm(srv.URL+"/2010-04-01/Accounts/AC123/Messages.json", v)
+	require.NoError(t, err)
+	if !assert.Equal(t, 201, resp.StatusCode) {
+		data, _ := io.ReadAll(resp.Body)
+		t.Log(string(data))
+		return
+	}
+
+	a.Device("+23456789012").ExpectSMS("Hello, world!")
+}
