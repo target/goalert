892e021b53de0bd80d9494f44791b575ecd036b7
Nathaniel Caza <mastercactapus@gmail.com>
2023-02-28 14:38:09 -0600

    cleanup rotation handoff

--- web/src/app/rotations/HandoffSummary.tsx
+++ web/src/app/rotations/HandoffSummary.tsx
@@ -1,3 +1,4 @@
+import { Duration } from 'luxon'
 import React from 'react'
 import { RotationType } from '../../schema'
 import { Time } from '../util/Time'
@@ -9,42 +10,63 @@ export interface HandoffSummaryProps {
   timeZone: string
 }
 
+export const HourlyHandoffSummary: React.FC<HandoffSummaryProps> = (props) => {
+  return (
+    <span>
+      Hands off every{' '}
+      {Duration.fromObject({ hours: props.shiftLength }).toHuman()} from{' '}
+      <Time time={props.start} zone={props.timeZone} />.
+    </span>
+  )
+}
+
+export const DailyHandoffSummary: React.FC<HandoffSummaryProps> = (props) => {
+  const prefix =
+    'Hands off every ' +
+    Duration.fromObject({ days: props.shiftLength }).toHuman() +
+    ' at '
+
+  return (
+    <Time
+      prefix={prefix}
+      time={props.start}
+      zone={props.timeZone}
+      format='clock'
+      suffix='.'
+    />
+  )
+}
+
+export const WeeklyHandoffSummary: React.FC<HandoffSummaryProps> = (props) => {
+  const prefix =
+    'Hands off every ' +
+    Duration.fromObject({ weeks: props.shiftLength }).toHuman() +
+    ' on '
+
+  return (
+    <Time
+      prefix={prefix}
+      time={props.start}
+      zone={props.timeZone}
+      format='weekday-clock'
+      suffix='.'
+    />
+  )
+}
+
 // handoffSummary returns the summary description for the rotation
 export const HandoffSummary: React.FC<HandoffSummaryProps> =
-  function HandoffSummary(rotation: HandoffSummaryProps): JSX.Element {
-    const tz = rotation.timeZone
+  function HandoffSummary(props: HandoffSummaryProps): JSX.Element {
+    if (!props.timeZone) return <span>Loading handoff information...</span>
 
-    if (!tz) return <span>Loading handoff information...</span>
-
-    if (rotation.type === 'hourly') {
-      return (
-        <span>
-          Hands off every{' '}
-          {rotation.shiftLength === 1
-            ? 'hour'
-            : rotation.shiftLength + ' hours'}
-          .
-        </span>
-      )
+    switch (props.type) {
+      case 'hourly':
+        return <HourlyHandoffSummary {...props} />
+      case 'daily':
+        return <DailyHandoffSummary {...props} />
+      case 'weekly':
+        return <WeeklyHandoffSummary {...props} />
     }
 
-    const unit = rotation.type === 'daily' ? 'day' : 'week'
-    const lengthDesc =
-      rotation.shiftLength === 1
-        ? unit
-        : rotation.shiftLength + ' ' + unit + 's'
-
-    const prefix = `Hands off every ${lengthDesc} ${
-      unit === 'day' ? 'at' : 'on'
-    } `
-
-    return (
-      <Time
-        prefix={prefix}
-        time={rotation.start}
-        zone={tz}
-        format={unit === 'day' ? 'clock' : 'weekday-clock'}
-        suffix='.'
-      />
-    )
+    throw new Error('unknown rotation type: ' + props.type)
   }
