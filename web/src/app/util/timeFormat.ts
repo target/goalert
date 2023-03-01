531ea59a1c0ddd07c5218548d52d0e65b53339b4
Nathaniel Caza <mastercactapus@gmail.com>
2023-03-01 10:30:39 -0600

    add deleted format func

--- web/src/app/util/timeFormat.ts
+++ web/src/app/util/timeFormat.ts
@@ -7,6 +7,38 @@ import {
 } from 'luxon'
 import { ExplicitZone } from './luxon-helpers'
 
+export function formatTimeSince(
+  _since: DateTime | string,
+  _now = DateTime.utc(),
+): string {
+  if (!_since) return ''
+  const since = _since instanceof DateTime ? _since : DateTime.fromISO(_since)
+  const now = _now instanceof DateTime ? _now : DateTime.fromISO(_now)
+  const diff = now.diff(since)
+
+  if (diff.as('minutes') < 1) {
+    return `< 1m ago`
+  }
+
+  if (diff.as('hours') < 1) {
+    return `${Math.floor(diff.as('minutes'))}m ago`
+  }
+
+  if (diff.as('days') < 1) {
+    return `${Math.floor(diff.as('hours'))}h ago`
+  }
+
+  if (diff.as('months') < 1) {
+    return `${Math.floor(diff.as('days'))}d ago`
+  }
+
+  if (diff.as('years') < 1) {
+    return `> ${Math.floor(diff.as('months'))}mo ago`
+  }
+
+  return `> ${Math.floor(diff.as('years'))}y ago`
+}
+
 export type TimeFormatOpts = {
   time: string | DateTime
   zone?: string
