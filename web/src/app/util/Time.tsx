4971669b63808e309184ca31668c3b5d00aece9b
Nathaniel Caza <mastercactapus@gmail.com>
2023-03-01 09:37:39 -0600

    cleanup Time

--- web/src/app/util/Time.tsx
+++ web/src/app/util/Time.tsx
@@ -33,54 +33,45 @@ const TimeTimestamp: React.FC<TimeTimestampProps> = (props) => {
   const local = formatTimestamp({ ...props, time, now, zone: 'local' })
 
   const title =
-    display === local
-      ? undefined
-      : formatTimestamp({ ...props, time, zone: 'local' }) + ' in local time'
-
+    formatTimestamp({ ...props, time, zone: 'local' }) + ' in local time'
   const zoneStr =
-    display === local
-      ? ''
-      : ' ' + DateTime.fromISO(time, { zone: props.zone }).toFormat('ZZZZ')
+    ' ' + DateTime.fromISO(time, { zone: props.zone }).toFormat('ZZZZ')
 
-  const hasValue = Boolean(props.time || props.zero)
+  const tag = props.time ? (
+    <time
+      dateTime={props.time}
+      title={display !== local ? title : undefined}
+      style={{
+        textDecorationStyle: 'dotted',
+        textDecorationLine: title ? 'underline' : 'none',
+      }}
+    >
+      {display}
+      {display !== local && zoneStr}
+    </time>
+  ) : (
+    props.zero
+  )
 
   return (
     <React.Fragment>
-      {hasValue && props.prefix}
-      {props.time ? (
-        <time
-          dateTime={props.time}
-          title={title}
-          style={{
-            textDecorationStyle: 'dotted',
-            textDecorationLine: title ? 'underline' : 'none',
-          }}
-        >
-          {display}
-          {zoneStr}
-        </time>
-      ) : (
-        props.zero
-      )}
-      {hasValue && props.suffix}
+      {tag && props.prefix}
+      {tag}
+      {tag && props.suffix}
     </React.Fragment>
   )
 }
 
 type TimeDurationProps = TimeBaseProps & {
   precise?: boolean
-} & { duration: DurationLikeObject | string | Duration }
+  duration: DurationLikeObject | string | Duration
+}
 
 const TimeDuration: React.FC<TimeDurationProps> = (props) => {
-  let dur: Duration
-  if ('duration' in props) {
-    dur =
-      typeof props.duration === 'string'
-        ? Duration.fromISO(props.duration)
-        : Duration.fromObject(props.duration)
-  } else {
-    dur = Duration.fromObject(props)
-  }
+  const dur =
+    typeof props.duration === 'string'
+      ? Duration.fromISO(props.duration)
+      : Duration.fromObject(props.duration)
 
   return (
     <React.Fragment>
@@ -88,13 +79,12 @@ const TimeDuration: React.FC<TimeDurationProps> = (props) => {
       <time dateTime={dur.toISO()}>
         {props.precise ? toRelativePrecise(dur) : dur.toHuman()}
       </time>
-
       {props.suffix}
     </React.Fragment>
   )
 }
 
-export type TimeProps = TimeDurationProps | TimeTimestampProps
+export type TimeProps = TimeTimestampProps | TimeDurationProps
 
 // Time will render a <time> element using Luxon to format the time.
 export const Time: React.FC<TimeProps> = (props) => {
