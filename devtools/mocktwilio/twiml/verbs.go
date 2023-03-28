0842f3e8cc77441ae294518ae21d362311e81dfb
Nathaniel Caza <mastercactapus@gmail.com>
2023-03-28 10:22:58 -0500

    add remaining verbs

--- devtools/mocktwilio/twiml/verbs.go
+++ devtools/mocktwilio/twiml/verbs.go
@@ -2,10 +2,104 @@ package twiml
 
 import (
 	"encoding/xml"
+	"time"
 )
 
 // Hangup is a verb that hangs up the call.
 type Hangup struct{}
+
+// The Pause verb waits silently for a specific number of seconds.
+//
+// If Pause is the first verb in a TwiML document, Twilio will wait the specified number of seconds before picking up the call.
+// https://www.twilio.com/docs/voice/twiml/pause
+type Pause struct {
+	// Dur derives from the `length` attribute and indicates the length of the pause.
+	//
+	// https://www.twilio.com/docs/voice/twiml/pause#attributes-length
+	Dur time.Duration `xml:"-"`
+}
+
+func (p *Pause) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	type PauseRaw Pause
+	var pp struct {
+		PauseRaw
+		Length NullInt `xml:"length,attr"`
+	}
+	if err := d.DecodeElement(&pp, &start); err != nil {
+		return err
+	}
+	*p = Pause(pp.PauseRaw)
+	if pp.Length.Valid {
+		p.Dur = time.Duration(pp.Length.Value) * time.Second
+	} else {
+		p.Dur = time.Second
+	}
+	return nil
+}
+
+func (p Pause) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	type PauseRaw Pause
+	var pp struct {
+		PauseRaw
+		Length *int `xml:"length,attr,omitempty"`
+	}
+	sec := int(p.Dur / time.Second)
+	pp.Length = &sec
+	pp.PauseRaw = PauseRaw(p)
+	return e.EncodeElement(pp, start)
+}
+
+// The Redirect verb redirects the current call to a new TwiML document.
+type Redirect struct {
+	// Method defaults to POST.
+	//
+	// https://www.twilio.com/docs/voice/twiml/redirect#attributes-method
+	Method string `xml:"method,attr,omitempty"`
+
+	// URL is the URL to redirect to.
+	URL string `xml:",chardata"`
+}
+
+func (r *Redirect) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	type RedirectRaw Redirect
+	var rr struct {
+		RedirectRaw
+	}
+	if err := d.DecodeElement(&rr, &start); err != nil {
+		return err
+	}
+	*r = Redirect(rr.RedirectRaw)
+	if r.Method == "" {
+		r.Method = "POST"
+	}
+	return nil
+}
+
+// The Reject verb rejects an incoming call.
+type Reject struct {
+	// Reason defaults to 'rejected'.
+	//
+	// https://www.twilio.com/docs/voice/twiml/reject#attributes-reason
+	Reason string `xml:"reason,attr,omitempty"`
+}
+
+type RejectReason int
+
+const (
+	RejectReasonRejected RejectReason = iota
+	RejectReasonBusy
+)
+
+func (r *Reject) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	if err := d.DecodeElement(&r, &start); err != nil {
+		return err
+	}
+	if r.Reason == "" {
+		r.Reason = "rejected"
+	}
+
+	return nil
+}
 
 // Say is a TwiML verb that plays back a message to the caller.
 type Say struct {
