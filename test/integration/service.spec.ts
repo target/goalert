a4e86737eb5bd4d8d35b786e32e0d67fa5767a8e
nataliekorzh <nkorzh@oberlin.edu>
2023-03-29 16:33:52 -0500

    filtering by label, value and integration key

--- test/integration/service.spec.ts
+++ test/integration/service.spec.ts
@@ -33,6 +33,44 @@ test('Service', async ({ page, isMobile }) => {
   // Lastly ensure there is a link to a policy named "<name> Policy"
   await expect(page.getByRole('link', { name: name + ' Policy' })).toBeVisible()
 
+  // Now let's edit the service name
+  await page.getByRole('button', { name: 'Edit' }).click()
+
+  name = 'pw-service ' + c.name()
+  await page.fill('input[name=name]', name)
+  await page.click('[role=dialog] button[type=submit]')
+
+  await expect(page.getByRole('heading', { name, level: 1 })).toBeVisible()
+
+  // Create a label and value
+  const key = `${c.word({ length: 4 })}/${c.word({ length: 3 })}`
+  const value = c.word({ length: 8 })
+  await page
+    .getByRole('link', { name: 'Labels Group together services' })
+    .click()
+  await page.getByTestId('create-label').click()
+  await page.getByPlaceholder('Start typing...').fill(key)
+  await page.getByText('Create "' + key + '"').click()
+  await page.getByLabel('Value', { exact: true }).fill(value)
+  await page.getByRole('button', { name: 'Submit' }).click()
+
+  // return to service
+  await page.getByRole('link', { name: name, exact: true }).click()
+
+  // make integration key
+  const intKey = c.word({ length: 5 }) + ' Key'
+  await page
+    .getByRole('link', {
+      name: 'Integration Keys Manage keys used to create alerts',
+    })
+    .click()
+  await page.getByTestId('create-key').click()
+  await page.getByLabel('Name').click()
+  await page.getByLabel('Name').fill(intKey)
+  await page.getByRole('button', { name: 'Submit' }).click()
+
+  await page.goto('./services')
+
   // Check that filter content doesn't exist yet
   await expect(
     page.getByRole('button', { name: 'filter-done' }),
@@ -44,32 +82,55 @@ test('Service', async ({ page, isMobile }) => {
 
   // check exists? or is that redundant
 
-  // filter by label key
-
-  // reset filters?
-
   // check that can't filter by value without a key
-  await expect(page.getByLabel('Select Label Value Key')).toBeDisabled()
+  await expect(page.getByLabel('Select Label Value')).toBeDisabled()
+
+  // filter by label key
+  await page
+    .locator('div')
+    .filter({ hasText: /^Select Label Key$/ })
+    .getByRole('button', { name: 'Open' })
+    .click()
+  await page.getByRole('option', { name: key }).getByRole('listitem').click()
+  await page.getByRole('button', { name: 'Done' }).click()
+
+  // check if filtered?
+  await expect(
+    page.getByRole('link', { name: name + ' ' + description }),
+  ).toBeVisible()
 
   // filter with key and value
+  await page.getByRole('button', { name: 'Search Services by Filters' }).click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Select Label Value$/ })
+    .getByRole('button', { name: 'Open' })
+    .click()
+  await page.getByRole('option', { name: value }).getByRole('listitem').click()
+  await page.getByRole('button', { name: 'Done' }).click()
+
+  await expect(
+    page.getByRole('link', { name: name + ' ' + description }),
+  ).toBeVisible()
+
+  // reset filters?
+  await page.getByRole('button', { name: 'Search Services by Filters' }).click()
+  await page.getByRole('button', { name: 'Reset' }).click()
 
   // filter by integration key
+  await page.getByRole('button', { name: 'Search Services by Filters' }).click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Select Integration Key$/ })
+    .getByRole('button', { name: 'Open' })
+    .click()
+  await page.getByRole('option', { name: intKey }).getByRole('listitem').click()
+  await page.getByRole('button', { name: 'Done' }).click()
 
   // load in filters from URL
 
   // close the filter
   // check its gone?
-
-  // Now let's edit the service name
-  await page.getByRole('button', { name: 'Edit' }).click()
-
-  name = 'pw-service ' + c.name()
-  await page.fill('input[name=name]', name)
-  await page.click('[role=dialog] button[type=submit]')
-
-  await expect(page.getByRole('heading', { name, level: 1 })).toBeVisible()
-
-  await page.goto('./services')
 
   // We should be on the services list page, so let's try searching for the service we just created. We add a space to the beginning of the name to ensure we are searching for the full name and not a substring.
   if (isMobile) {
