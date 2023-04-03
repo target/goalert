229a34f6c9e6198c21d4791cff3304c0a1e3f51b
Natalie Korzh <48296543+nataliekorzh@users.noreply.github.com>
2023-04-03 18:10:57 -0500

    Delete service-gen.spec.ts

--- test/integration/service-gen.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test'
-import { userSessionFile } from './lib'
-
-test.describe.configure({ mode: 'parallel' })
-test.use({ storageState: userSessionFile })
