4c9bb0d0784a221e123a9d193ae8ffffcacc21d8
Nathaniel Caza <mastercactapus@gmail.com>
2023-04-05 13:55:06 -0500

    send oncall notification with group update

--- notification/slack/usergrouperror.go
+++ notification/slack/usergrouperror.go
@@ -11,24 +11,6 @@ import (
 	"github.com/target/goalert/notification"
 )
 
-// userGroupErrorMissing is a template for when a user-group update fails because one or more users are missing from Slack.
-var userGroupErrorMissing = template.Must(template.New("userGroupErrorMissing").Parse(`Hey everyone! I couldn't update {{.GroupRef}} because I couldn't find the following user(s) in Slack: {{.MissingUserRefs}}
-
-If you could have them click the button below to connect their Slack account, that would be great! Hopefully I'll be able to update the user-group next time.
-
-{{.LinkedUserRefs}}`))
-
-// userGroupErrorEmpty is a template for when a user-group update fails because there are no users on-call.
-var userGroupErrorEmpty = template.Must(template.New("userGroupErrorMissing").Parse(`Hey everyone! I couldn't update {{.GroupRef}} because there is nobody on-call for {{.ScheduleRef}}.
-
-Since a Slack user-group cannot be empty, I'm going to leave it as-is for now.`))
-
-// userGroupErrorUpdate is a template for when a user-group update fails for any reason other than missing users.
-var userGroupErrorUpdate = template.Must(template.New("userGroupErrorMissing").Parse(`Hey everyone! I couldn't update {{.GroupRef}} because I ran into a problem. Maybe touch base with the GoAlert admin(s) to see if they can help? I'm sorry for the inconvenience!
-
-Here's the ID I left with the error in my logs so they can find it:
-{{.ErrorRef}}`))
-
 // userGroupError has information about an error that occurred while updating a user-group.
 type userGroupError struct {
 	ErrorID      uuid.UUID
@@ -36,7 +18,8 @@ type userGroupError struct {
 	ScheduleID   string
 	ScheduleName string
 	Missing      []notification.User
-	Linked       []string
+
+	OnCall string
 
 	callbackFunc func(string, ...url.Values) string
 }
@@ -61,15 +44,6 @@ func (e userGroupError) MissingUserRefs() string {
 	return strings.Join(refs, ", ")
 }
 
-// LinkedUserRefs returns a string that can be used to reference the linked users in a Slack message.
-func (e userGroupError) LinkedUserRefs() string {
-	var refs []string
-	for _, u := range e.Linked {
-		refs = append(refs, fmt.Sprintf("<@%s>", u))
-	}
-	return strings.Join(refs, " ")
-}
-
 // ScheduleRef returns a string that can be used to reference the schedule in a Slack message.
 func (e userGroupError) ScheduleRef() string {
 	urlStr := e.callbackFunc("schedules/" + url.PathEscape(e.ScheduleID))
@@ -80,3 +54,25 @@ func (e userGroupError) ScheduleRef() string {
 func slackLink(url, label string) string {
 	return fmt.Sprintf("<%s|%s>", slackutilsx.EscapeMessage(url), slackutilsx.EscapeMessage(label))
 }
+
+// userGroupErrorMissing is a template for when a user-group update fails because one or more users are missing from Slack.
+var userGroupErrorMissing = template.Must(template.New("userGroupErrorMissing").Parse(`{{.OnCall}}
+
+Hey everyone! I couldn't update {{.GroupRef}} because I couldn't find the following user(s) in Slack: {{.MissingUserRefs}}
+
+If you could have them click the button below to connect their Slack account, that would be great! Hopefully I'll be able to update the user-group next time.`))
+
+// userGroupErrorEmpty is a template for when a user-group update fails because there are no users on-call.
+var userGroupErrorEmpty = template.Must(template.New("userGroupErrorEmpty").Parse(`{{.OnCall}}
+
+Hey everyone! I couldn't update {{.GroupRef}} because there is nobody on-call for {{.ScheduleRef}}.
+
+Since a Slack user-group cannot be empty, I'm going to leave it as-is for now.`))
+
+// userGroupErrorUpdate is a template for when a user-group update fails for any reason other than missing users.
+var userGroupErrorUpdate = template.Must(template.New("userGroupErrorUpdate").Parse(`{{.OnCall}}
+
+Hey everyone! I couldn't update {{.GroupRef}} because I ran into a problem. Maybe touch base with the GoAlert admin(s) to see if they can help? I'm sorry for the inconvenience!
+
+Here's the ID I left with the error in my logs so they can find it:
+{{.ErrorRef}}`))
--- notification/slack/usergroupsender.go
+++ notification/slack/usergroupsender.go
@@ -78,69 +78,55 @@ func (s *UserGroupSender) Send(ctx context.Context, msg notification.Message) (*
 	ugID, chanID, _ := strings.Cut(t.Dest.Value, ":")
 	cfg := config.FromContext(ctx)
 
+	onCallMsg := renderOnCallNotificationMessage(t, userSlackIDs)
+	var stateDetails string
+
 	// If any users are missing, we need to abort and let the channel know.
-	if len(missing) > 0 {
+	switch {
+	case len(missing) > 0:
 		// TODO: add link action button to invite missing users
 		var buf bytes.Buffer
 		err := userGroupErrorMissing.Execute(&buf, userGroupError{
 			GroupID:      ugID,
 			Missing:      missing,
-			Linked:       slackUsers,
 			callbackFunc: cfg.CallbackURL,
+			OnCall:       onCallMsg,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("execute template: %w", err)
 		}
-		err = s.withClient(ctx, func(c *slack.Client) error {
-			_, _, err := c.PostMessageContext(ctx, chanID, slack.MsgOptionText(buf.String(), false))
-			if err != nil {
-				return fmt.Errorf("post message to channel '%s': %w", chanID, err)
-			}
-			return nil
-		})
-		if err != nil {
-			return nil, err
-		}
-		return &notification.SentMessage{State: notification.StateSent, StateDetails: "missing users, sent error to channel"}, nil
-	}
+		onCallMsg = buf.String()
+		stateDetails = "missing users, sent error to channel"
 
-	// If no users are on-call, we need to abort and let the channel know.
-	//
-	// This is because we can't update the user group with no members.
-	if len(slackUsers) == 0 {
+		// If no users are on-call, we need to abort and let the channel know.
+		//
+		// This is because we can't update the user group with no members.
+	case len(slackUsers) == 0:
 		var buf bytes.Buffer
 		err := userGroupErrorEmpty.Execute(&buf, userGroupError{
 			GroupID:      ugID,
 			ScheduleID:   t.ScheduleID,
 			ScheduleName: t.ScheduleName,
 			callbackFunc: cfg.CallbackURL,
+			OnCall:       onCallMsg,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("execute template: %w", err)
 		}
+		onCallMsg = buf.String()
+		stateDetails = "empty user-group, sent error to channel"
+	default:
 		err = s.withClient(ctx, func(c *slack.Client) error {
-			_, _, err := c.PostMessageContext(ctx, chanID, slack.MsgOptionText(buf.String(), false))
+			_, err := c.UpdateUserGroupMembersContext(ctx, ugID, strings.Join(slackUsers, ","))
 			if err != nil {
-				return fmt.Errorf("post message to channel '%s': %w", chanID, err)
+				return fmt.Errorf("update user group '%s': %w", ugID, err)
 			}
+
 			return nil
 		})
-		if err != nil {
-			return nil, err
-		}
-		return &notification.SentMessage{State: notification.StateSent, StateDetails: "empty user-group, sent error to channel"}, nil
 	}
 
-	err = s.withClient(ctx, func(c *slack.Client) error {
-		_, err := c.UpdateUserGroupMembersContext(ctx, ugID, strings.Join(slackUsers, ","))
-		if err != nil {
-			return fmt.Errorf("update user group '%s': %w", ugID, err)
-		}
-
-		return nil
-	})
-
-	// If there was an error, we need to abort and let the channel know.
+	// If there was an error, we need to let the channel know.
 	if err != nil {
 		errID := uuid.New()
 		log.Log(log.WithField(ctx, "SlackUGErrorID", errID), err)
@@ -148,22 +134,26 @@ func (s *UserGroupSender) Send(ctx context.Context, msg notification.Message) (*
 		err := userGroupErrorUpdate.Execute(&buf, userGroupError{
 			ErrorID: errID,
 			GroupID: ugID,
+			OnCall:  onCallMsg,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("execute template: %w", err)
 		}
-		err = s.withClient(ctx, func(c *slack.Client) error {
-			_, _, err := c.PostMessageContext(ctx, chanID, slack.MsgOptionText(buf.String(), false))
-			if err != nil {
-				return fmt.Errorf("post message to channel '%s': %w", chanID, err)
-			}
-			return nil
-		})
-		if err != nil {
-			return nil, err
-		}
-		return &notification.SentMessage{State: notification.StateSent, StateDetails: "failed to update user-group, sent error to channel and log"}, nil
+		onCallMsg = buf.String()
+		stateDetails = "failed to update user-group, sent error to channel and log"
 	}
 
-	return &notification.SentMessage{State: notification.StateDelivered}, nil
+	var ts string
+	err = s.withClient(ctx, func(c *slack.Client) error {
+		_, ts, err = c.PostMessageContext(ctx, chanID, slack.MsgOptionText(onCallMsg, false))
+		if err != nil {
+			return fmt.Errorf("post message to channel '%s': %w", chanID, err)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &notification.SentMessage{State: notification.StateDelivered, ExternalID: ts, StateDetails: stateDetails}, nil
 }
