3a24f89dfb054f7fbfe3c230f2b5884bc97c8fd4
Nathaniel Caza <mastercactapus@gmail.com>
2023-05-23 11:09:50 -0500

    use explicit types for DT (instead of relying on isValid)

--- web/src/app/schedules/temp-sched/TempSchedDialog.tsx
+++ web/src/app/schedules/temp-sched/TempSchedDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { useMutation, gql } from '@apollo/client'
 import Checkbox from '@mui/material/Checkbox'
 import DialogContentText from '@mui/material/DialogContentText'
