fa250a81e7ae8e27405aa543ac49409f2ad89d7e
Nathaniel Caza <mastercactapus@gmail.com>
2023-05-23 12:17:13 -0500

    handle start/end time mismatch

--- web/src/app/schedules/temp-sched/TempSchedDialog.tsx
+++ web/src/app/schedules/temp-sched/TempSchedDialog.tsx
@@ -258,9 +258,28 @@ export default function TempSchedDialog({
           optionalLabels
           disabled={loading}
           value={value}
-          onChange={(newValue: TempSchedValue) =>
+          onChange={(newValue: TempSchedValue) => {
+            const oldDur = DateTime.fromISO(value.end).diff(
+              DateTime.fromISO(value.start),
+            )
+            if (value.start !== newValue.start) {
+              const oldEnd = DateTime.fromISO(value.end)
+              const newStart = DateTime.fromISO(newValue.start)
+              if (newStart >= oldEnd) {
+                // if start time is put after end time, move end time forward
+                newValue.end = newStart.plus(oldDur).toISO()
+              }
+            } else if (value.end !== newValue.end) {
+              const newEnd = DateTime.fromISO(newValue.end)
+              const start = DateTime.fromISO(newValue.start)
+              if (newEnd <= start) {
+                // if end time is put before start time, move start time back
+                newValue.start = newEnd.minus(oldDur).toISO()
+              }
+            }
+
             setValue({ ...value, ...newValue })
-          }
+          }}
         >
           <Grid
             container
