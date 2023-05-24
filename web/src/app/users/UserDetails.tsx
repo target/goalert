fba31068e4abc276aa39d7795150dc2344c6f84a
AllenDing <Allen.Ding@target.com>
2023-05-24 09:47:45 -0500

    change user edit dialog icon from key to lock

--- web/src/app/users/UserDetails.tsx
+++ web/src/app/users/UserDetails.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useQuery, gql } from 'urql'
 import Delete from '@mui/icons-material/Delete'
-import KeyIcon from '@mui/icons-material/Key'
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 import DetailsPage from '../details/DetailsPage'
 import { UserAvatar } from '../util/avatars'
 import UserContactMethodList from './UserContactMethodList'
@@ -166,7 +166,7 @@ export default function UserDetails(props: {
   if (isAdmin || userID === currentUserID) {
     options.unshift({
       label: 'Edit',
-      icon: <KeyIcon />,
+      icon: <LockOpenIcon />,
       handleOnClick: () => setShowEdit(true),
     })
   }
