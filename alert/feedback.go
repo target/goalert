80b629cf897f2268d6ae2b40d4b6b249c0dcaac5
Nathaniel Cook <nathanieljcook@outlook.com>
2023-06-20 13:23:11 -0700

    remove comment

--- alert/feedback.go
+++ alert/feedback.go
@@ -2,7 +2,6 @@ package alert
 
 // Feedback represents user provided information about a given alert
 type Feedback struct {
-	// ID is the ID of the alert.
 	AlertID   int
 	Sentiment int
 	Note      string
