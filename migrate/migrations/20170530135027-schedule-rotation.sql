af86af39c57f80b6706513403ef5266b5671eb39
AllenDing <Allen.Ding@target.com>
2023-08-22 12:27:42 -0500

    revert migration update, create new migration

--- migrate/migrations/20170530135027-schedule-rotation.sql
+++ migrate/migrations/20170530135027-schedule-rotation.sql
@@ -11,7 +11,6 @@ INSERT INTO schedules (id, name, description, time_zone)
     SELECT id::UUID, name, description, 'America/Chicago' FROM schedule;
 
 CREATE TYPE enum_rotation_type AS ENUM (
-    'monthly',
     'weekly',
     'daily',
     'hourly'
