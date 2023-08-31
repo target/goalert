27a52a2ee97e519597133682ee678d3b65c12268
Nathaniel Caza <mastercactapus@gmail.com>
2023-08-31 09:28:17 -0500

    update harness to use v5

--- test/smoke/harness/harness.go
+++ test/smoke/harness/harness.go
@@ -21,8 +21,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/stdlib"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
