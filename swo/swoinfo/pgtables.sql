682dbcd749314c371ff37eee748caef3f32228f8
Nathaniel Caza <mastercactapus@gmail.com>
2023-08-31 14:03:19 -0500

    formatting

--- swo/swoinfo/pgtables.sql
+++ swo/swoinfo/pgtables.sql
@@ -1,6 +1,7 @@
 -- just for type info
-CREATE TABLE pg_stat_activity (
-    state TEXT,
+CREATE TABLE pg_stat_activity(
+    state text,
     XACT_START timestamptz NOT NULL,
-    application_name TEXT
+    application_name text
 );
+
--- swo/swosync/changelog.sql
+++ swo/swosync/changelog.sql
@@ -1,49 +1,35 @@
-CREATE UNLOGGED TABLE change_log (
-    id BIGSERIAL PRIMARY KEY,
-    table_name TEXT NOT NULL,
-    row_id TEXT NOT NULL
+CREATE UNLOGGED TABLE change_log(
+    id bigserial PRIMARY KEY,
+    table_name text NOT NULL,
+    row_id text NOT NULL
 );
 
-ALTER TABLE change_log
-SET (
-        autovacuum_enabled = FALSE,
-        toast.autovacuum_enabled = FALSE
-    );
+ALTER TABLE change_log SET (autovacuum_enabled = FALSE, toast.autovacuum_enabled = FALSE);
 
-CREATE
-OR REPLACE FUNCTION fn_process_change_log() RETURNS TRIGGER AS $$
-DECLARE cur_state enum_switchover_state := 'idle';
-
+CREATE OR REPLACE FUNCTION fn_process_change_log()
+    RETURNS TRIGGER
+    AS $$
+DECLARE
+    cur_state enum_switchover_state := 'idle';
 BEGIN
-SELECT INTO cur_state current_state
-FROM switchover_state;
-
-IF cur_state != 'in_progress' THEN RETURN NEW;
-
-END IF;
-
-IF (TG_OP = 'DELETE') THEN
-INSERT INTO change_log (table_name, row_id)
-VALUES (
-        TG_TABLE_NAME,
-        cast(OLD .id AS TEXT)
-    );
-
-RETURN OLD;
-
-ELSE
-INSERT INTO change_log (table_name, row_id)
-VALUES (
-        TG_TABLE_NAME,
-        cast(NEW .id AS TEXT)
-    );
-
-RETURN NEW;
-
-END IF;
-
-RETURN NULL;
-
+    SELECT
+        INTO cur_state current_state
+    FROM
+        switchover_state;
+    IF cur_state != 'in_progress' THEN
+        RETURN NEW;
+    END IF;
+    IF (TG_OP = 'DELETE') THEN
+        INSERT INTO change_log(table_name, row_id)
+            VALUES (TG_TABLE_NAME, cast(OLD.id AS text));
+        RETURN OLD;
+    ELSE
+        INSERT INTO change_log(table_name, row_id)
+            VALUES (TG_TABLE_NAME, cast(NEW.id AS text));
+        RETURN NEW;
+    END IF;
+    RETURN NULL;
 END;
+$$
+LANGUAGE 'plpgsql';
 
-$$ LANGUAGE 'plpgsql'
