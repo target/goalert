9d84cd61ed86bc9d3505f7b6db2a6cc9f5ff1e56
Nathaniel Caza <mastercactapus@gmail.com>
2023-09-07 11:44:00 -0500

    update tables

---
+++ migrate/migrations/20230907112347-graphql-api-key.sql
@@ -0,0 +1,28 @@
+-- +migrate Up
+CREATE TABLE gql_api_keys(
+    id uuid PRIMARY KEY,
+    name text NOT NULL UNIQUE,
+    description text NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    created_by uuid REFERENCES users(id) ON DELETE SET NULL,
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_by uuid REFERENCES users(id) ON DELETE SET NULL,
+    policy jsonb NOT NULL,
+    expires_at timestamp with time zone NOT NULL
+);
+
+CREATE TABLE gql_api_key_usage(
+    id bigserial PRIMARY KEY,
+    api_key_id uuid REFERENCES gql_api_keys(id) ON DELETE CASCADE,
+    used_at timestamp with time zone NOT NULL DEFAULT now(),
+    user_agent text,
+    ip_address inet
+);
+
+CREATE INDEX idx_gql_most_recent_use ON gql_api_key_usage(api_key_id, used_at DESC);
+
+-- +migrate Down
+DROP TABLE gql_api_key_usage;
+
+DROP TABLE gql_api_keys;
+
