3fcc9bceb2bf64e87bcc174154b9214fd2e7a025
Nathaniel Caza <mastercactapus@gmail.com>
2023-10-05 11:51:20 -0500

    simplify api key form

--- web/src/app/admin/admin-api-keys/AdminAPIKeyForm.tsx
+++ web/src/app/admin/admin-api-keys/AdminAPIKeyForm.tsx
@@ -1,67 +1,40 @@
-import React, { useState, useEffect, SyntheticEvent } from 'react'
+import React from 'react'
 import Grid from '@mui/material/Grid'
 import { FormContainer, FormField } from '../../forms'
 import { FieldError } from '../../util/errutil'
-import { GQLAPIKey } from '../../../schema'
+import { CreateGQLAPIKeyInput, UpdateGQLAPIKeyInput } from '../../../schema'
 import AdminAPIKeyExpirationField from './AdminAPIKeyExpirationField'
 import { gql, useQuery } from 'urql'
 import { GenericError } from '../../error-pages'
 import Spinner from '../../loading/components/Spinner'
-import { TextField, Autocomplete, MenuItem } from '@mui/material'
-import CheckIcon from '@mui/icons-material/Check'
-import { DateTime } from 'luxon'
+import { TextField, MenuItem } from '@mui/material'
+import MaterialSelect from '../../selection/MaterialSelect'
 
 const query = gql`
   query ListGQLFieldsQuery {
     listGQLFields
   }
 `
-const MaxDetailsLength = 6 * 1024 // 6KiB
 
-// property object for this component
-interface AdminAPIKeyFormProps {
-  value: GQLAPIKey
-  errors: FieldError[]
-  onChange: (key: GQLAPIKey) => void
-  disabled?: boolean
-  reqAllowFieldsFlag: boolean
-  setReqAllowFieldsFlag: (param: boolean) => void
-  create: boolean
+type EditProps = {
+  value: UpdateGQLAPIKeyInput
+  onChange: (key: UpdateGQLAPIKeyInput) => void
+  create?: false
 }
+
+type CreateProps = {
+  value: CreateGQLAPIKeyInput
+  onChange: (key: CreateGQLAPIKeyInput) => void
+  create: true
+}
+
+type AdminAPIKeyFormProps = {
+  errors: FieldError[]
+} & (EditProps | CreateProps)
 
 export default function AdminAPIKeyForm(
   props: AdminAPIKeyFormProps,
 ): JSX.Element {
-  const { ...containerProps } = props
-  const [expiresAt, setExpiresAt] = useState<string>(
-    DateTime.now().plus({ days: 7 }).toLocaleString({
-      weekday: 'short',
-      month: 'short',
-      day: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    }),
-  )
-  const [allowedFields, setAllowedFields] = useState<string[]>([])
-  // handle AllowedFields field option changes: sets selected value to state
-  const handleAutocompleteChange = (
-    event: SyntheticEvent<Element, Event>,
-    value: string[],
-  ): void => {
-    setAllowedFields(value)
-  }
-  // sets GQLAPIKey updated value to state
-  useEffect(() => {
-    if (props.create) {
-      const valTemp = props.value
-      valTemp.expiresAt = new Date(expiresAt).toISOString()
-      valTemp.allowedFields = allowedFields
-      props.setReqAllowFieldsFlag(valTemp.allowedFields.length <= 0)
-      props.onChange(valTemp)
-    }
-  })
   const [{ data, fetching, error }] = useQuery({
     query,
   })
@@ -75,29 +48,20 @@ export default function AdminAPIKeyForm(
   }
 
   return (
-    <FormContainer {...containerProps}>
+    <FormContainer {...props}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <FormField
-            fullWidth
-            label='Name'
-            name='name'
-            required
-            component={TextField}
-            value={props.value.name}
-          />
+          <FormField fullWidth name='name' required component={TextField} />
         </Grid>
         <Grid item xs={12}>
           <FormField
             fullWidth
-            label='Description'
             name='description'
             multiline
             rows={4}
             required
             component={TextField}
-            charCount={MaxDetailsLength}
-            value={props.value.description}
+            charCount={255}
             hint='Markdown Supported'
           />
         </Grid>
@@ -107,10 +71,8 @@ export default function AdminAPIKeyForm(
             component={TextField}
             select
             required
-            label='Role'
             name='role'
             disabled={!props.create}
-            value={props.value.role}
           >
             <MenuItem value='user' key='user'>
               User
@@ -121,44 +83,29 @@ export default function AdminAPIKeyForm(
           </FormField>
         </Grid>
         <Grid item xs={12}>
-          <AdminAPIKeyExpirationField
-            setValue={setExpiresAt}
-            value={props.value.expiresAt}
+          <FormField
+            fullWidth
+            component={AdminAPIKeyExpirationField}
+            select
+            required
+            name='expiresAt'
             disabled={!props.create}
           />
         </Grid>
         <Grid item xs={12}>
-          <Autocomplete
-            multiple
-            options={data.listGQLFields}
-            getOptionLabel={(option: string) => option}
-            onChange={handleAutocompleteChange}
+          <FormField
+            fullWidth
+            component={MaterialSelect}
+            name='allowedFields'
             disabled={!props.create}
+            clientSideFilter
             disableCloseOnSelect
-            defaultValue={props.value.allowedFields}
-            renderInput={(params) => (
-              <FormField
-                {...params}
-                fullWidth
-                label='Allowed Fields'
-                name='allowedFields'
-                required={props.reqAllowFieldsFlag && props.create}
-                component={TextField}
-                disabled={!props.create}
-              />
-            )}
-            renderOption={(props, option, { selected }) => (
-              <MenuItem
-                {...props}
-                key={option}
-                value={option}
-                sx={{ justifyContent: 'space-between' }}
-              >
-                {option}
-                {selected ? <CheckIcon color='info' /> : null}
-              </MenuItem>
-            )}
-            style={{ width: '100%' }}
+            optionsLimit={10}
+            options={data.listGQLFields.map((field: string) => ({
+              label: field,
+              value: field,
+            }))}
+            multiple
           />
         </Grid>
       </Grid>
