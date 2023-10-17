fe6b794503f8d3b61b793d069c8b5159c87b83a7
Nathaniel Caza <mastercactapus@gmail.com>
2023-10-17 14:08:31 -0500

    fix down migration

--- migrate/migrations/20231017112713-api-soft-delete-name.sql
+++ migrate/migrations/20231017112713-api-soft-delete-name.sql
@@ -8,8 +8,7 @@ WHERE
     deleted_at IS NULL;
 
 -- +migrate Down
-ALTER TABLE gql_api_keys
-    DROP CONSTRAINT gql_api_keys_name_key;
+DROP INDEX gql_api_keys_name_key;
 
 CREATE UNIQUE INDEX gql_api_keys_name_key ON public.gql_api_keys(name);
 
