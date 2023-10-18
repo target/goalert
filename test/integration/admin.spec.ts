ac8aee2c9bd448b55eb30a52f83b9396b88a50c1
KatriannaSydlik-Badgerow <Katrianna.SydlikBadgerow@target.com>
2023-10-18 10:07:28 -0500

    update tests

--- test/integration/admin.spec.ts
+++ test/integration/admin.spec.ts
@@ -7,22 +7,40 @@ test.describe.configure({ mode: 'parallel' })
 test.use({ storageState: adminSessionFile })
 
 test('Admin', async ({ page }) => {
-  const testMetricsOverview = async (text: string): Promise<void> => {
-    await expect(
-      page.locator('.MuiCard-root', {
-        has: page.locator('div').filter({ hasText: text }),
-      }),
-    ).toBeVisible()
-  }
-
   const name = 'pw-service ' + c.name()
   const description = c.sentence()
   await createService(page, name, description)
 
   await page.goto('./admin/service-metrics')
 
-  await testMetricsOverview('1Total Services')
-  await testMetricsOverview('1Services Missing Integrations')
-  await testMetricsOverview('0Services Missing Notifications')
-  await testMetricsOverview('0Services Reaching Alert Limit')
+  // wait for services to finish loading
+  await expect(page.locator('.MuiDataGrid-overlay')).toHaveCount(0)
+
+  // get totalServices count
+  const totalServices = await page
+    .locator('.MuiCardHeader-title')
+    .nth(0)
+    .innerText()
+  expect(parseInt(totalServices)).toBeGreaterThan(0)
+
+  // get services missing integrations count
+  const missingInts = await page
+    .locator('.MuiCardHeader-title')
+    .nth(1)
+    .innerText()
+  expect(parseInt(missingInts)).toBeGreaterThan(0)
+
+  // get services missing notifications count
+  const missingNotifs = await page
+    .locator('.MuiCardHeader-title')
+    .nth(2)
+    .innerText()
+  expect(missingNotifs).toBe('0')
+
+  // get services reaching alert limit
+  const alertLimit = await page
+    .locator('.MuiCardHeader-title')
+    .nth(3)
+    .innerText()
+  expect(alertLimit).toBe('0')
 })
