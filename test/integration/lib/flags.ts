a5b7cdf3b865d01cf82b7cefb5afb48ea000e4b7
Nathaniel Caza <mastercactapus@gmail.com>
2023-11-01 17:30:27 -0500

    fix experimental flag handling in playwright

--- test/integration/lib/flags.ts
+++ test/integration/lib/flags.ts
@@ -16,3 +16,32 @@ export function baseURLFromFlags(flags: string[]): string {
 
   return srv.url.replace(/\/health$/, '')
 }
+
+const validFlagRx = /^[a-z-]+$/
+
+// collectFlags will return a list of all unique flag combinations used in the
+// provided data.
+export function collectFlags(data: string): Array<string> {
+  const flags = new Set<string>()
+
+  while (true) {
+    const idx = data.indexOf('baseURLFromFlags(')
+    if (idx < 0) break
+
+    data = data.slice(idx + 17)
+    const end = data.indexOf(')')
+    if (end < 0) break
+
+    const flagSet = JSON.parse(data.slice(0, end).replace(/'/g, '"'))
+    if (!Array.isArray(flagSet)) continue
+
+    flagSet.sort().forEach((f) => {
+      if (!validFlagRx.test(f)) throw new Error('invalid flag ' + f)
+    })
+    flags.add(flagSet.sort().join(','))
+
+    data = data.slice(end)
+  }
+
+  return Array.from(flags.keys()).sort()
+}
