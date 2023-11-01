3830bdd51ea4e1de99cfc22a3e8e1efcca899d2c
Nathaniel Caza <mastercactapus@gmail.com>
2023-11-01 12:15:11 -0500

    add api key integration test

---
+++ test/integration/gqlapikeys.spec.ts
@@ -0,0 +1,152 @@
+import { test, expect } from '@playwright/test'
+import { adminSessionFile, baseURLFromFlags } from './lib'
+import Chance from 'chance'
+const c = new Chance()
+
+test.describe.configure({ mode: 'parallel' })
+test.use({ storageState: adminSessionFile })
+
+// 1. Create a new API key (using query)
+// 2. Verify the API key is listed in the table
+// 3. Duplicate the API key
+// 4. Verify the duplicate API key is listed in the table
+// 5. Use the duplicate to delete the original
+// 6. Verify the original is no longer listed in the table
+// 7. Verify deleting the duplicate using the original fails (key deleted)
+// 8. Delete the duplicate via the UI
+
+const query = `
+query ListAPIKeys {
+    gqlAPIKeys {
+        id
+        name
+    }
+}
+
+mutation DeleteAPIKey($id: ID!) {
+    deleteGQLAPIKey(id: $id)
+}
+`
+
+test('GQL API keys', async ({ page, request }) => {
+  const baseName =
+    'apikeytest ' +
+    c.string({ length: 12, casing: 'lower', symbols: false, alpha: true })
+
+  // add 3 as a suffix so that the duplicate code will increment it to 4, and we can distinguish it from the original.
+  const originalName = baseName + ' 3'
+  const duplicateName = baseName + ' 4'
+
+  const descrtiption = c.sentence({ words: 5 })
+
+  await page.goto(baseURLFromFlags(['gql-api-keys']))
+
+  // click on Admin, then API Keys
+  await page.click('text=Admin')
+  await page.click('text=API Keys')
+  await page.click('text=Create API Key')
+
+  await page.fill('[name="name"]', originalName)
+  await page.fill('[name="description"]', descrtiption)
+  await page.click('[aria-haspopup="listbox"]')
+  // click the li with the text "Admin"
+  await page.click('li:text("Admin")')
+
+  await page.click('text=example query')
+  await page.fill('[name="query"]', query)
+
+  await page.click('text=Submit')
+
+  // get the token from `[aria-label="Copy"]`
+  const originalToken = await page.textContent('[aria-label="Copy"]')
+
+  await page.click('text=Okay')
+
+  // expect we have a <p> tag with the name as the text
+  await expect(page.locator('p', { hasText: originalName })).toBeVisible()
+
+  // click on it to open the drawer
+  await page.locator('p', { hasText: originalName }).click()
+
+  await page.click('text=Duplicate')
+  await page.click('text=Submit')
+
+  const duplicateToken = await page.textContent('[aria-label="Copy"]')
+
+  await page.click('text=Okay')
+
+  await expect(page.locator('p', { hasText: duplicateName })).toBeVisible()
+
+  const gqlURL =
+    baseURLFromFlags(['gql-api-keys']).replace(/\/$/, '') + '/api/graphql'
+  let resp = await request.post(gqlURL, {
+    headers: {
+      Authorization: `Bearer ${duplicateToken}`,
+    },
+    data: { query, operationName: 'ListAPIKeys' },
+  })
+
+  expect(resp.status()).toBe(200)
+  const data = await resp.json()
+
+  expect(data).toHaveProperty('data')
+  expect(data.data).toHaveProperty('gqlAPIKeys')
+
+  const originalID = data.data.gqlAPIKeys.find(
+    (key: any) => key.name === originalName,
+  ).id
+  const duplicateID = data.data.gqlAPIKeys.find(
+    (key: any) => key.name === duplicateName,
+  ).id
+  expect(originalID).toBeDefined()
+  expect(duplicateID).toBeDefined()
+
+  // Delete the original using the duplicate via fetch call
+  resp = await request.post(gqlURL, {
+    headers: {
+      Authorization: `Bearer ${duplicateToken}`,
+    },
+    data: {
+      query,
+      operationName: 'DeleteAPIKey',
+      variables: {
+        id: originalID,
+      },
+    },
+  })
+
+  expect(resp.status()).toBe(200)
+
+  await page.reload()
+
+  await expect(page.locator('p', { hasText: originalName })).not.toBeVisible()
+
+  // Delete the original using the duplicate via fetch call
+  resp = await request.post(gqlURL, {
+    headers: {
+      Authorization: `Bearer ${originalToken}`,
+    },
+    data: {
+      query,
+      operationName: 'DeleteAPIKey',
+      variables: {
+        id: duplicateID,
+      },
+    },
+  })
+
+  // expect the delete to fail, since the original was already deleted
+  expect(resp.status()).toBe(401)
+
+  // Delete the duplicate via the UI menu
+  // find a div with a <p> tag with the text duplicateName, then click [aria-label="Other Actions"]
+  await page
+    .locator('li', { hasText: duplicateName })
+    .locator('[aria-label="Other Actions"]')
+    .click()
+  await page.locator('[role=menuitem]', { hasText: 'Delete' }).click()
+  await page.click('text=Confirm')
+
+  // expect the duplicate to be gone
+  await expect(page.locator('p', { hasText: duplicateName })).not.toBeVisible()
+})
