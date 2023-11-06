aa59b6bb2994063957e65b4d302d129cc31b8732
Nathaniel Caza <mastercactapus@gmail.com>
2023-11-06 16:05:36 -0600

    add new types to queries

--- label/queries.sql
+++ label/queries.sql
@@ -11,17 +11,23 @@ SELECT
 FROM
     labels
 WHERE
-    tgt_service_id = $1;
+    tgt_service_id = sqlc.narg(service_id)::uuid
+    OR tgt_schedule_id = sqlc.narg(schedule_id)::uuid
+    OR tgt_rotation_id = sqlc.narg(rotation_id)::uuid
+    OR tgt_ep_id = sqlc.narg(ep_id)::uuid;
 
 -- name: LabelDeleteKeyByTarget :exec
 DELETE FROM labels
 WHERE key = $1
-    AND tgt_service_id = $2;
+    AND (tgt_service_id = sqlc.narg(service_id)::uuid,
+        OR tgt_schedule_id = sqlc.narg(schedule_id)::uuid,
+        OR tgt_rotation_id = sqlc.narg(rotation_id)::uuid,
+        OR tgt_ep_id = sqlc.narg(ep_id)::uuid);
 
 -- name: LabelSetByTarget :exec
-INSERT INTO labels(key, value, tgt_service_id)
-    VALUES ($1, $2, $3)
-ON CONFLICT (key, tgt_service_id)
+INSERT INTO labels(key, value, tgt_service_id, tgt_schedule_id, tgt_rotation_id, tgt_ep_id)
+    VALUES ($1, $2, sqlc.narg(service_id)::uuid, sqlc.narg(schedule_id)::uuid, sqlc.narg(rotation_id)::uuid, sqlc.narg(ep_id)::uuid)
+ON CONFLICT (key, tgt_service_id, tgt_schedule_id, tgt_rotation_id, tgt_ep_id)
     DO UPDATE SET
         value = $2;
 
