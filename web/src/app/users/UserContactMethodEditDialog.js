756fd12479d8858d168531407096dc0fd0e39b9d
KatriannaSydlik-Badgerow <Katrianna.SydlikBadgerow@target.com>
2023-11-16 13:55:06 -0600

    use urql

--- web/src/app/users/UserContactMethodEditDialog.js
+++ web/src/app/users/UserContactMethodEditDialog.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
-import { gql } from '@apollo/client'
+import { gql, useMutation, useQuery } from 'urql'
 import p from 'prop-types'
-import { Mutation } from '@apollo/client/react/components'
 import { fieldErrors, nonFieldErrors } from '../util/errutil'
 import FormDialog from '../dialogs/FormDialog'
 import UserContactMethodForm from './UserContactMethodForm'
-import Query from '../util/Query'
 import { pick } from 'lodash'
 
 const query = gql`
@@ -30,66 +28,58 @@ export default function UserContactMethodEditDialog({
   onClose,
   contactMethodID,
 }) {
+  const [{ data, fetching }] = useQuery({
+    query,
+    variables: { id: contactMethodID },
+  })
+
+  const defaultValue = {
+    name: data.userContactMethod.name,
+    type: data.userContactMethod.type,
+    value: data.userContactMethod.value,
+    statusUpdates: data.userContactMethod.statusUpdates,
+  }
+
+  const [mutationStatus, commit] = useMutation(mutation)
+  const { error } = mutationStatus
   const [value, setValue] = useState(null)
 
-  function renderDialog(commit, status, defaultValue) {
-    const { loading, error } = status
-    const fieldErrs = fieldErrors(error)
-
-    return (
-      <FormDialog
-        title='Edit Contact Method'
-        loading={loading}
-        errors={nonFieldErrors(error)}
-        onClose={onClose}
-        onSubmit={() => {
-          const updates = pick(value, 'name', 'statusUpdates')
-          // the form uses the 'statusUpdates' enum but the mutation simply
-          // needs to know if the status updates should be enabled or not via
-          // the 'enableStatusUpdates' boolean
-          if ('statusUpdates' in updates) {
-            delete Object.assign(updates, {
-              enableStatusUpdates: updates.statusUpdates === 'ENABLED',
-            }).statusUpdates
-          }
-          return commit({
-            variables: {
-              input: {
-                ...updates,
-                id: contactMethodID,
-              },
-            },
-          })
-        }}
-        form={
-          <UserContactMethodForm
-            errors={fieldErrs}
-            disabled={loading}
-            edit
-            value={value || defaultValue}
-            onChange={(value) => setValue(value)}
-          />
-        }
-      />
-    )
-  }
-
-  function renderMutation({ name, type, value, statusUpdates }) {
-    return (
-      <Mutation mutation={mutation} onCompleted={onClose}>
-        {(commit, status) =>
-          renderDialog(commit, status, { name, type, value, statusUpdates })
-        }
-      </Mutation>
-    )
-  }
+  const fieldErrs = fieldErrors(error)
 
   return (
-    <Query
-      query={query}
-      variables={{ id: contactMethodID }}
-      render={({ data }) => renderMutation(data.userContactMethod)}
-      noPoll
+    <FormDialog
+      title='Edit Contact Method'
+      loading={fetching}
+      errors={nonFieldErrors(error)}
+      onClose={onClose}
+      onSubmit={() => {
+        const updates = pick(value, 'name', 'statusUpdates')
+        // the form uses the 'statusUpdates' enum but the mutation simply
+        // needs to know if the status updates should be enabled or not via
+        // the 'enableStatusUpdates' boolean
+        if ('statusUpdates' in updates) {
+          delete Object.assign(updates, {
+            enableStatusUpdates: updates.statusUpdates === 'ENABLED',
+          }).statusUpdates
+        }
+        return commit({
+          variables: {
+            input: {
+              ...updates,
+              id: contactMethodID,
+            },
+          },
+        })
+      }}
+      form={
+        <UserContactMethodForm
+          errors={fieldErrs}
+          disabled={fetching}
+          edit
+          value={value || defaultValue}
+          onChange={(value) => setValue(value)}
+        />
+      }
     />
   )
 }
