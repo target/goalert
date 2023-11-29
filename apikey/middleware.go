e36a34fe50d57054d1ce18589740afede7698df6
Nathaniel Caza <mastercactapus@gmail.com>
2023-11-29 12:00:45 -0600

    fix behavior of setting query if omitted

--- apikey/middleware.go
+++ apikey/middleware.go
@@ -10,18 +10,18 @@ import (
 
 type Middleware struct{}
 
-var _ graphql.OperationContextMutator = Middleware{}
+var _ graphql.OperationParameterMutator = Middleware{}
 
 func (Middleware) ExtensionName() string                          { return "GQLAPIKeyMiddleware" }
 func (Middleware) Validate(schema graphql.ExecutableSchema) error { return nil }
 
-func (Middleware) MutateOperationContext(ctx context.Context, rc *graphql.OperationContext) *gqlerror.Error {
+func (Middleware) MutateOperationParameters(ctx context.Context, rc *graphql.RawParams) *gqlerror.Error {
 	p := PolicyFromContext(ctx)
 	if p == nil {
 		return nil
 	}
 
-	if rc.RawQuery == "" {
+	if rc.Query == "" {
 		// Allow query to be omitted for API key requests,
 		// since they are always fixed to the key itself.
 		//
@@ -31,10 +31,10 @@ func (Middleware) MutateOperationContext(ctx context.Context, rc *graphql.Operat
 		// This helps with things like key rotations, where the
 		// query may not be known to the client, or would otherwise
 		// be difficult to update.
-		rc.RawQuery = p.Query
+		rc.Query = p.Query
 	}
 
-	if p.Query != rc.RawQuery {
+	if p.Query != rc.Query {
 		return &gqlerror.Error{
 			Err:     permission.Unauthorized(),
 			Message: "wrong query for API key",
