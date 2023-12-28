8bcbc95505d4b213c4dbca475ee6d4e95055071b
Nathaniel Caza <mastercactapus@gmail.com>
2023-12-28 10:40:53 -0600

    add test wrapper for teltext

--- web/src/app/util/TelTextField.spec.tsx
+++ web/src/app/util/TelTextField.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { test, expect } from '@playwright/experimental-ct-react'
-import TelTextField from './TelTextField'
+import { TestWrapper } from './TelTextField.story'
 
 test.use({ viewport: { width: 500, height: 500 } })
 
@@ -23,26 +23,17 @@ test('should work', async ({ mount, page }) => {
     })
   })
 
-  let newValue = ''
+  const component = await mount(<TestWrapper />)
+  await component.locator('input').fill('17635550123')
 
-  let component = await mount(
-    <TelTextField
-      value='17635550123'
-      onChange={(v) => {
-        newValue = v.target.value
-      }}
-    />,
-  )
   // ensure we have an SVG with attribute `data-testid="CheckIcon"`
-
   await expect(component.locator('svg[data-testid="CheckIcon"]')).toBeVisible()
 
   await component.locator('input').fill('123abc456')
 
-  expect(newValue).toBe('+123456') // should be stripped of non-numeric characters
+  // value won't have the '+' because it's in the adornment, we should also
+  // expect the input value to strip the non-numeric characters.
+  await expect(component.locator('input')).toHaveValue('123456')
 
-  await component.unmount()
-
-  component = await mount(<TelTextField value='1111' />)
   await expect(component.locator('svg[data-testid="CloseIcon"]')).toBeVisible()
 })
