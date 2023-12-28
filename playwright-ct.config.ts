f067770739869e79051cc3d79fafbbf8bf57f29f
Nathaniel Caza <mastercactapus@gmail.com>
2023-12-28 11:21:55 -0600

    add `ct-` prefix to differentiate component tests

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  testMatch: '**/*.spec.tsx',
+  testMatch: '**/*.ct-spec.tsx',
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
