a5d3cfa6e8b09a2aad6f0dce609fe9c723c6b487
Nathaniel Caza <mastercactapus@gmail.com>
2023-12-28 11:18:30 -0600

    remove webkit

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -39,9 +39,5 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 })
