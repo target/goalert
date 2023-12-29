3c205b53a2469f452d149cce461c926c02975e15
Nathaniel Caza <mastercactapus@gmail.com>
2023-12-29 10:55:13 -0600

    just focus on chrome

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -35,9 +35,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
   ],
 })
