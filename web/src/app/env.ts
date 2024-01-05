0b201844927cd730b32203ba164979d4d2893e4e
Nathaniel Caza <mastercactapus@gmail.com>
2024-01-05 16:08:14 -0600

    dev: Use Storybook for component testing (#3571)
    * add baseline
    
    * get cli tests working
    
    * default handlers
    
    * switch to @storybook/test
    
    * speed up component tests
    
    * remove playwright-ct
    
    * remove ct files
    
    * add storybook UI target
    
    * fix addons
    
    * fix decorator type annotations
    
    * add decorator types
    
    * add ensure yarn before running yarn storybook
    
    * single gql mock
    
    * remove extra flag
    
    * add playwright for playwright-jest
    
    * revert
    
    * fix dep issues
    
    * limit CI concurrency
    
    * handle update on first contact method
    
    * prefer duration if set
    
    * add interactable teltextfield variant
    
    * make all interactable

--- web/src/app/env.ts
+++ web/src/app/env.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable camelcase */
 
-export const pathPrefix = global.pathPrefix
-export const applicationName = global.applicationName
+export const pathPrefix = global.pathPrefix || ''
+export const applicationName = global.applicationName || 'GoAlert'
 
-export const GOALERT_VERSION = global.GOALERT_VERSION
+export const GOALERT_VERSION = global.GOALERT_VERSION || 'dev'
 
 export const isCypress = Boolean(global.Cypress)
