8e7aeaec9ede39d66e0f26ac95fed7f9144e2d3a
Tony Vu <Tony.T.Vu@outlook.com>
2024-01-11 18:42:58 -0600

    util: add DestinationChip and DestinationInputChip components (#3586)
    * add destination chip components and tests
    
    * fix breaking tests
    
    * flatten props
    
    * refactor props and add comment
    
    * add loading an no icon stories
    
    * validate onDelete gets called
    
    * capitalize Loading
    
    * fix tests
    
    ---------
    
    Co-authored-by: TonyTVu <tony.t.vu@target.com>
    Co-authored-by: Nathaniel Caza <mastercactapus@gmail.com>

---
+++ web/src/app/util/DestinationInputChip.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { DestinationDisplayInfo, DestinationInput } from '../../schema'
+import { gql, useQuery } from 'urql'
+import DestinationChip from './DestinationChip'
+
+export type DestinationInputChipProps = {
+  value: DestinationInput
+  onDelete?: () => void
+}
+
+const query = gql`
+  query DestDisplayInfo($input: DestinationInput!) {
+    destinationDisplayInfo(input: $input) {
+      text
+      iconURL
+      iconAltText
+      linkURL
+    }
+  }
+`
+
+const context = {
+  suspense: false,
+}
+
+// This is a simple wrapper around DestinationChip that takes a DestinationInput
+// instead of a DestinationDisplayInfo. It's useful for showing the destination
+// chips in the policy details page.
+export default function DestinationInputChip(
+  props: DestinationInputChipProps,
+): React.ReactNode {
+  const [{ data, error }] = useQuery<{
+    destinationDisplayInfo: DestinationDisplayInfo
+  }>({
+    query,
+    variables: {
+      input: props.value,
+    },
+    requestPolicy: 'cache-first',
+    context,
+  })
+
+  return (
+    <DestinationChip
+      iconAltText={data?.destinationDisplayInfo.iconAltText || ''}
+      iconURL={data?.destinationDisplayInfo.iconURL || ''}
+      linkURL={data?.destinationDisplayInfo.linkURL || ''}
+      text={data?.destinationDisplayInfo.text || ''}
+      error={error?.message}
+      onDelete={props.onDelete}
+    />
+  )
+}
