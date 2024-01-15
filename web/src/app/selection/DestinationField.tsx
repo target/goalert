657686fb1a2cb19e6581ddb772884af3c2e87be2
Nathaniel Caza <mastercactapus@gmail.com>
2024-01-15 15:40:16 -0600

    fix multi-field spacing

--- web/src/app/selection/DestinationField.tsx
+++ web/src/app/selection/DestinationField.tsx
@@ -3,6 +3,7 @@ import { DestinationType, FieldValueInput } from '../../schema'
 import DestinationInputDirect from './DestinationInputDirect'
 import { useDestinationType } from '../util/RequireConfig'
 import DestinationSearchSelect from './DestinationSearchSelect'
+import { Grid } from '@mui/material'
 
 export type DestinationFieldProps = {
   value: FieldValueInput[]
@@ -24,54 +25,62 @@ export default function DestinationField(
 ): React.ReactNode {
   const dest = useDestinationType(props.destType)
 
-  return dest.requiredFields.map((field) => {
-    const fieldValue =
-      (props.value || []).find((v) => v.fieldID === field.fieldID)?.value || ''
+  return (
+    <Grid container spacing={2}>
+      {dest.requiredFields.map((field) => {
+        const fieldValue =
+          (props.value || []).find((v) => v.fieldID === field.fieldID)?.value ||
+          ''
 
-    function handleChange(newValue: string): void {
-      if (!props.onChange) return
+        function handleChange(newValue: string): void {
+          if (!props.onChange) return
 
-      const newValues = (props.value || [])
-        .filter((v) => v.fieldID !== field.fieldID)
-        .concat({
-          fieldID: field.fieldID,
-          value: newValue,
-        })
+          const newValues = (props.value || [])
+            .filter((v) => v.fieldID !== field.fieldID)
+            .concat({
+              fieldID: field.fieldID,
+              value: newValue,
+            })
 
-      props.onChange(newValues)
-    }
+          props.onChange(newValues)
+        }
 
-    const fieldErrMsg =
-      props.destFieldErrors?.find((err) => err.fieldID === field.fieldID)
-        ?.message || ''
+        const fieldErrMsg =
+          props.destFieldErrors?.find((err) => err.fieldID === field.fieldID)
+            ?.message || ''
 
-    if (field.isSearchSelectable)
-      return (
-        <DestinationSearchSelect
-          key={field.fieldID}
-          {...field}
-          value={fieldValue}
-          destType={props.destType}
-          disabled={props.disabled || !dest.enabled}
-          onChange={(val) => handleChange(val)}
-          error={!!fieldErrMsg}
-          hint={fieldErrMsg || field.hint}
-          hintURL={fieldErrMsg ? '' : field.hintURL}
-        />
-      )
+        if (field.isSearchSelectable)
+          return (
+            <Grid key={field.fieldID} item xs={12} sm={12} md={6}>
+              <DestinationSearchSelect
+                {...field}
+                value={fieldValue}
+                destType={props.destType}
+                disabled={props.disabled || !dest.enabled}
+                onChange={(val) => handleChange(val)}
+                error={!!fieldErrMsg}
+                hint={fieldErrMsg || field.hint}
+                hintURL={fieldErrMsg ? '' : field.hintURL}
+              />
+            </Grid>
+          )
 
-    return (
-      <DestinationInputDirect
-        key={field.fieldID}
-        {...field}
-        value={fieldValue}
-        destType={props.destType}
-        disabled={props.disabled || !dest.enabled}
-        onChange={(e) => handleChange(e.target.value)}
-        error={!!fieldErrMsg}
-        hint={fieldErrMsg || field.hint}
-        hintURL={fieldErrMsg ? '' : field.hintURL}
-      />
-    )
-  })
+        return (
+          <Grid key={field.fieldID} item xs={12} sm={12} md={6}>
+            <DestinationInputDirect
+              {...field}
+              value={fieldValue}
+              destType={props.destType}
+              disabled={props.disabled || !dest.enabled}
+              onChange={(e) => handleChange(e.target.value)}
+              error={!!fieldErrMsg}
+              hint={fieldErrMsg || field.hint}
+              hintURL={fieldErrMsg ? '' : field.hintURL}
+            />
+          </Grid>
+        )
+      })}
+    </Grid>
+  )
+  return
 }
