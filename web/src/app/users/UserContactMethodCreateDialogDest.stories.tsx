fe63824e8849715f785b101d3be0c1264670c4d5
Ethan-Haynes <Ethan.Haynes@target.com>
2024-02-06 15:36:36 -0600

    fix for linting

--- web/src/app/users/UserContactMethodCreateDialogDest.stories.tsx
+++ web/src/app/users/UserContactMethodCreateDialogDest.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { Meta, StoryObj, } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 import UserContactMethodCreateDialogDest from './UserContactMethodCreateDialogDest'
 import { expect } from '@storybook/jest'
 import { screen, userEvent, waitFor } from '@storybook/testing-library'
@@ -7,32 +7,46 @@ import { handleDefaultConfig, defaultConfig } from '../storybook/graphql'
 import { useArgs } from '@storybook/preview-api'
 import { HttpResponse, graphql } from 'msw'
 
-
 const meta = {
   title: 'util/UserContactMethodCreateDialogDest',
   component: UserContactMethodCreateDialogDest,
   tags: ['autodocs'],
   parameters: {
     msw: {
-      handlers: [handleDefaultConfig,
+      handlers: [
+        handleDefaultConfig,
         graphql.query('UserConflictCheck', () => {
-    return HttpResponse.json({
-      data: { users: { nodes: [{name: defaultConfig.user.name ,id: defaultConfig.user.id}] }},
-    })
-  }),
-  graphql.query('useExpFlag', () => {
-    return HttpResponse.json({
-      data: { users: { nodes: [{name: defaultConfig.user.name ,id: defaultConfig.user.id}] }},
-    })
-  }),
-  graphql.mutation('CreateUserContactMethodInput', ({ variables: vars }) => {
-        return HttpResponse.json({
-        data: {
-            createUserContactMethod: { id: '00000000-0000-0000-0000-000000000000'}
-        },
-        })
-    }),
-    graphql.query('ValidateDestination', ({ variables: vars }) => {
+          return HttpResponse.json({
+            data: {
+              users: {
+                nodes: [
+                  { name: defaultConfig.user.name, id: defaultConfig.user.id },
+                ],
+              },
+            },
+          })
+        }),
+        graphql.query('useExpFlag', () => {
+          return HttpResponse.json({
+            data: {
+              users: {
+                nodes: [
+                  { name: defaultConfig.user.name, id: defaultConfig.user.id },
+                ],
+              },
+            },
+          })
+        }),
+        graphql.mutation('CreateUserContactMethodInput', () => {
+          return HttpResponse.json({
+            data: {
+              createUserContactMethod: {
+                id: '00000000-0000-0000-0000-000000000000',
+              },
+            },
+          })
+        }),
+        graphql.query('ValidateDestination', ({ variables: vars }) => {
           return HttpResponse.json({
             data: {
               destinationFieldValidate:
@@ -41,8 +55,8 @@ const meta = {
                 vars.input.value === 'valid@email.com',
             },
           })
-        })
-    ],
+        }),
+      ],
     },
   },
   render: function Component(args) {
@@ -64,22 +78,21 @@ export const SingleField: Story = {
     title: 'Create New Contact Method',
     subtitle: 'Create New Contact Method Subtitle',
   },
-  play: async ({ canvasElement }) => {
+  play: async () => {
     await userEvent.clear(screen.getByLabelText('Phone Number'))
-    
+
     await waitFor(async () => {
       await userEvent.type(screen.getByLabelText('Phone Number'), '12225558989')
-    });
+    })
     await expect(await screen.findByTestId('CheckIcon')).toBeVisible()
 
-
-    const submitButton = await screen.getByRole('button', { name: /SUBMIT/i });
+    const submitButton = await screen.getByRole('button', { name: /SUBMIT/i })
     await userEvent.click(submitButton)
 
     await userEvent.clear(screen.getByLabelText('Name'))
     await userEvent.type(screen.getByLabelText('Name'), 'TEST')
 
-    const retryButton = await screen.getByRole('button', { name: /RETRY/i });
+    const retryButton = await screen.getByRole('button', { name: /RETRY/i })
     await userEvent.click(retryButton)
   },
 }
@@ -90,16 +103,18 @@ export const MultiField: Story = {
     title: 'Create New Contact Method',
     subtitle: 'Create New Contact Method Subtitle',
   },
-  play: async ({ canvasElement }) => {
+  play: async () => {
     // Select the next Dest Type
     await userEvent.click(await screen.getByLabelText('Dest Type'))
-    await userEvent.click(await screen.getByText('Multi Field Destination Type'))
+    await userEvent.click(
+      await screen.getByText('Multi Field Destination Type'),
+    )
 
     // ensure information for phone number renders correctly
     await userEvent.clear(screen.getByLabelText('First Item'))
     await waitFor(async () => {
       await userEvent.type(screen.getByLabelText('First Item'), '12225558989')
-    });
+    })
     await expect(await screen.findByTestId('CheckIcon')).toBeVisible()
 
     // ensure information for email renders correctly
@@ -107,7 +122,10 @@ export const MultiField: Story = {
       screen.getByPlaceholderText('foobar@example.com'),
     ).toBeVisible()
     await userEvent.clear(screen.getByLabelText('Second Item'))
-    await userEvent.type(screen.getByLabelText('Second Item'), 'valid@email.com')
+    await userEvent.type(
+      screen.getByLabelText('Second Item'),
+      'valid@email.com',
+    )
 
     // ensure information for slack renders correctly
     await expect(screen.getByPlaceholderText('slack user ID')).toBeVisible()
@@ -115,17 +133,15 @@ export const MultiField: Story = {
     await userEvent.clear(screen.getByLabelText('Third Item'))
     await userEvent.type(screen.getByLabelText('Third Item'), '@slack')
 
-
     // Try to submit without all feilds complete
-    const submitButton = await screen.getByRole('button', { name: /SUBMIT/i });
+    const submitButton = await screen.getByRole('button', { name: /SUBMIT/i })
     await userEvent.click(submitButton)
 
     // Name field
     await userEvent.clear(screen.getByLabelText('Name'))
     await userEvent.type(screen.getByLabelText('Name'), 'TEST')
 
-
-    const retryButton = await screen.getByRole('button', { name: /RETRY/i });
+    const retryButton = await screen.getByRole('button', { name: /RETRY/i })
     await userEvent.click(retryButton)
   },
 }
@@ -136,7 +152,7 @@ export const DisabledField: Story = {
     title: 'Create New Contact Method',
     subtitle: 'Create New Contact Method Subtitle',
   },
-  play: async ({ canvasElement }) => {
+  play: async () => {
     // Open option select
     await userEvent.click(await screen.getByLabelText('Dest Type'))
 
@@ -144,7 +160,7 @@ export const DisabledField: Story = {
     const disabledOption = await screen.getByText('This is disabled')
     // Ensure no clicked occurred
     userEvent.click(disabledOption, {
-        pointerEventsCheck: 0,
+      pointerEventsCheck: 0,
     })
   },
 }
