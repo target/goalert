bc85505de8ec9a302ff7b7b7e2a62def53ee1a00
AllenDing <Allen.Ding@target.com>
2024-02-14 13:01:29 -0600

    revert uneeded change

--- web/src/app/util/safeURL.ts
+++ web/src/app/util/safeURL.ts
@@ -30,7 +30,7 @@ export function safeURL(url: string, label: string): boolean {
 
   // handle http protocols
   if (!/https?:\/\//.test(url)) return false // require absolute URLs
-  if (!/[./]/.test(url)) return true // don't consider it a path/url without slashes or periods
+  if (!/[./]/.test(label)) return true // don't consider it a path/url without slashes or periods
   if (url.startsWith(label)) return true // if it matches the beginning, then it's fine
   if (url.replace(/^https?:\/\//, '').startsWith(label)) return true // same prefix without protocol
   if (url.replace(/^https?:\/\//, '').startsWith('www.' + label)) return true // same prefix without protocol
