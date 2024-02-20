e7035d9b3c600b56137fa729df3ec68150c65d1d
Tony Vu <Tony.T.Vu@outlook.com>
2024-02-20 10:01:25 -0600

    users: use generic destinations for user contact method list component (#3684)
    * add user CM list dest component
    
    * [WIP] add component tests for user CM list dest
    
    * add component tests for user CM list
    
    * remove console log
    
    * unify into one function
    
    * remove non-current user test
    
    * remove hard coded dest usage
    
    * fix issue where hint url not displaying for mult field dests
    
    * combine test assertions
    
    * remove repeated assertions
    
    * update comment to trigger github action
    
    * remove lines to meet pr size limit
    
    * remove function
    
    * fix CM title label
    
    * rename variable
    
    * use destType.name
    
    * update story text
    
    ---------
    
    Co-authored-by: Nathaniel Caza <mastercactapus@gmail.com>

--- web/src/app/users/UserDetails.tsx
+++ web/src/app/users/UserDetails.tsx
@@ -5,6 +5,7 @@ import LockOpenIcon from '@mui/icons-material/LockOpen'
 import DetailsPage from '../details/DetailsPage'
 import { UserAvatar } from '../util/avatars'
 import UserContactMethodList from './UserContactMethodList'
+import UserContactMethodListDest from './UserContactMethodListDest'
 import { AddAlarm, SettingsPhone } from '@mui/icons-material'
 import SpeedDial from '../util/SpeedDial'
 import UserNotificationRuleList from './UserNotificationRuleList'
@@ -21,6 +22,7 @@ import { QuerySetFavoriteButton } from '../util/QuerySetFavoriteButton'
 import { EscalationPolicyStep } from '../../schema'
 import { useIsWidthDown } from '../util/useWidth'
 import UserShiftsCalendar from './UserShiftsCalendar'
+import { useExpFlag } from '../util/useExpFlag'
 
 const userQuery = gql`
   query userInfo($id: ID!) {
@@ -88,6 +90,7 @@ export default function UserDetails(props: {
   userID: string
   readOnly: boolean
 }): JSX.Element {
+  const hasDestTypesFlag = useExpFlag('dest-types')
   const userID = props.userID
   const { userID: currentUserID, isAdmin } = useSessionInfo()
 
@@ -236,7 +239,17 @@ export default function UserDetails(props: {
         subheader={user.email}
         pageContent={
           <Grid container spacing={2}>
-            <UserContactMethodList userID={userID} readOnly={props.readOnly} />
+            {hasDestTypesFlag ? (
+              <UserContactMethodListDest
+                userID={userID}
+                readOnly={props.readOnly}
+              />
+            ) : (
+              <UserContactMethodList
+                userID={userID}
+                readOnly={props.readOnly}
+              />
+            )}
             <UserNotificationRuleList
               userID={userID}
               readOnly={props.readOnly}
