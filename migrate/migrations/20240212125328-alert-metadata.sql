acc9b1239aab1cc4c5808d91e18988da263bfe97
shivanishendge <49693251+shivanishendge@users.noreply.github.com>
2024-03-26 11:37:36 +0530

    Update migrate/migrations/20240212125328-alert-metadata.sql
    Co-authored-by: Nathaniel Caza <mastercactapus@gmail.com>

--- migrate/migrations/20240212125328-alert-metadata.sql
+++ migrate/migrations/20240212125328-alert-metadata.sql
@@ -1,12 +1,8 @@
 -- +migrate Up
-CREATE TABLE alerts_metadata (
-	alert_id integer NOT NULL,
-	id uuid DEFAULT gen_random_uuid() NOT NULL,
-	metadata jsonb,
-	CONSTRAINT alerts_metadata_alert_id_fkey FOREIGN KEY (alert_id) REFERENCES alerts(id) ON DELETE CASCADE,
-	CONSTRAINT alerts_metadata_pkey PRIMARY KEY (id, alert_id),
-	CONSTRAINT alerts_metadata_id UNIQUE (alert_id)
+CREATE TABLE alert_data (
+	alert_id BIGINT PRIMARY KEY REFERENCES alerts (id) ON DELETE CASCADE,
+	metadata jsonb
 );
 
 -- +migrate Down
-DROP TABLE alerts_metadata;
+DROP TABLE alert_data;
