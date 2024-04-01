695fa9f70d6dfd206855ce53c6485363f23fed91
Nathaniel Caza <mastercactapus@gmail.com>
2024-04-01 14:12:54 -0500

    add id column for switchover

--- migrate/migrations/20240212125328-alert-metadata.sql
+++ migrate/migrations/20240212125328-alert-metadata.sql
@@ -1,8 +1,10 @@
 -- +migrate Up
-CREATE TABLE alert_data (
-	alert_id BIGINT PRIMARY KEY REFERENCES alerts (id) ON DELETE CASCADE,
-	metadata jsonb
+CREATE TABLE alert_data(
+    alert_id bigint PRIMARY KEY REFERENCES alerts(id) ON DELETE CASCADE,
+    metadata jsonb,
+    id bigserial UNIQUE NOT NULL
 );
 
 -- +migrate Down
 DROP TABLE alert_data;
+
