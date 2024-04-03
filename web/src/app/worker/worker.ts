e287ff0df8e327b353f8425b60713cb9d5f0a242
Nathaniel Caza <mastercactapus@gmail.com>
2024-04-03 14:19:58 -0500

    add check on worker method name

--- web/src/app/worker/worker.ts
+++ web/src/app/worker/worker.ts
@@ -2,6 +2,13 @@ import methods from './methods'
 
 self.onmessage = (e) => {
   const method = e.data.method as keyof typeof methods
+
+  if (!(method in methods)) {
+    // Shouldn't happen, but ensures that we can't unknowingly
+    // call a method that doesn't exist, or is on the prototype.
+    throw new Error('Invalid method')
+  }
+
   const result = methods[method](e.data.arg)
   self.postMessage(result)
 }
