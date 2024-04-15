cf6daa14a848ffa65734da0cea4966fde614add2
Nathaniel Caza <mastercactapus@gmail.com>
2024-04-15 14:47:29 -0500

    add optional external_system_name

---
+++ migrate/migrations/20240415143348-ext-int-keys.sql
@@ -0,0 +1,14 @@
+-- +migrate Up
+ALTER TABLE integration_keys
+    ADD COLUMN external_system_name TEXT,
+    DROP CONSTRAINT integration_keys_name_service_id_key;
+
+DROP INDEX IF EXISTS integration_keys_name_service_id_key;
+
+CREATE UNIQUE INDEX integration_keys_name_service_id_key ON public.integration_keys USING btree(name, service_id, external_system_name);
+
+-- +migrate Down
+ALTER TABLE integration_keys
+    DROP COLUMN external_system_name,
+    ADD CONSTRAINT integration_keys_name_service_id_key UNIQUE (name, service_id);
+
