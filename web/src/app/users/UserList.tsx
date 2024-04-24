aeab851433f967b9a889ee00d79623c792091ee7
Nathaniel Caza <mastercactapus@gmail.com>
2024-04-24 14:44:36 -0500

    move UserList off of QueryList

--- web/src/app/users/UserList.tsx
+++ web/src/app/users/UserList.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { gql } from 'urql'
+import React, { Suspense, useState } from 'react'
+import { gql, useQuery } from 'urql'
 import { UserAvatar } from '../util/avatars'
-import QueryList from '../lists/QueryList'
 import UserPhoneNumberFilterContainer from './UserPhoneNumberFilterContainer'
 import UserCreateDialog from './UserCreateDialog'
 import { useSessionInfo } from '../util/RequireConfig'
+import ListPageControls from '../lists/ListPageControls'
+import Search from '../util/Search'
+import FlatList from '../lists/FlatList'
+import { UserConnection } from '../../schema'
+import { useURLParam } from '../actions'
+import { FavoriteIcon } from '../util/SetFavoriteButton'
 
 const query = gql`
   query usersQuery($input: UserSearchOptions) {
-    data: users(input: $input) {
+    users(input: $input) {
       nodes {
         id
         name
@@ -23,34 +28,77 @@ const query = gql`
   }
 `
 
+const context = { suspense: false }
+
 function UserList(): JSX.Element {
-  const { isAdmin, ready } = useSessionInfo()
+  const { isAdmin } = useSessionInfo()
+  const [create, setCreate] = useState(false)
+  const [search] = useURLParam<string>('search', '')
+  const [pageCursors, setPageCursors] = useState(['']) // array of cursor values for previous pages
+  const currentCursor = pageCursors[pageCursors.length - 1]
+
+  const inputVars = {
+    favoritesFirst: true,
+    search,
+    CMValue: '',
+    after: currentCursor,
+  }
+  if (search.startsWith('phone=')) {
+    inputVars.CMValue = search.replace(/^phone=/, '')
+    inputVars.search = ''
+  }
+
+  const [q] = useQuery<{ users: UserConnection }>({
+    query,
+    variables: { input: inputVars },
+    context,
+  })
+  const nextCursor = q.data?.users.pageInfo.hasNextPage
+    ? q.data?.users.pageInfo.endCursor
+    : ''
+  // cache the next page
+  useQuery({
+    query,
+    variables: { input: { ...inputVars, after: nextCursor } },
+    context,
+    pause: !nextCursor,
+  })
 
   return (
     <React.Fragment>
-      <QueryList
-        query={query}
-        variables={{ input: { favoritesFirst: true } }}
-        mapDataNode={(n) => ({
-          title: n.name,
-          subText: n.email,
-          url: n.id,
-          isFavorite: n.isFavorite,
-          icon: <UserAvatar userID={n.id} />,
-        })}
-        mapVariables={(vars) => {
-          if (vars?.input?.search?.startsWith('phone=')) {
-            vars.input.CMValue = vars.input.search.replace(/^phone=/, '')
-            vars.input.search = ''
-          }
-          return vars
-        }}
-        searchAdornment={<UserPhoneNumberFilterContainer />}
-        renderCreateDialog={(onClose) => {
-          return <UserCreateDialog onClose={onClose} />
-        }}
+      <Suspense>
+        {create && <UserCreateDialog onClose={() => setCreate(false)} />}
+      </Suspense>
+      <ListPageControls
         createLabel='User'
-        hideCreate={!ready || (ready && !isAdmin)}
+        onBack={
+          pageCursors.length > 1
+            ? () => setPageCursors(pageCursors.slice(0, -1))
+            : undefined
+        }
+        onNext={
+          nextCursor
+            ? () => setPageCursors([...pageCursors, nextCursor])
+            : undefined
+        }
+        loading={q.fetching}
+        onCreateClick={isAdmin ? () => setCreate(true) : undefined}
+        slots={{
+          search: <Search endAdornment={<UserPhoneNumberFilterContainer />} />,
+          list: (
+            <FlatList
+              items={
+                q.data?.users.nodes.map((u) => ({
+                  title: u.name,
+                  subText: u.email,
+                  url: u.id,
+                  secondaryAction: u.isFavorite ? <FavoriteIcon /> : undefined,
+                  icon: <UserAvatar userID={u.id} />,
+                })) || []
+              }
+            />
+          ),
+        }}
       />
     </React.Fragment>
   )
