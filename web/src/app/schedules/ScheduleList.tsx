7ac150fb9e309140675d753b6b09ed0059de91cd
Nathaniel Caza <mastercactapus@gmail.com>
2024-05-01 16:05:35 -0500

    move schedule list to urql (#3827)

--- web/src/app/schedules/ScheduleList.tsx
+++ web/src/app/schedules/ScheduleList.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { gql } from 'urql'
-import QueryList from '../lists/QueryList'
+import React, { Suspense, useState } from 'react'
+import { gql, useQuery } from 'urql'
 import ScheduleCreateDialog from './ScheduleCreateDialog'
+import { useURLParam } from '../actions'
+import { ScheduleConnection } from '../../schema'
+import ListPageControls from '../lists/ListPageControls'
+import Search from '../util/Search'
+import FlatList from '../lists/FlatList'
+import { FavoriteIcon } from '../util/SetFavoriteButton'
 
 const query = gql`
   query schedulesQuery($input: ScheduleSearchOptions) {
-    data: schedules(input: $input) {
+    schedules(input: $input) {
       nodes {
         id
         name
@@ -20,21 +25,62 @@ const query = gql`
   }
 `
 
+const context = { suspense: false }
 export default function ScheduleList(): JSX.Element {
+  const [search] = useURLParam<string>('search', '')
+  const [create, setCreate] = useState(false)
+  const [cursor, setCursor] = useState('')
+
+  const inputVars = {
+    favoritesFirst: true,
+    search,
+    after: cursor,
+  }
+
+  const [q] = useQuery<{ schedules: ScheduleConnection }>({
+    query,
+    variables: { input: inputVars },
+    context,
+  })
+  const nextCursor = q.data?.schedules.pageInfo.hasNextPage
+    ? q.data?.schedules.pageInfo.endCursor
+    : ''
+  // cache the next page
+  useQuery({
+    query,
+    variables: { input: { ...inputVars, after: nextCursor } },
+    context,
+    pause: !nextCursor,
+  })
+
   return (
-    <QueryList
-      query={query}
-      variables={{ input: { favoritesFirst: true } }}
-      mapDataNode={(n) => ({
-        title: n.name,
-        subText: n.description,
-        url: n.id,
-        isFavorite: n.isFavorite,
-      })}
-      renderCreateDialog={(onClose) => (
-        <ScheduleCreateDialog onClose={onClose} />
-      )}
-      createLabel='Schedule'
-    />
+    <React.Fragment>
+      <Suspense>
+        {create && <ScheduleCreateDialog onClose={() => setCreate(false)} />}
+      </Suspense>
+      <ListPageControls
+        createLabel='Schedule'
+        nextCursor={nextCursor}
+        onCursorChange={setCursor}
+        loading={q.fetching}
+        onCreateClick={() => setCreate(true)}
+        slots={{
+          search: <Search />,
+          list: (
+            <FlatList
+              emptyMessage='No results'
+              items={
+                q.data?.schedules.nodes.map((u) => ({
+                  title: u.name,
+                  subText: u.description,
+                  url: u.id,
+                  secondaryAction: u.isFavorite ? <FavoriteIcon /> : undefined,
+                })) || []
+              }
+            />
+          ),
+        }}
+      />
+    </React.Fragment>
   )
 }
