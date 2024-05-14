f3ba9c845c58a3392ac20475b008080792f0917e
Nathaniel Caza <mastercactapus@gmail.com>
2024-05-14 09:47:12 -0500

    Add first-login integration test
    - New test file added for new user's first login process
    - Performs checks to ensure the welcome dialog, contact method verification, and their corresponding actions work as expected
    - Includes cancel and delete operations for contact methods
    - Uses randomized user credentials for diversified testing

---
+++ test/integration/first-login.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test'
+import { dropdownSelect, userSessionFile } from './lib'
+import Chance from 'chance'
+const c = new Chance()
+
+test.describe.configure({ mode: 'parallel' })
+test.use({ storageState: userSessionFile })
+
+// test create, edit, verify, and delete of an EMAIL contact method
+test('first time setup', async ({ page }) => {
+  await page.goto('.?isFirstLogin=1')
+  // ensure dialog is shown
+  await expect(
+    page.locator('[role=dialog]', { hasText: 'Welcome to GoAlert' }),
+  ).toBeVisible()
+
+  const name = 'first-setup-email ' + c.name()
+  const email = 'first-setup-email-' + c.email()
+  await page.fill('input[name=name]', name)
+  await dropdownSelect(page, 'input[name="dest.type"]', 'Email')
+  await page.fill('input[name=email-address]', email)
+  await page.click('[role=dialog] button[type=submit]')
+  await expect(
+    page.locator('[role=dialog]', { hasText: 'Verify Contact Method' }),
+  ).toBeVisible()
+
+  // cancel out
+  await page.locator('[role=dialog] button', { hasText: 'Cancel' }).click()
+
+  // ensure dialog is  not shown
+  await expect(page.locator('[role=dialog]')).not.toBeVisible()
+
+  await page.goto('./profile')
+  await page
+    .locator('.MuiCard-root', {
+      has: page.locator('div > div > h2', { hasText: 'Contact Methods' }),
+    })
+    .locator('li', { hasText: email })
+    .locator('[aria-label="Other Actions"]')
+    .click()
+  await page.getByRole('menuitem', { name: 'Delete' }).click()
+  await page.getByRole('button', { name: 'Confirm' }).click()
+
+  await expect(page.locator('[role=dialog]')).not.toBeVisible()
+})
