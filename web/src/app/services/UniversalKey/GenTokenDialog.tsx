481ed3e814bc1930a62c07c658fa733808c50799
Nathaniel Caza <mastercactapus@gmail.com>
2024-05-21 10:39:33 -0500

    Add token rotation dialogs and controls
    Co-authored-by: Nathaniel Cook <Nathaniel.Cook@target.com>

---
+++ web/src/app/services/UniversalKey/GenTokenDialog.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { FormHelperText, Grid, Typography } from '@mui/material'
+import CopyText from '../../util/CopyText'
+import { gql, useMutation } from 'urql'
+import FormDialog from '../../dialogs/FormDialog'
+import { nonFieldErrors } from '../../util/errutil'
+
+interface TokenCopyDialogProps {
+  keyID: string
+  onClose: () => void
+}
+
+const mutation = gql`
+  mutation ($id: ID!) {
+    generateKeyToken(id: $id)
+  }
+`
+
+function GenerateTokenText(): JSX.Element {
+  return (
+    <Typography>
+      Generates an auth bearer token that can immediately be used for this
+      integration.
+    </Typography>
+  )
+}
+
+function CopyTokenText({ token }: { token: string }): JSX.Element {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <CopyText title={token} value={token} placement='bottom' />
+        <FormHelperText>
+          Please copy and save the token as this is the only time you'll be able
+          to view it.
+        </FormHelperText>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default function GenTokenDialog({
+  keyID,
+  onClose,
+}: TokenCopyDialogProps): JSX.Element {
+  const [status, commit] = useMutation(mutation)
+  const token = status.data?.generateKeyToken
+
+  return (
+    <FormDialog
+      alert={!!token}
+      title={token ? 'Auth Token' : 'Generate Auth Token'}
+      onClose={onClose}
+      errors={nonFieldErrors(status.error)}
+      disableBackdropClose
+      primaryActionLabel={!token ? 'Generate' : 'Done'}
+      form={!token ? <GenerateTokenText /> : <CopyTokenText token={token} />}
+      onSubmit={() =>
+        !token
+          ? commit(
+              {
+                id: keyID,
+              },
+              { additionalTypenames: ['IntegrationKey', 'Service'] },
+            )
+          : onClose()
+      }
+    />
+  )
+}
--- web/src/app/services/UniversalKey/UniversalKeyPage.tsx
+++ web/src/app/services/UniversalKey/UniversalKeyPage.tsx
@@ -1,16 +1,138 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { gql, useQuery } from 'urql'
+import { Redirect } from 'wouter'
+import { GenericError, ObjectNotFound } from '../../error-pages'
+import { IntegrationKey, Service } from '../../../schema'
+import DetailsPage from '../../details/DetailsPage'
+import { Action } from '../../details/CardActions'
+import GenTokenDialog from './GenTokenDialog'
+import PromoteTokenDialog from './PromoteTokenDialog'
+import DeleteSecondaryTokenDialog from './DeleteSecondaryTokenDialog'
 
 interface UniversalKeyPageProps {
   serviceID: string
-  keyName: string
+  keyID: string
 }
 
-export default function UniversalKeyPage({
-  serviceID,
-  keyName,
-}: UniversalKeyPageProps): JSX.Element {
-  if (serviceID && keyName) {
-    return <div />
+const query = gql`
+  query UniversalKeyPage($keyID: ID!, $serviceID: ID!) {
+    integrationKey(id: $keyID) {
+      id
+      name
+      serviceID
+      tokenInfo {
+        primaryHint
+        secondaryHint
+      }
+    }
+    service(id: $serviceID) {
+      id
+      name
+    }
   }
-  return <div />
+`
+
+export default function UniversalKeyPage(
+  props: UniversalKeyPageProps,
+): React.ReactNode {
+  const [genDialogOpen, setGenDialogOpen] = useState(false)
+  const [promoteDialogOpen, setPromoteDialogOpen] = useState(false)
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false)
+
+  const [q] = useQuery<{
+    integrationKey: IntegrationKey
+    service: Service
+  }>({
+    query,
+    variables: {
+      keyID: props.keyID,
+      serviceID: props.serviceID,
+    },
+  })
+
+  // Redirect to the correct service if the key is not in the service
+  if (
+    q.data &&
+    q.data.integrationKey &&
+    q.data.integrationKey.serviceID !== props.serviceID
+  ) {
+    return (
+      <Redirect
+        to={`/services/${q.data.integrationKey.serviceID}/integration-keys/${props.keyID}`}
+      />
+    )
+  }
+
+  if (q.error) {
+    return <GenericError error={q.error.message} />
+  }
+  if (!q.data) return <ObjectNotFound type='integration key' />
+
+  const primaryHint = q.data.integrationKey.tokenInfo.primaryHint
+  const secondaryHint = q.data.integrationKey.tokenInfo.secondaryHint
+
+  const desc = secondaryHint
+    ? `Primary Auth Token: ${primaryHint}\nSecondary Auth Token: ${secondaryHint}`
+    : `Auth Token: ${primaryHint || 'N/A'}`
+
+  function makeGenerateButtons(): Array<Action> {
+    if (primaryHint && !secondaryHint) {
+      return [
+        {
+          label: 'Generate Secondary Token',
+          handleOnClick: () => setGenDialogOpen(true),
+        },
+      ]
+    }
+
+    if (primaryHint && secondaryHint) {
+      return [
+        {
+          label: 'Delete Secondary Token',
+          handleOnClick: () => setDeleteDialogOpen(true),
+        },
+        {
+          label: 'Promote Secondary Token',
+          handleOnClick: () => setPromoteDialogOpen(true),
+        },
+      ]
+    }
+
+    return [
+      {
+        label: 'Generate Auth Token',
+        handleOnClick: () => setGenDialogOpen(true),
+      },
+    ]
+  }
+
+  return (
+    <React.Fragment>
+      <DetailsPage
+        title={q.data.integrationKey.name}
+        subheader={`Service: ${q.data.service.name}`}
+        details={desc}
+        primaryActions={makeGenerateButtons()}
+        pageContent={<div />}
+      />
+      {genDialogOpen && (
+        <GenTokenDialog
+          keyID={props.keyID}
+          onClose={() => setGenDialogOpen(false)}
+        />
+      )}
+      {promoteDialogOpen && (
+        <PromoteTokenDialog
+          keyID={props.keyID}
+          onClose={() => setPromoteDialogOpen(false)}
+        />
+      )}
+      {deleteDialogOpen && (
+        <DeleteSecondaryTokenDialog
+          keyID={props.keyID}
+          onClose={() => setDeleteDialogOpen(false)}
+        />
+      )}
+    </React.Fragment>
+  )
 }
