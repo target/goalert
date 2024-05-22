1afade05ad33e3891f5baf192cccb1773e035780
Nathaniel Caza <mastercactapus@gmail.com>
2024-05-22 09:49:53 -0500

    fix no rows

--- integrationkey/uik.go
+++ integrationkey/uik.go
@@ -208,9 +208,9 @@ func (s *Store) TokenHints(ctx context.Context, db gadb.DBTX, id uuid.UUID) (pri
 	}
 
 	row, err := gadb.New(s.db).IntKeyTokenHints(ctx, id)
-	// if errors.Is(err, sql.ErrNoRows) {
-	// 	return "", "", err
-	// }
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", "", nil
+	}
 	if err != nil {
 		return "", "", err
 	}
