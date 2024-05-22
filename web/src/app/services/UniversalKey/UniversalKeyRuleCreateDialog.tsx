1e35d0def88af5fa60ae81e317e3f23bbac0a207
Forfold <Nathaniel.Cook@Target.com>
2024-05-22 09:25:36 -0700

    add notice confirmation if no actions present

--- web/src/app/services/UniversalKey/UniversalKeyRuleCreateDialog.tsx
+++ web/src/app/services/UniversalKey/UniversalKeyRuleCreateDialog.tsx
@@ -4,6 +4,7 @@ import UniversalKeyRuleForm from './UniversalKeyRuleForm'
 import { gql, useMutation } from 'urql'
 import { nonFieldErrors } from '../../util/errutil'
 import { KeyRuleInput } from '../../../schema'
+import { Checkbox, FormControlLabel } from '@mui/material'
 
 interface UniversalKeyRuleCreateDialogProps {
   keyID: string
@@ -27,13 +28,22 @@ export default function UniversalKeyRuleCreateDialogProps(
     actions: [],
   })
   const [createStatus, commit] = useMutation(mutation)
+  const [hasConfirmed, setHasConfirmed] = useState(false)
+  const [hasSubmitted, setHasSubmitted] = useState(false)
+
+  const noActionsNoConf = value.actions.length === 0 && !hasConfirmed
 
   return (
     <FormDialog
       title='Create Rule'
       onClose={props.onClose}
-      onSubmit={() =>
-        commit(
+      onSubmit={() => {
+        if (noActionsNoConf) {
+          setHasSubmitted(true)
+          return
+        }
+
+        return commit(
           {
             input: {
               keyID: props.keyID,
@@ -49,9 +59,32 @@ export default function UniversalKeyRuleCreateDialogProps(
         ).then(() => {
           props.onClose()
         })
-      }
+      }}
       form={<UniversalKeyRuleForm value={value} onChange={setValue} />}
       errors={nonFieldErrors(createStatus.error)}
+      notices={
+        hasSubmitted
+          ? [
+              {
+                type: 'WARNING',
+                message: 'No actions',
+                details:
+                  'If you submit with no actions created, nothing will happen on this step',
+                action: (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={hasConfirmed}
+                        onChange={() => setHasConfirmed(!hasConfirmed)}
+                      />
+                    }
+                    label='I acknowledge the impact of this'
+                  />
+                ),
+              },
+            ]
+          : []
+      }
     />
   )
 }
