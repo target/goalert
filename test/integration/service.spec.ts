4fa1ece521c393b7ff4c2b5be4a627301a7b0a79
Nathaniel Caza <mastercactapus@gmail.com>
2024-06-25 16:29:16 -0500

    fix retry

--- test/integration/service.spec.ts
+++ test/integration/service.spec.ts
@@ -73,7 +73,7 @@ test('Heartbeat Monitors', async ({ page, isMobile }) => {
   // Use valid name instead
   let hmName = c.word({ length: 5 }) + ' Monitor'
   await page.getByLabel('Name').fill(hmName)
-  await page.getByRole('button', { name: 'Submit' }).click()
+  await page.getByRole('button', { name: 'Retry' }).click()
 
   // Should see the heartbeat monitor created
   await expect(page.getByText(hmName)).toBeVisible()
