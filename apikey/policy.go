9b9605622a65c8580702252b0bc279a54c4fb7b8
Nathaniel Caza <mastercactapus@gmail.com>
2024-06-26 11:00:16 -0500

    Clarify versioning rule in GQLPolicy comments
    - Specify that changes to existing fields must require version incrementing.

--- apikey/policy.go
+++ apikey/policy.go
@@ -4,9 +4,9 @@ import "github.com/target/goalert/permission"
 
 // GQLPolicy is a GraphQL API key policy.
 //
-// Any changes to this MUST require the version to be incremented.
+// Any changes to existing fields MUST require the version to be incremented.
 //
-// If new fields are added, for example, they MUST be set to `omitempty`
+// If new fields are added, they MUST be set to `omitempty`
 // to ensure existing keys don't break.
 //
 // It MUST be possible to unmarshal & re-marshal the policy without changing the data for existing keys.
