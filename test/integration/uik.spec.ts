8483a68d85bbcdca4a59f476219782314c4b032c
Nathaniel Caza <mastercactapus@gmail.com>
2024-06-28 13:25:36 -0500

    Refactored and expanded UI integration tests
    - Modularized setup process for reusability
    - Added tests for creating and managing rules
    - Implemented tests for managing auth tokens
    - Improved assertions and error handling in existing tests
    
    Co-authored-by: Nathaniel Cook <nathanieljcook@outlook.com>
    Co-authored-by: AllenDing <Allen.Ding@target.com>

--- test/integration/uik.spec.ts
+++ test/integration/uik.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 import {
   baseURLFromFlags,
   dropdownSelect,
@@ -15,7 +15,7 @@ test.use({
   baseURL: baseURLFromFlags(['univ-keys']),
 })
 
-test('Integration Keys', async ({ page, playwright, isMobile }) => {
+async function setup(page: Page, isMobile: boolean): Promise<string> {
   const intKeyName = 'uik-key ' + c.name()
   const serviceName = 'uik-service ' + c.name()
   const serviceDescription = c.sentence()
@@ -39,6 +39,93 @@ test('Integration Keys', async ({ page, playwright, isMobile }) => {
     ).toBeVisible()
   }
   await expect(bread.getByRole('link', { name: intKeyName })).toBeVisible()
+
+  return serviceURL
+}
+
+test('create universal key, add rule with action', async ({
+  page,
+  isMobile,
+}) => {
+  await setup(page, isMobile)
+  const ruleName = c.name()
+  const ruleDesc = c.sentence({ words: 5 })
+  const ruleNewDesc = c.sentence({ words: 3 })
+
+  // create a rule
+  await page.getByRole('button', { name: 'Create Rule' }).click()
+  await page.fill('input[name=name]', ruleName)
+  await page.fill('input[name=description]', ruleDesc)
+  await page.fill('textarea[name=conditionExpr]', 'true')
+
+  // add an action to the rule and submit
+  await dropdownSelect(page, 'Destination Type', 'Alert')
+  await expect(
+    page.getByLabel('Create Rule').getByTestId('no-actions'),
+  ).toBeVisible()
+  await page.getByRole('button', { name: 'Add Action' }).click()
+  await expect(
+    page.getByLabel('Create Rule').getByTestId('no-actions'),
+  ).toBeHidden()
+  await expect(
+    page.locator('span', { hasText: 'Create new alert' }),
+  ).toBeVisible()
+  await page.getByRole('button', { name: 'Submit' }).click()
+  await expect(page.locator('[role=dialog]')).toBeHidden()
+
+  // verify
+  await expect(page.locator('body')).toContainText(ruleName)
+  await expect(page.locator('body')).toContainText(ruleDesc)
+
+  // start editing
+  await page.getByRole('button', { name: 'Other Actions' }).click()
+  await page.getByRole('menuitem', { name: 'Edit' }).click()
+
+  // update description, delete the action, submit
+  await page.fill('input[name=description]', ruleNewDesc)
+  await page
+    .locator('div', { hasText: 'Create new alert' })
+    .locator('[data-testid=CancelIcon]')
+    .click()
+  await expect(
+    page.getByLabel('Edit Rule').getByTestId('no-actions'),
+  ).toBeVisible()
+  await page.getByRole('button', { name: 'Submit' }).click()
+
+  // see warning for no actions- check and submit
+  await expect(page.getByText('WARNING: No actions')).toBeVisible()
+  await page
+    .locator('label', { hasText: 'I acknowledge the impact of this' })
+    .locator('input[type=checkbox]')
+    .click()
+  await page.getByRole('button', { name: 'Retry' }).click()
+  await expect(page.locator('[role=dialog]')).toBeHidden()
+
+  // verify name does not change, with new description
+  await expect(page.locator('body')).toContainText(ruleName)
+  await expect(page.locator('body')).toContainText(ruleNewDesc)
+
+  // delete the rule
+  await page.getByRole('button', { name: 'Other Actions' }).click()
+  await page.getByRole('menuitem', { name: 'Delete' }).click()
+  expect(
+    page.locator('[data-cy=dialog-title]', { hasText: 'Are you sure?' }),
+  ).toBeVisible()
+  await page.getByRole('button', { name: 'Confirm' }).click()
+
+  // verify
+  await expect(page.locator('[data-cy=list-empty-message]')).toHaveText(
+    'No rules exist for this integration key.',
+  )
+})
+
+test('create primary auth token then promote a second auth token', async ({
+  page,
+  playwright,
+  isMobile,
+}) => {
+  const serviceURL = await setup(page, isMobile)
+
   await expect(page.locator('[data-cy=details]')).toContainText(
     'Auth Token: N/A',
   )
@@ -49,7 +136,7 @@ test('Integration Keys', async ({ page, playwright, isMobile }) => {
   const origPrimaryToken = await page
     .getByRole('button', { name: 'Copy' })
     .textContent()
-  await expect(origPrimaryToken).not.toBeNull()
+  expect(origPrimaryToken).not.toBeNull()
   await page.getByRole('button', { name: 'Done' }).click()
 
   function tokenHint(token: string): string {
@@ -64,10 +151,11 @@ test('Integration Keys', async ({ page, playwright, isMobile }) => {
     baseURL: baseURLFromFlags(['univ-keys']),
     extraHTTPHeaders: { 'Content-Type': 'application/json', Cookie: '' },
   })
+
   async function testKey(key: string, status: number): Promise<void> {
     const auth = { Authorization: key ? 'Bearer ' + key : '' }
     const resp = await req.post('/api/v2/uik', { headers: auth, data: {} })
-    await expect(resp.status()).toBe(status)
+    expect(resp.status()).toBe(status)
   }
 
   await testKey('', 401)
@@ -78,7 +166,7 @@ test('Integration Keys', async ({ page, playwright, isMobile }) => {
   const firstSecondaryToken = await page
     .getByRole('button', { name: 'Copy' })
     .textContent()
-  await expect(origPrimaryToken).not.toBeNull()
+  expect(origPrimaryToken).not.toBeNull()
   await page.getByRole('button', { name: 'Done' }).click()
 
   await expect(page.locator('[data-cy=details]')).toContainText(
@@ -106,7 +194,7 @@ test('Integration Keys', async ({ page, playwright, isMobile }) => {
   const secondSecondaryToken = await page
     .getByRole('button', { name: 'Copy' })
     .textContent()
-  await expect(secondSecondaryToken).not.toBeNull()
+  expect(secondSecondaryToken).not.toBeNull()
   await page.getByRole('button', { name: 'Done' }).click()
 
   await page.getByRole('button', { name: 'Promote Secondary Token' }).click()
