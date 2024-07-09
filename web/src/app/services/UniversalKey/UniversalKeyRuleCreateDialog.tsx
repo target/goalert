59cc1b4f7a7c72713eb2b2bace0b20756c19f801
Forfold <Nathaniel.Cook@Target.com>
2024-07-09 10:27:49 -0700

    cleanup form

--- web/src/app/services/UniversalKey/UniversalKeyRuleCreateDialog.tsx
+++ web/src/app/services/UniversalKey/UniversalKeyRuleCreateDialog.tsx
@@ -33,24 +33,12 @@ export default function UniversalKeyRuleCreateDialogProps(
   const noActionsNoConf = value.actions.length === 0 && !hasConfirmed
 
   const errs = useErrorConsumer(m.error)
-  const form = (
-    <UniversalKeyRuleForm
-      value={value}
-      onChange={setValue}
-      nameError={errs.getErrorByField(/Rules.+\.Name/)}
-      descriptionError={errs.getErrorByField(/Rules.+\.Description/)}
-      conditionError={errs.getErrorByPath(
-        'updateKeyConfig.input.setRule.conditionExpr',
-      )}
-    />
-  )
 
   return (
     <FormDialog
       title='Create Rule'
       maxWidth='lg'
       onClose={props.onClose}
-      errors={errs.remainingLegacy()}
       onSubmit={() => {
         if (noActionsNoConf) {
           setHasSubmitted(true)
@@ -71,7 +59,18 @@ export default function UniversalKeyRuleCreateDialogProps(
           props.onClose()
         })
       }}
-      form={form}
+      form={
+        <UniversalKeyRuleForm
+          value={value}
+          onChange={setValue}
+          nameError={errs.getErrorByField(/Rules.+\.Name/)}
+          descriptionError={errs.getErrorByField(/Rules.+\.Description/)}
+          conditionError={errs.getErrorByPath(
+            'updateKeyConfig.input.setRule.conditionExpr',
+          )}
+        />
+      }
+      errors={errs.remainingLegacy()}
       notices={getNotice(hasSubmitted, hasConfirmed, setHasConfirmed)}
     />
   )
