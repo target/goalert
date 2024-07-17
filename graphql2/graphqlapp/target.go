d3d40e243b820206836ca951d75c0c5555a48f3c
Nathaniel Caza <mastercactapus@gmail.com>
2024-07-17 14:55:41 -0500

    Refactor Name resolver logic
    - Removed redundant error dependency
    - Simplified target type handling
    - Leveraged DisplayInfo for name retrieval

--- graphql2/graphqlapp/target.go
+++ graphql2/graphqlapp/target.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/target/goalert/assignment"
 	"github.com/target/goalert/graphql2"
-
-	"github.com/pkg/errors"
 )
 
 type Target App
@@ -14,42 +12,15 @@ type Target App
 func (a *App) Target() graphql2.TargetResolver { return (*Target)(a) }
 
 func (t *Target) Name(ctx context.Context, raw *assignment.RawTarget) (string, error) {
-	if raw.Name != "" {
-		return raw.Name, nil
-	}
-	switch raw.Type {
-	case assignment.TargetTypeRotation:
-		r, err := (*App)(t).FindOneRotation(ctx, raw.ID)
-		if err != nil {
-			return "", err
-		}
-		return r.Name, nil
-	case assignment.TargetTypeUser:
-		u, err := (*App)(t).FindOneUser(ctx, raw.ID)
-		if err != nil {
-			return "", err
-		}
-		return u.Name, nil
-	case assignment.TargetTypeEscalationPolicy:
-		ep, err := (*App)(t).FindOnePolicy(ctx, raw.ID)
-		if err != nil {
-			return "", err
-		}
-		return ep.Name, nil
-	case assignment.TargetTypeSchedule:
-		sched, err := (*App)(t).FindOneSchedule(ctx, raw.ID)
-		if err != nil {
-			return "", err
-		}
-		return sched.Name, nil
-	case assignment.TargetTypeService:
-		svc, err := (*App)(t).FindOneService(ctx, raw.ID)
-		if err != nil {
-			return "", err
-		}
-		return svc.Name, nil
-
+	dest, err := CompatTargetToDest(raw)
+	if err != nil {
+		return "", err
 	}
 
-	return "", errors.New("unhandled target type " + raw.Type.String())
+	info, err := t.DestReg.DisplayInfo(ctx, dest)
+	if err != nil {
+		return "", err
+	}
+
+	return info.Text, nil
 }
