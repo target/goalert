238d5eb136763308e51c9ae908bc2d2fc5e8ae34
Nathaniel Caza <mastercactapus@gmail.com>
2024-07-17 15:20:19 -0500

    Add support for resolving target names
    - Return `raw.Name` if available
    - Resolve names based on target type if `raw.Name` is empty

--- graphql2/graphqlapp/target.go
+++ graphql2/graphqlapp/target.go
@@ -12,6 +12,24 @@ type Target App
 func (a *App) Target() graphql2.TargetResolver { return (*Target)(a) }
 
 func (t *Target) Name(ctx context.Context, raw *assignment.RawTarget) (string, error) {
+	if raw.Name != "" {
+		return raw.Name, nil
+	}
+	switch raw.Type {
+	case assignment.TargetTypeEscalationPolicy:
+		ep, err := (*App)(t).FindOnePolicy(ctx, raw.ID)
+		if err != nil {
+			return "", err
+		}
+		return ep.Name, nil
+	case assignment.TargetTypeService:
+		svc, err := (*App)(t).FindOneService(ctx, raw.ID)
+		if err != nil {
+			return "", err
+		}
+		return svc.Name, nil
+	}
+
 	dest, err := CompatTargetToDest(raw)
 	if err != nil {
 		return "", err
