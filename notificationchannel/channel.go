2ce9197394b064c60339866447533173b7a5c5f0
Nathaniel Caza <mastercactapus@gmail.com>
2024-07-31 14:54:49 -0500

    Remove unused validation dependency and normalize method
    - Clean up imports by removing `validate`
    - Eliminate `Normalize` method from Channel struct

--- notificationchannel/channel.go
+++ notificationchannel/channel.go
@@ -3,7 +3,6 @@ package notificationchannel
 import (
 	"github.com/google/uuid"
 	"github.com/target/goalert/gadb"
-	"github.com/target/goalert/validation/validate"
 )
 
 type Channel struct {
@@ -16,13 +15,4 @@ func (c *Channel) fromRow(row gadb.NotificationChannel) {
 	c.ID = row.ID
 	c.Name = row.Name
 	c.Dest = row.Dest.DestV1
-}
-
-func (c Channel) Normalize() (*Channel, error) {
-	if c.ID == uuid.Nil {
-		c.ID = uuid.New()
-	}
-
-	err := validate.Text("Name", c.Name, 1, 255)
-	return &c, err
 }
