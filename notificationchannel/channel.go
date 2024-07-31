f747097aedae2fb6eb0bea5c0ba9cc606444365f
Nathaniel Caza <mastercactapus@gmail.com>
2024-07-31 14:15:57 -0500

    Simplified channel validation logic
    - Removed type-specific validation in favor of centralizing `Dest` validation
    - Streamlined struct fields to use `Dest` directly

--- notificationchannel/channel.go
+++ notificationchannel/channel.go
@@ -1,25 +1,21 @@
 package notificationchannel
 
 import (
-	"strings"
-
 	"github.com/google/uuid"
 	"github.com/target/goalert/gadb"
 	"github.com/target/goalert/validation/validate"
 )
 
 type Channel struct {
-	ID    uuid.UUID
-	Name  string
-	Type  Type
-	Value string
+	ID   uuid.UUID
+	Name string
+	Dest gadb.DestV1
 }
 
 func (c *Channel) fromRow(row gadb.NotificationChannel) {
 	c.ID = row.ID
 	c.Name = row.Name
-	c.Type = Type(row.Type)
-	c.Value = row.Value
+	c.Dest = row.Dest.DestV1
 }
 
 func (c Channel) Normalize() (*Channel, error) {
@@ -27,23 +23,6 @@ func (c Channel) Normalize() (*Channel, error) {
 		c.ID = uuid.New()
 	}
 
-	err := validate.Many(
-		validate.Text("Name", c.Name, 1, 255),
-		validate.OneOf("Type", c.Type, TypeSlackChan, TypeWebhook, TypeSlackUG),
-	)
-
-	switch c.Type {
-	case TypeSlackUG:
-		grp, ch, _ := strings.Cut(c.Value, ":")
-		err = validate.Many(err,
-			validate.RequiredText("Value.GroupID", grp, 1, 32),
-			validate.RequiredText("Value.ChannelID", ch, 1, 32),
-		)
-	case TypeSlackChan:
-		err = validate.Many(err, validate.RequiredText("Value", c.Value, 1, 32))
-	case TypeWebhook:
-		err = validate.Many(err, validate.URL("Value", c.Value))
-	}
-
+	err := validate.Text("Name", c.Name, 1, 255)
 	return &c, err
 }
