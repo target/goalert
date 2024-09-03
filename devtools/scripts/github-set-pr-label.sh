c513dbad87298b7ad58b06cd28f8312e289f14e6
Nathaniel Caza <mastercactapus@gmail.com>
2024-09-03 16:25:53 -0500

    simplify

--- devtools/scripts/github-set-pr-label.sh
+++ devtools/scripts/github-set-pr-label.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-ensure_env() {
-    if [ -z "$1" ]; then
-        echo "$2 is not set"
-        exit 1
-    fi
-}
-
-if [ -z "$GITHUB_API_URL" ]; then
-    GITHUB_API_URL=https://api.github.com
+if [ -z "PR_NUMBER" ]; then
+    echo "PR_NUMBER is not set"
+    exit 1
 fi
 
-ensure_env "$GITHUB_API_URL" "GITHUB_API_URL"
-ensure_env "$PR_NUMBER" "PR_NUMBER"
-ensure_env "$OWNER" "OWNER"
-ensure_env "$REPO" "REPO"
 LABEL=$(./devtools/scripts/git-diff-label-calc.sh --debug)
 
-curl -sSL \
-    -H "Authorization: token $GITHUB_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    -X PATCH \
-    -H "Content-Type: application/json" \
-    -d "{\"labels\":[\"test/$LABEL\"]}" \
-    "$GITHUB_API_URL/repos/$OWNER/$REPO/issues/$PR_NUMBER"
+gh pr edit "$PR_NUMBER" --add-label "test/$LABEL"
