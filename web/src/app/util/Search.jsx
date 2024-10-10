92dacf0e49ea314bb89115513c5bc52e4c88d581
Nathaniel Caza <mastercactapus@gmail.com>
2024-10-10 14:32:56 -0500

    lint

--- web/src/app/util/Search.jsx
+++ web/src/app/util/Search.jsx
@@ -97,7 +97,7 @@ export default function Search(props) {
         value={search}
         size='small'
         fullWidth={props.fullWidth}
-        className={props.transition ?? true ? classes.transition : null}
+        className={(props.transition ?? true) ? classes.transition : null}
         sx={(theme) => ({
           minWidth: 250,
           backgroundColor: theme.palette.mode === 'dark' ? 'inherit' : 'white',
