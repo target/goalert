07078581764c188da92bdc3ec3c139c88625f5ae
Nathaniel Caza <mastercactapus@gmail.com>
2024-11-14 13:12:09 -0600

    migrate: add uik_samples table and update schema

---
+++ migrate/migrations/20241114120056-signals-samples.sql
@@ -0,0 +1,17 @@
+-- +migrate Up
+CREATE TABLE uik_samples(
+    id uuid PRIMARY KEY,
+    key_id uuid NOT NULL REFERENCES integration_keys(id) ON DELETE CASCADE,
+    request_data jsonb NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
+    failed boolean NOT NULL DEFAULT FALSE,
+    user_note text
+);
+
+CREATE INDEX uik_samples_uik_created_idx ON uik_samples(key_id, created_at DESC)
+WHERE
+    user_note IS NULL;
+
+-- +migrate Down
+DROP TABLE uik_samples;
+
