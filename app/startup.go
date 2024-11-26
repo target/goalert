187b5a098a9769240648aa08e1e3a07d7b12e6a1
Nathaniel Caza <mastercactapus@gmail.com>
2024-11-26 11:05:43 -0600

    fix(startup): handle startup error before proceeding with initialization

--- app/startup.go
+++ app/startup.go
@@ -60,6 +60,9 @@ func (app *App) startup(ctx context.Context) error {
 
 	app.initStartup(ctx, "Startup.River", app.initRiver)
 	app.initStartup(ctx, "Startup.DBStores", app.initStores)
+	if app.startupErr != nil {
+		return app.startupErr
+	}
 	ctx = app.ConfigStore.Config().Context(ctx)
 
 	// init twilio before engine
