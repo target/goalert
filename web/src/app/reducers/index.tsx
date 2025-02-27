8c623907811a5bc1418b5e35d5e48876bfcd1fbc
KatieMSB <katriannasb@gmail.com>
2025-02-27 14:26:54 -0600

    convert reducers to typescript

--- web/src/app/reducers/index.tsx
+++ web/src/app/reducers/index.tsx
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux'
+import { combineReducers, Reducer } from 'redux'
 import auth from './auth'
 import main from './main'
 
-export default function createRootReducer() {
+export default function createRootReducer(): Reducer {
   return combineReducers({
     auth, // auth status
     main, // reducer for new user setup flag
