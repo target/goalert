eb87df8f33037be64e0e13b64761ceca1d704a74
Nathaniel Caza <mastercactapus@gmail.com>
2025-02-28 12:24:03 -0600

    feat: enhance arch-check script with additional warning for Go version

--- devtools/scripts/arch-check.sh
+++ devtools/scripts/arch-check.sh
@@ -12,4 +12,5 @@ if ! [ "$GO_ARCH" = "$SYS_ARCH" ]; then
 
     echo "\033[1;33mWARNING: GOARCH ($GO_ARCH) does not match your system architecture ($SYS_ARCH)\033[0m"
     echo "\033[1;33mThis may cause issues when building or starting the project.\033[0m"
+    echo "\033[1;33mPlease ensure that you have the correct version of Go installed.\033[0m"
 fi
