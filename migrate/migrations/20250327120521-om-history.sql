fe8a76eb9967bff0550d7cc53796018f218d2c79
Nathaniel Caza <mastercactapus@gmail.com>
2025-03-27 12:23:10 -0500

    feat: add message status history table and triggers for outgoing messages

---
+++ migrate/migrations/20250327120521-om-history.sql
@@ -0,0 +1,39 @@
+-- +migrate Up
+CREATE TABLE message_status_history(
+    id bigserial PRIMARY KEY,
+    message_id uuid NOT NULL REFERENCES outgoing_messages(id) ON DELETE CASCADE,
+    timestamp timestamptz NOT NULL DEFAULT now(),
+    status enum_outgoing_messages_status NOT NULL,
+    status_details text NOT NULL
+);
+
+CREATE FUNCTION fn_insert_message_status_history()
+    RETURNS TRIGGER
+    AS $$
+BEGIN
+    INSERT INTO message_status_history(message_id, status, status_details)
+        VALUES(NEW.id, NEW.last_status, NEW.status_details);
+    RETURN new;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_message_status_history
+    AFTER UPDATE OF last_status ON outgoing_messages
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_insert_message_status_history();
+
+CREATE TRIGGER trg_insert_message_status_history
+    AFTER INSERT ON outgoing_messages
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_insert_message_status_history();
+
+-- +migrate Down
+DROP TRIGGER trg_insert_message_status_history ON outgoing_messages;
+
+DROP TRIGGER trg_update_message_status_history ON outgoing_messages;
+
+DROP FUNCTION fn_insert_message_status_history();
+
+DROP TABLE message_status_history;
+
