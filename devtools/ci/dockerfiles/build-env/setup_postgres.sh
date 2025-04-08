60f497a2a462001a7ee390151de02a4f07379680
Nathaniel Caza <mastercactapus@gmail.com>
2025-04-08 11:32:56 -0500

    build: update PostgreSQL installation in Dockerfile and add setup scripts for multiple versions

---
+++ devtools/ci/dockerfiles/build-env/setup_postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+for VERSION in $*; do
+    echo "Initializing PostgreSQL $VERSION"
+    PGDATA=/var/lib/postgresql/$VERSION/data
+    mkdir -p $PGDATA /run/postgresql /var/log/postgresql/$VERSION
+
+    chown postgres $PGDATA /run/postgresql /var/log/postgresql/$VERSION
+    su postgres -c "/usr/lib/postgresql/$VERSION/bin/initdb $PGDATA"
+    echo "host all  all    0.0.0.0/0  md5" >>$PGDATA/pg_hba.conf
+    echo "listen_addresses='*'" >>$PGDATA/postgresql.conf
+    echo "fsync = off" >>$PGDATA/postgresql.conf
+    echo "full_page_writes = off" >>$PGDATA/postgresql.conf
+done
--- devtools/ci/dockerfiles/build-env/start_postgres.sh
+++ devtools/ci/dockerfiles/build-env/start_postgres.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 set -e
 
-su postgres -c "/usr/lib/postgresql/13/bin/pg_ctl start -w -l /var/log/postgresql/server.log"
+# check if any PostgreSQL version is already running
+if [ -f /var/lib/postgresql/.version ]; then
+    VERSION=$(cat /var/lib/postgresql/.version)
+    if [ -f /var/lib/postgresql/$VERSION/data/postmaster.pid ]; then
+        stop_postgres
+    fi
+fi
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    VERSION=13 # Default to PostgreSQL 13 for compatibility
+fi
+
+echo "Starting PostgreSQL $VERSION"
+
+export PGDATA=/var/lib/postgresql/$VERSION/data
+su postgres -c "/usr/lib/postgresql/$VERSION/bin/pg_ctl start -w -l /var/log/postgresql/$VERSION/server.log"
+
+echo "$VERSION" >/var/lib/postgresql/.version
