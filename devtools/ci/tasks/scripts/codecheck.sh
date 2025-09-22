a3fd9f2313b47ac6253d2b257c1848271c126a8e
Nathaniel Caza <mastercactapus@gmail.com>
2025-09-22 11:10:25 -0500

    update deps

--- devtools/ci/tasks/scripts/codecheck.sh
+++ devtools/ci/tasks/scripts/codecheck.sh
@@ -2,7 +2,7 @@
 set -e
 
 # assert build-env versions are identical
-BUILD_ENV_VER=go1.25.0
+BUILD_ENV_VER=go1.25.1
 for file in $(find devtools -name 'Dockerfile*'); do
   if ! grep -q "goalert/build-env" "$file"; then
     continue
