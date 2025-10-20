ece3aab236c81fdbc89f0d776732087bd851d2ec
Nathaniel Caza <mastercactapus@gmail.com>
2025-10-20 10:17:27 -0500

    pg_format

--- migrate/migrations/20251003121427-alert-status-direct-event.sql
+++ migrate/migrations/20251003121427-alert-status-direct-event.sql
@@ -1,10 +1,10 @@
 -- +migrate Up
-CREATE OR REPLACE FUNCTION fn_track_alert_status()
+CREATE OR REPLACE FUNCTION fn_track_alert_status ()
     RETURNS TRIGGER
     AS $$
 BEGIN
-    INSERT INTO river_job(args, kind, max_attempts, priority)
-        VALUES(json_build_object('AlertID', NEW.id), 'status-manager-look-for-work', 25, 2);
+    INSERT INTO river_job (args, kind, max_attempts, priority)
+        VALUES (json_build_object('AlertID', NEW.id), 'status-manager-look-for-work', 25, 2);
     PERFORM
         pg_notify(current_schema() || '.river_insert', '{"queue":"status-manager"}');
     RETURN NEW;
@@ -15,11 +15,11 @@ LANGUAGE plpgsql;
 CREATE TRIGGER trg_track_alert_status_update
     AFTER UPDATE ON alerts
     FOR EACH ROW
-    WHEN(NEW.status IS DISTINCT FROM OLD.status)
-    EXECUTE FUNCTION fn_track_alert_status();
+    WHEN (NEW.status IS DISTINCT FROM OLD.status)
+    EXECUTE FUNCTION fn_track_alert_status ();
 
 -- +migrate Down
 DROP TRIGGER IF EXISTS trg_track_alert_status_update ON alerts;
 
-DROP FUNCTION IF EXISTS fn_track_alert_status();
+DROP FUNCTION IF EXISTS fn_track_alert_status ();
 
--- migrate/migrations/20251003123243-rotation-direct-event.sql
+++ migrate/migrations/20251003123243-rotation-direct-event.sql
@@ -1,50 +1,41 @@
 -- +migrate Up
-CREATE OR REPLACE FUNCTION fn_job_rotation(id uuid)
-  RETURNS VOID
-  AS $$
+CREATE OR REPLACE FUNCTION fn_job_rotation (id uuid)
+    RETURNS VOID
+    AS $$
 DECLARE
-  key_name text := 'rotation_job_' || replace(id::text, '-', '_');
+    key_name text := 'rotation_job_' || replace(id::text, '-', '_');
 BEGIN
-  -- Only run once per transaction per ID
-  IF current_setting('local.' || key_name, TRUE) IS NOT NULL THEN
-    RETURN;
-  END IF;
-  -- Mark this ID as processed in this transaction
-  PERFORM
-    set_config('local.' || key_name, 'true', TRUE);
-  INSERT INTO river_job(
-    args,
-    kind,
-    max_attempts,
-    priority)
-  VALUES (
-    json_build_object(
-      'RotationID', id),
-    'rotation-manager-update',
-    25,
-    2);
-  PERFORM
-    pg_notify(current_schema() || '.river_insert', '{"queue":"rotation-manager"}');
+    -- Only run once per transaction per ID
+    IF current_setting('local.' || key_name, TRUE) IS NOT NULL THEN
+        RETURN;
+    END IF;
+    -- Mark this ID as processed in this transaction
+    PERFORM
+        set_config('local.' || key_name, 'true', TRUE);
+    INSERT INTO river_job (args, kind, max_attempts, priority)
+        VALUES (json_build_object('RotationID', id), 'rotation-manager-update', 25, 2);
+    PERFORM
+        pg_notify(current_schema() || '.river_insert', '{"queue":"rotation-manager"}');
 END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION fn_track_rotation_updates()
-  RETURNS TRIGGER
-  AS $$
+CREATE OR REPLACE FUNCTION fn_track_rotation_updates ()
+    RETURNS TRIGGER
+    AS $$
 BEGIN
-  IF TG_TABLE_NAME = 'rotations' THEN
-    PERFORM
-      fn_job_rotation(NEW.id);
-  ELSIF TG_OP = 'DELETE' THEN
-    PERFORM
-      fn_job_rotation(OLD.rotation_id);
-    RETURN OLD;
-  ELSE
-    PERFORM
-      fn_job_rotation(NEW.rotation_id);
-  END IF;
-  RETURN NEW;
+    IF TG_TABLE_NAME = 'rotations' THEN
+        PERFORM
+            fn_job_rotation (NEW.id);
+    ELSIF TG_OP = 'DELETE' THEN
+        PERFORM
+            fn_job_rotation (OLD.rotation_id);
+        RETURN OLD;
+    ELSE
+        PERFORM
+            fn_job_rotation (NEW.rotation_id);
+    END IF;
+    RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
@@ -53,59 +44,44 @@ LOCK entity_updates;
 
 -- convert existing updates to jobs
 WITH distinct_rotations AS (
-  SELECT DISTINCT
-    entity_id
-  FROM
-    entity_updates
-  WHERE
-    entity_type = 'rotation')
-INSERT INTO river_job(
-  args,
-  kind,
-  max_attempts,
-  priority)
+    SELECT DISTINCT
+        entity_id
+    FROM
+        entity_updates
+    WHERE
+        entity_type = 'rotation')
+INSERT INTO river_job (args, kind, max_attempts, priority)
 SELECT
-  json_build_object('RotationID', entity_id),
-  'rotation-manager-update',
-  25,
-  2
+    json_build_object('RotationID', entity_id),
+    'rotation-manager-update',
+    25,
+    2
 FROM
-  distinct_rotations;
+    distinct_rotations;
 
 DELETE FROM entity_updates
 WHERE entity_type = 'rotation';
 
 -- +migrate Down
-CREATE OR REPLACE FUNCTION fn_track_rotation_updates()
-  RETURNS TRIGGER
-  AS $$
+CREATE OR REPLACE FUNCTION fn_track_rotation_updates ()
+    RETURNS TRIGGER
+    AS $$
 BEGIN
-  IF TG_TABLE_NAME = 'rotations' THEN
-    INSERT INTO entity_updates(
-      entity_type,
-      entity_id)
-    VALUES(
-      'rotation',
-      NEW.id);
-  ELSIF TG_OP = 'DELETE' THEN
-    INSERT INTO entity_updates(
-      entity_type,
-      entity_id)
-    VALUES(
-      'rotation',
-      OLD.rotation_id);
-    RETURN OLD;
-  ELSE
-    INSERT INTO entity_updates(
-      entity_type,
-      entity_id)
-    VALUES(
-      'rotation',
-      NEW.rotation_id);
-  END IF;
-  RETURN NEW;
+    IF TG_TABLE_NAME = 'rotations' THEN
+        INSERT INTO entity_updates (entity_type, entity_id)
+            VALUES ('rotation', NEW.id);
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO entity_updates (entity_type, entity_id)
+            VALUES ('rotation', OLD.rotation_id);
+        RETURN OLD;
+    ELSE
+        INSERT INTO entity_updates (entity_type, entity_id)
+            VALUES ('rotation', NEW.rotation_id);
+    END IF;
+    RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS fn_job_rotation(uuid);
+DROP FUNCTION IF EXISTS fn_job_rotation (uuid);
+
